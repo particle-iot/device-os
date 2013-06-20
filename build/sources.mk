28a205e927b843ea0971517155945fb72b0c9a7f
Satish Nair <satishgn77@gmail.com>
2013-06-20 19:38:53 +0530

    #24 - Makefile toolchain with relative path (tested on windows)

---
+++ build/sources.mk
@@ -0,0 +1,29 @@
+########################################
+#   @author  Spark Application Team    #
+#   @version V1.0.0                    #
+#   @date    20-June-2013              #
+########################################
+
+ELF_SRCS := 
+O_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+OBJ_SRCS := 
+ASM_SRCS := 
+SECONDARY_SIZE := 
+OBJS := 
+C_DEPS := 
+ASM_DEPS := 
+SECONDARY_FLASH := 
+EXECUTABLES := 
+SECONDARY_LIST := 
+S_UPPER_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+startup \
+src \
+libraries/STM32_USB-FS-Device_Driver/src \
+libraries/STM32F10x_StdPeriph_Driver/src \
+libraries/CC3000_Host_Driver \
+
