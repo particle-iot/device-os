85e70ef02ec666d59c05ca8f212bfff24785ff20
Satish Nair <satishgn77@gmail.com>
2013-08-26 23:37:26 +0530

    Updated source files for use with core-common-lib (WIP)

--- inc/main.h
+++ inc/main.h
@@ -27,14 +27,45 @@
 /* Exported constants --------------------------------------------------------*/
 
 /* Exported macro ------------------------------------------------------------*/
+
+/*
+ * In Eclipse Project Properties -> C/C++ Build -> Settings -> Tool Settings
+ * -> ARM Sourcery Windows GCC C/C++ Compiler -> Preprocessor -> Defined symbol (-D),
+ * Add : "DFU_BUILD_ENABLE"
+ *
+ * In Eclipse Project Properties -> C/C++ Build -> Settings -> Tool Settings
+ * -> ARM Sourcery Windows GCC C/C++ Linker -> General -> Script file (-T),
+ * Browse & select linker file : "linker_stm32f10x_md_dfu.ld"
+ */
+
+#ifdef DFU_BUILD_ENABLE
+
+/*
+ * Use the JTAG IOs as standard GPIOs (D3 to D7)
+ * Note that once the JTAG IOs are disabled, the connection with the host debugger
+ * is lost and cannot be re-established as long as the JTAG IOs remain disabled.
+ */
+#define SWD_JTAG_DISABLE
+
+/*
+ * Use Independent Watchdog to force a system reset when a software error occurs
+ * During JTAG program/debug, the Watchdog has to be disabled so that it does not
+ * upset the debugger
+ */
+#define IWDG_RESET_ENABLE
+
+#endif
+
 /* Flash Memory address where the System Flags will be saved and loaded from  */
-#define SYSTEM_FLAGS_ADDRESS	((uint32_t)0x08004C00)
+#define SYSTEM_FLAGS_ADDRESS		((uint32_t)0x08004C00)
 
 /* CC3000 EEPROM - Spark File Data Storage */
 #define NVMEM_SPARK_FILE_ID			14	//Do not change this ID
 #define NVMEM_SPARK_FILE_SIZE		16	//Change according to requirement
 #define WLAN_PROFILE_FILE_OFFSET	0
 #define ERROR_COUNT_FILE_OFFSET		1
+
+#define USART_RX_DATA_SIZE			256	//2048
 
 /* Exported functions ------------------------------------------------------- */
 void Timing_Decrement(void);
@@ -53,5 +84,12 @@ char *WLAN_Driver_Patch(unsigned long *length);
 char *WLAN_BootLoader_Patch(unsigned long *length);
 
 void Start_OTA_Update(void);
+
+void USB_USART_Init(uint32_t baudRate);
+uint8_t USB_USART_Available_Data(void);
+int32_t USB_USART_Receive_Data(void);
+void USB_USART_Send_Data(uint8_t Data);
+void Handle_USBAsynchXfer(void);
+void Get_SerialNum(void);
 
 #endif /* __MAIN_H */
--- src/main.c
+++ src/main.c
@@ -9,9 +9,11 @@
  */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "usb_conf.h"
 #include "usb_lib.h"
 #include "usb_desc.h"
 #include "usb_pwr.h"
+#include "usb_prop.h"
 #include "sst25vf_spi.h"
 #include "spark_utilities.h"
 
@@ -64,7 +66,24 @@ char device_name[] = "CC3000";
 
 unsigned char NVMEM_Spark_File_Data[NVMEM_SPARK_FILE_SIZE];
 
+uint8_t  USART_Rx_Buffer[USART_RX_DATA_SIZE];
+uint32_t USART_Rx_ptr_in = 0;
+uint32_t USART_Rx_ptr_out = 0;
+uint32_t USART_Rx_length  = 0;
+
+uint8_t USB_Rx_Buffer[VIRTUAL_COM_PORT_DATA_SIZE];
+uint16_t USB_Rx_length = 0;
+uint16_t USB_Rx_ptr = 0;
+
+uint8_t  USB_Tx_State = 0;
+uint8_t  USB_Rx_State = 0;
+
+uint32_t USB_USART_BaudRate = 9600;
+
+static void IntToUnicode (uint32_t value , uint8_t *pbuf , uint8_t len);
+
 /* Extern variables ----------------------------------------------------------*/
+extern LINE_CODING linecoding;
 
 /* Private function prototypes -----------------------------------------------*/
 
@@ -79,6 +98,16 @@ unsigned char NVMEM_Spark_File_Data[NVMEM_SPARK_FILE_SIZE];
  *******************************************************************************/
 int main(void)
 {
+#ifdef DFU_BUILD_ENABLE
+	/* Set the Vector Table(VT) base location at 0xC000 */
+	NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0xC000);
+#endif
+
+#ifdef SWD_JTAG_DISABLE
+    /* Disable the Serial Wire JTAG Debug Port SWJ-DP */
+    GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, ENABLE);
+#endif
+
 	Set_System();
 
 #if defined (USE_SPARK_CORE_V02)
@@ -768,6 +797,199 @@ void Start_OTA_Update(void)
 	BKP_WriteBackupRegister(BKP_DR10, 0x5000);
 
 	NVIC_SystemReset();
+}
+
+/*******************************************************************************
+* Function Name  : USB_USART_Init
+* Description    : Start USB-USART protocol.
+* Input          : baudRate.
+* Return         : None.
+*******************************************************************************/
+void USB_USART_Init(uint32_t baudRate)
+{
+	linecoding.bitrate = baudRate;
+	USB_Disconnect_Config();
+	Set_USBClock();
+	USB_Interrupts_Config();
+	USB_Init();
+}
+
+/*******************************************************************************
+* Function Name  : USB_USART_Available_Data.
+* Description    : Return the length of available data received from USB.
+* Input          : None.
+* Return         : Length.
+*******************************************************************************/
+uint8_t USB_USART_Available_Data(void)
+{
+	if(bDeviceState == CONFIGURED)
+	{
+		if(USB_Rx_State == 1)
+		{
+			return (USB_Rx_length - USB_Rx_ptr);
+		}
+	}
+
+	return 0;
+}
+
+/*******************************************************************************
+* Function Name  : USB_USART_Receive_Data.
+* Description    : Return data sent by USB Host.
+* Input          : None
+* Return         : Data.
+*******************************************************************************/
+int32_t USB_USART_Receive_Data(void)
+{
+	if(bDeviceState == CONFIGURED)
+	{
+		if(USB_Rx_State == 1)
+		{
+			if((USB_Rx_length - USB_Rx_ptr) == 1)
+			{
+				USB_Rx_State = 0;
+
+				/* Enable the receive of data on EP3 */
+				SetEPRxValid(ENDP3);
+			}
+
+			return USB_Rx_Buffer[USB_Rx_ptr++];
+		}
+	}
+
+	return -1;
+}
+
+/*******************************************************************************
+* Function Name  : USB_USART_Send_Data.
+* Description    : Send Data from USB_USART to USB Host.
+* Input          : Data.
+* Return         : None.
+*******************************************************************************/
+void USB_USART_Send_Data(uint8_t Data)
+{
+	if(bDeviceState == CONFIGURED)
+	{
+		USART_Rx_Buffer[USART_Rx_ptr_in] = Data;
+
+		USART_Rx_ptr_in++;
+
+		/* To avoid buffer overflow */
+		if(USART_Rx_ptr_in == USART_RX_DATA_SIZE)
+		{
+			USART_Rx_ptr_in = 0;
+		}
+	}
+}
+
+/*******************************************************************************
+* Function Name  : Handle_USBAsynchXfer.
+* Description    : send data to USB.
+* Input          : None.
+* Return         : None.
+*******************************************************************************/
+void Handle_USBAsynchXfer (void)
+{
+
+  uint16_t USB_Tx_ptr;
+  uint16_t USB_Tx_length;
+
+  if(USB_Tx_State != 1)
+  {
+    if (USART_Rx_ptr_out == USART_RX_DATA_SIZE)
+    {
+      USART_Rx_ptr_out = 0;
+    }
+
+    if(USART_Rx_ptr_out == USART_Rx_ptr_in)
+    {
+      USB_Tx_State = 0;
+      return;
+    }
+
+    if(USART_Rx_ptr_out > USART_Rx_ptr_in) /* rollback */
+    {
+      USART_Rx_length = USART_RX_DATA_SIZE - USART_Rx_ptr_out;
+    }
+    else
+    {
+      USART_Rx_length = USART_Rx_ptr_in - USART_Rx_ptr_out;
+    }
+
+    if (USART_Rx_length > VIRTUAL_COM_PORT_DATA_SIZE)
+    {
+      USB_Tx_ptr = USART_Rx_ptr_out;
+      USB_Tx_length = VIRTUAL_COM_PORT_DATA_SIZE;
+
+      USART_Rx_ptr_out += VIRTUAL_COM_PORT_DATA_SIZE;
+      USART_Rx_length -= VIRTUAL_COM_PORT_DATA_SIZE;
+    }
+    else
+    {
+      USB_Tx_ptr = USART_Rx_ptr_out;
+      USB_Tx_length = USART_Rx_length;
+
+      USART_Rx_ptr_out += USART_Rx_length;
+      USART_Rx_length = 0;
+    }
+    USB_Tx_State = 1;
+    UserToPMABufferCopy(&USART_Rx_Buffer[USB_Tx_ptr], ENDP1_TXADDR, USB_Tx_length);
+    SetEPTxCount(ENDP1, USB_Tx_length);
+    SetEPTxValid(ENDP1);
+  }
+
+}
+
+/*******************************************************************************
+* Function Name  : Get_SerialNum.
+* Description    : Create the serial number string descriptor.
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+void Get_SerialNum(void)
+{
+  uint32_t Device_Serial0, Device_Serial1, Device_Serial2;
+
+  Device_Serial0 = *(uint32_t*)ID1;
+  Device_Serial1 = *(uint32_t*)ID2;
+  Device_Serial2 = *(uint32_t*)ID3;
+
+  Device_Serial0 += Device_Serial2;
+
+  if (Device_Serial0 != 0)
+  {
+    IntToUnicode (Device_Serial0, &Virtual_Com_Port_StringSerial[2] , 8);
+    IntToUnicode (Device_Serial1, &Virtual_Com_Port_StringSerial[18], 4);
+  }
+}
+
+/*******************************************************************************
+* Function Name  : HexToChar.
+* Description    : Convert Hex 32Bits value into char.
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+static void IntToUnicode (uint32_t value , uint8_t *pbuf , uint8_t len)
+{
+  uint8_t idx = 0;
+
+  for( idx = 0 ; idx < len ; idx ++)
+  {
+    if( ((value >> 28)) < 0xA )
+    {
+      pbuf[ 2* idx] = (value >> 28) + '0';
+    }
+    else
+    {
+      pbuf[2* idx] = (value >> 28) + 'A' - 10;
+    }
+
+    value = value << 4;
+
+    pbuf[ 2* idx + 1] = 0;
+  }
 }
 
 #ifdef USE_FULL_ASSERT
