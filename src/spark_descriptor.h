c48a2b7dc761b40f03d1bff44fd3ae1e38415c5c
Zachary Crockett <towynlin@gmail.com>
2013-09-23 13:01:43 -0500

    Add descriptor function pointer for calling user functions

--- src/spark_descriptor.h
+++ src/spark_descriptor.h
@@ -2,4 +2,5 @@ struct SparkDescriptor
 {
   unsigned char *num_funcs_ptr;
   const char **function_keys;
+  int (*call_function)(unsigned char *function_key, unsigned char *arg);
 };
--- tests/TestDescriptor.cpp
+++ tests/TestDescriptor.cpp
@@ -1,6 +1,18 @@
 #include "UnitTest++.h"
 #include "spark_descriptor.h"
 
+struct FunctionFixture {
+  static int execute_a_function(unsigned char *func_key, unsigned char *arg);
+};
+
+int FunctionFixture::execute_a_function(unsigned char *func_key, unsigned char *arg)
+{
+  unsigned char *prevent_warning;
+  prevent_warning = func_key;
+  prevent_warning = arg;
+  return 99;
+}
+
 SUITE(Descriptor)
 {
   TEST(DescriptorKnowsNumberOfRegisteredFunctions)
@@ -17,5 +29,15 @@ SUITE(Descriptor)
     SparkDescriptor descriptor;
     descriptor.function_keys = function_keys;
     CHECK_EQUAL("bean_alert", descriptor.function_keys[2]);
+  }
+
+  TEST_FIXTURE(FunctionFixture, DescriptorCanCallRegisteredFunction)
+  {
+    SparkDescriptor descriptor;
+    descriptor.call_function = execute_a_function;
+    unsigned char *function_key = (unsigned char *)"brew";
+    unsigned char *arg= (unsigned char *)"32,240";
+    int return_value = descriptor.call_function(function_key, arg);
+    CHECK_EQUAL(99, return_value);
   }
 }
