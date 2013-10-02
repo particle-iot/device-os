064ca3d8b925fdf693ad3b0142a645db0c104424
Satish Nair <satishgn77@gmail.com>
2013-10-02 22:09:22 +0530

    Replaced ping with socket connect to check for internet availabilty

--- SPARK_Firmware_Driver/src/spark_wlan.c
+++ SPARK_Firmware_Driver/src/spark_wlan.c
@@ -27,11 +27,6 @@ char _password[] = "password";
 // Auth options are WLAN_SEC_UNSEC, WLAN_SEC_WPA, WLAN_SEC_WEP, and WLAN_SEC_WPA2
 unsigned char _auth = WLAN_SEC_WPA2;
 
-/*
-unsigned char ping_IP_Addr[4] = {8, 8, 8, 8};
-netapp_pingreport_args_t pingReport;
-*/
-
 unsigned char NVMEM_Spark_File_Data[NVMEM_SPARK_FILE_SIZE];
 
 __IO uint8_t SPARK_WLAN_RESET;
@@ -44,6 +39,8 @@ __IO uint8_t SPARK_FLASH_UPDATE;
 __IO uint8_t SPARK_LED_FADE;
 
 __IO uint8_t Spark_Error_Count;
+
+int Internet_Test(void);
 
 void Set_NetApp_Timeout(void)
 {
@@ -174,7 +171,7 @@ void Start_Smart_Config(void)
 	SPARK_WLAN_STARTED = 1;
 
 	/* Mask out all non-required events */
-	wlan_set_event_mask(HCI_EVNT_WLAN_KEEPALIVE | HCI_EVNT_WLAN_UNSOL_INIT);
+	wlan_set_event_mask(HCI_EVNT_WLAN_KEEPALIVE | HCI_EVNT_WLAN_UNSOL_INIT | HCI_EVNT_WLAN_ASYNC_PING_REPORT);
 
 #if defined (USE_SPARK_CORE_V02)
     LED_SetRGBColor(RGB_COLOR_GREEN);
@@ -249,25 +246,6 @@ void WLAN_Async_Callback(long lEventType, char *data, unsigned char length)
 			}
 			break;
 
-		case HCI_EVNT_WLAN_ASYNC_PING_REPORT:
-/*
-			memset(&pingReport, 0 , sizeof(netapp_pingreport_args_t));
-			memcpy(&pingReport, data, length);
-			//"pingReport" is incorrect due to bug in CC3000 API implementation so is not reliable at present
-			if(pingReport.packets_sent == pingReport.packets_received)
-			{
-				//Google Server Reached
-				NVMEM_Spark_File_Data[ERROR_COUNT_FILE_OFFSET] = 3;
-			}
-			else
-			{
-				//Bad Internet Connection
-				NVMEM_Spark_File_Data[ERROR_COUNT_FILE_OFFSET] = 2;
-			}
-			nvmem_write(NVMEM_SPARK_FILE_ID, 1, ERROR_COUNT_FILE_OFFSET, &NVMEM_Spark_File_Data[ERROR_COUNT_FILE_OFFSET]);
-*/
-			break;
-
 		case HCI_EVENT_CC3000_CAN_SHUT_DOWN:
 			WLAN_CAN_SHUTDOWN = 1;
 			break;
@@ -314,7 +292,7 @@ void SPARK_WLAN_Setup(void)
 	SPARK_WLAN_STARTED = 1;
 
 	/* Mask out all non-required events from CC3000 */
-	wlan_set_event_mask(HCI_EVNT_WLAN_KEEPALIVE | HCI_EVNT_WLAN_UNSOL_INIT);
+	wlan_set_event_mask(HCI_EVNT_WLAN_KEEPALIVE | HCI_EVNT_WLAN_UNSOL_INIT | HCI_EVNT_WLAN_ASYNC_PING_REPORT);
 
 	if(NVMEM_SPARK_Reset_SysFlag == 0x0001 || nvmem_read(NVMEM_SPARK_FILE_ID, NVMEM_SPARK_FILE_SIZE, 0, NVMEM_Spark_File_Data) != 0)
 	{
@@ -472,13 +450,17 @@ void SPARK_WLAN_Loop(void)
 
 		if(Spark_Connect() < 0)
 		{
-			/*
-			//Do not use this as it doesn't work
-			//netapp_ping_send((unsigned long *)ping_IP_Addr, 4, 100, 1000);
-			*/
+			if(Internet_Test() < 0)
+			{
+				//No Internet Connection
+				Spark_Error_Count = 2;
+			}
+			else
+			{
+				//Cloud not Reachable
+				Spark_Error_Count = 3;
+			}
 
-			//Cloud not Reachable OR Bad Internet Connection
-			Spark_Error_Count = 2;
 			NVMEM_Spark_File_Data[ERROR_COUNT_FILE_OFFSET] = Spark_Error_Count;
 			nvmem_write(NVMEM_SPARK_FILE_ID, 1, ERROR_COUNT_FILE_OFFSET, &NVMEM_Spark_File_Data[ERROR_COUNT_FILE_OFFSET]);
 
@@ -528,4 +510,45 @@ void SPARK_WLAN_Timing(void)
 			SPARK_WLAN_RESET = 1;
 		}
 	}
+}
+
+int Internet_Test(void)
+{
+	long testSocket;
+	sockaddr testSocketAddr;
+	int testResult = 0;
+
+    testSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (testSocket < 0)
+    {
+        return -1;
+    }
+
+	// the family is always AF_INET
+    testSocketAddr.sa_family = AF_INET;
+
+	// the destination port: 53
+    testSocketAddr.sa_data[0] = 0;
+    testSocketAddr.sa_data[1] = 53;
+
+	// the destination IP address: 8.8.8.8
+	testSocketAddr.sa_data[2] = 8;
+	testSocketAddr.sa_data[3] = 8;
+	testSocketAddr.sa_data[4] = 8;
+	testSocketAddr.sa_data[5] = 8;
+
+	testResult = connect(testSocket, &testSocketAddr, sizeof(testSocketAddr));
+
+	if (testResult < 0)
+	{
+		// Unable to connect
+		return -1;
+	}
+	else
+	{
+		closesocket(testSocket);
+	}
+
+    return testResult;
 }
