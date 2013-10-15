bdd182ab048ab0e2b2231b36e2e13cb07b2cbefe
mohit <mohit@sparkdevices.com>
2013-10-15 15:14:21 -0500

    updated the wiring_string with misc string functions

--- src/spark_wiring_string.cpp
+++ src/spark_wiring_string.cpp
@@ -22,8 +22,12 @@
 #include "spark_wiring_string.h"
 #include <stdio.h>
 
+//------------------------------------------------------------------------------------------
+#define BUFSIZE (sizeof(long) * 8 + 1)
 //These are very crude implementations - will refine later
 //------------------------------------------------------------------------------------------
+
+//convert double to ascii
 char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
   char fmt[20];
   sprintf(fmt, "%%%d.%df", width, prec);
@@ -31,33 +35,132 @@ char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
   return sout;
 }
 
-void itoa(int value, char *sp, int radix)
-{
-    char tmp[16];// be careful with the length of the buffer
-    char *tp = tmp;
-    int i;
-    unsigned v;
-    int sign;
-
-    sign = (radix == 10 && value < 0);
-    if (sign)   v = -value;
-    else    v = (unsigned)value;
-
-    while (v || tp == tmp)
-    {
-        i = v % radix;
-        v /= radix; // v/=radix uses less CPU clocks than v=v/radix does
-        if (i < 10)
-          *tp++ = i+'0';
-        else
-          *tp++ = i + 'a' - 10;
-    }
-
-    if (sign)
-    *sp++ = '-';
-    while (tp > tmp)
-    *sp++ = *--tp;
+//utility function used by ultoa()
+void str_reverse(char* buffer){
+	char *i, *j;
+	char c;
+	i=buffer;
+	j=buffer + strlen(buffer)-1;
+	while(i<j){
+		c = *i;
+		*i = *j;
+		*j = c;
+		++i;
+		--j;
+	}
 }
+
+//convert long to string
+char *ltoa(long N, char *str, int base)
+{
+      register int i = 2;
+      long uarg;
+      char *tail, *head = str, buf[BUFSIZE];
+
+      if (36 < base || 2 > base)
+            base = 10;                    /* can only use 0-9, A-Z        */
+      tail = &buf[BUFSIZE - 1];           /* last character position      */
+      *tail-- = '\0';
+
+      if (10 == base && N < 0L)
+      {
+            *head++ = '-';
+            uarg    = -N;
+      }
+      else  uarg = N;
+
+      if (uarg)
+      {
+            for (i = 1; uarg; ++i)
+            {
+                  register ldiv_t r;
+
+                  r       = ldiv(uarg, base);
+                  *tail-- = (char)(r.rem + ((9L < r.rem) ?
+                                  ('A' - 10L) : '0'));
+                  uarg    = r.quot;
+            }
+      }
+      else  *tail-- = '0';
+
+      memcpy(head, ++tail, i);
+      return str;
+}
+
+//convert unsigned long to string
+char* ultoa(unsigned long a, char* buffer, uint8_t radix){
+	if(radix<2 || radix>36){
+		return NULL;
+	}
+	char* ptr=buffer;
+	div_t result;
+	if(a==0){
+		ptr[0] = '0';
+		ptr[1] = '\0';
+		return buffer;
+	}
+	while(a){
+		/* toolchain bug??
+		result = div(a, radix);
+		*/
+		result.quot = a/radix;
+		result.rem = a%radix;
+		*ptr = result.rem;
+		if(result.rem<10){
+			*ptr += '0';
+		}else{
+			*ptr += 'a'-10;
+		}
+		++ptr;
+		a = result.quot;
+	}
+	*ptr = '\0';
+	str_reverse(buffer);
+	return buffer;
+}
+
+//convert unsigned int to string
+char* utoa(unsigned a, char* buffer, uint8_t radix){
+	return ultoa((unsigned)a, buffer, radix);
+}
+
+char* itoa(int a, char* buffer, uint8_t radix){
+	if(a<0){
+		*buffer = '-';
+		a = -a;
+	}
+	ultoa(a, buffer + 1, radix);
+	return buffer;
+}
+
+
+// void itoa(int value, char *sp, int radix)
+// {
+//     char tmp[16];// be careful with the length of the buffer
+//     char *tp = tmp;
+//     int i;
+//     unsigned v;
+//     int sign;
+
+//     sign = (radix == 10 && value < 0);
+//     if (sign)   v = -value;
+//     else    v = (unsigned)value;
+
+//     while (v || tp == tmp)
+//     {
+//         i = v % radix;
+//         v /= radix; // v/=radix uses less CPU clocks than v=v/radix does
+//         if (i < 10)
+//           *tp++ = i+'0';
+//         else
+//           *tp++ = i + 'a' - 10;
+//     }
+
+//     if (sign)
+//     *sp++ = '-';
+//     while (tp > tmp)
+//     *sp++ = *--tp;
+// }
 
 //------------------------------------------------------------------------------------------
 
@@ -101,13 +204,13 @@ String::String(char c)
 	*this = buf;
 }
 
-// String::String(unsigned char value, unsigned char base)
-// {
-// 	init();
-// 	char buf[9];
-// 	utoa(value, buf, base);
-// 	*this = buf;
-// }
+String::String(unsigned char value, unsigned char base)
+{
+	init();
+	char buf[9];
+	utoa(value, buf, base);
+	*this = buf;
+}
 
 String::String(int value, unsigned char base)
 {
@@ -117,29 +220,29 @@ String::String(int value, unsigned char base)
 	*this = buf;
 }
 
-// String::String(unsigned int value, unsigned char base)
-// {
-// 	init();
-// 	char buf[17];
-// 	utoa(value, buf, base);
-// 	*this = buf;
-// }
+String::String(unsigned int value, unsigned char base)
+{
+	init();
+	char buf[17];
+	utoa(value, buf, base);
+	*this = buf;
+}
 
-// String::String(long value, unsigned char base)
-// {
-// 	init();
-// 	char buf[34];
-// 	ltoa(value, buf, base);
-// 	*this = buf;
-// }
+String::String(long value, unsigned char base)
+{
+	init();
+	char buf[34];
+	ltoa(value, buf, base);
+	*this = buf;
+}
 
-// String::String(unsigned long value, unsigned char base)
-// {
-// 	init();
-// 	char buf[33];
-// 	ultoa(value, buf, base);
-// 	*this = buf;
-// }
+String::String(unsigned long value, unsigned char base)
+{
+	init();
+	char buf[33];
+	ultoa(value, buf, base);
+	*this = buf;
+}
 
 String::String(float value, int decimalPlaces)
 {
@@ -302,12 +405,12 @@ unsigned char String::concat(char c)
 	return concat(buf, 1);
 }
 
-// unsigned char String::concat(unsigned char num)
-// {
-// 	char buf[4];
-// 	itoa(num, buf, 10);
-// 	return concat(buf, strlen(buf));
-// }
+unsigned char String::concat(unsigned char num)
+{
+	char buf[4];
+	itoa(num, buf, 10);
+	return concat(buf, strlen(buf));
+}
 
 unsigned char String::concat(int num)
 {
@@ -316,26 +419,26 @@ unsigned char String::concat(int num)
 	return concat(buf, strlen(buf));
 }
 
-// unsigned char String::concat(unsigned int num)
-// {
-// 	char buf[6];
-// 	utoa(num, buf, 10);
-// 	return concat(buf, strlen(buf));
-// }
+unsigned char String::concat(unsigned int num)
+{
+	char buf[6];
+	utoa(num, buf, 10);
+	return concat(buf, strlen(buf));
+}
 
-// unsigned char String::concat(long num)
-// {
-// 	char buf[12];
-// 	ltoa(num, buf, 10);
-// 	return concat(buf, strlen(buf));
-// }
+unsigned char String::concat(long num)
+{
+	char buf[12];
+	ltoa(num, buf, 10);
+	return concat(buf, strlen(buf));
+}
 
-// unsigned char String::concat(unsigned long num)
-// {
-// 	char buf[11];
-// 	ultoa(num, buf, 10);
-// 	return concat(buf, strlen(buf));
-// }
+unsigned char String::concat(unsigned long num)
+{
+	char buf[11];
+	ultoa(num, buf, 10);
+	return concat(buf, strlen(buf));
+}
 
 unsigned char String::concat(float num)
 {
