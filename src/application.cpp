7dfb7980c88efaa822987b1c2e20abf7113ab21b
Zachary Crockett <towynlin@gmail.com>
2013-10-31 17:27:43 -0500

    Clear out application

--- src/application.cpp
+++ src/application.cpp
@@ -1,14 +1,9 @@
 #include "application.h"
 
-int sensor_value = 0;
-
 void setup()
 {
-  Spark.variable("sensor", &sensor_value, INT);
-  pinMode(A0, INPUT);
 }
 
 void loop()
 {
-  sensor_value = analogRead(A0);
 }
