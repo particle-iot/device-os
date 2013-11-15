1fd0160c1abb6a4f9f0f119ba95a5eb47b5e9c3c
Satish Nair <satishgn77@gmail.com>
2013-11-15 16:08:45 +0530

    Removed virtual keyword to fix TCPClient connect call

--- inc/spark_wiring_tcpclient.h
+++ inc/spark_wiring_tcpclient.h
@@ -17,18 +17,18 @@ public:
 	TCPClient(uint8_t sock);
 
 	uint8_t status();
-	virtual int connect(IPAddress ip, uint16_t port);
-	virtual int connect(const char *host, uint16_t port);
-	virtual size_t write(uint8_t);
-	virtual size_t write(const uint8_t *buf, size_t size);
-	virtual int available();
-	virtual int read();
-	virtual int read(uint8_t *buf, size_t size);
-	virtual int peek();
-	virtual void flush();
-	virtual void stop();
-	virtual uint8_t connected();
-	virtual operator bool();
+	int connect(IPAddress ip, uint16_t port);
+	int connect(const char *host, uint16_t port);
+	size_t write(uint8_t);
+	size_t write(const uint8_t *buf, size_t size);
+	int available();
+	int read();
+	int read(uint8_t *buf, size_t size);
+	int peek();
+	void flush();
+	void stop();
+	uint8_t connected();
+	operator bool();
 
 	friend class TCPServer;
 
