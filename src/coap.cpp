2f090dd63c1866356cb9bf723e1ebb41d4fa248f
mohit <mohit@sparkdevices.com>
2013-11-16 16:08:53 -0600

    added license and copyright header to all files

--- src/coap.cpp
+++ src/coap.cpp
@@ -1,3 +1,27 @@
+/**
+  ******************************************************************************
+  * @file    coap.cpp
+  * @authors  Zachary Crockett
+  * @version V1.0.0
+  * @date    15-Nov-2013
+  * @brief   COAP
+  ******************************************************************************
+  Copyright (c) 2013 Spark Labs, Inc.  All rights reserved.
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, either
+  version 3 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this program; if not, see <http://www.gnu.org/licenses/>.
+  ******************************************************************************
+  */
 #include "coap.h"
 
 CoAPCode::Enum CoAP::code(const unsigned char *message)
--- src/spark_protocol.h
+++ src/spark_protocol.h
@@ -1,3 +1,27 @@
+/**
+  ******************************************************************************
+  * @file    spark_protocol.h
+  * @authors  Zachary Crockett
+  * @version V1.0.0
+  * @date    15-Nov-2013
+  * @brief   SPARK PROTOCOL
+  ******************************************************************************
+  Copyright (c) 2013 Spark Labs, Inc.  All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, either
+  version 3 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <http://www.gnu.org/licenses/>.
+  ******************************************************************************
+  */
 #include "spark_descriptor.h"
 #include "coap.h"
 #include "tropicssl/rsa.h"
