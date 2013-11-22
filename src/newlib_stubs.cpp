5c6e139079434776d1d441c359bff85cd1e692c3
Satish Nair <satishgn77@gmail.com>
2013-11-22 18:01:12 +0530

    Code cleanup (deleted code not required by the toolchain)

--- src/newlib_stubs.cpp
+++ src/newlib_stubs.cpp
@@ -30,26 +30,6 @@
 #include <stdlib.h>
 
 /*
- * The default pulls in 70K of garbage
- */
-
-namespace __gnu_cxx {
-void __verbose_terminate_handler()
-{
-	for(;;);
-}
-}
-
-/*
- * The default pulls in about 12K of garbage
- */
-
-extern "C" void __cxa_pure_virtual()
-{
-	for(;;);
-}
-
-/*
  * Implement C++ new/delete operators using the heap
  */
 
