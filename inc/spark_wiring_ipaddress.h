277741eb39da39cbd355d83796d15654262ba5df
Mohit <mohit@ubuntu.(none)>
2013-12-03 10:25:19 -0600

    added license headers to tcpip files

--- inc/spark_wiring_ipaddress.h
+++ inc/spark_wiring_ipaddress.h
@@ -1,7 +1,27 @@
-/*
-  TCP/UDP library
-  API declarations borrowed from Arduino & Wiring
-  Modified by Spark
+/**
+ ******************************************************************************
+ * @file    spark_wiring_ipaddress.h
+ * @author  Satish Nair
+ * @version V1.0.0
+ * @date    10-Nov-2013
+ * @brief   Header for spark_wiring_ipaddress.cpp module
+ ******************************************************************************
+  Copyright (c) 2013 Spark Labs, Inc.  All rights reserved.
+  Copyright (c) 2011 Adrian McEwen
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, either
+  version 3 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <http://www.gnu.org/licenses/>.
+  ******************************************************************************
  */
 
 #ifndef __SPARK_WIRING_IPADDRESS_H
--- inc/spark_wiring_tcpserver.h
+++ inc/spark_wiring_tcpserver.h
@@ -1,7 +1,26 @@
-/*
-  TCP/UDP library
-  API declarations borrowed from Arduino & Wiring
-  Modified by Spark
+/**
+ ******************************************************************************
+ * @file    spark_wiring_tcpserver.h
+ * @author  Satish Nair
+ * @version V1.0.0
+ * @date    13-March-2013
+ * @brief   Header for spark_wiring_tcpserver.cpp module
+ ******************************************************************************
+  Copyright (c) 2013 Spark Labs, Inc.  All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, either
+  version 3 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <http://www.gnu.org/licenses/>.
+  ******************************************************************************
  */
 
 #ifndef __SPARK_WIRING_TCPSERVER_H
