fe3886448c9feadae440fd187016e6166827d59c
Mohit <mohit@ubuntu.(none)>
2014-01-22 10:42:49 -0600

    set aucInactivity back to 60 seconds

--- src/spark_wlan.cpp
+++ src/spark_wlan.cpp
@@ -77,7 +77,7 @@ void Set_NetApp_Timeout(void)
 	unsigned long aucDHCP = 14400;
 	unsigned long aucARP = 3600;
 	unsigned long aucKeepalive = 10;
-	unsigned long aucInactivity = 20;
+	unsigned long aucInactivity = 60;
 
 	netapp_timeout_values(&aucDHCP, &aucARP, &aucKeepalive, &aucInactivity);
 }
