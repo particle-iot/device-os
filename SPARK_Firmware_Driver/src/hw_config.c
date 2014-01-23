a7c397b1d2808acebf864c2a721cb19897b887ae
Satish Nair <satishgn77@gmail.com>
2014-01-23 12:43:20 +0530

    Make SPI chip select operations atomic

--- SPARK_Firmware_Driver/src/hw_config.c
+++ SPARK_Firmware_Driver/src/hw_config.c
@@ -983,10 +983,12 @@ void CC3000_SPI_DMA_Channels(FunctionalState NewState)
 /* Select CC3000: ChipSelect pin low */
 void CC3000_CS_LOW(void)
 {
+	__disable_irq();
 	CC3000_SPI->CR1 &= ((uint16_t)0xFFBF);
 	sFLASH_CS_HIGH();
 	CC3000_SPI->CR1 = CC3000_SPI_CR | ((uint16_t)0x0040);
 	GPIO_ResetBits(CC3000_WIFI_CS_GPIO_PORT, CC3000_WIFI_CS_PIN);
+	__enable_irq();
 }
 
 /* Deselect CC3000: ChipSelect pin high */
@@ -1164,10 +1166,12 @@ void sFLASH_SPI_Init(void)
 /* Select sFLASH: Chip Select pin low */
 void sFLASH_CS_LOW(void)
 {
+	__disable_irq();
 	sFLASH_SPI->CR1 &= ((uint16_t)0xFFBF);
 	CC3000_CS_HIGH();
 	sFLASH_SPI->CR1 = sFLASH_SPI_CR | ((uint16_t)0x0040);
 	GPIO_ResetBits(sFLASH_MEM_CS_GPIO_PORT, sFLASH_MEM_CS_PIN);
+	__enable_irq();
 }
 
 /* Deselect sFLASH: Chip Select pin high */
