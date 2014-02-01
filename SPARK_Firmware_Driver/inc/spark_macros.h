b8f3800358c6df2ca5469e6c1261fe05532c8ba7
david_s5 <david_s5@nscdg.com>
2014-02-01 09:28:15 -1000

    Fixed token Pasting in CCASERT

--- SPARK_Firmware_Driver/inc/spark_macros.h
+++ SPARK_Firmware_Driver/inc/spark_macros.h
@@ -15,8 +15,11 @@
 #define INVALID_CASE(c) PANIC(InvalidCase,"Invalid Case %d",(c))
 #define UNUSED(var) (void)(var)
 
+#define _CAT(a, b) a ## b
+#define CAT(a, b) _CAT(a, b)
+
 #define CCASSERT(predicate) _x_CCASSERT_LINE(predicate, __LINE__)
-#define _x_CCASSERT_LINE(predicate, line) typedef char constraint_violated_on_line_##line[2*((predicate)!=0)-1];
+#define _x_CCASSERT_LINE(predicate, line) typedef char CAT(constraint_violated_on_line_,line)[2*((predicate)!=0)-1];
 
 // Seconds to Us
 #define S2u(s) ((s)*1000000)
