c4836e91f3b0bb96fe5b6af4b2786f05bafc3976
Zachary Crockett <towynlin@gmail.com>
2014-03-17 16:23:03 -0500

    Fix typo :)

--- SPARK_Firmware_Driver/inc/panic_codes.h
+++ SPARK_Firmware_Driver/inc/panic_codes.h
@@ -22,7 +22,7 @@ def_panic_codes(System,RGB_COLOR_RED,OutOfHeap)
 
 def_panic_codes(System,RGB_COLOR_RED,SPIOverRun)
 
-def_panic_codes(Sofwtare,RGB_COLOR_RED,AssertionFailure)
-def_panic_codes(Sofwtare,RGB_COLOR_RED,InvalidCase)
-def_panic_codes(Sofwtare,RGB_COLOR_RED,PureVirtualCall)
+def_panic_codes(Software,RGB_COLOR_RED,AssertionFailure)
+def_panic_codes(Software,RGB_COLOR_RED,InvalidCase)
+def_panic_codes(Software,RGB_COLOR_RED,PureVirtualCall)
 
