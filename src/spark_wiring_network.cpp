c3c1015566cf7178dd21e9a82ba14924c25631b3
timothybrown <me@timb.us>
2014-03-17 05:09:12 -0400

    Added NetworkClass::RSSI
    This function gives access to Relative Signal Strength Indicator (RSSI)
    data from the CC3000’s scan table, which is refreshed every few
    minutes. It returns signal strength in the range of -127 to 0 dB.

--- src/spark_wiring_network.cpp
+++ src/spark_wiring_network.cpp
@@ -1,12 +1,12 @@
 /**
  ******************************************************************************
  * @file    spark_wiring_network.cpp
- * @author  Satish Nair
- * @version V1.0.0
- * @date    10-Nov-2013
+ * @author  Satish Nair, Timothy Brown
+ * @version V1.0.1
+ * @date    15-Mar-2014
  * @brief   
  ******************************************************************************
-  Copyright (c) 2013 Spark Labs, Inc.  All rights reserved.
+  Copyright (c) 2014 Spark Labs, Inc.  All rights reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -53,6 +53,40 @@ IPAddress NetworkClass::gatewayIP()
 char* NetworkClass::SSID()
 {
 	return (char *)ip_config.uaSSID;
+}
+
+/*	***********************************************	*/
+/*	* Network.RSSI() - @TimothyBrown - 2014.03.17 *	*/
+/*	***********************************************	*/
+/*	----------------------------------------------------------------------------------------------------------------------------------------------	*/
+/*		This function returns the WiFi signal strength in -dB [-127 to 0]. Error Values: 1 [CC3000 Problem]; 2 [Function Timeout/Bad Data].			*/
+/*		It reads through all entires in the table, continuing even after finding the correct SSID; this prevents stale entries on the next call.	*/
+/*		There is a one second timeout on the function for safety; this also forces the function to re-run if the results table is corrupt.			*/
+/*		Note: The CC3000 only updates the RSSI status every few minutes, so this function doesn't need to be called constantly in the main loop.	*/
+/*	----------------------------------------------------------------------------------------------------------------------------------------------	*/
+int8_t NetworkClass::RSSI()
+{
+	_functionTimeout = millis() + 1000;
+	_loopCount = 0;
+	_returnValue = 0;
+	while (millis() < _functionTimeout)
+	{
+		while (_loopCount++ < 16)
+		{
+			unsigned char wlan_scan_results_table[50];
+			char wlan_scan_results_ssid[32];
+			if(wlan_ioctl_get_scan_results(0, wlan_scan_results_table) != 0) return(1);
+			for (int i = 12; i <= 43; i++)
+			{
+				int arrayPos = i - 12;
+				wlan_scan_results_ssid[arrayPos] = wlan_scan_results_table[i];
+			}
+			if (*wlan_scan_results_ssid == *ip_config.uaSSID) _returnValue = ((wlan_scan_results_table[8] >> 1) - 127);
+			if (wlan_scan_results_table[0] == 0) break;
+		}
+		if (_returnValue != 0) return(_returnValue);
+	}
+	return(2);
 }
 
 NetworkClass Network;
