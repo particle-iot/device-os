9766b74e9e2f20120487a1d68e4b86d6dff4acda
Jacob Rosenthal <jakerosenthal@gmail.com>
2014-05-02 19:23:49 -0700

    Actually dont need print flashstrings, as they'll already have been unrolled

--- inc/spark_wiring_print.h
+++ inc/spark_wiring_print.h
@@ -63,7 +63,6 @@ class Print
     }
     virtual size_t write(const uint8_t *buffer, size_t size);
     
-    size_t print(const __FlashStringHelper *);
     size_t print(const String &);
     size_t print(const char[]);
     size_t print(char);
@@ -75,7 +74,6 @@ class Print
     size_t print(double, int = 2);
     size_t print(const Printable&);
 
-    size_t println(const __FlashStringHelper *);
     size_t println(const String &s);
     size_t println(const char[]);
     size_t println(char);
