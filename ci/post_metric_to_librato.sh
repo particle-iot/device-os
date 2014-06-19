9caa5866eb32abe3d9a07d881a5cf24f89776d54
Joe Goggins <joe.goggins@gmail.com>
2014-06-19 00:18:38 -0500

    Tweak

--- ci/post_metric_to_librato.sh
+++ ci/post_metric_to_librato.sh
@@ -1,7 +1,7 @@
 guaranteed_ram=4096
 available_ram=4100
 curl \
-  -u joe@spark.io:${LIBRATO_ACCESS_TOKEN} \
+  -u ${LIBRATO_USER}:${LIBRATO_ACCESS_TOKEN} \
   -d 'source=core-firmware-travis-ci' \
   -d 'gauges[0][name]=spark_dev.ci.firmware.memory.available' \
   -d "gauges[0][value]=$available_ram" \
