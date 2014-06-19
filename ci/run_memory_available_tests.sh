cc651a7d0b86e37fd73b6536d12a417ef5a21f8c
Joe Goggins <joe.goggins@gmail.com>
2014-06-19 15:27:30 -0500

    travis-ci hit librato tweak

--- ci/run_memory_available_tests.sh
+++ ci/run_memory_available_tests.sh
@@ -30,6 +30,19 @@ export available_ram=`cat variable_get_output.txt`
 if [[ $available_ram =~ ^[0-9]+$ ]]; then
   export available_ram_minus_handshake=$(( $available_ram - $handshake_ram ))
   export headroom=$(( $available_ram_minus_handshake - $guaranteed_ram ))
+  curl \
+   -u ${LIBRATO_USER}:${LIBRATO_ACCESS_TOKEN} \
+   -d 'source=core-firmware-travis-ci' \
+   -d 'gauges[0][name]=spark_dev.ci.firmware.memory.available' \
+   -d "gauges[0][value]=$available_ram" \
+   -d 'gauges[1][name]=spark_dev.ci.firmware.memory.available_minus_handshake' \
+   -d "gauges[1][value]=$available_ram_minus_handshake" \
+   -d 'gauges[2][name]=spark_dev.ci.firmware.memory.guanantee_headroom' \
+   -d "gauges[2][value]=$headroom" \
+   -d 'gauges[3][name]=spark_dev.ci.firmware.memory.guaranteed' \
+   -d "gauges[3][value]=$guaranteed_ram" \
+   -X POST \
+   https://metrics-api.librato.com/v1/metrics
   if [[ $headroom -ge 0 ]]; then
     echo "$headroom bytes of headroom after this commit, use it wisely grasshoppa."
     exit 0
