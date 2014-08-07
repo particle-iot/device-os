53624fced7d35e7ad018c22e22dfd2b72487229d
Jack Bates <jack@nottheoilrig.com>
2014-08-07 11:26:24 -0700

    There's currently no advantage to using dual slow interleaved mode for analogRead()
    See http://community.spark.io/t/why-dual-slow-interleaved/5550

--- inc/spark_wiring.h
+++ inc/spark_wiring.h
@@ -123,8 +123,7 @@
 #define SCL  1
 
 #define ADC1_DR_ADDRESS		((uint32_t)0x4001244C)
-#define ADC_DMA_BUFFERSIZE	10
-#define ADC_SAMPLING_TIME	ADC_SampleTime_7Cycles5	//Allowed values: 1.5, 7.5 and 13.5 for "Dual slow interleaved mode"
+#define ADC_SAMPLING_TIME	ADC_SampleTime_239Cycles5
 
 #define TIM_PWM_FREQ 500 //500Hz
 
