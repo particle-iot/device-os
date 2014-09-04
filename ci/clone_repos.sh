312381b9c44f63409935963e8a229fe623efbe38
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-09-04 17:52:51 +0200

    switch travis-ci to clone mater branches of dependency repos now that the refactoring has been merged into master.

--- ci/clone_repos.sh
+++ ci/clone_repos.sh
@@ -2,5 +2,5 @@
 
 # $1 - branch name to clone
 
-git clone --depth 50 --branch=$1 git://github.com/spark/core-common-lib.git
-git clone --depth 50 --branch=$1 git://github.com/spark/core-communication-lib.git
+git clone --depth 50 --branch=master git://github.com/spark/core-common-lib.git
+git clone --depth 50 --branch=master git://github.com/spark/core-communication-lib.git
