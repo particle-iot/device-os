9427fd8cf7f2ca4a6fb4dea102c482944aa5109b
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-09-06 18:58:47 +0200

    isAlpha, isUpper etc.. arduino-compatible functions, with API/smoke tests. Created spark_wiring_constants.h for hardware-neutral wiring definitions such as HIGH, LOW and boolean.

--- inc/spark_wiring.h
+++ inc/spark_wiring.h
@@ -25,7 +25,6 @@
 
 #ifndef SPARK_WIRING_H
 #define SPARK_WIRING_H
-
 #include "stm32f10x.h"
 #include "config.h"
 #include "spark_macros.h"
@@ -36,31 +35,8 @@
 #include "spark_wiring_printable.h"
 #include "spark_wiring_ipaddress.h"
 #include "spark_wiring_wifi.h"
-
-/*
-* Basic variables
-*/
-
-#if !defined(min)
-#   define min(a,b)                ((a)<(b)?(a):(b))
-#endif
-#if !defined(max)
-#   define max(a,b)                ((a)>(b)?(a):(b))
-#endif
-#if !defined(constrain)
-#   define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
-#endif
-#if !defined(round)
-#   define round(x)                ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
-#endif
-
-#define HIGH 0x1
-#define LOW 0x0
-
-#define boolean bool
-
-//#define NULL ((void *)0)
-#define NONE ((uint8_t)0xFF)
+#include "spark_wiring_character.h"
+#include "spark_wiring_constants.h"
 
 /*
 * Pin mapping. Borrowed from Wiring
