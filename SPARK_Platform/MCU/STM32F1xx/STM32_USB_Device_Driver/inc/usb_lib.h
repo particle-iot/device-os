f2140d02a0b5547868b66c20054cea3f0ba99a0a
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-09-12 20:06:50 +0200

    revert comment out

--- SPARK_Platform/MCU/STM32F1xx/STM32_USB_Device_Driver/inc/usb_lib.h
+++ SPARK_Platform/MCU/STM32F1xx/STM32_USB_Device_Driver/inc/usb_lib.h
@@ -28,7 +28,7 @@
 #define __USB_LIB_H
 
 /* Includes ------------------------------------------------------------------*/
-//#include "hw_config.h"
+#include "hw_config.h"
 #include "usb_type.h"
 #include "usb_regs.h"
 #include "usb_def.h"
