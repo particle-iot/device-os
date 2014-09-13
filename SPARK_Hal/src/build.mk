2f4adef19d4079fda769b4f1b26136294d34c66f
Satish Nair <satishgn77@gmail.com>
2014-09-13 12:57:39 +0530

    Only complile completed xxx_hal.c files

--- SPARK_Hal/src/build.mk
+++ SPARK_Hal/src/build.mk
@@ -9,21 +9,21 @@ TARGET_HAL_SRC_PATH = $(TARGET_HAL_PATH)/src
 
 INCLUDE_DIRS += $(TARGET_HAL_PATH)/inc
 
-CSRC += $(TARGET_HAL_SRC_PATH)/core_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/eeprom_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/i2c_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/interrupt_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/spi_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/usart_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/core_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/eeprom_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/i2c_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/interrupt_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/spi_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/usart_hal.c
 CSRC += $(TARGET_HAL_SRC_PATH)/usb_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/wlan_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/adc_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/dac_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/pwm_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/timer_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/rtc_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/pinmap_hal.c
-CSRC += $(TARGET_HAL_SRC_PATH)/network_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/wlan_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/adc_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/dac_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/pwm_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/timer_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/rtc_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/pinmap_hal.c
+#CSRC += $(TARGET_HAL_SRC_PATH)/network_hal.c
 
 # C++ source files included in this build.
 CPPSRC +=
