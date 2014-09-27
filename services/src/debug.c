4bbf3977a861646a856acafa7f3a0918fc4d0b99
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-09-27 04:23:10 +0200

    debug is in services and cannot be dependent on hw_config.h (new build system caught this dependency error.) Changed GetSystemMsTick to the equivalent HAL function call.

--- services/src/debug.c
+++ services/src/debug.c
@@ -10,9 +10,9 @@
 #include <stdarg.h>
 #include <string.h>
 #include "config.h"
-#include "hw_config.h"
 #include "spark_macros.h"
 #include "debug.h"
+#include "timer_hal.h"
 
 
 uint32_t log_level_at_run_time = LOG_LEVEL_AT_RUN_TIME;
@@ -31,7 +31,7 @@ void log_print_(int level, int line, const char *func, const char *file, const c
         va_list args;
         va_start(args, msg);
         file = file ? strrchr(file,'/') + 1 : "";
-        int trunc = snprintf(_buffer, arraySize(_buffer), "%010lu:<%s> %s %s(%d):", GetSystem1MsTick(), levels[level], func, file, line);
+        int trunc = snprintf(_buffer, arraySize(_buffer), "%010lu:<%s> %s %s(%d):", HAL_Timer_Get_Milli_Seconds(), levels[level], func, file, line);
         if (debug_output_)
         {
             debug_output_(_buffer);
