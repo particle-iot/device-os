f82a9e77e8fe818f3e0dee5c298743b7936dedab
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-09-28 14:40:58 +0200

    core-v1 sources auto-discovered - saves having to modify sources.mk each time a new source file is added.

--- hal/src/core-v1/sources.mk
+++ hal/src/core-v1/sources.mk
@@ -9,31 +9,12 @@ HAL_SRC_COREV1_PATH = $(TARGET_HAL_PATH)/src/core-v1
 INCLUDE_DIRS += $(HAL_SRC_COREV1_PATH)
 
 # C source files included in this build.
-CSRC += $(HAL_SRC_COREV1_PATH)/adc_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/core_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/core_subsys_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/delay_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/eeprom_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/gpio_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/i2c_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/inet_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/interrupts_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/ota_flash_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/pinmap_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/pwm_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/rtc_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/servo_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/socket_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/spi_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/timer_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/tone_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/usart_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/usb_hal.c
-CSRC += $(HAL_SRC_COREV1_PATH)/wlan_hal.c
+CSRC += $(call target_files,$(HAL_SRC_COREV1_PATH)/,*.c)
+
 
 # C++ source files included in this build.
-CPPSRC += $(HAL_SRC_COREV1_PATH)/deviceid_hal.cpp
-# WIP - CPPSRC += $(HAL_SRC_COREV1_PATH)/memory_hal.cpp
+CPPSRC += $(call target_files,$(HAL_SRC_COREV1_PATH)/,*.cpp)
+
 
 # ASM source files included in this build.
 ASRC +=
