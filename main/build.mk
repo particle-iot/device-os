2ea2fcb6b03af2197924ae4ed70d95f8a4f174d3
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-09-29 15:47:32 +0200

    AS isn't called directly, but via gcc, so need to wrap as gcc-pass-to-assembler arguments

--- main/build.mk
+++ main/build.mk
@@ -50,7 +50,7 @@ LDFLAGS += -u _printf_float
 
 ASRC += $(COMMON_BUILD)/arm/startup/startup_$(STM32_DEVICE).S 
 ASFLAGS += -I$(COMMON_BUILD)/arm/startup
-ASFLAGS += --defsym SPARK_INIT_STARTUP=1
+ASFLAGS +=  -Wa,--defsym -Wa,SPARK_INIT_STARTUP=1
 
 endif
 
