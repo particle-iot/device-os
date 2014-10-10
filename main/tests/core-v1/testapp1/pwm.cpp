92e11ea0f3c800a2d8288fe530f024677f5de693
Satish Nair <satishgn77@gmail.com>
2014-10-10 17:24:12 +0530

    Updated PWM test application (testapp1/pwm.cpp)

--- main/tests/core-v1/testapp1/pwm.cpp
+++ main/tests/core-v1/testapp1/pwm.cpp
@@ -1,50 +1,56 @@
 
 #include "application.h"
+#include "pwm_hal.h"
 #include "unit-test/unit-test.h"
 
 test(PWM_NoAnalogWriteWhenPinModeIsNotSetToOutput) {
+    pin_t pin = A0;//pin under test
     // when
-    pinMode(A0, INPUT);//pin set to INPUT mode
-    analogWrite(A0, 50);
+    pinMode(pin, INPUT);//pin set to INPUT mode
+    analogWrite(pin, 50);
     // then
-    assertNotEqual(HAL_PWM_Get_AnalogValue(A0), 50);
+    assertNotEqual(HAL_PWM_Get_AnalogValue(pin), 50);
     //To Do : Add test for remaining pins if required
 }
 
 test(PWM_NoAnalogWriteWhenPinSelectedIsNotTimerChannel) {
+    pin_t pin = D5;//pin under test
     // when
-    pinMode(D5, OUTPUT);//D5 is not a Timer channel
-    analogWrite(D5, 100);
+    pinMode(pin, OUTPUT);//D5 is not a Timer channel
+    analogWrite(pin, 100);
     // then
     //analogWrite works on fixed PWM frequency of 500Hz
-    assertNotEqual(HAL_PWM_Get_Frequency(D5), TIM_PWM_FREQ);
+    assertNotEqual(HAL_PWM_Get_Frequency(pin), TIM_PWM_FREQ);
     //To Do : Add test for remaining pins if required
 }
 
 test(PWM_NoAnalogWriteWhenPinSelectedIsOutOfRange) {
+    pin_t pin = 21;//pin under test (not a valid user pin)
     // when
-    pinMode(21, OUTPUT);//21 is not a user pin
-    analogWrite(21, 100);
+    pinMode(pin, OUTPUT);//21 is not a user pin
+    analogWrite(pin, 100);
     // then
-    assertNotEqual(HAL_PWM_Get_AnalogValue(21), 100);
+    assertNotEqual(HAL_PWM_Get_AnalogValue(pin), 100);
     //To Do : Add test for remaining pins if required
 }
 
 test(PWM_AnalogWriteOnPinResultsInCorrectFrequency) {
+    pin_t pin = A0;//pin under test
     // when
-    pinMode(A0, OUTPUT);
-    analogWrite(A0, 150);
+    pinMode(pin, OUTPUT);
+    analogWrite(pin, 150);
     // then
     //analogWrite works on fixed PWM frequency of 500Hz
-    assertEqual(HAL_PWM_Get_Frequency(A0), TIM_PWM_FREQ);
+    assertEqual(HAL_PWM_Get_Frequency(pin), TIM_PWM_FREQ);
     //To Do : Add test for remaining pins if required
 }
 
 test(PWM_AnalogWriteOnPinResultsInCorrectAnalogValue) {
+    pin_t pin = A1;//pin under test
     // when
-    pinMode(A1, OUTPUT);
-    analogWrite(A1, 200);
+    pinMode(pin, OUTPUT);
+    analogWrite(pin, 200);
     // then
-    assertEqual(HAL_PWM_Get_AnalogValue(A1), 200);
+    assertEqual(HAL_PWM_Get_AnalogValue(pin), 200);
     //To Do : Add test for remaining pins if required
 }
