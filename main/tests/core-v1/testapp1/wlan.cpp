c0f6054542735dc7312991d0c8b28d0eb992c100
Technobly <technobly@gmail.com>
2014-10-20 23:08:46 -0500

    Removed duplicate WLAN test, gave order to tests

--- main/tests/core-v1/testapp1/wlan.cpp
+++ main/tests/core-v1/testapp1/wlan.cpp
@@ -3,27 +3,11 @@
 
 #include "inet_hal.h"
 
-test(WLAN_Ping_To_Symmetrical_Address)
-{
-    WiFi.connect();
-    const int tries = 5;
-    int count = WiFi.ping(IPAddress(8,8,8,8), tries);    
-    assertEqual(count, tries);
-}
-
-test(WLAN_Ping_To_Asymmetrical_Address)
-{
-    WiFi.connect();
-    const int tries = 5;        
-    int count = WiFi.ping(IPAddress(62, 116, 130, 8), tries);    
-    assertEqual(count, tries);
-}
-
 const uint32_t spark_io = (62u<<24) | (116u << 16) | (130u<<8) | 8;
 const uint32_t device_spark_io = (54u<<24) | (208u << 16) | (229u<<8) | 4u;
 const char* spark_io_str = "spark.io";
 
-test(WLAN_Lookup_IP_From_Hostname)
+test(WLAN_Test1_Lookup_IP_From_Hostname)
 {
     char hostname[25];
     strcpy(hostname, spark_io_str);
@@ -33,13 +17,32 @@ test(WLAN_Lookup_IP_From_Hostname)
     assertMoreOrEqual(result, 0); // cc3000 returns >=0 on success
 }
 
-test(WLAN_Ping_By_Hostname)
+test(WLAN_Test2_Ping_To_Symmetrical_Address)
+{
+    WiFi.connect();
+    const int tries = 5;
+    int count = WiFi.ping(IPAddress(8,8,8,8), tries);    
+    assertEqual(count, tries);
+}
+
+test(WLAN_Test3_Ping_To_Asymmetrical_Address)
+{
+    WiFi.connect();
+    const int tries = 5;        
+    int count = WiFi.ping(IPAddress(62, 116, 130, 8), tries);    
+    assertEqual(count, tries);
+}
+
+// todo - duplicated test, determine if this is necessary... ping by hostname doesn't appear to be supported
+#if 0
+test(WLAN_Test4_Ping_By_Hostname)
 {    
     WiFi.connect();
     const int tries = 5;        
     int count = WiFi.ping(IPAddress(62, 116, 130, 8), tries);
     assertEqual(count, tries);    
 }
+#endif
 
 // todo - this doesn't work, it works fine cross compiled in the unit tests suite
 #if 0
@@ -54,7 +57,7 @@ test(IPAddress_Construct_From_Uint32)
 }
 #endif
 
-test(IPAddress_Construct_From_Octets)
+test(WLAN_Test5_IPAddress_Construct_From_Octets)
 {
     IPAddress ip(54, 208, 229, 4);
 
@@ -64,15 +67,14 @@ test(IPAddress_Construct_From_Octets)
     assertEqual(ip[3], 4);
 }
 
-
-test(IPAddress_Construct_Octets_Equal_Uint32)
+test(WLAN_Test6_IPAddress_Construct_Octets_Equal_Uint32)
 {
     IPAddress ip(1,2,3,4);
     
     assertTrue(ip==0x01020304);    
 }
 
-test(IPAddress_Construct_Uin32_Equal_Octets)
+test(WLAN_Test7_IPAddress_Construct_Uin32_Equal_Octets)
 {
     IPAddress ip(0x01020304);
     
