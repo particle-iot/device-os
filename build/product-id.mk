7ccab61edd07285248b2575ce33c5c7cc3cd3411
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-10-21 20:40:43 +0200

    removed unused code

--- build/product-id.mk
+++ build/product-id.mk
@@ -50,15 +50,6 @@ PRODUCT_DESC=GCC xcompile
 SPARK_NO_PLATFORM=1
 endif
 
-ifeq ("$(SPARK_PRODUCT_ID)","4")
-STM32_DEVICE=STM32F20X_HD
-PLATFORM_NAME=core-v2
-PLATFORM_MCU=STM32F2xx
-PLATFORM_NET=WICED
-PRODUCT_DESC=Spark core-v2/WICED
-endif
-
-
 ifeq ("$(PLATFORM_MCU)","")
 $(error PLATFORM_MCU not defined. Check product id $(SPARK_PRODUCT_ID))
 endif
