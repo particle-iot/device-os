bfae7a1cd154426edb86b978cddc12af4f8fbefc
Satish Nair <satishgn77@gmail.com>
2014-10-22 18:35:41 +0530

    Implemented new HAL functions for backup register handling

--- bootloader/src/core-v1/dfu_hal.c
+++ bootloader/src/core-v1/dfu_hal.c
@@ -24,6 +24,7 @@
  */
 
 /* Includes ------------------------------------------------------------------*/
+#include "usb_hal.h"
 #include "usb_lib.h"
 #include "usb_conf.h"
 #include "usb_prop.h"
@@ -78,6 +79,43 @@ void HAL_DFU_USB_Init(void)
 
     // USB System initialization
     USB_Init();
+}
+
+int32_t HAL_Core_Backup_Register(uint32_t BKP_DR)
+{
+    switch(BKP_DR)
+    {
+        case BKP_DR_01: return BKP_DR1;  break;
+        case BKP_DR_02: return BKP_DR2;  break;
+        case BKP_DR_03: return BKP_DR3;  break;
+        case BKP_DR_04: return BKP_DR4;  break;
+        case BKP_DR_05: return BKP_DR5;  break;
+        case BKP_DR_06: return BKP_DR6;  break;
+        case BKP_DR_07: return BKP_DR7;  break;
+        case BKP_DR_08: return BKP_DR8;  break;
+        case BKP_DR_09: return BKP_DR9;  break;
+        case BKP_DR_10: return BKP_DR10; break;
+    }
+    return -1;
+}
+
+void HAL_Core_Write_Backup_Register(uint32_t BKP_DR, uint32_t Data)
+{
+    uint32_t BKP_DR_Address = HAL_Core_Backup_Register(BKP_DR);
+    if(BKP_DR_Address != -1)
+    {
+        BKP_WriteBackupRegister(BKP_DR_Address, Data);
+    }
+}
+
+uint32_t HAL_Core_Read_Backup_Register(uint32_t BKP_DR)
+{
+    uint32_t BKP_DR_Address = HAL_Core_Backup_Register(BKP_DR);
+    if(BKP_DR_Address != -1)
+    {
+        return BKP_ReadBackupRegister(BKP_DR_Address);
+    }
+    return 0xFFFFFFFF;
 }
 
 /*******************************************************************************
--- bootloader/src/core-v2/dfu_hal.c
+++ bootloader/src/core-v2/dfu_hal.c
@@ -24,6 +24,7 @@
  */
 
 /* Includes ------------------------------------------------------------------*/
+#include "dfu_hal.h"
 #include "usbd_dfu_core.h"
 #include "usbd_usr.h"
 #include "usbd_desc.h"
@@ -57,4 +58,41 @@ void HAL_DFU_USB_Init(void)
             &USR_desc,
             &DFU_cb,
             &USR_cb);
+}
+
+int32_t HAL_Core_Backup_Register(uint32_t BKP_DR)
+{
+    switch(BKP_DR)
+    {
+        case BKP_DR_01: return RTC_BKP_DR1;  break;
+        case BKP_DR_02: return RTC_BKP_DR2;  break;
+        case BKP_DR_03: return RTC_BKP_DR3;  break;
+        case BKP_DR_04: return RTC_BKP_DR4;  break;
+        case BKP_DR_05: return RTC_BKP_DR5;  break;
+        case BKP_DR_06: return RTC_BKP_DR6;  break;
+        case BKP_DR_07: return RTC_BKP_DR7;  break;
+        case BKP_DR_08: return RTC_BKP_DR8;  break;
+        case BKP_DR_09: return RTC_BKP_DR9;  break;
+        case BKP_DR_10: return RTC_BKP_DR10; break;
+    }
+    return -1;
+}
+
+void HAL_Core_Write_Backup_Register(uint32_t BKP_DR, uint32_t Data)
+{
+    uint32_t BKP_DR_Address = HAL_Core_Backup_Register(BKP_DR);
+    if(BKP_DR_Address != -1)
+    {
+        RTC_WriteBackupRegister(BKP_DR_Address, Data);
+    }
+}
+
+uint32_t HAL_Core_Read_Backup_Register(uint32_t BKP_DR)
+{
+    uint32_t BKP_DR_Address = HAL_Core_Backup_Register(BKP_DR);
+    if(BKP_DR_Address != -1)
+    {
+        return RTC_ReadBackupRegister(BKP_DR_Address);
+    }
+    return 0xFFFFFFFF;
 }
