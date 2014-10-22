ed3f1777b77b799fc3453e95da8e2395f709984c
Satish Nair <satishgn77@gmail.com>
2014-10-22 14:56:11 +0530

    Added include.mk and sources.mk files to STM32_USB_xxx subfolders

---
+++ platform/MCU/STM32F2xx/STM32_USB_Device_Driver/src/sources.mk
@@ -0,0 +1,27 @@
+# This file is a makefile included from the top level makefile which
+# defines the sources built for the target.
+
+# Define the prefix to this directory. 
+# Note: The name must be unique within this build and should be
+#       based on the root of the project
+TARGET_USB_FS_SRC_PATH = $(TARGET_USB_FS_PATH)/src
+
+# C source files included in this build.
+CSRC += $(TARGET_USB_FS_SRC_PATH)/usbd_core.c
+CSRC += $(TARGET_USB_FS_SRC_PATH)/usbd_req.c
+CSRC += $(TARGET_USB_FS_SRC_PATH)/usbd_ioreq.c
+CSRC += $(TARGET_USB_FS_SRC_PATH)/usbd_cdc_core.c
+CSRC += $(TARGET_USB_FS_SRC_PATH)/usbd_cdc_if_template.c
+CSRC += $(TARGET_USB_FS_SRC_PATH)/usbd_dfu_core.c
+CSRC += $(TARGET_USB_FS_SRC_PATH)/usbd_dfu_mal.c
+CSRC += $(TARGET_USB_FS_SRC_PATH)/usbd_flash_if.c
+CSRC += $(TARGET_USB_FS_SRC_PATH)/usbd_mem_if_template.c
+CSRC += $(TARGET_USB_FS_SRC_PATH)/usbd_otp_if.c
+CSRC += $(TARGET_USB_FS_SRC_PATH)/usbd_hid_core.c
+
+# C++ source files included in this build.
+CPPSRC +=
+
+# ASM source files included in this build.
+ASRC +=
+
