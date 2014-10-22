b6f1a5d928ab1e9fc397f6d0356552a93be8bf7e
Satish Nair <satishgn77@gmail.com>
2014-10-22 13:30:46 +0530

    Added include.mk and sources.mk files to STM32F2xx subfolders

---
+++ platform/MCU/STM32F2xx/SPARK_Firmware_Driver/src/sources.mk
@@ -0,0 +1,21 @@
+# This file is a makefile included from the top level makefile which
+# defines the sources built for the target.
+
+# Define the prefix to this directory. 
+# Note: The name must be unique within this build and should be
+#       based on the root of the project
+TARGET_SPARK_SRC_PATH = $(TARGET_SPARK_PATH)/src
+
+INCLUDE_DIRS += SPARK_Services/inc
+
+# C source files included in this build.
+# CSRC += $(TARGET_SPARK_SRC_PATH)/hw_config.c
+CSRC += $(TARGET_SPARK_SRC_PATH)/system_stm32f2xx.c
+
+# C++ source files included in this build.
+CPPSRC +=
+
+# ASM source files included in this build.
+ASRC +=
+
+
