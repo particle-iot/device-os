49282a69f924cde33d6a51fe12ea50dad6af96b8
Satish Nair <satishgn77@gmail.com>
2014-10-31 17:17:11 +0530

    Added basic interrupt handler code for main firmware

--- platform/MCU/STM32F2xx/SPARK_Firmware_Driver/src/sources.mk
+++ platform/MCU/STM32F2xx/SPARK_Firmware_Driver/src/sources.mk
@@ -9,8 +9,9 @@ TARGET_SPARK_SRC_PATH = $(TARGET_SPARK_PATH)/src
 INCLUDE_DIRS += SPARK_Services/inc
 
 # C source files included in this build.
-CSRC += $(TARGET_SPARK_SRC_PATH)/hw_config.c
 CSRC += $(TARGET_SPARK_SRC_PATH)/system_stm32f2xx.c
+CSRC += $(TARGET_SPARK_SRC_PATH)/hw_config.c
+# CSRC += $(TARGET_SPARK_SRC_PATH)/stm32_it.c
 
 # C++ source files included in this build.
 CPPSRC +=
