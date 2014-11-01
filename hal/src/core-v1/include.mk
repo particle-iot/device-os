88ff39b06b0977ced9206a380c29c7ece6eaa224
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-11-01 04:08:17 +0100

    linking is only needed for modules using hal library.

--- hal/src/core-v1/include.mk
+++ hal/src/core-v1/include.mk
@@ -12,6 +12,9 @@ ifneq (,$(findstring platform,$(DEPENDENCIES)))
 INCLUDE_DIRS += $(HAL_SRC_COREV1_PATH)
 endif
 
+
+ifneq (,$(findstring hal,$(MAKE_DEPENDENCIES)))
+
 LDFLAGS += -T$(COMMON_BUILD)/arm/linker/linker_$(STM32_DEVICE_LC)_dfu.ld
 LDFLAGS += --specs=nano.specs -lc -lnosys
 LDFLAGS += -u _printf_float
@@ -20,3 +23,4 @@ ASRC += $(COMMON_BUILD)/arm/startup/startup_$(STM32_DEVICE_LC).S
 ASFLAGS += -I$(COMMON_BUILD)/arm/startup
 ASFLAGS +=  -Wa,--defsym -Wa,SPARK_INIT_STARTUP=1
 
+endif
