6d946b791b8c876d805408d242a9b7b8d07f4ecc
Satish Nair <satishgn77@gmail.com>
2014-11-03 17:27:49 +0530

    Move shared usb_bsp.c from bootloader to platform subfolder

--- platform/MCU/STM32F2xx/SPARK_Firmware_Driver/src/sources.mk
+++ platform/MCU/STM32F2xx/SPARK_Firmware_Driver/src/sources.mk
@@ -11,6 +11,7 @@ INCLUDE_DIRS += SPARK_Services/inc
 # C source files included in this build.
 CSRC += $(TARGET_SPARK_SRC_PATH)/system_stm32f2xx.c
 CSRC += $(TARGET_SPARK_SRC_PATH)/hw_config.c
+CSRC += $(TARGET_SPARK_SRC_PATH)/usb_bsp.c
 # CSRC += $(TARGET_SPARK_SRC_PATH)/stm32_it.c
 
 # C++ source files included in this build.
