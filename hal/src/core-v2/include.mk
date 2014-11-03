5f7013a6a064ede3e0a6f346685caa4bdd78cb26
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-11-03 22:30:01 +0100

    wip on core-v2 linking

--- hal/src/core-v2/include.mk
+++ hal/src/core-v2/include.mk
@@ -16,16 +16,17 @@ ifneq (,$(findstring hal,$(MAKE_DEPENDENCIES)))
 # if hal is used as a make dependency (linked) then add linker commands
 
 HAL_LIB_COREV2 = $(HAL_SRC_COREV2_PATH)/lib
-HAL_WICED_LIBS += $(sort $(call rwildcard,$(HAL_LIB_COREV2)/,*.a))
+HAL_WICED_LIBS += Platform_BCM9WCDUSI09 FreeRTOS LwIP WICED SPI_Flash_Library_BCM9WCDUSI09 WWD_FreeRTOS_Interface_BCM9WCDUSI09 WICED_FreeRTOS_Interface WWD_LwIP_Interface_FreeRTOS WICED_LwIP_Interface Supplicant_BESL Lib_HTTP_Server Lib_DNS_Redirect_Daemon Lib_DNS WWD_for_SDIO_FreeRTOS Lib_Wiced_RO_FS STM32F2xx Wiced_Network_LwIP_FreeRTOS Lib_DHCP_Server Lib_base64 common_GCC STM32F2xx_Peripheral_Drivers Ring_Buffer STM32F2xx_Peripheral_Libraries
+HAL_WICED_LIB_FILES += $(addprefix $(HAL_LIB_COREV2)/,$(addsuffix .a,$(HAL_WICED_LIBS)))
 
 WICED_MCU = $(HAL_SRC_COREV2_PATH)/wiced/platform/MCU/STM32F2xx/GCC
-LDFLAGS += -Wl,--start-group $(HAL_WICED_LIBS) -Wl,--end-group
+LDFLAGS += -Wl,--start-group $(HAL_WICED_LIB_FILES) -Wl,--end-group 
 LDFLAGS += -T$(WICED_MCU)/app_no_bootloader.ld
 LDFLAGS += -L$(WICED_MCU)/STM32F2x5
 LDFLAGS += -Wl,--defsym,__STACKSIZE__=1400
 LDFLAGS += --specs=nano.specs -lc -lnosys
 LDFLAGS += -u _printf_float
-
+LDFLAGS += -Wl,-Map,$(TARGET_BASE).map
 endif
 
 # not using assembler startup script, but will use startup linked in with wiced
