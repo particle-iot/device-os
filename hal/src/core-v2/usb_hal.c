38d195386b06ba00632ca2fcdb06f1146fe6f7b6
Satish Nair <satishgn77@gmail.com>
2014-11-05 18:38:05 +0530

    Added basic usb_hal.c template for core-v2

--- hal/src/core-v2/usb_hal.c
+++ hal/src/core-v2/usb_hal.c
@@ -3,7 +3,7 @@
  * @file    usb_hal.c
  * @author  Satish Nair
  * @version V1.0.0
- * @date    10-Sept-2014
+ * @date    05-Nov-2014
  * @brief   USB Virtual COM Port and HID device HAL
  ******************************************************************************
   Copyright (c) 2013-14 Spark Labs, Inc.  All rights reserved.
@@ -25,12 +25,10 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "usb_hal.h"
+#include "usbd_cdc_core.h"
+#include "usbd_usr.h"
 #include "usb_conf.h"
-#include "usb_lib.h"
-#include "usb_desc.h"
-#include "usb_pwr.h"
-#include "usb_prop.h"
-#include "delay_hal.h"
+#include "usbd_desc.h"
 
 /* Private typedef -----------------------------------------------------------*/
 
@@ -40,28 +38,12 @@
 
 /* Private variables ---------------------------------------------------------*/
 #ifdef USB_CDC_ENABLE
-uint8_t  USART_Rx_Buffer[USART_RX_DATA_SIZE];
-uint32_t USART_Rx_ptr_in = 0;
-uint32_t USART_Rx_ptr_out = 0;
-uint32_t USART_Rx_length  = 0;
-
-uint8_t USB_Rx_Buffer[CDC_DATA_SIZE];
-uint16_t USB_Rx_length = 0;
-uint16_t USB_Rx_ptr = 0;
-
-uint8_t  USB_Tx_State = 0;
-uint8_t  USB_Rx_State = 0;
-
-uint32_t USB_USART_BaudRate = 9600;
-
-__IO uint8_t PrevXferComplete;
+//To Do
 #endif
 
 /* Extern variables ----------------------------------------------------------*/
-extern LINE_CODING linecoding;
 
 /* Private function prototypes -----------------------------------------------*/
-static void IntToUnicode (uint32_t value , uint8_t *pbuf , uint8_t len);
 
 #if defined (USB_CDC_ENABLE) || defined (USB_HID_ENABLE)
 /*******************************************************************************
@@ -72,12 +54,7 @@ static void IntToUnicode (uint32_t value , uint8_t *pbuf , uint8_t len);
  *******************************************************************************/
 void SPARK_USB_Setup(void)
 {
-  USB_Disconnect_Config();
-  USB_Cable_Config(DISABLE);
-  HAL_Delay_Microseconds(100000);
-  Set_USBClock();
-  USB_Interrupts_Config();
-  USB_Init();
+    //To Do
 }
 
 /*******************************************************************************
@@ -89,19 +66,7 @@ void SPARK_USB_Setup(void)
  *******************************************************************************/
 void Get_SerialNum(void)
 {
-  uint32_t Device_Serial0, Device_Serial1, Device_Serial2;
-
-  Device_Serial0 = *(uint32_t*)ID1;
-  Device_Serial1 = *(uint32_t*)ID2;
-  Device_Serial2 = *(uint32_t*)ID3;
-
-  Device_Serial0 += Device_Serial2;
-
-  if (Device_Serial0 != 0)
-  {
-    IntToUnicode (Device_Serial0, &USB_StringSerial[2] , 8);
-    IntToUnicode (Device_Serial1, &USB_StringSerial[18], 4);
-  }
+    //To Do
 }
 #endif
 
@@ -114,8 +79,7 @@ void Get_SerialNum(void)
  *******************************************************************************/
 void USB_USART_Init(uint32_t baudRate)
 {
-  linecoding.bitrate = baudRate;
-  SPARK_USB_Setup();
+    //To Do
 }
 
 /*******************************************************************************
@@ -126,15 +90,8 @@ void USB_USART_Init(uint32_t baudRate)
  *******************************************************************************/
 uint8_t USB_USART_Available_Data(void)
 {
-  if(bDeviceState == CONFIGURED)
-  {
-    if(USB_Rx_State == 1)
-    {
-      return (USB_Rx_length - USB_Rx_ptr);
-    }
-  }
-
-  return 0;
+    //To Do
+    return 0;
 }
 
 /*******************************************************************************
@@ -145,23 +102,8 @@ uint8_t USB_USART_Available_Data(void)
  *******************************************************************************/
 int32_t USB_USART_Receive_Data(void)
 {
-  if(bDeviceState == CONFIGURED)
-  {
-    if(USB_Rx_State == 1)
-    {
-      if((USB_Rx_length - USB_Rx_ptr) == 1)
-      {
-        USB_Rx_State = 0;
-
-        /* Enable the receive of data on EP3 */
-        SetEPRxValid(ENDP3);
-      }
-
-      return USB_Rx_Buffer[USB_Rx_ptr++];
-    }
-  }
-
-  return -1;
+    //To Do
+    return -1;
 }
 
 /*******************************************************************************
@@ -172,24 +114,7 @@ int32_t USB_USART_Receive_Data(void)
  *******************************************************************************/
 void USB_USART_Send_Data(uint8_t Data)
 {
-  if(bDeviceState == CONFIGURED)
-  {
-    USART_Rx_Buffer[USART_Rx_ptr_in] = Data;
-
-    USART_Rx_ptr_in++;
-
-    /* To avoid buffer overflow */
-    if(USART_Rx_ptr_in == USART_RX_DATA_SIZE)
-    {
-      USART_Rx_ptr_in = 0;
-    }
-
-    if(CC3000_Read_Interrupt_Pin())
-    {
-      //Delay 100us to avoid losing the data
-      HAL_Delay_Microseconds(100);
-    }
-  }
+    //To Do
 }
 #endif
 
@@ -203,45 +128,7 @@ void USB_USART_Send_Data(uint8_t Data)
  *******************************************************************************/
 void USB_HID_Send_Report(void *pHIDReport, size_t reportSize)
 {
-  if (bDeviceState == CONFIGURED)
-  {
-    /* Reset the control token to inform upper layer that a transfer is ongoing */
-    PrevXferComplete = 0;
-
-    /* Copy HID Report to ENDP1 Tx Packet Memory Area*/
-    UserToPMABufferCopy((uint8_t *)pHIDReport, ENDP1_TXADDR, reportSize);
-    SetEPTxCount(ENDP1, reportSize);
-
-    /* Enable endpoint for transmission */
-    SetEPTxValid(ENDP1);
-  }
+    //To Do
 }
 #endif
 
-/*******************************************************************************
- * Function Name  : HexToChar.
- * Description    : Convert Hex 32Bits value into char.
- * Input          : None.
- * Output         : None.
- * Return         : None.
- *******************************************************************************/
-static void IntToUnicode (uint32_t value , uint8_t *pbuf , uint8_t len)
-{
-  uint8_t idx = 0;
-
-  for( idx = 0 ; idx < len ; idx ++)
-  {
-    if( ((value >> 28)) < 0xA )
-    {
-      pbuf[ 2* idx] = (value >> 28) + '0';
-    }
-    else
-    {
-      pbuf[2* idx] = (value >> 28) + 'A' - 10;
-    }
-
-    value = value << 4;
-
-    pbuf[ 2* idx + 1] = 0;
-  }
-}
