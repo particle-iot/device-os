059c99d9473452a6d4603b06e4a9ac5bbf4bfddf
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-11-11 13:20:47 +0100

    additional BESL library required

--- hal/src/core-v2/include.mk
+++ hal/src/core-v2/include.mk
@@ -18,13 +18,14 @@ ifneq (,$(findstring hal,$(MAKE_DEPENDENCIES)))
 ifeq ("$(USE_WICED_SDK)","1")
 
 HAL_LIB_COREV2 = $(HAL_SRC_COREV2_PATH)/lib
-HAL_WICED_LIBS += common_GCC Platform_BCM9WCDUSI09 FreeRTOS LwIP WICED SPI_Flash_Library_BCM9WCDUSI09 WWD_FreeRTOS_Interface_BCM9WCDUSI09 WICED_FreeRTOS_Interface WWD_LwIP_Interface_FreeRTOS WICED_LwIP_Interface Supplicant_BESL Lib_HTTP_Server Lib_DNS_Redirect_Daemon Lib_DNS WWD_for_SDIO_FreeRTOS Lib_Wiced_RO_FS STM32F2xx Wiced_Network_LwIP_FreeRTOS Lib_DHCP_Server Lib_base64 STM32F2xx_Peripheral_Drivers Ring_Buffer STM32F2xx_Peripheral_Libraries
+HAL_WICED_LIBS += BESL.ARM_CM3.release Platform_BCM9WCDUSI09 FreeRTOS LwIP WICED SPI_Flash_Library_BCM9WCDUSI09 WWD_FreeRTOS_Interface_BCM9WCDUSI09 WICED_FreeRTOS_Interface WWD_LwIP_Interface_FreeRTOS WICED_LwIP_Interface Supplicant_BESL Lib_HTTP_Server Lib_DNS_Redirect_Daemon Lib_DNS WWD_for_SDIO_FreeRTOS Lib_Wiced_RO_FS STM32F2xx Wiced_Network_LwIP_FreeRTOS Lib_DHCP_Server Lib_base64 STM32F2xx_Peripheral_Drivers Ring_Buffer STM32F2xx_Peripheral_Libraries common_GCC
 
 HAL_WICED_LIB_FILES += $(addprefix $(HAL_LIB_COREV2)/,$(addsuffix .a,$(HAL_WICED_LIBS)))
 WICED_MCU = $(HAL_SRC_COREV2_PATH)/wiced/platform/MCU/STM32F2xx/GCC
 
-# LDFLAGS += -Wl,--start-group $(HAL_WICED_LIB_FILES) -Wl,--end-group )
-LDFLAGS += --specs=nano.specs -lc -lnosys -Wl,--whole-archive $(HAL_WICED_LIB_FILES) -Wl,--no-whole-archive
+#LDFLAGS += -Wl,--start-group $(HAL_WICED_LIB_FILES) -Wl,--end-group
+LDFLAGS += --specs=nano.specs -lc -lnosys
+LDFLAGS += -Wl,--whole-archive $(HAL_WICED_LIB_FILES) -Wl,--no-whole-archive
 LDFLAGS += -T$(WICED_MCU)/app_no_bootloader.ld
 LDFLAGS += -L$(WICED_MCU)/STM32F2x5
 LDFLAGS += -Wl,--defsym,__STACKSIZE__=1400
