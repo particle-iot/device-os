a826007b3136ed6519c7d186ec51108f67872e39
Satish Nair <satishgn77@gmail.com>
2014-11-11 12:14:12 +0530

    bDeviceClass in usb device descriptor should be of CDC type

--- platform/MCU/STM32F2xx/SPARK_Firmware_Driver/src/usbd_desc.c
+++ platform/MCU/STM32F2xx/SPARK_Firmware_Driver/src/usbd_desc.c
@@ -69,7 +69,7 @@ uint8_t USBD_DeviceDesc[USB_SIZ_DEVICE_DESC] =
         USB_DEVICE_DESCRIPTOR_TYPE, /*bDescriptorType*/
         0x00,                       /*bcdUSB */
         0x02,
-        0x00,                       /*bDeviceClass*/
+        0x02,                       /*bDeviceClass: CDC*/
         0x00,                       /*bDeviceSubClass*/
         0x00,                       /*bDeviceProtocol*/
         USB_OTG_MAX_EP0_SIZE,      /*bMaxPacketSize*/
