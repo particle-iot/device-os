1e182dd58f430708b6135d2ee6ff71714e1d8ac3
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-11-12 03:14:17 +0100

    OTA flash functions will be dependent upon the custom DCT struct that defines the standard WICED DCT data plus additional data used by the firmware (system flags, certificates, cloud endpoint address etc.)

---
+++ hal/src/core-v2/dct_hal.h
@@ -0,0 +1,30 @@
+/* 
+ * File:   dct_hal.h
+ * Author: mat
+ *
+ * Created on 11 November 2014, 09:32
+ */
+
+#ifndef DCT_HAL_H
+#define	DCT_HAL_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include "wiced_dct.h"    
+    
+/**
+ * Custom extensions to the DCT data stored
+ */    
+class dct_hal {
+    
+};
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DCT_HAL_H */
+
