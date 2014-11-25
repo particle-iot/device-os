ba27def4789f710c2cb1e1e6b43b2a88a6198bf3
Satish Nair <satishgn77@gmail.com>
2014-11-25 16:17:15 +0530

    Added USI_PART_NO macro. Both bootloader and usb serial tested on BM-14

--- bootloader/src/core-v2/sources.mk
+++ bootloader/src/core-v2/sources.mk
@@ -4,7 +4,7 @@ CSRC += $(call target_files,$(BOOTLOADER_SRC_COREV2_PATH)/,*.c)
 
 
 HAL_LIB_COREV2 = $(HAL_SRC_COREV2_PATH)/lib
-HAL_WICED_LIBS += Platform_BCM9WCDUSI09 SPI_Flash_Library_BCM9WCDUSI09 STM32F2xx
+HAL_WICED_LIBS += Platform_$(USI_PART_NO) SPI_Flash_Library_$(USI_PART_NO) STM32F2xx
 
 HAL_WICED_LIB_FILES += $(addprefix $(HAL_LIB_COREV2)/,$(addsuffix .a,$(HAL_WICED_LIBS)))
 
