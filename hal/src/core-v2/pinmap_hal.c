2b19aac31400f45d77b33c89dc03deb7c80946e8
Technobly <technobly@gmail.com>
2014-12-10 09:52:03 -0600

    added gpio pinmap lookup table

--- hal/src/core-v2/pinmap_hal.c
+++ hal/src/core-v2/pinmap_hal.c
@@ -33,36 +33,36 @@
 STM32_Pin_Info PIN_MAP[TOTAL_PINS] =
 {
 /*
- * gpio_peripheral : ???
- * gpio_pin : ???
- * adc_channel : ???
- * timer_peripheral : ???
- * timer_ch : ???
+ * gpio_peripheral (GPIOA, GPIOB or GPIOC)
+ * gpio_pin (0-15)
+ * adc_channel (0-15 or NONE. Note we don't define the peripheral because our chip only has one)
+ * timer_peripheral (TIM1 - TIM5, or NONE)
+ * timer_ch (1-3, or NONE)
  * pin_mode (NONE by default, can be set to OUTPUT, INPUT, or other types)
  * timer_ccr (0 by default, store the CCR value for TIM interrupt use)
  * user_property (0 by default, user variable storage)
  */
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
-  { NULL, NONE, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 }
+  { GPIOB, GPIO_Pin_7, NONE, TIM4, TIM_Channel_2, PIN_MODE_NONE, 0, 0 },
+  { GPIOB, GPIO_Pin_6, NONE, TIM4, TIM_Channel_1, PIN_MODE_NONE, 0, 0 },
+  { GPIOB, GPIO_Pin_5, NONE, TIM3, TIM_Channel_2, PIN_MODE_NONE, 0, 0 },
+  { GPIOB, GPIO_Pin_4, NONE, TIM3, TIM_Channel_1, PIN_MODE_NONE, 0, 0 },
+  { GPIOB, GPIO_Pin_3, NONE, TIM2, TIM_Channel_1, PIN_MODE_NONE, 0, 0 },
+  { GPIOA, GPIO_Pin_15, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
+  { GPIOA, GPIO_Pin_14, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
+  { GPIOA, GPIO_Pin_13, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
+  { GPIOA, GPIO_Pin_8, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
+  { GPIOA, GPIO_Pin_9, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 },
+  { GPIOC, GPIO_Pin_5, ADC_Channel_15, NULL, NONE, PIN_MODE_NONE, 0, 0 },
+  { GPIOC, GPIO_Pin_3, ADC_Channel_13, NULL, NONE, PIN_MODE_NONE, 0, 0 },
+  { GPIOC, GPIO_Pin_2, ADC_Channel_12, NULL, NONE, PIN_MODE_NONE, 0, 0 },
+  { GPIOA, GPIO_Pin_5, ADC_Channel_5, TIM2, TIM_Channel_1, PIN_MODE_NONE, 0, 0 },
+  { GPIOA, GPIO_Pin_6, ADC_Channel_6, TIM3, TIM_Channel_1, PIN_MODE_NONE, 0, 0 },
+  { GPIOA, GPIO_Pin_7, ADC_Channel_7, TIM3, TIM_Channel_2, PIN_MODE_NONE, 0, 0 },
+  { GPIOA, GPIO_Pin_4, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 }, // need to define DAC
+  { GPIOA, GPIO_Pin_0, NONE, TIM5, TIM_Channel_1, PIN_MODE_NONE, 0, 0 },
+  { GPIOA, GPIO_Pin_10, ADC_Channel_3, TIM1, TIM_Channel_3, PIN_MODE_NONE, 0, 0 },
+  { GPIOA, GPIO_Pin_9, ADC_Channel_2, TIM1, TIM_Channel_2, PIN_MODE_NONE, 0, 0 },
+  { GPIOC, GPIO_Pin_7, NONE, NULL, NONE, PIN_MODE_NONE, 0, 0 }
 };
 
 
