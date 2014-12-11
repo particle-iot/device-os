67c1e7b650f88132b3307d4a55517a06fdbf4509
Satish Nair <satishgn77@gmail.com>
2014-12-11 14:47:58 +0530

    Implemented Serial Flash driver for Macronix's part no.: MX25L8006E

--- platform/MCU/STM32F2xx/SPARK_Firmware_Driver/inc/spi_flash.h
+++ platform/MCU/STM32F2xx/SPARK_Firmware_Driver/inc/spi_flash.h
@@ -1,47 +1,53 @@
-/*
- * Copyright 2014, Broadcom Corporation
- * All Rights Reserved.
- *
- * This is UNPUBLISHED PROPRIETARY SOURCE CODE of Broadcom Corporation;
- * the contents of this file may not be disclosed to third parties, copied
- * or duplicated in any form, in whole or in part, without the prior
- * written permission of Broadcom Corporation.
- */
-#ifndef INCLUDED_SPI_FLASH_API_H
-#define INCLUDED_SPI_FLASH_API_H
+/**
+  ******************************************************************************
+  * @file    spi_flash.h
+  * @author  Satish Nair
+  * @version V1.0.0
+  * @date    10-Dec-2014
+  * @brief   Header for spi flash driver
+  ******************************************************************************
+  Copyright (c) 2013-14 Spark Labs, Inc.  All rights reserved.
 
-#ifdef __cplusplus
- extern "C" {
-#endif
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, either
+  version 3 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <http://www.gnu.org/licenses/>.
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __SPI_FLASH_H
+#define __SPI_FLASH_H
 
 #include <stdint.h>
 
-
-
-typedef enum
-{
-    SFLASH_WRITE_NOT_ALLOWED = 0,
-    SFLASH_WRITE_ALLOWED     = 1,
-
-} sflash_write_allowed_t;
-
-typedef /*@abstract@*/ /*@immutable@*/ struct
-{
-    uint32_t device_id;
-    void * platform_peripheral;
-    sflash_write_allowed_t write_allowed;
-} sflash_handle_t;
-
-
-int init_sflash         ( /*@out@*/ sflash_handle_t* const handle, /*@shared@*/ void* peripheral_id, sflash_write_allowed_t write_allowed_in );
-int sflash_read         ( const sflash_handle_t* const handle, unsigned long device_address, /*@out@*/  /*@dependent@*/ void* const data_addr, unsigned int size );
-int sflash_write        ( const sflash_handle_t* const handle, unsigned long device_address,  /*@observer@*/ const void* const data_addr, unsigned int size );
-int sflash_chip_erase   ( const sflash_handle_t* const handle );
-int sflash_sector_erase ( const sflash_handle_t* const handle, unsigned long device_address );
-int sflash_get_size     ( const sflash_handle_t* const handle, /*@out@*/ unsigned long* size );
+#define sFLASH_PAGESIZE     0x1000 /* 4096 bytes sector size that needs to be erased */
 
 #ifdef __cplusplus
-}
-#endif
+extern "C" {
+#endif /* __cplusplus */
 
-#endif /* INCLUDED_SPI_FLASH_API_H */
+/* High level functions. */
+void sFLASH_Init(void);
+void sFLASH_EraseSector(uint32_t SectorAddr);
+void sFLASH_EraseBulk(void);
+void sFLASH_WriteBuffer(const uint8_t *pBuffer, uint32_t WriteAddr, uint32_t NumByteToWrite);
+void sFLASH_ReadBuffer(uint8_t *pBuffer, uint32_t ReadAddr, uint32_t NumByteToRead);
+uint32_t sFLASH_ReadID(void);
+
+/* Flash Self Test Routine */
+int sFLASH_SelfTest(void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* __SPI_FLASH_H */
