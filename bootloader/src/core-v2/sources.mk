946278b1e6f2bd549ed8f58794e6aeda9bac2fe9
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-12-16 11:57:34 +0100

    bootloader dependent upon STM32F2xx which is a RTOS-specific library, so fetch this from the RTOS directory

--- bootloader/src/core-v2/sources.mk
+++ bootloader/src/core-v2/sources.mk
@@ -4,9 +4,10 @@ CSRC += $(call target_files,$(BOOTLOADER_SRC_COREV2_PATH)/,*.c)
 
 
 HAL_LIB_COREV2 = $(HAL_SRC_COREV2_PATH)/lib
-HAL_WICED_LIBS += Platform_$(PLATFORM_NET) SPI_Flash_Library_$(PLATFORM_NET) STM32F2xx
+HAL_WICED_LIBS += Platform_$(PLATFORM_NET) SPI_Flash_Library_$(PLATFORM_NET)
 
 HAL_WICED_LIB_FILES += $(addprefix $(HAL_LIB_COREV2)/,$(addsuffix .a,$(HAL_WICED_LIBS)))
+HAL_WICED_LIB_FILES += $(HAL_LIB_COREV2)/FreeRTOS/STM32F2xx.a
 
 LIBS_EXT += -Wl,--whole-archive $(HAL_WICED_LIB_FILES) -Wl,--no-whole-archive
 
