edf9aff260421059cdec8849029dfbcdc0f557b1
Satish Nair <satishgn77@gmail.com>
2014-12-26 13:43:35 +0530

    Added TIMx_irq handlers and corrected formatting indentation

--- hal/src/core-v2/core_hal.c
+++ hal/src/core-v2/core_hal.c
@@ -51,11 +51,11 @@ void HAL_USART1_Handler(void);
 
 void override_interrupts(void) {
 
-	memcpy(&link_ram_interrupt_vectors_location, &link_interrupt_vectors_location, &link_ram_interrupt_vectors_location_end-&link_ram_interrupt_vectors_location);
-	uint32_t* isrs = (uint32_t*)&link_ram_interrupt_vectors_location;
-	isrs[SysTickIndex] = (uint32_t)SysTickOverride;
-	isrs[USART1Index] = (uint32_t)HAL_USART1_Handler;
-	SCB->VTOR = (unsigned long)isrs;
+    memcpy(&link_ram_interrupt_vectors_location, &link_interrupt_vectors_location, &link_ram_interrupt_vectors_location_end-&link_ram_interrupt_vectors_location);
+    uint32_t* isrs = (uint32_t*)&link_ram_interrupt_vectors_location;
+    isrs[SysTickIndex] = (uint32_t)SysTickOverride;
+    isrs[USART1Index] = (uint32_t)HAL_USART1_Handler;
+    SCB->VTOR = (unsigned long)isrs;
 }
 
 
@@ -70,6 +70,10 @@ volatile uint8_t IWDG_SYSTEM_RESET;
 
 /* Private function prototypes -----------------------------------------------*/
 void Mode_Button_EXTI_irq(void);
+void (*HAL_TIM1_Handler)(void);
+void (*HAL_TIM3_Handler)(void);
+void (*HAL_TIM4_Handler)(void);
+void (*HAL_TIM5_Handler)(void);
 
 /* Extern variables ----------------------------------------------------------*/
 extern __IO uint16_t BUTTON_DEBOUNCED_TIME[];
@@ -77,7 +81,7 @@ extern __IO uint16_t BUTTON_DEBOUNCED_TIME[];
 /* Extern function prototypes ------------------------------------------------*/
 void HAL_Core_Init(void)
 {
-	wiced_core_init();
+    wiced_core_init();
 }
 
 /*******************************************************************************
@@ -89,213 +93,213 @@ void HAL_Core_Init(void)
  *******************************************************************************/
 void HAL_Core_Config(void)
 {
-	DECLARE_SYS_HEALTH(ENTERED_SparkCoreConfig);
+    DECLARE_SYS_HEALTH(ENTERED_SparkCoreConfig);
 #ifdef DFU_BUILD_ENABLE
-	//Currently this is done through WICED library API so commented.
-	//NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x20000);
-	USE_SYSTEM_FLAGS = 1;
+    //Currently this is done through WICED library API so commented.
+    //NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x20000);
+    USE_SYSTEM_FLAGS = 1;
 #endif
 
 #ifdef SWD_JTAG_DISABLE
-	/* Disable the Serial Wire JTAG Debug Port SWJ-DP */
-	//To Do
+    /* Disable the Serial Wire JTAG Debug Port SWJ-DP */
+    //To Do
 #endif
 
-	Set_System();
+    Set_System();
 
-	override_interrupts();
+    override_interrupts();
 
-	/* Register Mode Button Interrupt Handler (WICED hack for Mode Button usage) */
-	HAL_EXTI_Register_Handler(BUTTON1_EXTI_LINE, Mode_Button_EXTI_irq);
+    /* Register Mode Button Interrupt Handler (WICED hack for Mode Button usage) */
+    HAL_EXTI_Register_Handler(BUTTON1_EXTI_LINE, Mode_Button_EXTI_irq);
 
-	SysTick_Configuration();
+    SysTick_Configuration();
 
-	/* Enable CRC clock */
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_CRC, ENABLE);
+    /* Enable CRC clock */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_CRC, ENABLE);
 
-	HAL_RTC_Configuration();
+    HAL_RTC_Configuration();
 
-	/* Execute Stop mode if STOP mode flag is set via Spark.sleep(pin, mode) */
-	HAL_Core_Execute_Stop_Mode();
+    /* Execute Stop mode if STOP mode flag is set via Spark.sleep(pin, mode) */
+    HAL_Core_Execute_Stop_Mode();
 
-	LED_SetRGBColor(RGB_COLOR_WHITE);
-	LED_On(LED_RGB);
+    LED_SetRGBColor(RGB_COLOR_WHITE);
+    LED_On(LED_RGB);
 
 #ifdef IWDG_RESET_ENABLE
-	// ToDo this needs rework for new bootloader
-	/* Check if the system has resumed from IWDG reset */
-	if (RCC_GetFlagStatus(RCC_FLAG_IWDGRST) != RESET)
-	{
-		/* IWDGRST flag set */
-		IWDG_SYSTEM_RESET = 1;
+    // ToDo this needs rework for new bootloader
+    /* Check if the system has resumed from IWDG reset */
+    if (RCC_GetFlagStatus(RCC_FLAG_IWDGRST) != RESET)
+    {
+        /* IWDGRST flag set */
+        IWDG_SYSTEM_RESET = 1;
 
-		/* Clear reset flags */
-		RCC_ClearFlag();
-	}
+        /* Clear reset flags */
+        RCC_ClearFlag();
+    }
 
-	/* We are duplicating the IWDG call here for compatibility with old bootloader */
-	/* Set IWDG Timeout to 3 secs */
-	//IWDG_Reset_Enable(3 * TIMING_IWDG_RELOAD);
+    /* We are duplicating the IWDG call here for compatibility with old bootloader */
+    /* Set IWDG Timeout to 3 secs */
+    //IWDG_Reset_Enable(3 * TIMING_IWDG_RELOAD);
 #endif
 
 #ifdef DFU_BUILD_ENABLE
-	Load_SystemFlags();
+    Load_SystemFlags();
 #endif
 
 #ifdef USE_SERIAL_FLASH
-	sFLASH_Init();
+    sFLASH_Init();
 #endif
 }
 
 bool HAL_Core_Mode_Button_Pressed(uint16_t pressedMillisDuration)
 {
-	bool pressedState = false;
+    bool pressedState = false;
 
-	if(BUTTON_GetDebouncedTime(BUTTON1) >= pressedMillisDuration)
-	{
-		pressedState = true;
-	}
+    if(BUTTON_GetDebouncedTime(BUTTON1) >= pressedMillisDuration)
+    {
+        pressedState = true;
+    }
 
-	return pressedState;
+    return pressedState;
 }
 
 void HAL_Core_Mode_Button_Reset(void)
 {    
-	BUTTON_ResetDebouncedState(BUTTON1);
+    BUTTON_ResetDebouncedState(BUTTON1);
 }
 
 void HAL_Core_System_Reset(void)
 {  
-	NVIC_SystemReset();
+    NVIC_SystemReset();
 }
 
 void HAL_Core_Factory_Reset(void)
 {
-	system_flags.Factory_Reset_SysFlag = 0xAAAA;
-	Save_SystemFlags();
-	HAL_Core_System_Reset();
+    system_flags.Factory_Reset_SysFlag = 0xAAAA;
+    Save_SystemFlags();
+    HAL_Core_System_Reset();
 }
 
 void HAL_Core_Enter_Bootloader(void)
 {
-	RTC_WriteBackupRegister(RTC_BKP_DR10, 0xFFFF);
-	system_flags.FLASH_OTA_Update_SysFlag = 0xFFFF;
-	Save_SystemFlags();
-	HAL_Core_System_Reset();
+    RTC_WriteBackupRegister(RTC_BKP_DR10, 0xFFFF);
+    system_flags.FLASH_OTA_Update_SysFlag = 0xFFFF;
+    Save_SystemFlags();
+    HAL_Core_System_Reset();
 }
 
 void HAL_Core_Enter_Stop_Mode(uint16_t wakeUpPin, uint16_t edgeTriggerMode)
 {
-	if ((wakeUpPin < TOTAL_PINS) && (edgeTriggerMode <= FALLING))
-	{
-		uint16_t RTC_BKP_DR9_Data = wakeUpPin;//set wakeup pin mumber
-		RTC_BKP_DR9_Data |= (edgeTriggerMode << 8);//set edge trigger mode
-		RTC_BKP_DR9_Data |= (0xA << 12);//set stop mode flag
+    if ((wakeUpPin < TOTAL_PINS) && (edgeTriggerMode <= FALLING))
+    {
+        uint16_t RTC_BKP_DR9_Data = wakeUpPin;//set wakeup pin mumber
+        RTC_BKP_DR9_Data |= (edgeTriggerMode << 8);//set edge trigger mode
+        RTC_BKP_DR9_Data |= (0xA << 12);//set stop mode flag
 
-		/*************************************************/
-		//RTC_BKP_DR9_Data: 0xAXXX
-		//                    ||||
-		//                    ||----- octet wakeUpPin number
-		//                    |------ nibble edgeTriggerMode
-		//                    ------- nibble stop mode flag
-		/*************************************************/
+        /*************************************************/
+        //RTC_BKP_DR9_Data: 0xAXXX
+        //                    ||||
+        //                    ||----- octet wakeUpPin number
+        //                    |------ nibble edgeTriggerMode
+        //                    ------- nibble stop mode flag
+        /*************************************************/
 
-		/* Execute Stop mode on next system reset */
-		RTC_WriteBackupRegister(RTC_BKP_DR9, RTC_BKP_DR9_Data);
+        /* Execute Stop mode on next system reset */
+        RTC_WriteBackupRegister(RTC_BKP_DR9, RTC_BKP_DR9_Data);
 
-		/* Reset System */
-		NVIC_SystemReset();
-	}
+        /* Reset System */
+        NVIC_SystemReset();
+    }
 }
 
 void HAL_Core_Execute_Stop_Mode(void)
 {
-	if((RTC_ReadBackupRegister(RTC_BKP_DR9) >> 12) == 0xA)
-	{
-		uint16_t wakeUpPin = RTC_ReadBackupRegister(RTC_BKP_DR9) & 0xFF;
-		InterruptMode edgeTriggerMode = (InterruptMode)((RTC_ReadBackupRegister(RTC_BKP_DR9) >> 8) & 0x0F);
+    if((RTC_ReadBackupRegister(RTC_BKP_DR9) >> 12) == 0xA)
+    {
+        uint16_t wakeUpPin = RTC_ReadBackupRegister(RTC_BKP_DR9) & 0xFF;
+        InterruptMode edgeTriggerMode = (InterruptMode)((RTC_ReadBackupRegister(RTC_BKP_DR9) >> 8) & 0x0F);
 
-		/* Clear Stop mode system flag */
-		RTC_WriteBackupRegister(RTC_BKP_DR9, 0xFFFF);
+        /* Clear Stop mode system flag */
+        RTC_WriteBackupRegister(RTC_BKP_DR9, 0xFFFF);
 
-		if ((wakeUpPin < TOTAL_PINS) && (edgeTriggerMode <= FALLING))
-		{
-			PinMode wakeUpPinMode = INPUT;
+        if ((wakeUpPin < TOTAL_PINS) && (edgeTriggerMode <= FALLING))
+        {
+            PinMode wakeUpPinMode = INPUT;
 
-			/* Set required pinMode based on edgeTriggerMode */
-			switch(edgeTriggerMode)
-			{
-			case CHANGE:
-				wakeUpPinMode = INPUT;
-				break;
+            /* Set required pinMode based on edgeTriggerMode */
+            switch(edgeTriggerMode)
+            {
+                case CHANGE:
+                    wakeUpPinMode = INPUT;
+                    break;
 
-			case RISING:
-				wakeUpPinMode = INPUT_PULLDOWN;
-				break;
+                case RISING:
+                    wakeUpPinMode = INPUT_PULLDOWN;
+                    break;
 
-			case FALLING:
-				wakeUpPinMode = INPUT_PULLUP;
-				break;
-			}
-			HAL_Pin_Mode(wakeUpPin, wakeUpPinMode);
+                case FALLING:
+                    wakeUpPinMode = INPUT_PULLUP;
+                    break;
+            }
+            HAL_Pin_Mode(wakeUpPin, wakeUpPinMode);
 
-			/* Configure EXTI Interrupt : wake-up from stop mode using pin interrupt */
-			HAL_Interrupts_Attach(wakeUpPin, NULL, edgeTriggerMode);
+            /* Configure EXTI Interrupt : wake-up from stop mode using pin interrupt */
+            HAL_Interrupts_Attach(wakeUpPin, NULL, edgeTriggerMode);
 
-			/* Request to enter STOP mode with regulator in low power mode */
-			PWR_EnterSTOPMode(PWR_Regulator_LowPower, PWR_STOPEntry_WFI);
+            /* Request to enter STOP mode with regulator in low power mode */
+            PWR_EnterSTOPMode(PWR_Regulator_LowPower, PWR_STOPEntry_WFI);
 
-			/* At this stage the system has resumed from STOP mode */
-			/* Enable HSE, PLL and select PLL as system clock source after wake-up from STOP */
+            /* At this stage the system has resumed from STOP mode */
+            /* Enable HSE, PLL and select PLL as system clock source after wake-up from STOP */
 
-			/* Enable HSE */
-			RCC_HSEConfig(RCC_HSE_ON);
+            /* Enable HSE */
+            RCC_HSEConfig(RCC_HSE_ON);
 
-			/* Wait till HSE is ready */
-			if(RCC_WaitForHSEStartUp() != SUCCESS)
-			{
-				/* If HSE startup fails try to recover by system reset */
-				NVIC_SystemReset();
-			}
+            /* Wait till HSE is ready */
+            if(RCC_WaitForHSEStartUp() != SUCCESS)
+            {
+                /* If HSE startup fails try to recover by system reset */
+                NVIC_SystemReset();
+            }
 
-			/* Enable PLL */
-			RCC_PLLCmd(ENABLE);
+            /* Enable PLL */
+            RCC_PLLCmd(ENABLE);
 
-			/* Wait till PLL is ready */
-			while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
+            /* Wait till PLL is ready */
+            while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
 
-			/* Select PLL as system clock source */
-			RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+            /* Select PLL as system clock source */
+            RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
 
-			/* Wait till PLL is used as system clock source */
-			while(RCC_GetSYSCLKSource() != 0x08);
-		}
-	}
+            /* Wait till PLL is used as system clock source */
+            while(RCC_GetSYSCLKSource() != 0x08);
+        }
+    }
 }
 
 void HAL_Core_Enter_Standby_Mode(void)
 {
-	/* Execute Standby mode on next system reset */
-	RTC_WriteBackupRegister(RTC_BKP_DR9, 0xA5A5);
+    /* Execute Standby mode on next system reset */
+    RTC_WriteBackupRegister(RTC_BKP_DR9, 0xA5A5);
 
-	/* Reset System */
-	NVIC_SystemReset();
+    /* Reset System */
+    NVIC_SystemReset();
 }
 
 void HAL_Core_Execute_Standby_Mode(void)
 {
-	/* Should we execute System Standby mode */
-	if(RTC_ReadBackupRegister(RTC_BKP_DR9) == 0xA5A5)
-	{
-		/* Clear Standby mode system flag */
-		RTC_WriteBackupRegister(RTC_BKP_DR9, 0xFFFF);
+    /* Should we execute System Standby mode */
+    if(RTC_ReadBackupRegister(RTC_BKP_DR9) == 0xA5A5)
+    {
+        /* Clear Standby mode system flag */
+        RTC_WriteBackupRegister(RTC_BKP_DR9, 0xFFFF);
 
-		/* Request to enter STANDBY mode */
-		PWR_EnterSTANDBYMode();
+        /* Request to enter STANDBY mode */
+        PWR_EnterSTANDBYMode();
 
-		/* Following code will not be reached */
-		while(1);
-	}
+        /* Following code will not be reached */
+        while(1);
+    }
 }
 
 /**
@@ -306,57 +310,57 @@ void HAL_Core_Execute_Standby_Mode(void)
  */
 uint32_t HAL_Core_Compute_CRC32(uint8_t *pBuffer, uint32_t bufferSize)
 {
-	/* Hardware CRC32 calculation */
-	uint32_t i, j;
-	uint32_t Data;
+    /* Hardware CRC32 calculation */
+    uint32_t i, j;
+    uint32_t Data;
 
-	CRC_ResetDR();
+    CRC_ResetDR();
 
-	i = bufferSize >> 2;
+    i = bufferSize >> 2;
 
-	while (i--)
-	{
-		Data = *((uint32_t *)pBuffer);
-		pBuffer += 4;
+    while (i--)
+    {
+        Data = *((uint32_t *)pBuffer);
+        pBuffer += 4;
 
-		Data = __RBIT(Data);//reverse the bit order of input Data
-		CRC->DR = Data;
-	}
+        Data = __RBIT(Data);//reverse the bit order of input Data
+        CRC->DR = Data;
+    }
 
-	Data = CRC->DR;
-	Data = __RBIT(Data);//reverse the bit order of output Data
+    Data = CRC->DR;
+    Data = __RBIT(Data);//reverse the bit order of output Data
 
-	i = bufferSize & 3;
+    i = bufferSize & 3;
 
-	while (i--)
-	{
-		Data ^= (uint32_t)*pBuffer++;
+    while (i--)
+    {
+        Data ^= (uint32_t)*pBuffer++;
 
-		for (j = 0 ; j < 8 ; j++)
-		{
-			if (Data & 1)
-				Data = (Data >> 1) ^ 0xEDB88320;
-			else
-				Data >>= 1;
-		}
-	}
+        for (j = 0 ; j < 8 ; j++)
+        {
+            if (Data & 1)
+                Data = (Data >> 1) ^ 0xEDB88320;
+            else
+                Data >>= 1;
+        }
+    }
 
-	Data ^= 0xFFFFFFFF;
+    Data ^= 0xFFFFFFFF;
 
-	return Data;
+    return Data;
 }
 
 // todo find a technique that allows accessor functions to be inlined while still keeping
 // hardware independence.
 bool HAL_watchdog_reset_flagged() 
 {
-	return IWDG_SYSTEM_RESET;
+    return IWDG_SYSTEM_RESET;
 }
 
 void HAL_Notify_WDT()
 {    
-	KICK_WDT();
-	wiced_watchdog_kick();
+    KICK_WDT();
+    wiced_watchdog_kick();
 }
 
 /**
@@ -365,11 +369,11 @@ void HAL_Notify_WDT()
  */
 void application_start() {
 
-	// while this is linked as a single image, assume c'tors are handled by the
-	// WICED startup scripts.
-	HAL_Core_Config();
+    // while this is linked as a single image, assume c'tors are handled by the
+    // WICED startup scripts.
+    HAL_Core_Config();
 
-	app_setup_and_loop();
+    app_setup_and_loop();
 }
 
 /**
@@ -378,21 +382,21 @@ void application_start() {
  */
 void SysTickOverride(void)
 {    
-	void (*chain)(void) = (void (*)(void))((uint32_t*)&link_interrupt_vectors_location)[SysTickIndex];
+    void (*chain)(void) = (void (*)(void))((uint32_t*)&link_interrupt_vectors_location)[SysTickIndex];
 
-	chain();
+    chain();
 
-	System1MsTick();
+    System1MsTick();
 
-	if (TimingDelay != 0x00)
-	{
-		TimingDelay--;
-	}
+    if (TimingDelay != 0x00)
+    {
+        TimingDelay--;
+    }
 
-	if(HAL_SysTick_Handler)
-	{
-		HAL_SysTick_Handler();
-	}
+    if(HAL_SysTick_Handler)
+    {
+        HAL_SysTick_Handler();
+    }
 }
 
 /**
@@ -402,16 +406,29 @@ void SysTickOverride(void)
  */
 void Mode_Button_EXTI_irq(void)
 {
-	/* Clear the EXTI line pending bit (cleared in WICED GPIO IRQ handler) */
-	EXTI_ClearITPendingBit(BUTTON1_EXTI_LINE);
+    /* Clear the EXTI line pending bit (cleared in WICED GPIO IRQ handler) */
+    EXTI_ClearITPendingBit(BUTTON1_EXTI_LINE);
 
-	BUTTON_DEBOUNCED_TIME[BUTTON1] = 0x00;
+    BUTTON_DEBOUNCED_TIME[BUTTON1] = 0x00;
 
-	/* Disable BUTTON1 Interrupt */
-	BUTTON_EXTI_Config(BUTTON1, DISABLE);
+    /* Disable BUTTON1 Interrupt */
+    BUTTON_EXTI_Config(BUTTON1, DISABLE);
 
-	/* Enable TIM2 CC1 Interrupt */
-	TIM_ITConfig(TIM2, TIM_IT_CC1, ENABLE);
+    /* Enable TIM2 CC1 Interrupt */
+    TIM_ITConfig(TIM2, TIM_IT_CC1, ENABLE);
+}
+
+/**
+ * @brief  This function handles TIM1_CC_IRQ Handler.
+ * @param  None
+ * @retval None
+ */
+void TIM1_CC_irq(void)
+{
+    if(NULL != HAL_TIM1_Handler)
+    {
+        HAL_TIM1_Handler();
+    }
 }
 
 /**
@@ -421,22 +438,60 @@ void Mode_Button_EXTI_irq(void)
  */
 void TIM2_irq(void)
 {
-	if (TIM_GetITStatus(TIM2, TIM_IT_CC1) != RESET)
-	{
-		TIM_ClearITPendingBit(TIM2, TIM_IT_CC1);
+    if (TIM_GetITStatus(TIM2, TIM_IT_CC1) != RESET)
+    {
+        TIM_ClearITPendingBit(TIM2, TIM_IT_CC1);
 
-		if (BUTTON_GetState(BUTTON1) == BUTTON1_PRESSED)
-		{
-			BUTTON_DEBOUNCED_TIME[BUTTON1] += BUTTON_DEBOUNCE_INTERVAL;
-		}
-		else
-		{
-			/* Disable TIM2 CC1 Interrupt */
-			TIM_ITConfig(TIM2, TIM_IT_CC1, DISABLE);
+        if (BUTTON_GetState(BUTTON1) == BUTTON1_PRESSED)
+        {
+            BUTTON_DEBOUNCED_TIME[BUTTON1] += BUTTON_DEBOUNCE_INTERVAL;
+        }
+        else
+        {
+            /* Disable TIM2 CC1 Interrupt */
+            TIM_ITConfig(TIM2, TIM_IT_CC1, DISABLE);
 
-			/* Enable BUTTON1 Interrupt */
-			BUTTON_EXTI_Config(BUTTON1, ENABLE);
-		}
-	}
+            /* Enable BUTTON1 Interrupt */
+            BUTTON_EXTI_Config(BUTTON1, ENABLE);
+        }
+    }
 }
 
+/**
+ * @brief  This function handles TIM3_IRQ Handler.
+ * @param  None
+ * @retval None
+ */
+void TIM3_irq(void)
+{
+    if(NULL != HAL_TIM3_Handler)
+    {
+        HAL_TIM3_Handler();
+    }
+}
+
+/**
+ * @brief  This function handles TIM4_IRQ Handler.
+ * @param  None
+ * @retval None
+ */
+void TIM4_irq(void)
+{
+    if(NULL != HAL_TIM4_Handler)
+    {
+        HAL_TIM4_Handler();
+    }
+}
+
+/**
+ * @brief  This function handles TIM5_IRQ Handler.
+ * @param  None
+ * @retval None
+ */
+void TIM5_irq(void)
+{
+    if(NULL != HAL_TIM5_Handler)
+    {
+        HAL_TIM5_Handler();
+    }
+}
