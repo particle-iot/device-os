d527701e77c422cfcc131dd4ff7855e8d059eb59
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2015-01-08 21:56:47 +0100

    shuffled DCT - the areas for the existing keys were much larger than needed. added space for device public key.

--- platform/MCU/STM32F2xx/SPARK_Firmware_Driver/inc/dct.h
+++ platform/MCU/STM32F2xx/SPARK_Firmware_Driver/inc/dct.h
@@ -23,19 +23,24 @@ extern "C" {
 typedef struct application_dct {    
     platform_system_flags_t system_flags;
     uint16_t version;
-    uint8_t device_private_key[2048];
-    uint8_t server_public_key[2048];
+    uint8_t device_private_key[1216];  // sufficient for 2048 bits
+    uint8_t device_public_key[384];    // sufficient for 2048 bits
     uint8_t server_address[128];
-    uint8_t reserved[0];
+    uint8_t reserved1[320];
+    uint8_t server_public_key[768];     // 4096 bits
+    uint8_t reserved2[1280+128];    
+    uint8_t end[0];
 } application_dct_t;
 
 #define DCT_SYSTEM_FLAGS_OFFSET  (offsetof(application_dct_t, system_flags)) 
 #define DCT_DEVICE_PRIVATE_KEY_OFFSET (offsetof(application_dct_t, device_private_key)) 
+#define DCT_DEVICE_PUBLIC_KEY_OFFSET (offsetof(application_dct_t, device_public_key)) 
 #define DCT_SERVER_PUBLIC_KEY_OFFSET (offsetof(application_dct_t, server_public_key)) 
 #define DCT_SERVER_ADDRESS_OFFSET (offsetof(application_dct_t, server_address)) 
 
 #define DCT_SYSTEM_FLAGS_SIZE  (sizeof(application_dct_t::system_flags)) 
 #define DCT_DEVICE_PRIVATE_KEY_SIZE  (sizeof(application_dct_t::device_private_key)) 
+#define DCT_DEVICE_PUBLIC_KEY_SIZE  (sizeof(application_dct_t::device_public_key)) 
 #define DCT_SERVER_PUBLIC_KEY_SIZE  (sizeof(application_dct_t::server_public_key)) 
 #define DCT_SERVER_ADDRESS_SIZE  (sizeof(application_dct_t::server_address)) 
 
@@ -48,9 +53,12 @@ typedef struct application_dct {
 STATIC_ASSERT_DCT_OFFSET(system_flags, 0);
 STATIC_ASSERT_DCT_OFFSET(version, 32);
 STATIC_ASSERT_DCT_OFFSET(device_private_key, 34);
-STATIC_ASSERT_DCT_OFFSET(server_public_key, 2082);
-STATIC_ASSERT_DCT_OFFSET(server_address, 4130);
-STATIC_ASSERT_DCT_OFFSET(reserved, 4258);
+STATIC_ASSERT_DCT_OFFSET(device_public_key, 1250 /*34+1216*/);
+STATIC_ASSERT_DCT_OFFSET(server_address, 1634 /* 1250 + 384 */);
+STATIC_ASSERT_DCT_OFFSET(reserved1, 1762 /* 1634 + 128 */);
+STATIC_ASSERT_DCT_OFFSET(server_public_key, 2082 /* 1762 + 320 */);
+STATIC_ASSERT_DCT_OFFSET(reserved2, 2850 /* 2082 + 768 */);
+STATIC_ASSERT_DCT_OFFSET(end, 4258 /* 2850 + 1280 +128 */);
 
 
 /**
