e62578b0684352e5e343049f7029ba3427a22597
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2015-01-20 12:37:27 +0100

    put system in a platform specific build target directory

--- system/import.mk
+++ system/import.mk
@@ -1,5 +1,5 @@
 SYSTEM_MODULE_PATH ?= ../system
 include $(call rwildcard,$(SYSTEM_MODULE_PATH)/,include.mk)
 
-SYSTEM_LIB_DIR = $(BUILD_PATH_BASE)/system/$(ARCH)
+SYSTEM_LIB_DIR = $(BUILD_PATH_BASE)/system/$(BUILD_TARGET_PLATFORM)
 SYSTEM_LIB_DEP = $(SYSTEM_LIB_DIR)/libsystem.a
