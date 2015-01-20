d080989d3144fd510de6a0ef03b0e920c84bd566
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2015-01-20 12:36:55 +0100

    put wiring in platform-specific build target directory

--- wiring/import.mk
+++ wiring/import.mk
@@ -1,5 +1,5 @@
 WIRING_MODULE_PATH ?= ../wiring
 include $(call rwildcard,$(WIRING_MODULE_PATH)/,include.mk)
 
-WIRING_LIB_DIR = $(BUILD_PATH_BASE)/wiring/$(ARCH)
+WIRING_LIB_DIR = $(BUILD_PATH_BASE)/wiring/$(BUILD_TARGET_PLATFORM)
 WIRING_LIB_DEP = $(WIRING_LIB_DIR)/libwiring.a
