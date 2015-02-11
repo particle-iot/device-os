2ce4997ee9650fdcc56d691b2fa6027f58f6f112
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2015-02-11 23:40:01 +0100

    now that HAL always defines the wiced libs as HAL_WICED_LIBS_xxx (but optionally doesn't link them) the bootloader should use different symbol names to avoid confusion (and linking all HAL WICED libs.)

--- bootloader/src/core-v2/sources.mk
+++ bootloader/src/core-v2/sources.mk
@@ -4,10 +4,10 @@ CSRC += $(call target_files,$(BOOTLOADER_SRC_COREV2_PATH)/,*.c)
 
 
 HAL_LIB_COREV2 = $(HAL_SRC_COREV2_PATH)/lib
-HAL_WICED_LIBS += Platform_$(PLATFORM_NET) SPI_Flash_Library_$(PLATFORM_NET)
+WICED_LIBS = Platform_$(PLATFORM_NET) SPI_Flash_Library_$(PLATFORM_NET)
 
-HAL_WICED_LIB_FILES += $(addprefix $(HAL_LIB_COREV2)/,$(addsuffix .a,$(HAL_WICED_LIBS)))
-HAL_WICED_LIB_FILES += $(HAL_LIB_COREV2)/FreeRTOS/STM32F2xx.a
+WICED_LIB_FILES = $(addprefix $(HAL_LIB_COREV2)/,$(addsuffix .a,$(WICED_LIBS)))
+WICED_LIB_FILES = $(HAL_LIB_COREV2)/FreeRTOS/STM32F2xx.a
 
-LIBS_EXT += -Wl,--whole-archive $(HAL_WICED_LIB_FILES) -Wl,--no-whole-archive
+LIBS_EXT += -Wl,--whole-archive $(WICED_LIB_FILES) -Wl,--no-whole-archive
 
