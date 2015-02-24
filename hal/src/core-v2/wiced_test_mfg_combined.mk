41e4beb5857eed1706c151b11625be86d8ecc476
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2015-02-24 01:48:31 +0100

    tidy up

--- hal/src/core-v2/wiced_test_mfg_combined.mk
+++ hal/src/core-v2/wiced_test_mfg_combined.mk
@@ -20,67 +20,59 @@ BUILD_NAME=test_mfg_test-BCM9WCDUSI09-ThreadX-NetX-SDIO
 MFG_TEST_BIN=$(WICED)/build/$(BUILD_NAME)/binary/$(BUILD_NAME).bin
 MFG_TEST_MEM=$(OUT)/mfg_test_pad.bin
 
+CRC=crc32
+XXD=xxd
 OPTS=
 
-all: bootloader
-	
-	
-# bootloader mfg_test
-
-
+all: combined
+		
 clean:
 	cd "$(WICED)"; "$(WICED)/make" clean
 	-rm $(MFG_TEST_BIN)
 	-rm $(BOOTLOADER)
 	-rm $(DCT)
-	
-mfg_test: $(MFG_TEST_PAD)
-	dd if=/dev/zero ibs=1k count=16 | tr "\000" "\377" > $(BOOTLOADER_MEM)
-	dd if=$(BOOTLOADER_BIN) of=$(BOOTLOADER_MEM) conv=notrunc
-	
-
-
+		
 bootloader:
-	echo building $(BOOTLOADER_MEM)
+	@echo building $(BOOTLOADER_MEM)
 	$(MAKE) -C $(BOOTLOADER_DIR) PLATFORM_ID=$(PLATFORM_ID) all 
 	dd if=/dev/zero ibs=1k count=16 | tr "\000" "\377" > $(BOOTLOADER_MEM)
 	dd if=$(BOOTLOADER_BIN) of=$(BOOTLOADER_MEM) conv=notrunc
-	
 
+dct: 	
+	-rm $(DCT_MEM)
+	dd if=/dev/zero ibs=1k count=112 | tr "\000" "\377" > $(DCT_MEM)
+		
 $(MFG_TEST_BIN):
 	cd "$(WICED)"; "./make" $(CMD) $(OPTS)
-	$(call,echo,'Appending: CRC32 to the Flash Image')
-	$(VERBOSE)cp $@ $@.no_crc	
-	$(VERBOSE)$(CRC) $@.no_crc | cut -c 1-10 | $(XXD) -r -p >> $@
-	$(call,echo,)	
+	@echo Appending: CRC32 to the Flash Image
+	cp $@ $@.no_crc	
+	$(CRC) $@.no_crc | cut -c 1-10 | $(XXD) -r -p >> $@	
 
 $(MFG_TEST_MEM): $(MFG_TEST_BIN)
+	-rm $(MFG_TEST_MEM)
 	dd if=/dev/zero ibs=1k count=384 | tr "\000" "\377" > $(MFG_TEST_MEM)
 	dd if=$(MFG_TEST_BIN) of=$(MFG_TEST_MEM) conv=notrunc
-			
+
 mfg_test: $(MFG_TEST_MEM)
-
-dct: $(DCT_MEM)
-
-
-$(DCT_MEM):
-	dd if=/dev/zero ibs=1k count=112 | tr "\000" "\377" > $(DCT_MEM)
-
+	dd if=/dev/zero ibs=1k count=16 | tr "\000" "\377" > $(BOOTLOADER_MEM)
+	dd if=$(BOOTLOADER_BIN) of=$(BOOTLOADER_MEM) conv=notrunc
+			
 firmware:
-	echo building $(FIRMWARE_MEM)
-	$(MAKE) -C $(FIRMWARE_DIR) PLATFORM_ID=$(PLATFORM_ID) PRODUCT_ID=$(PLATFORM_ID) PRODUCT_FIRMWARE_REVISION=0 all 
+	@echo building $(FIRMWARE_MEM)
+	-rm $(FIRMWARE_MEM)
+	$(MAKE) -C $(FIRMWARE_DIR) PLATFORM_ID=$(PLATFORM_ID) PRODUCT_ID=$(PLATFORM_ID) PRODUCT_FIRMWARE_REVISION=0 all
 	dd if=/dev/zero ibs=1k count=384 | tr "\000" "\377" > $(FIRMWARE_MEM)
 	dd if=$(FIRMWARE_BIN) of=$(FIRMWARE_MEM) conv=notrunc
 	
 
 combined: bootloader dct mfg_test firmware
-	cat $(BOOTLOADER_MEM) $(DCT_MEM) $(MFG_TEST_MEM) $(FIRMWARE_MEM) > $(COMBINED_MEM)
-
+	-rm $(COMBINED_MEM)
+	cat $(BOOTLOADER_MEM) $(DCT_MEM) $(MFG_TEST_MEM) $(FIRMWARE_MEM) > $(COMBINED_MEM)	
 
 flash: combined
 	st-flash write $(COMBINED_MEM) 0x8000000
 
-.PHONY: mfg_test clean all bootloader dct mfg_test firmware
+.PHONY: mfg_test clean all bootloader dct mfg_test firmware $(MFG_TEST_BIN) $(MFG_TEST_MEM)
 
 
 
