783c08f4c71bf91b20cf70384c4b066702d2ed4a
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2015-03-07 08:47:27 +0100

    target folder for modular build has `-m` appended

--- bootloader/import.mk
+++ bootloader/import.mk
@@ -1,6 +1,6 @@
 BOOTLOADER_MODULE_PATH ?= $(PROJECT_ROOT)/bootloader
 BOOTLOADER_VERSION ?= 2
-BOOTLOADER_BUILD_PATH_EXT = $(BUILD_TARGET_PLATFORM)$(MODULAR_EXT)
+BOOTLOADER_BUILD_PATH_EXT = $(BUILD_TARGET_PLATFORM)
 
 # bring in the include folders from inc and src/<platform> is includes
 include $(call rwildcard,$(BOOTLOADER_MODULE_PATH)/inc/,include.mk)
