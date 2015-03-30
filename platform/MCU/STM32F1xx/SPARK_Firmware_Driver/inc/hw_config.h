1c7f18f8a7c4b33a4c44630aeef93d6d133b572e
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-03-30 07:34:51 +0200

    missing declaration - fixes compiler error in boot loader for core

--- platform/MCU/STM32F1xx/SPARK_Firmware_Driver/inc/hw_config.h
+++ platform/MCU/STM32F1xx/SPARK_Firmware_Driver/inc/hw_config.h
@@ -230,6 +230,7 @@ extern uint16_t OTA_FLASHED_Status_SysFlag;
 extern uint16_t Factory_Reset_SysFlag;
 extern uint8_t dfu_on_no_firmware;
 extern uint8_t Factory_Reset_Done_SysFlag;
+extern uint8_t StartupMode_SysFlag;
 
 extern unsigned char wlan_rx_buffer[];
 extern unsigned char wlan_tx_buffer[];
