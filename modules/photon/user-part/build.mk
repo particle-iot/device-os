4aed4e89d7b3d34f83849e9cf0b20f7b474ca07b
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-04-15 19:04:03 +0200

    support TARGET_DIR in modular user build too.

--- modules/photon/user-part/build.mk
+++ modules/photon/user-part/build.mk
@@ -7,6 +7,40 @@ $(error SYSTEM_PART2_MODULE_VERSION not defined)
 endif
 
 
+# TARGET_FILE sets the file stem of the target. It's typically only specified when building applications.
+ifdef TARGET_FILE
+TARGET_FILE_NAME = $(TARGET_FILE)
+endif
+
+# TARGET_DIR is the externally specified target directory
+ifdef TARGET_DIR
+TARGET_PATH = $(TARGET_DIR)
+endif
+
+
+ifdef APP
+# when TARGET_FILE is defined on the command line, 
+TARGET_FILE_NAME ?= $(notdir $(APP))
+TARGET_DIR_NAME ?= applications/$(APP)
+endif
+
+ifdef APPDIR
+# APPDIR is where the sources are found
+# if TARGET_DIR is not defined defaults to $(APPDIR)/target
+# if TARGET_FILE_NAME is not defined, defaults to the name of the $(APPDIR)
+TARGET_FILE_NAME ?= $(notdir $(APPDIR))
+TARGET_DIR_NAME = $(APPDIR)/target
+# do not use $(BUILD_PATH) since the TARGET_DIR specifies fully where the output should go
+TARGET_PATH ?= $(TARGET_DIR_NAME)
+BUILD_PATH = $(TARGET_PATH)/obj
+endif
+
+ifdef TEST
+TARGET_FILE_NAME ?= $(notdir $(TEST))
+TARGET_DIR_NAME ?= test/$(TEST)
+endif
+
+
 GLOBAL_DEFINES += MODULE_VERSION=$(USER_PART_MODULE_VERSION)
 GLOBAL_DEFINES += MODULE_FUNCTION=$(MODULE_FUNCTION_USER_PART)
 GLOBAL_DEFINES += MODULE_INDEX=1
