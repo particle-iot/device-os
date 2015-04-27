52a4916f7071f29c34652ceee1e82cdabc9a730b
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-04-27 11:11:14 +0200

    HardFault handler with debug info.

--- hal/src/core-v2/core_hal.c
+++ hal/src/core-v2/core_hal.c
@@ -70,8 +70,36 @@ void override_interrupts(void) {
     SCB->VTOR = (unsigned long)isrs;
 }
 
-void HardFault_Handler(void)
+void HardFault_Handler( void ) __attribute__( ( naked ) );
+
+void prvGetRegistersFromStack( uint32_t *pulFaultStackAddress )
 {
+    /* These are volatile to try and prevent the compiler/linker optimising them
+    away as the variables never actually get used.  If the debugger won't show the
+    values of the variables, make them global my moving their declaration outside
+    of this function. */
+    volatile uint32_t r0;
+    volatile uint32_t r1;
+    volatile uint32_t r2;
+    volatile uint32_t r3;
+    volatile uint32_t r12;
+    volatile uint32_t lr; /* Link register. */
+    volatile uint32_t pc; /* Program counter. */
+    volatile uint32_t psr;/* Program status register. */
+
+    r0 = pulFaultStackAddress[ 0 ];
+    r1 = pulFaultStackAddress[ 1 ];
+    r2 = pulFaultStackAddress[ 2 ];
+    r3 = pulFaultStackAddress[ 3 ];
+
+    r12 = pulFaultStackAddress[ 4 ];
+    lr = pulFaultStackAddress[ 5 ];
+    pc = pulFaultStackAddress[ 6 ];
+    psr = pulFaultStackAddress[ 7 ];
+
+    if (false)
+        r0++; r1++; r2++; r3++; r12++; lr++; pc++; psr++;
+    
     PANIC(HardFault,"HardFault");
 
     /* Go to infinite loop when Hard Fault exception occurs */
@@ -81,6 +109,21 @@ void HardFault_Handler(void)
 }
 
 
+
+void HardFault_Handler(void)
+{
+    __asm volatile
+    (
+        " tst lr, #4                                                \n"
+        " ite eq                                                    \n"
+        " mrseq r0, msp                                             \n"
+        " mrsne r0, psp                                             \n"
+        " ldr r1, [r0, #24]                                         \n"
+        " ldr r2, handler2_address_const                            \n"
+        " bx r2                                                     \n"
+        " handler2_address_const: .word prvGetRegistersFromStack    \n"
+    );
+}
 
 /* Private typedef -----------------------------------------------------------*/
 
