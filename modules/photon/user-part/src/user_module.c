604a6d9bea2175ac3638307906200ef28b441532
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-04-29 09:18:07 +0200

    sark required when compiling non-LTO with printf_float included.  externally visible on user module required for LTO build.

--- modules/photon/user-part/src/user_module.c
+++ modules/photon/user-part/src/user_module.c
@@ -9,6 +9,12 @@ DYNALIB_TABLE_EXTERN(user);
 /**
  * The module export table. This lists the addresses of individual library dynalib jump tables.
  */
-const void* const user_part_module[] = {
+__attribute__((externally_visible)) const void* const user_part_module[] = {
     DYNALIB_TABLE_NAME(user),
 };
+
+
+void* _sbrk( int incr )
+{
+    return 0;
+}
