c95f70d5e359178d668936eea846916889b79803
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-05-04 21:29:39 +0200

    generate an error when there are no application sources to compile. This helps detect typos on the command line.

--- user/build.mk
+++ user/build.mk
@@ -30,6 +30,12 @@ INCLUDE_DIRS += $(SOURCE_PATH)/$(USRSRC)  # add user sources to include path
 # add C and CPP files - if USRSRC is not empty, then add a slash
 CPPSRC += $(call target_files,$(USRSRC_SLASH),*.cpp)
 CSRC += $(call target_files,$(USRSRC_SLASH),*.c)    
+
+APPSOURCES=$(call target_files,$(USRSRC_SLASH),*.cpp)
+APPSOURCES+=$(call target_files,$(USRSRC_SLASH),*.c)
+ifeq ($(strip $(APPSOURCES)),)
+$(error "No sources found in $(SOURCE_PATH)/$(USRSRC)")
+endif
 else
 include $(usrmakefile)
 endif
