aba7afabda569fe687cc3364149c8ab6f5565d45
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-05-25 04:05:28 +0200

    bump system to next version

--- modules/photon/system-part1/import.mk
+++ modules/photon/system-part1/import.mk
@@ -1,4 +1,4 @@
-SYSTEM_PART1_MODULE_VERSION ?= 1
+SYSTEM_PART1_MODULE_VERSION ?= 2
 SYSTEM_PART1_MODULE_PATH ?= $(PROJECT_ROOT)/modules/photon/system-part1
 include $(call rwildcard,$(SYSTEM_PART1_MODULE_PATH)/,include.mk)
 
--- modules/photon/system-part2/import.mk
+++ modules/photon/system-part2/import.mk
@@ -1,3 +1,3 @@
-SYSTEM_PART2_MODULE_VERSION ?= 1
+SYSTEM_PART2_MODULE_VERSION ?= 2
 SYSTEM_PART2_MODULE_PATH ?= $(PROJECT_ROOT)/modules/photon/system-part2
 include $(call rwildcard,$(SYSTEM_PART2_MODULE_PATH)/,include.mk)
