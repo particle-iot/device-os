618bcc39050d65cd410c8815da44946b1c7b2077
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-06-15 21:58:16 +0200

    syncTime returns `bool` success code.

--- user/tests/wiring/api/cloud.cpp
+++ user/tests/wiring/api/cloud.cpp
@@ -24,35 +24,35 @@
 #include "testapi.h"
 
 test(api_spark_variable) {
-    
+
     int valueInt = 0;
     double valueDouble = 0;
     const char* UNUSED(constValueString) = "oh no!";
     char valueString[] = "oh yeah!";
     String valueSmartString(valueString);
-    
+
     API_COMPILE(Spark.variable("myint", &valueInt, INT));
-    
+
     API_COMPILE(Spark.variable("mydouble", &valueDouble, DOUBLE));
-    
+
     API_COMPILE(Spark.variable("mystring", valueString, STRING));
-        
-    API_NO_COMPILE(Spark.variable("mystring", constValueString, STRING));        
+
+    API_NO_COMPILE(Spark.variable("mystring", constValueString, STRING));
 }
 
 test(api_spark_function) {
-    int (*handler)(String) = NULL;    
-    API_COMPILE(Spark.function("name", handler));    
+    int (*handler)(String) = NULL;
+    API_COMPILE(Spark.function("name", handler));
 }
 
 test(api_spark_publish) {
-    
+
     API_COMPILE(Spark.publish("public event name"));
-    
+
     API_COMPILE(Spark.publish("public event name", "event data"));
-    
+
     API_COMPILE(Spark.publish("public event name", "event data"));
-    
+
     API_COMPILE(Spark.publish("public event name", "event data", 60));
 
     API_COMPILE(Spark.publish("public event name", "event data", 60, PUBLIC));
@@ -60,64 +60,64 @@ test(api_spark_publish) {
     API_COMPILE(Spark.publish("private event name", "event data", 60, PRIVATE));
 
     API_COMPILE(Spark.publish("public event name", PRIVATE));
-    
+
     API_COMPILE(Spark.publish("public event name", "event data", PRIVATE));
-    
+
     API_COMPILE(Spark.publish("public event name", PUBLIC));
 
-    
+
     API_COMPILE(Spark.publish(String("public event name")));
-    
+
     API_COMPILE(Spark.publish(String("public event name"), String("event data")));
-    
+
     API_COMPILE(Spark.publish(String("public event name"), String("event data")));
-    
+
     API_COMPILE(Spark.publish(String("public event name"), String("event data"), 60));
 
     API_COMPILE(Spark.publish(String("public event name"), String("event data"), 60, PUBLIC));
 
     API_COMPILE(Spark.publish(String("public event name"), String("event data"), 60, PRIVATE));
-    
+
     API_COMPILE(Spark.publish(String("public event name"), PRIVATE));
-    
+
     API_COMPILE(Spark.publish(String("public event name"), String("event data"), PRIVATE));
-    
+
     API_COMPILE(Spark.publish(String("public event name"), PUBLIC));
-    
+
 }
 
 test(api_spark_subscribe) {
 
     void (*handler)(const char *event_name, const char *data) = NULL;
-    
+
     API_COMPILE(Spark.subscribe("name", handler));
-    
+
     API_COMPILE(Spark.subscribe("name", handler, MY_DEVICES));
-    
+
     API_COMPILE(Spark.subscribe("name", handler, "1234"));
-    
+
 
     API_COMPILE(Spark.subscribe(String("name"), handler));
-    
+
     API_COMPILE(Spark.subscribe(String("name"), handler, MY_DEVICES));
-    
+
     API_COMPILE(Spark.subscribe(String("name"), handler, "1234"));
-    
+
 }
 
 test(api_spark_sleep) {
-    
+
     API_COMPILE(Spark.sleep(60));
 
     API_COMPILE(Spark.sleep(SLEEP_MODE_WLAN, 60));
-    
+
     API_COMPILE(Spark.sleep(SLEEP_MODE_DEEP, 60));
 
     API_COMPILE(Spark.sleep(A0, CHANGE));
     API_COMPILE(Spark.sleep(A0, RISING));
     API_COMPILE(Spark.sleep(A0, FALLING));
     API_COMPILE(Spark.sleep(A0, FALLING, 20));
-        
+
 }
 
 test(api_spark_connection) {
@@ -132,4 +132,8 @@ test(api_spark_connection) {
 test(api_spark_deviceID) {
     String id;
     API_COMPILE(id = Spark.deviceID());
-}    
+}
+
+test(api_spark_syncTime) {
+    API_COMPILE(Spark.syncTime());
+}
