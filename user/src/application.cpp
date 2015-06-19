2f8ca57ffb609804fd6bb8766872ad9ac9c62492
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-06-19 19:53:50 +0200

    revert to tinker

--- user/src/application.cpp
+++ user/src/application.cpp
@@ -1,86 +1,172 @@
+/**
+ ******************************************************************************
+ * @file    application.cpp
+ * @authors  Satish Nair, Zachary Crockett and Mohit Bhoite
+ * @version V1.0.0
+ * @date    05-November-2013
+ * @brief   Tinker application
+ ******************************************************************************
+  Copyright (c) 2013-2015 Particle Industries, Inc.  All rights reserved.
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, either
+  version 3 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
 #include "application.h"
+#include "stdarg.h"
 
-// serial connection
-int serialBaud = 9600;
+/* Function prototypes -------------------------------------------------------*/
+int tinkerDigitalRead(String pin);
+int tinkerDigitalWrite(String command);
+int tinkerAnalogRead(String pin);
+int tinkerAnalogWrite(String command);
 
-// socket parameters
-int serverPort = 23;
+SYSTEM_MODE(AUTOMATIC);
 
-// start TCP servers
-TCPServer server = TCPServer(serverPort);
-TCPClient client;
+/* This function is called once at start up ----------------------------------*/
+void setup()
+{
+    //Setup the Tinker application here
+    //Register all the Tinker functions
+    Spark.function("digitalread", tinkerDigitalRead);
+    Spark.function("digitalwrite", tinkerDigitalWrite);
 
-char myIpString[24];
-
-// allow us to use itoa() in this scope
-extern char* itoa(int a, char* buffer, unsigned char radix);
-
-enum tnetState {DISCONNECTED, CONNECTED};
-int telnetState = DISCONNECTED;
-
-SYSTEM_MODE(MANUAL);
-
-unsigned long activity_timeout = 0;
-
-void setup() {
-    Serial.begin(serialBaud); // open serial communications
-
-    if (System.mode()!=AUTOMATIC) {
-        String s = System.deviceID();
-        WiFi.connect();
-        while (!WiFi.ready()) {
-            Spark.process();
-        }
-        Spark.process();
-    }
-
-    server.begin(); // begin listening for TCP connections
-
-    IPAddress myIp = WiFi.localIP();
-    //sprintf(myIpString, "%d.%d.%d.%d", myIp[0], myIp[1], myIp[2], myIp[3]);
-    char num[4];
-    itoa(myIp[0],num,10);
-    strcat(myIpString,num); strcat(myIpString,".");
-    itoa(myIp[1],num,10);
-    strcat(myIpString,num); strcat(myIpString,".");
-    itoa(myIp[2],num,10);
-    strcat(myIpString,num); strcat(myIpString,".");
-    itoa(myIp[3],num,10);
-    strcat(myIpString,num);
-
-    Spark.variable("devIP", myIpString, STRING);
-    Serial.println(myIp);
+    Spark.function("analogread", tinkerAnalogRead);
+    Spark.function("analogwrite", tinkerAnalogWrite);
 }
 
-void loop() {
+/* This function loops forever --------------------------------------------*/
+void loop()
+{
+}
 
-    if (client.connected()) {
-        if (telnetState == DISCONNECTED)
-            Serial.println("client connected");    //Check for new connection on next loop()
+/*******************************************************************************
+ * Function Name  : tinkerDigitalRead
+ * Description    : Reads the digital value of a given pin
+ * Input          : Pin
+ * Output         : None.
+ * Return         : Value of the pin (0 or 1) in INT type
+                    Returns a negative number on failure
+ *******************************************************************************/
+int tinkerDigitalRead(String pin)
+{
+    //convert ascii to integer
+    int pinNumber = pin.charAt(1) - '0';
+    //Sanity check to see if the pin numbers are within limits
+    if (pinNumber< 0 || pinNumber >7) return -1;
 
-        telnetState = CONNECTED;
-        // echo all available bytes back to the client
-        int incomingByte = 0;
-        while (client.available()) {    //Read incoming TCP data if available and copy to Serial port
-            incomingByte = client.read();
-            Serial.write(char(incomingByte));
-            activity_timeout = millis();
-        }
-        if (incomingByte != 0)            //Make sure to flush outgoing serial data before looking at serial input
-            Serial.flush();
-        while (Serial.available() > 0) {     //Read incoming serial data if available and copy to TCP port
-            incomingByte = Serial.read();
-            client.write((char)incomingByte);     // write the char data to the client
-        }
+    if(pin.startsWith("D"))
+    {
+        pinMode(pinNumber, INPUT_PULLDOWN);
+        return digitalRead(pinNumber);
     }
-    else {
-        // if no client is yet connected, check for a new connection
-        if (telnetState == CONNECTED) {        //If client WAS connected before, so stop() to close connection
-            Serial.println("client.stop");    //Check for new connection on next loop()
-            client.stop();
-            telnetState = DISCONNECTED;
-        }
-        client = server.available();        //Update TCP client status - "client" is declared globally
+    else if (pin.startsWith("A"))
+    {
+        pinMode(pinNumber+10, INPUT_PULLDOWN);
+        return digitalRead(pinNumber+10);
     }
+    return -2;
+}
 
+/*******************************************************************************
+ * Function Name  : tinkerDigitalWrite
+ * Description    : Sets the specified pin HIGH or LOW
+ * Input          : Pin and value
+ * Output         : None.
+ * Return         : 1 on success and a negative number on failure
+ *******************************************************************************/
+int tinkerDigitalWrite(String command)
+{
+    bool value = 0;
+    //convert ascii to integer
+    int pinNumber = command.charAt(1) - '0';
+    //Sanity check to see if the pin numbers are within limits
+    if (pinNumber< 0 || pinNumber >7) return -1;
+
+    if(command.substring(3,7) == "HIGH") value = 1;
+    else if(command.substring(3,6) == "LOW") value = 0;
+    else return -2;
+
+    if(command.startsWith("D"))
+    {
+        pinMode(pinNumber, OUTPUT);
+        digitalWrite(pinNumber, value);
+        return 1;
+    }
+    else if(command.startsWith("A"))
+    {
+        pinMode(pinNumber+10, OUTPUT);
+        digitalWrite(pinNumber+10, value);
+        return 1;
+    }
+    else return -3;
+}
+
+/*******************************************************************************
+ * Function Name  : tinkerAnalogRead
+ * Description    : Reads the analog value of a pin
+ * Input          : Pin
+ * Output         : None.
+ * Return         : Returns the analog value in INT type (0 to 4095)
+                    Returns a negative number on failure
+ *******************************************************************************/
+int tinkerAnalogRead(String pin)
+{
+    //convert ascii to integer
+    int pinNumber = pin.charAt(1) - '0';
+    //Sanity check to see if the pin numbers are within limits
+    if (pinNumber< 0 || pinNumber >7) return -1;
+
+    if(pin.startsWith("D"))
+    {
+        return -3;
+    }
+    else if (pin.startsWith("A"))
+    {
+        return analogRead(pinNumber+10);
+    }
+    return -2;
+}
+
+/*******************************************************************************
+ * Function Name  : tinkerAnalogWrite
+ * Description    : Writes an analog value (PWM) to the specified pin
+ * Input          : Pin and Value (0 to 255)
+ * Output         : None.
+ * Return         : 1 on success and a negative number on failure
+ *******************************************************************************/
+int tinkerAnalogWrite(String command)
+{
+    //convert ascii to integer
+    int pinNumber = command.charAt(1) - '0';
+    //Sanity check to see if the pin numbers are within limits
+    if (pinNumber< 0 || pinNumber >7) return -1;
+
+    String value = command.substring(3);
+
+    if(command.startsWith("D"))
+    {
+        pinMode(pinNumber, OUTPUT);
+        analogWrite(pinNumber, value.toInt());
+        return 1;
+    }
+    else if(command.startsWith("A"))
+    {
+        pinMode(pinNumber+10, OUTPUT);
+        analogWrite(pinNumber+10, value.toInt());
+        return 1;
+    }
+    else return -2;
 }
