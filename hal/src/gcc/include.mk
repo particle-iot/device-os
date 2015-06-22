909957fb1effb702fc4dfff0fd43019aa1b2f6c0
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-06-22 06:02:55 +0200

    additional files for gcc build

---
+++ hal/src/gcc/include.mk
@@ -0,0 +1,25 @@
+
+ifneq (,$(findstring hal,$(MAKE_DEPENDENCIES)))
+
+LDFLAGS += -lc
+
+
+# additional libraries required by gcc build
+ifdef SYSTEMROOT
+LIBS += boost_system-mgw48-mt-1_57 ws2_32 wsock32
+else
+LIBS += boost_system
+endif
+LIB_DIRS += $(BOOST_ROOT)/stage/lib
+
+# gcc HAL is different for test driver and test subject
+ifeq "$(SPARK_TEST_DRIVER)" "1"
+HAL_TEST_FLAVOR+=-driver
+endif
+
+
+
+endif
+
+
+
