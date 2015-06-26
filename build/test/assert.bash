b28e42b314ee25b09458cd3acb0afe093c720136
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-06-26 21:39:17 +0200

    factored tests into per-platform files and separate out common build functionality.

--- build/test/assert.bash
+++ build/test/assert.bash
@@ -25,8 +25,8 @@ assert_failure() {
 
 assert_equal() {
   if [ "$1" != "$2" ]; then
-    { echo "expected: $1"
-      echo "actual:   $2"
+    { echo "expected: '$1'"
+      echo "actual:   '$2'"
     } | flunk
   fi
 }
