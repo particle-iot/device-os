2989ce867d915eb2463b8991638502eeabbfa603
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-07-21 12:40:47 +0200

    factored out the variables form the main module make script so that it's easy to reuse by components that don't want all the features of a regular module.

--- build/module.mk
+++ build/module.mk
@@ -18,85 +18,7 @@ include $(DEPS_INCLUDE_SCRIPTS)
 
 include $(call rwildcard,$(MODULE_PATH)/,build.mk)
 
-# Uncomment the following to enable serial bitrate specific dfu/ymodem flasher in code
-START_DFU_FLASHER_SERIAL_SPEED=14400
-# Uncommenting this increase the size of the firmware image because of ymodem addition
-START_YMODEM_FLASHER_SERIAL_SPEED=28800
-
-QUOTE='
-ifneq (,$(GLOBAL_DEFINES))
-CFLAGS += $(addprefix -D,$(GLOBAL_DEFINES))
-export GLOBAL_DEFINES
-endif
-
-ifdef TEACUP
-CFLAGS += -DTEACUP
-endif
-
-ifeq ("$(DEBUG_BUILD)","y")
-CFLAGS += -DDEBUG_BUILD
-COMPILE_LTO ?= n
-else
-CFLAGS += -DRELEASE_BUILD
-endif
-
-ifdef SPARK_TEST_DRIVER
-CFLAGS += -DSPARK_TEST_DRIVER=$(SPARK_TEST_DRIVER)
-endif
-
-ifeq ("$(SPARK_CLOUD)","n")
-CFLAGS += -DSPARK_NO_CLOUD
-endif
-
-ifeq ("$(SPARK_WIFI)","n")
-CFLAGS += -DSPARK_NO_WIFI
-endif
-
-
-# disable COMPILE_LTO when JTAG is enabled since it obfuscates the symbol mapping
-# breaking step debugging
-ifeq ($(USE_SWD),y)
-COMPILE_LTO ?= n
-endif
-
-ifeq ($(USE_SWD_JTAG),y)
-COMPILE_LTO ?= n
-endif
-
-WARNINGS_AS_ERRORS ?= y
-ifeq ($(WARNINGS_AS_ERRORS),y)
-CFLAGS += -Werror
-endif
-
-# add include directories
-CFLAGS += $(patsubst %,-I%,$(INCLUDE_DIRS)) -I.
-# Generate dependency files automatically.
-CFLAGS += -MD -MP -MF $@.d
-# Removed "-fdata-sections" as firmware doesn't work as expected
-CFLAGS += -ffunction-sections -fdata-sections -Wall -Wno-switch -Wno-error=deprecated-declarations -fmessage-length=0
-CFLAGS += -fno-strict-aliasing
-CFLAGS += -DSPARK=1
-
-ifdef START_DFU_FLASHER_SERIAL_SPEED
-CFLAGS += -DSTART_DFU_FLASHER_SERIAL_SPEED=$(START_DFU_FLASHER_SERIAL_SPEED)
-endif
-ifdef START_YMODEM_FLASHER_SERIAL_SPEED
-CFLAGS += -DSTART_YMODEM_FLASHER_SERIAL_SPEED=$(START_YMODEM_FLASHER_SERIAL_SPEED)
-endif
-
-CONLYFLAGS += -Wno-pointer-sign -std=gnu99
-
-LDFLAGS += $(LIBS_EXT)
-LDFLAGS += $(patsubst %,-L%,$(LIB_DIRS))
-ifneq ($(PLATFORM_ID),3)
-LDFLAGS += -Wl,--whole-archive $(patsubst %,-l%,$(LIBS)) -Wl,--no-whole-archive
-LDFLAGS += -L$(COMMON_BUILD)/arm/linker
-else
-LDFLAGS += $(patsubst %,-l%,$(LIBS))
-endif
-
-# Assembler flags
-ASFLAGS += -x assembler-with-cpp -fmessage-length=0
+include $(COMMON_BUILD)/module-defaults.mk
 
 # Collect all object and dep files
 ALLOBJ += $(addprefix $(BUILD_PATH)/, $(CSRC:.c=.o))
@@ -106,39 +28,6 @@ ALLOBJ += $(addprefix $(BUILD_PATH)/, $(patsubst $(COMMON_BUILD)/arm/%,%,$(ASRC:
 ALLDEPS += $(addprefix $(BUILD_PATH)/, $(CSRC:.c=.o.d))
 ALLDEPS += $(addprefix $(BUILD_PATH)/, $(CPPSRC:.cpp=.o.d))
 ALLDEPS += $(addprefix $(BUILD_PATH)/, $(patsubst $(COMMON_BUILD)/arm/%,%,$(ASRC:.S=.o.d)))
-
-ifeq (y,$(MODULAR_FIRMWARE))
-MODULAR_EXT = -m
-endif
-
-COMPILE_LTO ?= n
-ifeq (y,$(COMPILE_LTO))
-LTO_EXT = -lto
-endif
-
-ifeq ("$(TARGET_TYPE)","a")
-TARGET_FILE_PREFIX = lib
-endif
-
-# TARGET_FILE_NAME is the file name (minus extension) of the target produced
-# TARGET_NAME is the final filename, including any prefix
-TARGET_FILE_NAME ?= $(MODULE)
-TARGET_NAME ?= $(TARGET_FILE_PREFIX)$(TARGET_FILE_NAME)
-TARGET_PATH ?= $(BUILD_PATH)/$(call sanitize,$(TARGET_DIR_NAME))
-
-# add trailing slash
-ifneq ("$(TARGET_PATH)","$(dir $(TARGET_PATH))")
-TARGET_SEP = /
-endif
-
-TARGET_BASE ?= $(TARGET_PATH)$(TARGET_SEP)$(TARGET_NAME)
-TARGET ?= $(TARGET_BASE).$(TARGET_TYPE)
-
-# add BUILD_PATH_EXT with a preceeding slash if not empty.
-BUILD_PATH ?= $(BUILD_PATH_BASE)/$(MODULE)$(and $(BUILD_PATH_EXT),/$(BUILD_PATH_EXT))
-
-BUILD_TARGET_PLATFORM = platform-$(PLATFORM_ID)$(MODULAR_EXT)$(LTO_EXT)
-BUILD_PATH_EXT ?= $(BUILD_TARGET_PLATFORM)
 
 
 # All Target
