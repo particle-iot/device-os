ee1185c8922d193a3a79fed0a9321991e48a9851
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-07-24 00:04:38 +0200

    reworked system versioning to use preprocessor directives so that library writers can use conditional compilation to target different releases.

--- system/inc/system_version.h
+++ system/inc/system_version.h
@@ -26,13 +26,19 @@ extern "C" {
  * This file is referenced from https://github.com/spark/firmware/wiki/Firmware-Release-Checklist
  */
 
-const int PARTICLE_v040 = 0x00040000;
-const int PARTICLE_v041 = 0x00040100;
-const int PARTICLE_v042 = 0x00040200;
-const int PARTICLE_v043 = 0x00040300;
-const int PARTICLE_v044 = 0x00040400;
+#define SYSTEM_VERSION_v040  0x00040000
+#define SYSTEM_VERSION_v041  0x00040100
+#define SYSTEM_VERSION_v042  0x00040200
+#define SYSTEM_VERSION_v043  0x00040300
+#define SYSTEM_VERSION_v044  0x00040400
 
-const int SYSTEM_VERSION = PARTICLE_v044;
+#define SYSTEM_VERSION  SYSTEM_VERSION_v044
+
+#define SYSTEM_VERSION_040
+#define SYSTEM_VERSION_041
+#define SYSTEM_VERSION_042
+#define SYSTEM_VERSION_043
+#define SYSTEM_VERSION_044
 
 #define SYSTEM_VERSION_STRING "0.4.4"
 
