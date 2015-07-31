304a143bb21042668b0e09e546558c01ea0fc9b2
Technobly <technobly@gmail.com>
2015-07-31 01:32:34 -0500

    revert Photon core_hal.c

--- hal/src/photon/core_hal.c
+++ hal/src/photon/core_hal.c
@@ -41,8 +41,6 @@ const unsigned HardFaultIndex = 3;
 const unsigned UsageFaultIndex = 6;
 const unsigned SysTickIndex = 15;
 const unsigned USART1Index = 53;
-const unsigned USART2Index = 54;
-const unsigned USART3Index = 55;
 const unsigned ButtonExtiIndex = BUTTON1_EXTI_IRQ_INDEX;
 const unsigned TIM7Index = 71;
 
@@ -63,8 +61,6 @@ void HAL_Core_Setup_override_interrupts(void) {
     isrs[UsageFaultIndex] = (uint32_t)UsageFault_Handler;
     isrs[SysTickIndex] = (uint32_t)SysTickOverride;
     isrs[USART1Index] = (uint32_t)HAL_USART1_Handler;
-    isrs[USART2Index] = (uint32_t)HAL_USART2_Handler;
-    isrs[USART3Index] = (uint32_t)HAL_USART3_Handler;
     isrs[ButtonExtiIndex] = (uint32_t)Mode_Button_EXTI_irq;
     isrs[TIM7Index] = (uint32_t)TIM7_override;  // WICED uses this for a JTAG watchdog handler
     SCB->VTOR = (unsigned long)isrs;
