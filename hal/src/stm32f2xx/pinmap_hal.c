2bf5c4e4e482b360f0eaef0f1dd9519edff17387
wesner0019 <wesner0019@gmx.com>
2015-07-31 11:08:00 -0500

    Changed pin names to P1SX

--- hal/src/stm32f2xx/pinmap_hal.c
+++ hal/src/stm32f2xx/pinmap_hal.c
@@ -71,12 +71,12 @@ STM32_Pin_Info __PIN_MAP[TOTAL_PINS] =
 /* RGBG          */ { GPIOA, GPIO_Pin_2, GPIO_PinSource2, NONE, NONE, TIM2, TIM_Channel_3, PIN_MODE_NONE, 0, 0 },
 /* RGBB          */ { GPIOA, GPIO_Pin_3, GPIO_PinSource3, NONE, NONE, TIM2, TIM_Channel_4, PIN_MODE_NONE, 0, 0 }
 #if PLATFORM_ID == 8 // P1
-/* B0        */,{ GPIOB, GPIO_Pin_0, GPIO_PinSource0, ADC_Channel_8, NONE, TIM3, TIM_Channel_3, PIN_MODE_NONE, 0, 0 },
-/* B1        */ { GPIOB, GPIO_Pin_1, GPIO_PinSource1, ADC_Channel_9, NONE, TIM3, TIM_Channel_4, PIN_MODE_NONE, 0, 0 },
-/* B2        */ { GPIOC, GPIO_Pin_0, GPIO_PinSource0, ADC_Channel_12, NONE, NONE, NONE, PIN_MODE_NONE, 0, 0 },
-/* B3        */ { GPIOC, GPIO_Pin_4, GPIO_PinSource4, ADC_Channel_14, NONE, NONE, NONE, PIN_MODE_NONE, 0, 0 },
-/* B4        */ { GPIOC, GPIO_Pin_13, GPIO_PinSource13, NONE, NONE, NONE, NONE, PIN_MODE_NONE, 0, 0 },
-/* B5        */ { GPIOC, GPIO_Pin_1, GPIO_PinSource1, ADC_Channel_11, NONE, NONE, NONE, PIN_MODE_NONE, 0, 0 }
+/* P1S0        */,{ GPIOB, GPIO_Pin_0, GPIO_PinSource0, ADC_Channel_8, NONE, TIM3, TIM_Channel_3, PIN_MODE_NONE, 0, 0 },
+/* P1S1        */ { GPIOB, GPIO_Pin_1, GPIO_PinSource1, ADC_Channel_9, NONE, TIM3, TIM_Channel_4, PIN_MODE_NONE, 0, 0 },
+/* P1S2        */ { GPIOC, GPIO_Pin_0, GPIO_PinSource0, ADC_Channel_12, NONE, NONE, NONE, PIN_MODE_NONE, 0, 0 },
+/* P1S3        */ { GPIOC, GPIO_Pin_4, GPIO_PinSource4, ADC_Channel_14, NONE, NONE, NONE, PIN_MODE_NONE, 0, 0 },
+/* P1S4        */ { GPIOC, GPIO_Pin_13, GPIO_PinSource13, NONE, NONE, NONE, NONE, PIN_MODE_NONE, 0, 0 },
+/* P1S5        */ { GPIOC, GPIO_Pin_1, GPIO_PinSource1, ADC_Channel_11, NONE, NONE, NONE, PIN_MODE_NONE, 0, 0 }
 #endif
 #if PLATFORM_ID == 10 // Electron
 /* TXD_UC        */,{ GPIOB, GPIO_Pin_10, GPIO_PinSource10, NONE, NONE, TIM2, TIM_Channel_3, PIN_MODE_NONE, 0, 0 },
