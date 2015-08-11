785a4fa37bc61f09a3d00a0ae07012e2c2a29a6e
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-08-11 06:47:28 +0200

    renamed spark_wiring_progmen to spark_wiring_arduino. Thanks @technobly!

--- wiring/inc/spark_wiring.h
+++ wiring/inc/spark_wiring.h
@@ -34,7 +34,7 @@
 #include "config.h"
 #include "spark_macros.h"
 #include "debug.h"
-#include "spark_wiring_progmem.h"
+#include "spark_wiring_arduino.h"
 #include "spark_wiring_constants.h"
 #include "spark_wiring_stream.h"
 #include "spark_wiring_printable.h"
