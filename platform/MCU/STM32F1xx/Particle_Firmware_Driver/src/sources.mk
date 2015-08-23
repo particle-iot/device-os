3117253fa8d1294f28c3aba6084c0b1a3b769e3f
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-08-23 21:36:24 +0200

    SPARK_Firmware_Driver -> Particle_Firmware_Driver in platform module

--- platform/MCU/STM32F1xx/Particle_Firmware_Driver/src/sources.mk
+++ platform/MCU/STM32F1xx/Particle_Firmware_Driver/src/sources.mk
@@ -4,18 +4,18 @@
 # Define the prefix to this directory.
 # Note: The name must be unique within this build and should be
 #       based on the root of the project
-TARGET_SPARK_SRC_PATH = $(TARGET_SPARK_PATH)/src
+TARGET_PARTICLE_SRC_PATH = $(TARGET_PARTICLE_PATH)/src
 
-INCLUDE_DIRS += SPARK_Services/inc
+INCLUDE_DIRS += PARTICLE_Services/inc
 
 # C source files included in this build.
-CSRC += $(TARGET_SPARK_SRC_PATH)/cc3000_spi.c
-CSRC += $(TARGET_SPARK_SRC_PATH)/hw_config.c
-CSRC += $(TARGET_SPARK_SRC_PATH)/sst25vf_spi.c
-CSRC += $(TARGET_SPARK_SRC_PATH)/system_stm32f10x.c
-CSRC += $(TARGET_SPARK_SRC_PATH)/spi_bus.c
-CSRC += $(TARGET_SPARK_SRC_PATH)/usb_endp.c
-CSRC += $(TARGET_SPARK_SRC_PATH)/usb_pwr.c
+CSRC += $(TARGET_PARTICLE_SRC_PATH)/cc3000_spi.c
+CSRC += $(TARGET_PARTICLE_SRC_PATH)/hw_config.c
+CSRC += $(TARGET_PARTICLE_SRC_PATH)/sst25vf_spi.c
+CSRC += $(TARGET_PARTICLE_SRC_PATH)/system_stm32f10x.c
+CSRC += $(TARGET_PARTICLE_SRC_PATH)/spi_bus.c
+CSRC += $(TARGET_PARTICLE_SRC_PATH)/usb_endp.c
+CSRC += $(TARGET_PARTICLE_SRC_PATH)/usb_pwr.c
 
 # C++ source files included in this build.
 CPPSRC +=
