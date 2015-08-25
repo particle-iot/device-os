7e371130fdf2c9abd68d5472a426e8f67547ea48
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-08-25 10:36:30 +0200

    start to factor out OTA module handling from photon to shared stm32 folder so it can be used on the Core.

--- hal/src/photon/ota_flash_hal.cpp
+++ hal/src/photon/ota_flash_hal.cpp
@@ -36,73 +36,9 @@
 #include "parse_server_address.h"
 #include "spark_macros.h"
 #include "bootloader.h"
+#include "ota_module.h"
 
 #define OTA_CHUNK_SIZE          512
-
-/**
- * Checks if the minimum required dependencies for the given module are satisfied.
- * @param bounds    The bounds of the module to check.
- * @return {@code true} if the dependencies are satisfied, {@code false} otherwise.
- */
-bool validate_module_dependencies(const module_bounds_t* bounds, bool userPartOptional);
-const module_bounds_t* find_module_bounds(uint8_t module_function, uint8_t module_index);
-
-/**
- * Find the module_info at a given address. No validation is done so the data
- * pointed to should not be trusted.
- * @param bounds
- * @return
- */
-inline const module_info_t* locate_module(const module_bounds_t* bounds) {
-    return FLASH_ModuleInfo(FLASH_INTERNAL, bounds->start_address);
-}
-
-/**
- * Determines if a given address is in range.
- * @param test      The address to test
- * @param start     The start range (inclusive)
- * @param end       The end range (inclusive)
- * @return {@code true} if the address is within range.
- */
-inline bool in_range(uint32_t test, uint32_t start, uint32_t end)
-{
-    return test>=start && test<=end;
-}
-
-/**
- * Fetches and validates the module info found at a given location.
- * @param target        Receives the module into
- * @param bounds        The location where to retrieve the module from.
- * @param userDepsOptional
- * @return {@code true} if the module info can be read via the info, crc, suffix pointers.
- */
-bool fetch_module(hal_module_t* target, const module_bounds_t* bounds, bool userDepsOptional, uint16_t check_flags=0)
-{
-    memset(target, 0, sizeof(*target));
-
-    target->bounds = *bounds;
-    if (NULL!=(target->info = locate_module(bounds)))
-    {
-        target->validity_checked = MODULE_VALIDATION_RANGE | MODULE_VALIDATION_DEPENDENCIES | MODULE_VALIDATION_PLATFORM | check_flags;
-        target->validity_result = 0;
-        const uint8_t* module_end = (const uint8_t*)target->info->module_end_address;
-        const module_bounds_t* expected_bounds = find_module_bounds(module_function(target->info), module_index(target->info));
-        if (expected_bounds && in_range(uint32_t(module_end), expected_bounds->start_address, expected_bounds->end_address)) {
-            target->validity_result |= MODULE_VALIDATION_RANGE;
-            target->validity_result |= (PRODUCT_ID==module_platform_id(target->info)) ? MODULE_VALIDATION_PLATFORM : 0;
-            // the suffix ends at module_end, and the crc starts after module end
-            target->crc = (module_info_crc_t*)module_end;
-            target->suffix = (module_info_suffix_t*)(module_end-sizeof(module_info_suffix_t));
-            if (validate_module_dependencies(bounds, userDepsOptional))
-                target->validity_result |= MODULE_VALIDATION_DEPENDENCIES;
-            if ((target->validity_checked & MODULE_VALIDATION_INTEGRITY) && FLASH_VerifyCRC32(FLASH_INTERNAL, bounds->start_address, module_length(target->info)))
-                target->validity_result |= MODULE_VALIDATION_INTEGRITY;
-        }
-        else
-            target->info = NULL;
-    }
-    return target->info!=NULL;
-}
 
 #if MODULAR_FIRMWARE
 const module_bounds_t module_bootloader = { 0x4000, 0x8000000, 0x8004000, MODULE_FUNCTION_BOOTLOADER, 0, MODULE_STORE_MAIN };
@@ -121,6 +57,7 @@ const module_bounds_t* module_bounds[] = { &module_bootloader, &module_user, &mo
 
 const module_bounds_t module_ota = { 0x60000, 0x8080000, 0x80E0000, MODULE_FUNCTION_NONE, 0, MODULE_STORE_SCRATCHPAD};
 #endif
+
 
 /**
  * Finds the location where a given module is stored. The module is identified
--- hal/src/photon/sources.mk
+++ hal/src/photon/sources.mk
@@ -1,11 +1,12 @@
 
 HAL_SRC_STM32F2XX_PATH = $(TARGET_HAL_PATH)/src/stm32f2xx
+HAL_SRC_STM32_PATH = $(TARGET_HAL_PATH)/src/stm32
 HAL_SRC_PHOTON_PATH = $(TARGET_HAL_PATH)/src/photon
 
 # private includes - WICED is not exposed to the HAL clients
 
 HAL_WICED_INCLUDE_DIRS +=   include
-HAL_WICED_INCLUDE_DIRS +=   platforms/$(PLATFORM_NET)   
+HAL_WICED_INCLUDE_DIRS +=   platforms/$(PLATFORM_NET)
 HAL_WICED_INCLUDE_DIRS +=   libraries/daemons/DNS_redirect \
 			    libraries/daemons/HTTP_server \
 			    libraries/protocols/DNS \
@@ -44,7 +45,7 @@ ifeq "$(HAL_WICED_RTOS)" "FreeRTOS"
 HAL_WICED_INCLUDE_DIRS +=   wiced/RTOS/FreeRTOS/WWD/ARM_CM3 \
 			    wiced/RTOS/FreeRTOS/ver7.5.2/Source/include \
 			    wiced/RTOS/FreeRTOS/ver7.5.2/Source/portable/GCC/ARM_CM3
-			    
+
 endif
 
 
@@ -53,6 +54,7 @@ INCLUDE_DIRS += $(addprefix $(HAL_SRC_PHOTON_PATH)/,$(sort $(HAL_WICED_INCLUDE_D
 INCLUDE_DIRS += $(dir $(call rwildcard,$(HAL_SRC_PHOTON_PATH)/wiced/security,*.h))
 INCLUDE_DIRS += $(dir $(call rwildcard,$(HAL_SRC_PHOTON_PATH)/wiced/WWD,*.h))
 INCLUDE_DIRS += $(HAL_SRC_STM32F2XX_PATH)
+INCLUDE_DIRS += $(HAL_SRC_STM32_PATH)
 
 CSRC += $(call target_files,$(HAL_SRC_PHOTON_PATH)/,*.c)
 CPPSRC += $(call target_files,$(HAL_SRC_PHOTON_PATH)/,*.cpp)
@@ -60,14 +62,17 @@ CPPSRC += $(call target_files,$(HAL_SRC_PHOTON_PATH)/,*.cpp)
 CSRC += $(call target_files,$(HAL_SRC_STM32F2XX_PATH)/,*.c)
 CPPSRC += $(call target_files,$(HAL_SRC_STM32F2XX_PATH)/,*.cpp)
 
+CSRC += $(call target_files,$(HAL_SRC_STM32_PATH)/,*.c)
+CPPSRC += $(call target_files,$(HAL_SRC_STM32_PATH)/,*.cpp)
 
-CFLAGS += -DSFLASH_APPS_HEADER_LOC=0x0000 -DUSE_STDPERIPH_DRIVER -D_STM32F215RGT6_ -D_STM3x_ -D_STM32x_ -DMAX_WATCHDOG_TIMEOUT_SECONDS=22 -DFIRMWARE_WITH_PMK_CALC_SUPPORT -DADD_LWIP_EAPOL_SUPPORT -DNXD_EXTENDED_BSD_SOCKET_SUPPORT -DOPENSSL -DSTDC_HEADERS -DUSE_SRP_SHA_512 -DADD_NETX_EAPOL_SUPPORT -DUSE_MICRORNG -DWWD_STARTUP_DELAY=10 -DBOOTLOADER_MAGIC_NUMBER=0x4d435242 -DNETWORK_NetX=1 -DNetX_VERSION=\"v5.5_sp1\" -DNX_INCLUDE_USER_DEFINE_FILE -D__fd_set_defined -DSYS_TIME_H_AVAILABLE -DRTOS_ThreadX=1 -DThreadX_VERSION=\"v5.6\" -DTX_INCLUDE_USER_DEFINE_FILE -DWWD_DIRECT_RESOURCES 
+
+CFLAGS += -DSFLASH_APPS_HEADER_LOC=0x0000 -DUSE_STDPERIPH_DRIVER -D_STM32F215RGT6_ -D_STM3x_ -D_STM32x_ -DMAX_WATCHDOG_TIMEOUT_SECONDS=22 -DFIRMWARE_WITH_PMK_CALC_SUPPORT -DADD_LWIP_EAPOL_SUPPORT -DNXD_EXTENDED_BSD_SOCKET_SUPPORT -DOPENSSL -DSTDC_HEADERS -DUSE_SRP_SHA_512 -DADD_NETX_EAPOL_SUPPORT -DUSE_MICRORNG -DWWD_STARTUP_DELAY=10 -DBOOTLOADER_MAGIC_NUMBER=0x4d435242 -DNETWORK_NetX=1 -DNetX_VERSION=\"v5.5_sp1\" -DNX_INCLUDE_USER_DEFINE_FILE -D__fd_set_defined -DSYS_TIME_H_AVAILABLE -DRTOS_ThreadX=1 -DThreadX_VERSION=\"v5.6\" -DTX_INCLUDE_USER_DEFINE_FILE -DWWD_DIRECT_RESOURCES
 
 # ASM source files included in this build.
 ASRC +=
 
 
 
-# Uncomment this to build the bootloader source file. 
+# Uncomment this to build the bootloader source file.
 #$(HAL_PLATFORM_SRC_PATH)/bootloader_platform_$(PLATFORM_ID).cc $(COMMON_BUILD)/target/bootloader/platform-$(PLATFORM_ID)-lto/bootloader.bin
 #	xxd -i $< > $@
