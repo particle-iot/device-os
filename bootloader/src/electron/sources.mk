7ca5c972a2c168284c172075adeed2bb61942ba1
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-08-28 03:33:12 +0200

    electron bootloader compiles. pull in the DCT implementation from electron HAL. (Cannot link the HAL due to duplicate USB descriptor in HAL and in bootloader.)

--- bootloader/src/electron/sources.mk
+++ bootloader/src/electron/sources.mk
@@ -1,4 +1,6 @@
 BOOTLOADER_SRC_COREV2_PATH = $(BOOTLOADER_MODULE_PATH)/src/electron
 include $(BOOTLOADER_MODULE_PATH)/src/stm32f2xx/sources.mk
 
-
+CSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/electron/,dct_hal.c)
+CSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/electron/,watchdog_hal.c)
+$(info $(CSRC))
