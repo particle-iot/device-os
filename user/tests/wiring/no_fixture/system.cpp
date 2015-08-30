96631c0c615fe29d8abcc1514aea7dbec0f2b665
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-08-30 08:47:52 +0200

    notes on failing freeMemory test on the core.

--- user/tests/wiring/no_fixture/system.cpp
+++ user/tests/wiring/no_fixture/system.cpp
@@ -4,11 +4,18 @@
 
 test(System_FreeMemory)
 {
+    // this test didn't work on the core attempting to allocate the current value of
+    // freeMemory(), presumably because of fragmented heap from
+    // relatively large allocations during the handshake, so the request was satisfied
+    // without calling _sbrk()
+    // 4096 was chosen to be small enough to allocate, but large enough to force _sbrk() to be called.)
+
     uint32_t free1 = System.freeMemory();
-    char* m1 = new char[free1-1024];
-    uint32_t free2 = System.freeMemory();
-    delete[] m1;
-
-    assertLess(free2, free1);
-
+    if (free1>128) {
+        void* m1 = malloc(1024*6);
+        uint32_t free2 = System.freeMemory();
+        free(m1);
+        assertLess(free2, free1);
+    }
 }
+
