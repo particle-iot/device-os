491b2be5c6d4835426dc48756317d3855e4ea053
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-09-13 23:13:57 +0200

    system-part2 makes use of wiring utilities, but we don't want to include the SPI and I2C globals since that brings in a lot of unnecessary code. Previously the symbols SPARK_WIRING_NO_I2C/SPI were used to exclude these globals, but since this applies to a shared library, the build would then break when compiling other modules (e.g. the application) that requires I2C or SPI (Typically undefined reference to Wire.)  The fix is to separate out the globals into a new module so that only the modules that need the wiring globals get them by depending on the new wiring_globals project.
    digitalWrite et al are used by wifitester - since the full version of these methods requires access to I2C/SPI, we now have cutdown versions when building wifitester into modular firmware.

--- wiring/src/spark_wiring_i2c.cpp
+++ wiring/src/spark_wiring_i2c.cpp
@@ -27,21 +27,6 @@
 #include "spark_wiring_i2c.h"
 #include "i2c_hal.h"
 
-#ifndef SPARK_WIRING_NO_I2C
-
-TwoWire Wire(HAL_I2C_INTERFACE1);
-
-#if Wiring_Wire1
-TwoWire Wire1(HAL_I2C_INTERFACE2);
-#endif
-
-/* System PMIC and Fuel Guage I2C3 */
-#if Wiring_Wire3
-TwoWire Wire3(HAL_I2C_INTERFACE3);
-#endif
-
-#endif //SPARK_WIRING_NO_I2C
-
 // Constructors ////////////////////////////////////////////////////////////////
 
 TwoWire::TwoWire(HAL_I2C_Interface i2c)
--- wiring/src/spark_wiring_spi.cpp
+++ wiring/src/spark_wiring_spi.cpp
@@ -28,20 +28,6 @@
 #include "core_hal.h"
 #include "spark_macros.h"
 
-#ifndef SPARK_WIRING_NO_SPI
-
-SPIClass SPI(HAL_SPI_INTERFACE1);
-
-#if Wiring_SPI1
-SPIClass SPI1(HAL_SPI_INTERFACE2);
-#endif
-
-#if Wiring_SPI2
-SPIClass SPI2(HAL_SPI_INTERFACE3);
-#endif
-
-#endif //SPARK_WIRING_NO_SPI
-
 SPIClass::SPIClass(HAL_SPI_Interface spi)
 {
   _spi = spi;
