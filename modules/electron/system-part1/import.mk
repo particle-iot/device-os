fb718304785c65d4590a001d249199d95bcdb1f4
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-09-18 06:16:51 +0200

    factored out common parts of photon system-part1. electron system-part1

--- modules/electron/system-part1/import.mk
+++ modules/electron/system-part1/import.mk
@@ -1,5 +1,5 @@
-SYSTEM_PART1_MODULE_VERSION ?= 6
-SYSTEM_PART1_MODULE_PATH ?= $(PROJECT_ROOT)/modules/photon/system-part1
+SYSTEM_PART1_MODULE_VERSION ?= 1
+SYSTEM_PART1_MODULE_PATH ?= $(PROJECT_ROOT)/modules/electron/system-part1
 include $(call rwildcard,$(SYSTEM_PART1_MODULE_PATH)/,include.mk)
 include $(call rwildcard,$(SHARED_MODULAR)/,include.mk)
 
---
+++ modules/electron/system-part1/src/init_dynalib.c
@@ -0,0 +1,4 @@
+
+#define DYNALIB_EXPORT
+#include "module_system_part1_init.h"
+#include "module_system_part1_init_dynalib.h"
