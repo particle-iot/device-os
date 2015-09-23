aea9454a6b67dd25d261e423a7ed7acf9cb18c5c
Bryce Kahle <bkahle@gmail.com>
2015-09-22 18:08:47 -0700

    Switch to particle-cli

--- ci/create_spark_cli_json.sh
+++ ci/create_spark_cli_json.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-_path=$HOME/.spark
+_path=$HOME/.particle
 mkdir -p $_path
-cat <<EOS > ${_path}/spark.config.json
+cat <<EOS > ${_path}/particle.config.json
 {
-  "apiUrl": "https://api.spark.io",
+  "apiUrl": "https://api.particle.io",
   "access_token": "${SPARK_ACCESS_TOKEN}",
   "username": "${SPARK_USER}"
 }
