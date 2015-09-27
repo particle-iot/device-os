3cc0af6ea933ddd9e36e4e20e77c76bd5b7efd49
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-09-27 16:28:19 +0200

    don't redefine stringify() if already present.

--- services/inc/spark_macros.h
+++ services/inc/spark_macros.h
@@ -39,8 +39,10 @@
 #define CCASSERT(predicate) _x_CCASSERT_LINE(predicate, __LINE__)
 #define _x_CCASSERT_LINE(predicate, line) typedef char CAT(constraint_violated_on_line_,line)[2*((predicate)!=0)-1];
 
+#ifndef stringify
 #define stringify(x) __stringify(x)
 #define __stringify(x) #x
+#endif
 
 // Seconds to Us
 #define S2u(s) ((s)*1000000)
