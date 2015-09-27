b7ef29e936b611c7935b9caf9867d27ba511ba0f
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-09-27 21:02:02 +0200

    add debugging info to hard fault handler for the core

--- hal/src/core/stm32_it.c
+++ hal/src/core/stm32_it.c
@@ -88,13 +88,66 @@ void NMI_Handler(void)
  * Output         : None
  * Return         : None
  *******************************************************************************/
+void HardFault_Handler( void ) __attribute__( ( naked ) );
+
+__attribute__((externally_visible)) void prvGetRegistersFromStack( uint32_t *pulFaultStackAddress )
+{
+    /* These are volatile to try and prevent the compiler/linker optimising them
+    away as the variables never actually get used.  If the debugger won't show the
+    values of the variables, make them global my moving their declaration outside
+    of this function. */
+    volatile uint32_t r0;
+    volatile uint32_t r1;
+    volatile uint32_t r2;
+    volatile uint32_t r3;
+    volatile uint32_t r12;
+    volatile uint32_t lr; /* Link register. */
+    volatile uint32_t pc; /* Program counter. */
+    volatile uint32_t psr;/* Program status register. */
+
+    r0 = pulFaultStackAddress[ 0 ];
+    r1 = pulFaultStackAddress[ 1 ];
+    r2 = pulFaultStackAddress[ 2 ];
+    r3 = pulFaultStackAddress[ 3 ];
+
+    r12 = pulFaultStackAddress[ 4 ];
+    lr = pulFaultStackAddress[ 5 ];
+    pc = pulFaultStackAddress[ 6 ];
+    psr = pulFaultStackAddress[ 7 ];
+
+    /* Silence "variable set but not used" error */
+    if (false) {
+      (void)r0; (void)r1; (void)r2; (void)r3; (void)r12; (void)lr; (void)pc; (void)psr;
+    }
+
+    if (SCB->CFSR & (1<<25) /* DIVBYZERO */) {
+        // stay consistent with the core and cause 5 flashes
+        UsageFault_Handler();
+    }
+    else {
+        PANIC(HardFault,"HardFault");
+
+        /* Go to infinite loop when Hard Fault exception occurs */
+        while (1)
+        {
+        }
+    }
+}
+
+
 void HardFault_Handler(void)
 {
-	/* Go to infinite loop when Hard Fault exception occurs */
-        PANIC(HardFault,"HardFault");
-	while (1)
-	{
-	}
+    __asm volatile
+    (
+        " tst lr, #4                                                \n"
+        " ite eq                                                    \n"
+        " mrseq r0, msp                                             \n"
+        " mrsne r0, psp                                             \n"
+        " ldr r1, [r0, #24]                                         \n"
+        " ldr r2, handler2_address_const                            \n"
+        " bx r2                                                     \n"
+        " handler2_address_const: .word prvGetRegistersFromStack    \n"
+    );
 }
 
 /*******************************************************************************
