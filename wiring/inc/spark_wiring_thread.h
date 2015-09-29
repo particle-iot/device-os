c24be3374144b3a2776904c6698c40624ae0b8a8
Julien Vanier <jvanier@gmail.com>
2015-09-29 14:01:59 -0400

    Remove PeriodicWork for now
    The naming and API will be worked out in another pull request

--- wiring/inc/spark_wiring_thread.h
+++ wiring/inc/spark_wiring_thread.h
@@ -104,33 +104,6 @@ private:
     }
 };
 
-class PeriodicWork
-{
-public:
-    PeriodicWork(system_tick_t period_) 
-        : period(period_), previousWake(0)
-    {
-    }
-
-    void setPeriod(system_tick_t period_) {
-        this->period = period;
-    }
-
-    void start() {
-        if(previousWake == 0) {
-            previousWake = millis();
-        }
-    }
-
-    void end() {
-        os_thread_delay_until(&previousWake, period);
-    }
-
-protected:
-    system_tick_t period;
-    system_tick_t previousWake;
-};
-
 class CriticalSection {
 public:
     CriticalSection() {
