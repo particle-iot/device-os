990798b74272dc4d2b196e7c5784c4b0611ff3b3
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-10-12 04:05:20 +0200

    gcc implementation of Serial maps to stdin/stdout

---
+++ hal/src/gcc/usb_hal.cpp
@@ -0,0 +1,120 @@
+/**
+ ******************************************************************************
+ * @file    usb_device.c
+ * @author  Matthew McGowan
+ * @version V1.0.0
+ * @date    27-Sept-2014
+ * @brief   USB Virtual COM Port and HID device HAL
+ ******************************************************************************
+  Copyright (c) 2013-2015 Particle Industries, Inc.  All rights reserved.
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, either
+  version 3 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "usb_hal.h"
+#include <stdint.h>
+#include <iostream>
+#include <stdio.h>
+#include <sys/poll.h>
+#include <termios.h>
+#include <unistd.h>
+
+uint32_t last_baudRate;
+/*******************************************************************************
+ * Function Name  : USB_USART_Init
+ * Description    : Start USB-USART protocol.
+ * Input          : baudRate.
+ * Return         : None.
+ *******************************************************************************/
+void USB_USART_Init(uint32_t baudRate)
+{
+    last_baudRate = baudRate;
+}
+
+/*******************************************************************************
+ * Function Name  : USB_USART_Available_Data.
+ * Description    : Return the length of available data received from USB.
+ * Input          : None.
+ * Return         : Length.
+ *******************************************************************************/
+uint8_t USB_USART_Available_Data(void)
+{
+    struct pollfd stdin_poll = { .fd = STDIN_FILENO
+            , .events = POLLIN | POLLRDBAND | POLLRDNORM | POLLPRI };
+    int ret = poll(&stdin_poll, 1, 0);
+    return ret;
+}
+
+int32_t last = -1;
+
+/*******************************************************************************
+ * Function Name  : USB_USART_Receive_Data.
+ * Description    : Return data sent by USB Host.
+ * Input          : None
+ * Return         : Data.
+ *******************************************************************************/
+int32_t USB_USART_Receive_Data(uint8_t peek)
+{
+    if (last<0 && USB_USART_Available_Data()) {
+        uint8_t data = 0;
+        if (read(0, &data, 1))
+            last = data;
+    }
+    int32_t result = last;
+    if (!peek)
+        last = -1;      // consume the data
+    return result;
+}
+
+/*******************************************************************************
+ * Function Name  : USB_USART_Send_Data.
+ * Description    : Send Data from USB_USART to USB Host.
+ * Input          : Data.
+ * Return         : None.
+ *******************************************************************************/
+void USB_USART_Send_Data(uint8_t Data)
+{
+    std::cout.write((const char*)&Data, 1);
+}
+
+#ifdef USB_HID_ENABLE
+/*******************************************************************************
+ * Function Name : USB_HID_Send_Report.
+ * Description   : Send HID Report Info to Host.
+ * Input         : pHIDReport and reportSize.
+ * Output        : None.
+ * Return value  : None.
+ *******************************************************************************/
+void USB_HID_Send_Report(void *pHIDReport, size_t reportSize)
+{
+}
+#endif
+
+
+
+unsigned int USB_USART_Baud_Rate(void)
+{
+    return last_baudRate;
+}
+
+void USB_USART_LineCoding_BitRate_Handler(void (*handler)(uint32_t bitRate))
+{
+}
+
+int32_t USB_USART_Flush_Output(unsigned timeout, void* reserved)
+{
+    return 0;
+}
