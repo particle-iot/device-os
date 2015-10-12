560029659cc24f58628813cd996be9413cf7f2e5
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-10-12 03:52:35 +0200

    optimization disabled for debug build

--- build/gcc-tools.mk
+++ build/gcc-tools.mk
@@ -7,8 +7,14 @@ include $(COMMON_BUILD)/common-tools.mk
 # default flags for targeting ARM
 #
 
+GCC_OPTIMIZE=3
+ifeq ($(DEBUG_BUILD),y)
+     GCC_OPTIMIZE=0
+endif
+
 # C compiler flags
-CFLAGS +=  -g3 -m64
+CFLAGS +=  -g3 -m64 -O$(GCC_OPTIMIZE) -gdwarf-2
+CFLAGS += -Wno-unused-local-typedefs -Wno-return-type-c-linkage
 
 ASFLAGS +=  -g3
 
