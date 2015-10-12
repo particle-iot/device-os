69fda382f84ba71b215a2747df602785b2645208
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-10-12 04:26:25 +0200

    pins as const int now that gcc hal is all cpp

--- hal/inc/pinmap_hal.h
+++ hal/inc/pinmap_hal.h
@@ -70,6 +70,7 @@ STM32_Pin_Info* HAL_Pin_Map(void);
 /*
 * Pin mapping. Borrowed from Wiring
 */
+#if PLATFORM_ID!=3
 #if PLATFORM_ID == 10 // Electron
 #define TOTAL_PINS 47
 #elif PLATFORM_ID == 8 // P1
@@ -198,6 +199,78 @@ STM32_Pin_Info* HAL_Pin_Map(void);
 
 #define LSBFIRST 0
 #define MSBFIRST 1
+
+#else
+const pin_t TOTAL_PINS = 21;
+const pin_t TOTAL_ANALOG_PINS = 8;
+const pin_t FIRST_ANALOG_PIN = 10;
+const pin_t D0 = 0;
+const pin_t D1 = 1;
+const pin_t D2 = 2;
+const pin_t D3 = 3;
+const pin_t D4 = 4;
+const pin_t D5 = 5;
+const pin_t D6 = 6;
+const pin_t D7 = 7;
+
+const pin_t A0 = 10;
+const pin_t A1 = 11;
+const pin_t A2 = 12;
+const pin_t A3 = 13;
+const pin_t A4 = 14;
+const pin_t A5 = 15;
+const pin_t A6 = 16;
+
+// WKP pin is also an ADC on Photon
+const pin_t A7 = 17;
+
+// RX and TX pins are also ADCs on Photon
+const pin_t A8 = 18;
+const pin_t A9 = 19;
+
+const pin_t RX = 18;
+const pin_t TX = 19;
+
+const pin_t BTN = 20;
+
+// WKP pin on Photon
+const pin_t WKP = 17;
+
+// Timer pins
+
+const pin_t TIMER2_CH1 = 10;
+const pin_t TIMER2_CH2 = 11;
+const pin_t TIMER2_CH3 = 18;
+const pin_t TIMER2_CH4 = 19;
+
+const pin_t TIMER3_CH1 = 14;
+const pin_t TIMER3_CH2 = 15;
+const pin_t TIMER3_CH3 = 16;
+const pin_t TIMER3_CH4 = 17;
+
+const pin_t TIMER4_CH1 = 1;
+const pin_t TIMER4_CH2 = 0;
+
+// SPI pins
+
+const pin_t SS   = 12;
+const pin_t SCK  = 13;
+const pin_t MISO = 14;
+const pin_t MOSI = 15;
+
+// I2C pins
+
+const pin_t SDA  = 0;
+const pin_t SCL  = 1;
+
+// DAC pins on Photon
+const pin_t DAC1 = 16;
+const pin_t DAC2 = 13;
+
+const uint8_t LSBFIRST = 0;
+const uint8_t MSBFIRST = 1;
+
+#endif // PLATFORM_ID==3
 
 /* Exported functions --------------------------------------------------------*/
 
