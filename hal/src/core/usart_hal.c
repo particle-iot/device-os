ad49a1a4ec575dcd4f74e4f8bb4c6bca3f9d8beb
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-10-13 01:56:26 +0200

    fix copypasta in usart code. addresses issue #682

--- hal/src/core/usart_hal.c
+++ hal/src/core/usart_hal.c
@@ -117,7 +117,7 @@ void HAL_USART_Init(HAL_USART_Serial serial, Ring_Buffer *rx_buffer, Ring_Buffer
   usartMap[serial]->usart_tx_buffer = tx_buffer;
 
   memset(usartMap[serial]->usart_rx_buffer, 0, sizeof(Ring_Buffer));
-  memset(usartMap[serial]->usart_rx_buffer, 0, sizeof(Ring_Buffer));
+  memset(usartMap[serial]->usart_tx_buffer, 0, sizeof(Ring_Buffer));
 
   usartMap[serial]->usart_enabled = false;
   usartMap[serial]->usart_transmitting = false;
@@ -212,7 +212,7 @@ void HAL_USART_End(HAL_USART_Serial serial)
   GPIO_PinRemapConfig(usartMap[serial]->usart_pin_remap, DISABLE);
 
   memset(usartMap[serial]->usart_rx_buffer, 0, sizeof(Ring_Buffer));
-  memset(usartMap[serial]->usart_rx_buffer, 0, sizeof(Ring_Buffer));
+  memset(usartMap[serial]->usart_tx_buffer, 0, sizeof(Ring_Buffer));
 
   usartMap[serial]->usart_enabled = false;
   usartMap[serial]->usart_transmitting = false;
