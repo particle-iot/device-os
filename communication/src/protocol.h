d2f7134c94e446b70e1752d33a1da65d49a51dad
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-11-11 06:53:35 +0100

    protocol.h -> protocol.cpp

--- communication/src/protocol.h
+++ communication/src/protocol.h
@@ -167,189 +167,17 @@ protected:
 	 * Produces and transmits a describe message.
 	 * @param desc_flags Flags describing the information to provide. A combination of {@code DESCRIBE_APPLICATION) and {@code DESCRIBE_SYSTEM) flags.
 	 */
-	ProtocolError send_description(token_t token, message_id_t msg_id,
-			int desc_flags)
-	{
-		Message message;
-		channel.create(message);
-		uint8_t* buf = message.buf();
-		message.set_id(msg_id);
-		size_t desc = Messages::description(buf, msg_id, token);
-
-		BufferAppender appender(buf + desc, message.capacity());
-		appender.append("{");
-		bool has_content = false;
-
-		if (desc_flags && DESCRIBE_APPLICATION)
-		{
-			has_content = true;
-			appender.append("\"f\":[");
-
-			int num_keys = descriptor.num_functions();
-			int i;
-			for (i = 0; i < num_keys; ++i)
-			{
-				if (i)
-				{
-					appender.append(',');
-				}
-				appender.append('"');
-
-				const char* key = descriptor.get_function_key(i);
-				size_t function_name_length = strlen(key);
-				if (MAX_FUNCTION_KEY_LENGTH < function_name_length)
-				{
-					function_name_length = MAX_FUNCTION_KEY_LENGTH;
-				}
-				appender.append((const uint8_t*) key, function_name_length);
-				appender.append('"');
-			}
-
-			appender.append("],\"v\":{");
-
-			num_keys = descriptor.num_variables();
-			for (i = 0; i < num_keys; ++i)
-			{
-				if (i)
-				{
-					appender.append(',');
-				}
-				appender.append('"');
-				const char* key = descriptor.get_variable_key(i);
-				size_t variable_name_length = strlen(key);
-				SparkReturnType::Enum t = descriptor.variable_type(key);
-				if (MAX_VARIABLE_KEY_LENGTH < variable_name_length)
-				{
-					variable_name_length = MAX_VARIABLE_KEY_LENGTH;
-				}
-				appender.append((const uint8_t*) key, variable_name_length);
-				appender.append("\":");
-				appender.append('0' + (char) t);
-			}
-			appender.append('}');
-		}
-
-		if (descriptor.append_system_info && (desc_flags & DESCRIBE_SYSTEM))
-		{
-			if (has_content)
-				appender.append(',');
-			descriptor.append_system_info(append_instance, &appender, nullptr);
-		}
-		appender.append('}');
-		int msglen = appender.next() - (uint8_t *) buf;
-		message.set_length(msglen);
-		return channel.send(message);
-	}
+	ProtocolError send_description(token_t token, message_id_t msg_id, int desc_flags);
 
 	/**
 	 * Decodes and dispatches a received message to its handler.
 	 */
-	ProtocolError handle_received_message(Message& message,
-			CoAPMessageType::Enum& message_type)
-	{
-		last_message_millis = callbacks.millis();
-		pinger.message_received();
-		uint8_t* queue = message.buf();
-		message_type = Messages::decodeType(queue, message.length());
-		token_t token = queue[4];
-		message_id_t msg_id = CoAP::message_id(queue);
-		ProtocolError error = NO_ERROR;
-		DEBUG("message type %d", message_type);
-		switch (message_type)
-		{
-		case CoAPMessageType::DESCRIBE:
-			// TODO - this violates CoAP since we are sending two different achnowledgements
-			error = send_description(token, msg_id,
-					DESCRIBE_SYSTEM | DESCRIBE_APPLICATION);
-			//if (!error)
-			//		error = send_description(token, msg_id, DESCRIBE_APPLICATION);
-			break;
-
-		case CoAPMessageType::FUNCTION_CALL:
-			return functions.handle_function_call(token, message, channel,
-					descriptor.call_function);
-
-		case CoAPMessageType::VARIABLE_REQUEST:
-		{
-			char variable_key[13];
-			variables.decode_variable_request(variable_key, message);
-			return variables.handle_variable_request(variable_key, message,
-					channel, token, CoAP::message_id(queue),
-					descriptor.variable_type, descriptor.get_variable);
-		}
-		case CoAPMessageType::SAVE_BEGIN:
-			// fall through
-		case CoAPMessageType::UPDATE_BEGIN:
-			return chunkedTransfer.handle_update_begin(token, message, channel,
-					callbacks.prepare_for_firmware_update, callbacks.millis);
-
-		case CoAPMessageType::CHUNK:
-			return chunkedTransfer.handle_chunk(token, message, channel,
-					callbacks.save_firmware_chunk, callbacks.calculate_crc,
-					callbacks.finish_firmware_update, callbacks.millis);
-
-		case CoAPMessageType::UPDATE_DONE:
-			return chunkedTransfer.handle_update_done(token, message, channel,
-					callbacks.finish_firmware_update, callbacks.millis);
-
-		case CoAPMessageType::EVENT:
-			return subscriptions.handle_event(message,
-					descriptor.call_event_handler);
-
-		case CoAPMessageType::KEY_CHANGE:
-			// TODO
-			break;
-
-		case CoAPMessageType::SIGNAL_START:
-			message.set_length(
-					Messages::coded_ack(message.buf(), token,
-							ChunkReceivedCode::OK, queue[2], queue[3]));
-			callbacks.signal(true, 0, NULL);
-			return channel.send(message);
-
-		case CoAPMessageType::SIGNAL_STOP:
-			message.set_length(
-					Messages::coded_ack(message.buf(), token,
-							ChunkReceivedCode::OK, queue[2], queue[3]));
-			callbacks.signal(false, 0, NULL);
-			return channel.send(message);
-
-		case CoAPMessageType::HELLO:
-			descriptor.ota_upgrade_status_sent();
-			break;
-
-		case CoAPMessageType::TIME:
-			handle_time_response(
-					queue[6] << 24 | queue[7] << 16 | queue[8] << 8 | queue[9]);
-			break;
-
-		case CoAPMessageType::PING:
-			message.set_length(
-					Messages::empty_ack(message.buf(), queue[2], queue[3]));
-			error = channel.send(message);
-			break;
-
-		case CoAPMessageType::EMPTY_ACK:
-		case CoAPMessageType::ERROR:
-		default:
-			; // drop it on the floor
-		}
-
-		// all's well
-		return error;
-	}
+	ProtocolError handle_received_message(Message& message, CoAPMessageType::Enum& message_type);
 
 	/**
 	 * Handles the time delivered from the cloud.
 	 */
-	void handle_time_response(uint32_t time)
-	{
-		// deduct latency
-		//uint32_t latency = last_chunk_millis ? (callbacks.millis()-last_chunk_millis)/2000 : 0;
-		//last_chunk_millis = 0;
-		// todo - compute connection latency
-		callbacks.set_time(time, 0, NULL);
-	}
+	void handle_time_response(uint32_t time);
 
 	/**
 	 * Copy an initialize a block of memory from a source to a target, where the source may be smaller than the target.
@@ -360,26 +188,9 @@ protected:
 	 * @param source			The source structure
 	 * @param source_size	The size of the source structure in bytes
 	 */
-	static void copy_and_init(void* target, size_t target_size,
-			const void* source, size_t source_size)
-	{
-		memcpy(target, source, source_size);
-		memset(((uint8_t*) target) + source_size, 0, target_size - source_size);
-	}
+	static void copy_and_init(void* target, size_t target_size, const void* source, size_t source_size);
 
-	void init(const SparkCallbacks &callbacks,
-			const SparkDescriptor &descriptor)
-	{
-		memset(&handlers, 0, sizeof(handlers));
-		// the actual instances referenced may be smaller if the caller is compiled
-		// against an older version of this library.
-		copy_and_init(&this->callbacks, sizeof(this->callbacks), &callbacks,
-				callbacks.size);
-		copy_and_init(&this->descriptor, sizeof(this->descriptor), &descriptor,
-				descriptor.size);
-
-		initialized = true;
-	}
+	void init(const SparkCallbacks &callbacks, const SparkDescriptor &descriptor);
 
 public:
 	Protocol(MessageChannel& channel) :
