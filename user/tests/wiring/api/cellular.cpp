9e887c12ae310e76b24b39dcc4fbfdc2871dfb63
Technobly <technobly@gmail.com>
2015-11-11 13:23:53 -0600

    added Cellular Wiring API tests

---
+++ user/tests/wiring/api/cellular.cpp
@@ -0,0 +1,34 @@
+/**
+ ******************************************************************************
+  Copyright (c) 2015 Particle Industries, Inc.  All rights reserved.
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, either
+  version 3 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************
+ */
+
+#include "testapi.h"
+
+#if PLATFORM_ID==10
+
+test(api_cellular_rssi) {
+    CellularSignal sig;
+
+    API_COMPILE(Cellular.RSSI());
+
+    API_COMPILE(sig = Cellular.RSSI());
+
+    API_COMPILE(Serial.println(sig));
+}
+
+#endif
