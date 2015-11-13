b09091647196378b940be3c01a5148da232cdfad
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-11-13 16:18:20 +0100

    fix for virtual device pinning cpu. (HAL delay functions were not being used, so delay() was busy waiting.)  Requires rebuilding boost via ci/build_boost.sh and a clean build of gcc main.

--- hal/src/gcc/delay_hal.cpp
+++ hal/src/gcc/delay_hal.cpp
@@ -17,16 +17,18 @@
  */
 
 #include "delay_hal.h"
-#inlcude "timer_hal.h"
+#include "timer_hal.h"
 
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread/thread.hpp>
 
-void HAL_Delay_Milliseconds(uint32_t millis) {
+void HAL_Delay_Milliseconds(uint32_t millis)
+{
     boost::this_thread::sleep(boost::posix_time::milliseconds(millis));
 }
 
-void HAL_Delay_Microseconds(uint32_t micros) {
+void HAL_Delay_Microseconds(uint32_t micros)
+{
     boost::this_thread::sleep(boost::posix_time::milliseconds(1));
 }
 
