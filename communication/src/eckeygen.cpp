0084162b46ad5e2d2db5a438c4fed2300a14bedf
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-12-02 19:22:17 +0100

    ec key generation tweaks

--- communication/src/eckeygen.cpp
+++ communication/src/eckeygen.cpp
@@ -7,17 +7,24 @@
 #if HAL_PLATFORM_CLOUD_UDP
 #include "mbedtls/pk.h"
 #include "mbedtls/asn1.h"
+#include <string.h>
 
 int gen_ec_key(uint8_t* buffer, size_t max_length, int (*f_rng) (void *, uint8_t* buf, size_t len), void *p_rng)
 {
 	mbedtls_pk_context key;
+	memset(&key, 0, sizeof(key));
 	int error = mbedtls_pk_setup(&key, mbedtls_pk_info_from_type(MBEDTLS_PK_ECKEY));
 	if (!error)
-		error = mbedtls_ecp_gen_key(MBEDTLS_ECP_DP_SECP256K1, mbedtls_pk_ec(key), f_rng, p_rng );
+		error = mbedtls_ecp_gen_key(MBEDTLS_ECP_DP_SECP256R1, mbedtls_pk_ec(key), f_rng, p_rng );
 	if (!error) {
 		int result = mbedtls_pk_write_key_der(&key, buffer, max_length);
 		if (result<0)
 			error = result;
+		else if (result>0)
+		{
+			// the key is written to the end of the buffer - align to the start
+			memmove(buffer, buffer+max_length-result, result);
+		}
 	}
 	mbedtls_pk_free(&key);
 	return error;
