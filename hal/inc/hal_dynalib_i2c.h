81b77df0af9aeb2f54b92e872ce104cce4dcda71
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-12-17 06:08:37 +0100

    the i2c v1 functions are only needed as placeholders on the photon/p1 - other platforms do not need these.

--- hal/inc/hal_dynalib_i2c.h
+++ hal/inc/hal_dynalib_i2c.h
@@ -38,6 +38,10 @@
 // GNINRAW
 
 DYNALIB_BEGIN(hal_i2c)
+
+// first edition of these functions that were released on the Photon/P1
+// They are not needed on other platforms.
+#if PLATFORM_ID==6 || PLATFORM_ID==8
 DYNALIB_FN(hal_i2c,HAL_I2C_Set_Speed_v1)
 DYNALIB_FN(hal_i2c,HAL_I2C_Enable_DMA_Mode_v1)
 DYNALIB_FN(hal_i2c,HAL_I2C_Stretch_Clock_v1)
@@ -54,6 +58,7 @@ DYNALIB_FN(hal_i2c,HAL_I2C_Flush_Data_v1)
 DYNALIB_FN(hal_i2c,HAL_I2C_Is_Enabled_v1)
 DYNALIB_FN(hal_i2c,HAL_I2C_Set_Callback_On_Receive_v1)
 DYNALIB_FN(hal_i2c,HAL_I2C_Set_Callback_On_Request_v1)
+#endif
 
 DYNALIB_FN(hal_i2c,HAL_I2C_Set_Speed)
 DYNALIB_FN(hal_i2c,HAL_I2C_Enable_DMA_Mode)
