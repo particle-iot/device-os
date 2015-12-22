bcc9a070cb3a3c4cee150971a5c27b104d146d2a
JT Zemp <jtzemp@gmail.com>
2015-12-22 14:47:27 -0700

    WIP tone() test for electron.

---
+++ user/tests/integration/tone/firmware/application.cpp
@@ -0,0 +1,36 @@
+#include "application.h"
+
+SerialDebugOutput debugOutput(9600, ALL_LEVEL);
+
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+int speakerPin = B1;
+
+// notes in the melody:
+int melody[] = {1908,2551,2551,2273,2551,0,2024,1908}; //C4,G3,G3,A3,G3,0,B3,C4
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {4,8,8,4,4,4,4,4 };
+
+void setup() {
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(speakerPin, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(speakerPin);
+  }
+}
+
+void loop() {
+
+}
