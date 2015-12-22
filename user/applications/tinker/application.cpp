4e0ad5b446dfe24fcdd141643278ac86f117754c
Technobly <technobly@gmail.com>
2015-12-22 15:17:59 -0600

    it's a bird, it's a plane

--- user/applications/tinker/application.cpp
+++ user/applications/tinker/application.cpp
@@ -2,18 +2,18 @@
  ******************************************************************************
   Copyright (c) 2015 Particle Industries, Inc.  All rights reserved.
 
-  This library is free software; you can redistribute it and/or
+  This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation, either
   version 3 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
+  This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
 
   You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, see <http://www.gnu.org/licenses/>.
+  License along with this program; if not, see <http://www.gnu.org/licenses/>.
   ******************************************************************************
  */
 
@@ -204,7 +204,7 @@ int tinkerAnalogWrite(String command)
     int pinNumber = command.charAt(1) - '0';
     //Sanity check to see if the pin numbers are within limits
 
-    if (pinNumber< 0 || pinNumber >7) return -1;
+    if (pinNumber < 0 || pinNumber > 7) return -1;
 
     if(command.startsWith("D"))
     {
