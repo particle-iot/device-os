78483d884ff4b62a47ec21652fcb8e2371721f31
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-01-08 19:22:50 +0100

    moves network notification functions to more generic places since they are not WLAN-specific

--- hal/inc/net_hal.h
+++ hal/inc/net_hal.h
@@ -8,6 +8,21 @@ extern "C" {
 
 uint32_t HAL_WLAN_SetNetWatchDog(uint32_t timeOutInuS);
 
+/**
+ * Notification that the wifi network has been connected to.
+ */
+void HAL_WLAN_notify_connected();
+void HAL_WLAN_notify_disconnected();
+
+/**
+ * Notification that an IP address has been received via DHCP.
+ * todo - what with the case of static IP config?
+ */
+void HAL_WLAN_notify_dhcp(bool dhcp);
+
+void HAL_WLAN_notify_can_shutdown();
+
+
 #ifdef __cplusplus
 }
 #endif
