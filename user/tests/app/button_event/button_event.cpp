82c692377cf5d84c91de0191a0fb1a464c0d13ad
Sergey Polyakov <sergeuz@gmail.com>
2016-01-20 20:49:31 +0200

    Increased test timeout and added LED indication

--- user/tests/app/button_event/button_event.cpp
+++ user/tests/app/button_event/button_event.cpp
@@ -12,7 +12,7 @@ struct Event {
     }
 };
 
-const uint32_t testTimeout = 5000;
+const uint32_t testTimeout = 10000;
 
 std::deque<Event> eventQueue;
 uint32_t testStarted = 0;
@@ -24,7 +24,7 @@ uint32_t eventData = 0;
                 Particle.process(); \
             } \
             if (eventQueue.empty() || eventQueue.front().type != (event)) { \
-                return 1; \
+                return stopTest(false); \
             } \
             eventData = eventQueue.front().data; \
             eventQueue.pop_front(); \
@@ -34,50 +34,57 @@ void eventHandler(system_event_t event, uint32_t data, void*) {
     eventQueue.push_back(Event(event, data));
 }
 
-void resetTest() {
+void startTest() {
+    digitalWrite(D7, HIGH); // User can now start clicking SETUP button
     eventQueue = std::deque<Event>();
     testStarted = millis();
 }
 
+int stopTest(bool ok) {
+    digitalWrite(D7, LOW);
+    return ok ? 0 : 1;
+}
+
 int testClicks(String arg) {
-    resetTest();
+    startTest();
     const int expectedClicks = arg.toInt();
     if (expectedClicks <= 0) {
-        return 1;
+        return stopTest(false);
     }
     int clicks = 0;
     do {
         // Waiting for button press event
         WAIT_EVENT(button_status);
         if (eventData != 0) {
-            return 1;
+            return stopTest(false);
         }
         // Waiting for button release event
         WAIT_EVENT(button_status);
         if (eventData == 0 || eventData > 1000) {
-            return 1;
+            return stopTest(false);
         }
         // Waiting for button click event
         WAIT_EVENT(button_click);
         if ((int)eventData != clicks + 1) {
-            return 1;
+            return stopTest(false);
         }
         ++clicks;
         if (clicks == expectedClicks) {
             // Waiting for final click event
             WAIT_EVENT(button_final_click);
             if ((int)eventData != clicks) {
-                return 1;
+                return stopTest(false);
             }
         }
     } while (clicks != expectedClicks && millis() - testStarted < testTimeout);
     if (clicks != expectedClicks) {
-        return 1;
+        return stopTest(false);
     }
-    return 0;
+    return stopTest(true);
 }
 
 void setup() {
+    pinMode(D7, OUTPUT);
     System.on(button_status | button_click | button_final_click, eventHandler);
     Particle.function("testClicks", testClicks);
 }
