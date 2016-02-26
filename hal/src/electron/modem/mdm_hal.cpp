bb68d80b1b4137cf60e0e991b48f1c75d247fb21
Technobly <technobly@gmail.com>
2016-02-26 00:40:24 -0600

    implements AT pass through with Tinker v0.4.8-rc.6
    AT commands can be sent from USB serial at 9600 baud
    AT command responses are also received over USB serial
    Full system logging is output on TX pin at 115200 baud
    default operation is machine to machine over USB serial (echo and assist off)
    for more human readable AT command interaction, use these special commands:
    :echo1; or :echo0; turns on/off command echoing
    :assist1; or :assist0; turns on/off ESCAPE and BACKSPACE keyboard assistance (try it)
    
     Conflicts:
    	hal/inc/hal_dynalib_cellular.h
    	hal/src/electron/cellular_hal.cpp
    	hal/src/electron/modem/mdm_hal.h

--- hal/src/electron/modem/mdm_hal.cpp
+++ hal/src/electron/modem/mdm_hal.cpp
@@ -224,6 +224,18 @@ void MDMParser::SMSreceived(int index) {
     sms_cb(sms_data, index); // call the SMS callback with the index of the new SMS
 }
 
+void MDMParser::setATresponseHandler(_CELLULAR_LOGGER_CB cb, void* data) {
+    logger_cb = cb;
+    logger_data = data;
+}
+
+void MDMParser::logATcommandResponse(const char* buf, int len) {
+    char response[MAX_SIZE + 64];
+    strncpy(response, buf, len);
+    response[len] = '\0';
+    logger_cb(logger_data, response);
+}
+
 int MDMParser::send(const char* buf, int len)
 {
 #ifdef MDM_DEBUG
@@ -301,6 +313,7 @@ int MDMParser::waitFinalResp(_CALLBACKPTR cb /* = NULL*/,
                                                         "..." ;
             DEBUG_D("%10.3f AT read %s", (HAL_Timer_Get_Milli_Seconds()-_debugTime)*0.001, s);
             dumpAtCmd(buf, len);
+            if (logger_cb) logATcommandResponse(buf, len);
             (void)s;
         }
 #endif
