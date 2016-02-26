ad6ec40764f2d729254a9d3928c067995351ea6d
jixing.zou <jixing.zou@redbear.hk>
2016-02-26 14:51:42 +0800

    Duo : Ajust functions of spark_wiring_btstack.

--- wiring/src/spark_wiring_btstack.cpp
+++ wiring/src/spark_wiring_btstack.cpp
@@ -6,7 +6,7 @@
 
 void BLEDevice::init(void)
 {
-	hal_btstack_init();
+    hal_btstack_init();
 }
 
 void BLEDevice::deInit(void)
@@ -18,29 +18,29 @@ void BLEDevice::deInit(void)
 /**
  * @brief Set timer.
  */
-void BLEDevice::setTimer(hal_timer_source_t *ts, uint32_t timeout_in_ms)
+void BLEDevice::setTimer(btstack_timer_source_t *ts, uint32_t timeout_in_ms)
 {
-	hal_btstack_setTimer(ts, timeout_in_ms);
+    hal_btstack_setTimer(ts, timeout_in_ms);
 }
 
 /**
  * @brief Set timer handler..
  */
-void BLEDevice::setTimerHandler(hal_timer_source_t *ts, void (*process)(void *_ts))
+void BLEDevice::setTimerHandler(btstack_timer_source_t *ts, void (*process)(btstack_timer_source_t *_ts))
 {
-	hal_btstack_setTimerHandler(ts, process);
+    hal_btstack_setTimerHandler(ts, process);
 }
 
 /**
  * @brief Add/Remove timer task to run loop.
  */
-void BLEDevice::addTimerToLoop(hal_timer_source_t *timer)
+void BLEDevice::addTimerToLoop(btstack_timer_source_t *timer)
 {
-	hal_btstack_addTimer(timer);
+    hal_btstack_addTimer(timer);
 }
-int BLEDevice::removeTimerFromLoop(hal_timer_source_t *timer)
+int BLEDevice::removeTimerFromLoop(btstack_timer_source_t *timer)
 {
-	return hal_btstack_removeTimer(timer);
+    return hal_btstack_removeTimer(timer);
 }
 
 /**
@@ -48,7 +48,7 @@ int BLEDevice::removeTimerFromLoop(hal_timer_source_t *timer)
  */
 uint32_t BLEDevice::getTimeMs(void)
 {
-	return hal_btstack_getTimeMs();
+    return hal_btstack_getTimeMs();
 }
 
 /**
@@ -58,96 +58,101 @@ uint32_t BLEDevice::getTimeMs(void)
  */
 void BLEDevice::debugLogger(bool flag)
 {
-	hal_btstack_debugLogger((uint8_t)flag);
+    hal_btstack_debugLogger((uint8_t)flag);
 }
 void BLEDevice::debugError(bool flag)
 {
-	hal_btstack_debugError(flag);
+    hal_btstack_debugError(flag);
 }
 void BLEDevice::enablePacketLogger(void)
 {
-	hal_btstack_enablePacketLogger();
+    hal_btstack_enablePacketLogger();
 }
 
 /**
  * @brief Get the address information in advertisement.
  */
-void BLEDevice::getAdvertisementAddr(uint8_t *addr_type, addr_t addr)
+void BLEDevice::getAdvertisementAddr(uint8_t *addr_type, bd_addr_t addr)
 {
-	hal_btstack_getAdvertisementAddr(addr_type, addr);
+    hal_btstack_getAdvertisementAddr(addr_type, addr);
 }
 
 /**
  * @brief Set random mode.
  */
-void BLEDevice::setRandomAddrMode(uint8_t random_addr_type)
+void BLEDevice::setRandomAddrMode(gap_random_address_type_t random_addr_type)
 {
-	hal_btstack_setRandomAddressMode(random_addr_type);
+    hal_btstack_setRandomAddressMode(random_addr_type);
 }
 
-void BLEDevice::setRandomAddr(addr_t addr)
+void BLEDevice::setRandomAddr(bd_addr_t addr)
 {
-	hal_btstack_setRandomAddr(addr);
+    hal_btstack_setRandomAddr(addr);
 }
 
-void BLEDevice::setPublicBDAddr(addr_t addr)
+void BLEDevice::setPublicBDAddr(bd_addr_t addr)
 {
-	hal_btstack_setPublicBdAddr(addr);
+    hal_btstack_setPublicBdAddr(addr);
 }
 
 void BLEDevice::setLocalName(const char *local_name)
 {
-	hal_btstack_setLocalName(local_name);
+    hal_btstack_setLocalName(local_name);
 }
 
 void BLEDevice::setAdvParams(advParams_t *adv_params)
 {
-	hal_btstack_setAdvParams(adv_params->adv_int_min, adv_params->adv_int_max, \
-			                 adv_params->adv_type, \
-							 adv_params->dir_addr_type, adv_params->dir_addr, \
-							 adv_params->channel_map, \
-							 adv_params->filter_policy);
+    hal_btstack_setAdvParams(adv_params->adv_int_min, adv_params->adv_int_max, \
+                             adv_params->adv_type, \
+                             adv_params->dir_addr_type, adv_params->dir_addr, \
+                             adv_params->channel_map, \
+                             adv_params->filter_policy);
 }
 
 void BLEDevice::setAdvData(uint16_t size, uint8_t *data)
 {
-	hal_btstack_setAdvData(size, data);
+    hal_btstack_setAdvData(size, data);
 }
 
 void BLEDevice::onConnectedCallback(void (*callback)(BLEStatus_t status, uint16_t handle))
 {
-	hal_btstack_setConnectedCallback(callback);
+    hal_btstack_setConnectedCallback(callback);
 }
 
 void BLEDevice::onDisconnectedCallback(void (*callback)(uint16_t handle))
 {
-	hal_btstack_setDisconnectedCallback(callback);
+    hal_btstack_setDisconnectedCallback(callback);
 }
 
 void BLEDevice::startAdvertising(void)
 {
-	hal_btstack_startAdvertising();
+    hal_btstack_startAdvertising();
 }
 
 void BLEDevice::stopAdvertising(void)
 {
-	hal_btstack_stopAdvertising();
+    hal_btstack_stopAdvertising();
 }
 
 
 void BLEDevice::disconect(uint16_t conn_handle)
 {
-	hal_btstack_disconnect(conn_handle);
+    hal_btstack_disconnect(conn_handle);
+}
+
+uint8_t BLEDevice::connect(bd_addr_t addr, bd_addr_type_t type)
+{
+    return hal_btstack_connect(addr, type);
 }
 
 void BLEDevice::addService(uint16_t uuid)
 {
-	hal_btstack_addServiceUUID16bits(uuid);
+    hal_btstack_addServiceUUID16bits(uuid);
 }
 
 void BLEDevice::addService(uint8_t *uuid)
 {
-	hal_btstack_addServiceUUID128bits(uuid);
+    hal_btstack_addServiceUUID128bits(uuid);
 }
 
 /**
@@ -157,61 +162,61 @@ void BLEDevice::addService(uint8_t *uuid)
  */
 uint16_t BLEDevice::addCharacteristic(uint16_t uuid, uint16_t flags, uint8_t *data, uint16_t data_len)
 {
-	return hal_btstack_addCharsUUID16bits(uuid, flags, data, data_len);
+    return hal_btstack_addCharsUUID16bits(uuid, flags, data, data_len);
 }
 uint16_t BLEDevice::addCharacteristic(uint8_t *uuid, uint16_t flags, uint8_t *data, uint16_t data_len)
 {
-	return hal_btstack_addCharsUUID128bits(uuid, flags, data, data_len);
+    return hal_btstack_addCharsUUID128bits(uuid, flags, data, data_len);
 }
 
 uint16_t BLEDevice::addCharacteristicDynamic(uint16_t uuid, uint16_t flags, uint8_t *data, uint16_t data_len)
 {
-	return hal_btstack_addCharsDynamicUUID16bits(uuid, flags, data, data_len);
+    return hal_btstack_addCharsDynamicUUID16bits(uuid, flags, data, data_len);
 }
 
 uint16_t BLEDevice::addCharacteristicDynamic(uint8_t *uuid, uint16_t flags, uint8_t *data, uint16_t data_len)
 {
-	return hal_btstack_addCharsDynamicUUID128bits(uuid, flags, data, data_len);
+    return hal_btstack_addCharsDynamicUUID128bits(uuid, flags, data, data_len);
 }
 
 void BLEDevice::onDataReadCallback(uint16_t (*cb)(uint16_t handle, uint8_t *buffer, uint16_t buffer_size))
 {
-	hal_btstack_setGattCharsRead(cb);
+    hal_btstack_setGattCharsRead(cb);
 }
 
 void BLEDevice::onDataWriteCallback(int (*cb)(uint16_t handle, uint8_t *buffer, uint16_t buffer_size))
 {
-	hal_btstack_setGattCharsWrite(cb);
+    hal_btstack_setGattCharsWrite(cb);
 }
 
 int BLEDevice::attServerCanSendPacket(void)
 {
-	return hal_btstack_attServerCanSend();
+    return hal_btstack_attServerCanSend();
 }
 
 int BLEDevice::sendNotify(uint16_t value_handle, uint8_t *value, uint16_t length)
 {
-	return hal_btstack_attServerSendNotify(value_handle, value, length);
+    return hal_btstack_attServerSendNotify(value_handle, value, length);
 }
 
 int BLEDevice::sendIndicate(uint16_t value_handle, uint8_t *value, uint16_t length)
 {
-	return hal_btstack_attServerSendIndicate(value_handle, value, length);
+    return hal_btstack_attServerSendIndicate(value_handle, value, length);
 }
 
 void BLEDevice::startScanning(void)
 {
-	hal_btstack_startScanning();
+    hal_btstack_startScanning();
 }
 
 void BLEDevice::stopScanning(void)
 {
-	hal_btstack_stopScanning();
+    hal_btstack_stopScanning();
 }
 
 void BLEDevice::onScanReportCallback(void (*cb)(advertisementReport_t *advertisement_report))
 {
-	hal_btstack_setBLEAdvertisementCallback(cb);
+    hal_btstack_setBLEAdvertisementCallback(cb);
 }
 
 
