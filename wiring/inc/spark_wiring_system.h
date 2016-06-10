pwallington/firmware

Store last reset reason and publish it to the cloud

wiring/inc/spark_wiring_system.h
@@ -254,6 +254,13 @@ public:
     }
 
 
+    inline int resetReason()
+    {
+        int reason = RESET_REASON_NONE;
+        HAL_Core_Get_System_Reset_Info(&reason, nullptr, nullptr);
+        return reason;
+    }
+
 private:
 
     inline uint8_t get_flag(system_flag_t flag)

particle-iot/device-os

wiring/inc/spark_wiring_system.h
@@ -262,6 +262,13 @@ public:
     }
 
 
+    inline int resetReason()
+    {
+        int reason = RESET_REASON_NONE;
+        HAL_Core_Get_Last_Reset_Info(&reason, nullptr, nullptr);
+        return reason;
+    }
+
 private:
 
     inline uint8_t get_flag(system_flag_t flag)