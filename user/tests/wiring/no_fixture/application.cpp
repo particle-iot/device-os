2705c1aa35687e887b34e40c86b7a53525cd4746
Technobly <technobly@gmail.com>
2016-08-02 21:37:38 -0500

    needs old logging api

--- user/tests/wiring/no_fixture/application.cpp
+++ user/tests/wiring/no_fixture/application.cpp
@@ -1,11 +1,6 @@
 #include "application.h"
 #include "unit-test/unit-test.h"
 
-// make clean all TEST=wiring/no_fixture PLATFORM=electron -s COMPILE_LTO=n program-dfu DEBUG_BUILD=y
-//
-// Serial1LogHandler log(LOG_LEVEL_ALL, {
-//     { "comm", LOG_LEVEL_NONE }, // filter out comm messages
-//     { "system", LOG_LEVEL_INFO } // only info level for system messages
-// });
+// Serial1DebugOutput debugOutput(9600, ALL_LEVEL);
 
 UNIT_TEST_APP();
