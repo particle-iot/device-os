8870980a4bfcaae30823cff27a0cee85ab52d379
Andrey Tolstoy <avtolstoy@gmail.com>
2016-09-07 00:52:26 +0700

    USBKeyboard rework

--- wiring/inc/spark_wiring_usbkeyboard.h
+++ wiring/inc/spark_wiring_usbkeyboard.h
@@ -30,45 +30,7 @@
 
 #ifdef SPARK_USB_KEYBOARD
 #include "spark_wiring.h"
-
-#define KEY_LEFT_CTRL		0x80
-#define KEY_LEFT_SHIFT		0x81
-#define KEY_LEFT_ALT		0x82
-#define KEY_LEFT_GUI		0x83
-#define KEY_RIGHT_CTRL		0x84
-#define KEY_RIGHT_SHIFT		0x85
-#define KEY_RIGHT_ALT		0x86
-#define KEY_RIGHT_GUI		0x87
-
-#define KEY_UP_ARROW		0xDA
-#define KEY_DOWN_ARROW		0xD9
-#define KEY_LEFT_ARROW		0xD8
-#define KEY_RIGHT_ARROW		0xD7
-#define KEY_BACKSPACE		0xB2
-#define KEY_TAB				0xB3
-#define KEY_RETURN			0xB0
-#define KEY_ESC				0xB1
-#define KEY_INSERT			0xD1
-#define KEY_DELETE			0xD4
-#define KEY_PAGE_UP			0xD3
-#define KEY_PAGE_DOWN		0xD6
-#define KEY_HOME			0xD2
-#define KEY_END				0xD5
-#define KEY_CAPS_LOCK		0xC1
-#define KEY_F1				0xC2
-#define KEY_F2				0xC3
-#define KEY_F3				0xC4
-#define KEY_F4				0xC5
-#define KEY_F5				0xC6
-#define KEY_F6				0xC7
-#define KEY_F7				0xC8
-#define KEY_F8				0xC9
-#define KEY_F9				0xCA
-#define KEY_F10				0xCB
-#define KEY_F11				0xCC
-#define KEY_F12				0xCD
-
-#define KEY_RAW       0x88
+#include "spark_wiring_usbkeyboard_scancode.h"
 
 typedef struct
 {
@@ -88,10 +50,11 @@ public:
 
 	void begin(void);
 	void end(void);
-  virtual size_t write(uint8_t k);
-	virtual size_t writeRaw(uint16_t k);
-	virtual size_t press(uint16_t k);
-	virtual size_t release(uint16_t k);
+  virtual size_t write(uint8_t ch);
+	virtual size_t writeKey(uint16_t k, uint16_t modifiers = 0);
+  virtual size_t click(uint16_t k, uint16_t modifiers = 0);
+	virtual size_t press(uint16_t k, uint16_t modifiers = 0);
+	virtual size_t release(uint16_t k, uint16_t modifiers = 0);
 	virtual void releaseAll(void);
 
 private:
