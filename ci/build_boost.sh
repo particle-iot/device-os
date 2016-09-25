fea9ded0fe51eba88b3cdf35e0fac8f6b03ebc3f
Julien Vanier <jvanier@gmail.com>
2016-09-24 23:00:45 -0400

    Revert changes to CI scripts to fix the build

--- ci/build_boost.sh
+++ ci/build_boost.sh
@@ -1,8 +1,6 @@
 # Load config
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export BOOST_BUILD_USER_CONFIG=$DIR/user-config.jam
 
 pushd $BOOST_ROOT
 ./bootstrap.sh
-./b2  --debug-configuration --with-thread  --with-system --with-program_options --with-random  --threading=single toolset=gcc-arm
+./b2  --with-thread  --with-system --with-program_options --with-random  --threading=single
 popd
