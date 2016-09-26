5af77e3a34368e659429ad7ad7f8e3f7bf5d0cb8
Julien Vanier <jvanier@gmail.com>
2016-09-26 12:48:29 -0400

    Revert changes to CI scripts to fix the build

--- ci/build_boost.sh
+++ ci/build_boost.sh
@@ -1,14 +1,6 @@
 # Load config
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export BOOST_BUILD_USER_CONFIG=$DIR/user-config.jam
 
 pushd $BOOST_ROOT
 ./bootstrap.sh
-
-# For desktop
-# ./b2  --with-thread  --with-system --with-program_options --with-random --with-regex --threading=single
-
-# For Raspberry Pi
-./b2  --with-thread  --with-system --with-program_options --with-random  --threading=single toolset=gcc-arm
-
+./b2  --with-thread  --with-system --with-program_options --with-random  --threading=single
 popd
