113322385ca455881a03c41a90a6a7a8f7a0ac22
Julien Vanier <jvanier@gmail.com>
2016-09-26 12:48:29 -0400

    Revert changes to CI scripts to fix the build

--- ci/install_boost.sh
+++ ci/install_boost.sh
@@ -1,6 +1,7 @@
-BOOST_VERSION=1_61_0_arm
+BOOST_VERSION=1_59_0
+BOOST_VERSION_DOT=1.59.0
 
-test -f boost_$BOOST_VERSION.tar.gz || wget --quiet http://downloads.sourceforge.net/project/boost/boost/1.61.0/boost_$BOOST_VERSION.tar.gz 
+test -f boost_$BOOST_VERSION.tar.gz || wget --quiet http://downloads.sourceforge.net/project/boost/boost/$BOOST_VERSION_DOT/boost_$BOOST_VERSION.tar.gz 
 export BOOST_HOME=$HOME/.ci/boost
 export BOOST_ROOT=$BOOST_HOME/boost_$BOOST_VERSION
 mkdir -p $BOOST_HOME
