4e70546e58651fad6de72de6018c6a5720cb1d53
Technobly <technobly@gmail.com>
2016-11-20 17:08:32 -0600

    add more wiring/threading tests, make more reliable for cellular

--- user/tests/wiring/threading/listening.cpp
+++ user/tests/wiring/threading/listening.cpp
@@ -1,9 +1,21 @@
 #include "application.h"
 #include "unit-test/unit-test.h"
+
+#if PLATFORM_THREADING
 #include "system_threading.h"
 #include <functional>
 
 volatile bool listening_test_flag;
+
+void network_power_cycle() {
+#if Wiring_WiFi
+    WiFi.off();
+    WiFi.on();
+#elif Wiring_Cellular
+    Cellular.off();
+    Cellular.on();
+#endif
+}
 
 void network_set_listen_timeout(uint16_t timeout) {
 #if Wiring_WiFi
@@ -50,6 +62,14 @@ void blocking_call() {
 }
 
 void listening_start_with_delay() {
+    waitFor(Particle.connected, 30000);
+    if (!Particle.connected()) {
+        Particle.disconnect();
+        network_power_cycle();
+        Particle.connect();
+        waitFor(Particle.connected, 100000);
+    }
+    assertTrue(Particle.connected()); // make sure we are not stuck trying to handshake before trying to enter listening mode
     listening_test_flag = false;
     network_listen(true);
     delay(1000); // Time for the system thread to enter listening mode
@@ -65,7 +85,7 @@ void listening_set_test_flag() {
 }
 
 // Following test ensures that handler function is invoked for SystemEvents::wifi_listen_update events
-int listening_update_time;
+volatile int listening_update_time;
 
 void listening_update_handler(system_event_t, int time, void*) {
     listening_update_time = time;
@@ -127,22 +147,24 @@ test(04_THREADING_listening_update_event_is_sent) {
     system_unsubscribe_event(all_events, nullptr, nullptr);
 
     listening_update_time = 0;
-	System.on(wifi_listen_update, listening_update_handler); // Register handler
+    System.on(wifi_listen_update, listening_update_handler); // Register handler
     listening_start_with_delay();
+    assertTrue(network_listening());
 
     uint32_t start = millis();
     while (!listening_update_time && ((millis() - start) < 4000)) {
-    		Particle.process();		// pump application events
+        Particle.process();   // pump application events
     }
     listening_stop_with_delay();
     System.off(listening_update_handler); // Unregister handler
 
-    assertMore(listening_update_time, 0);
+    assertMore((int)listening_update_time, 0);
 }
 
 // This test ensures the RTOS time slices between threads when the system is in listening mode
 test(05_THREADING_listening_application_thread_is_running) {
     listening_start_with_delay();
+    assertTrue(network_listening());
 
     uint32_t start = millis();
     while (millis() - start < 1000); // Busy wait
@@ -156,6 +178,7 @@ test(05_THREADING_listening_application_thread_is_running) {
 // Checks whether listening loop performs processing of the system thread's queued events
 test(06_THREADING_listening_loop_is_processing_system_events) {
     listening_start_with_delay();
+    assertTrue(network_listening());
 
     ActiveObjectBase *system = (ActiveObjectBase*)system_internal(1, nullptr); // Returns system thread instance
     system->invoke_async(std::function<void()>(listening_set_test_flag));
@@ -170,13 +193,14 @@ test(06_THREADING_listening_loop_is_processing_system_events) {
 
 test(07_THREADING_app_can_exit_listen_mode_761)
 {
-	// this isn't a complete test - we can't tell if the system thread
-	// is still stuck in the listening loop
-	listening_start_with_delay();
-	network_listen(false);
-	Particle.process();
-	delay(10);	// todo - find a better way to interact with the system thread
-	assertFalse(network_listening());
+    // this isn't a complete test - we can't tell if the system thread
+    // is still stuck in the listening loop
+    listening_start_with_delay();
+    assertTrue(network_listening());
+    network_listen(false);
+    Particle.process();
+    delay(10);	// todo - find a better way to interact with the system thread
+    assertFalse(network_listening());
 }
 
 test(08_THREADING_app_can_exit_listen_mode_via_timeout)
@@ -186,7 +210,7 @@ test(08_THREADING_app_can_exit_listen_mode_via_timeout)
     assertTrue(network_listening());
     assertEqual(network_get_listen_timeout(),3);
     Particle.process();
-    delay(4000);  // todo - find a better way to interact with the system thread
+    delay(5000);  // todo - find a better way to interact with the system thread
     assertFalse(network_listening());
 }
 
@@ -199,31 +223,53 @@ test(09_THREADING_app_can_cancel_listen_mode_timeout)
     network_set_listen_timeout(0);
     assertEqual(network_get_listen_timeout(),0);
     Particle.process();
-    delay(4000);  // todo - find a better way to interact with the system thread
+    delay(5000);  // todo - find a better way to interact with the system thread
     assertTrue(network_listening()); // still in listening mode
 
     listening_stop_with_delay(); // force exit listening mode
     assertFalse(network_listening());
 }
 
-test(10_THREADING_app_can_invoke_synchronous_fn_in_listening_mode)
+test(10_THREADING_app_can_extend_listen_mode_timeout)
 {
-	network_listen(true);
-	delay(10);		// time for the system thread to enter listening mode
-
-	// this is a blocking call that requires the system thread to
-	blocking_call();
-
-	network_listen(false);
+    network_set_listen_timeout(3);
+    listening_start_with_delay();
+    assertTrue(network_listening());
+    assertEqual(network_get_listen_timeout(),3);
+    network_set_listen_timeout(6);
+    assertEqual(network_get_listen_timeout(),6);
+    Particle.process();
+    delay(5000);  // todo - find a better way to interact with the system thread
+    assertTrue(network_listening()); // still in listening mode
+    delay(2000);
+    assertFalse(network_listening()); // exited listening mode
+    network_set_listen_timeout(0); // disable for rest of tests
 }
 
-test(11_THREADING_publish_during_listening_mode_issue_761)
+test(11_THREADING_app_can_invoke_synchronous_fn_in_listening_mode)
 {
-	listening_start_with_delay();
+    network_listen(true);
+    delay(10);  // time for the system thread to enter listening mode
 
-	bool result = Particle.publish("mdma", "codez");
-	network_listen(false);
-	assertFalse(result);
+    // this is a blocking call that requires the system thread to
+    blocking_call();
+
+    network_listen(false);
 }
 
+test(12_THREADING_publish_during_listening_mode_issue_761)
+{
+    listening_start_with_delay();
+
+    bool result = Particle.publish("mdma", "codez");
+    network_listen(false);
+    assertFalse(result);
+}
+
+#else // NO PLATFORM_THREADING
+test(00_THREADING_no_threading_on_this_device)
+{
+    assertTrue(true);
+}
+#endif // PLATFORM_THREADING
 
