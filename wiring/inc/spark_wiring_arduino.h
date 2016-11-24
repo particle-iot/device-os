1c7dce9bc8dac2b3a59f02d887bd0a9fbac7e946
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-11-24 04:53:45 +0100

    first pass at adding symbols and headers for increased arduino compatibility

--- wiring/inc/spark_wiring_arduino.h
+++ wiring/inc/spark_wiring_arduino.h
@@ -34,6 +34,77 @@
 #define pgm_read_byte_near(x) (*(x))
 #define pgm_read_word_near(x) ((uint16_t)(*(x)))
 
+#ifdef PARTICLE_ARDUINO_COMPATIBILITY
+
+#ifndef PI
+#define PI		3.1415926535897932384626433832795
+#endif
+
+#ifndef HALF_PI
+#define HALF_PI 	1.5707963267948966192313216916398
+#endif
+
+#ifndef TWO_PI
+#define TWO_PI		6.283185307179586476925286766559
+#endif
+
+#ifndef DEG_TO_RAD
+#define DEG_TO_RAD	0.017453292519943295769236907684886
+#endif
+
+#ifndef RAD_TO_DEG
+#define RAD_TO_DEG	57.295779513082320876798154814105
+#endif
+
+#ifndef EULER
+#define EULER		2.718281828459045235360287471352
+#endif
+
+#ifndef abs
+#define abs(x)	     ((x)>0?(x):-(x))
+#endif
+
+#ifndef round
+#define round(x)     ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
+#endif
+
+#ifndef radians
+#define radians(deg) ((deg)*DEG_TO_RAD)
+#endif
+
+#ifndef degrees
+#define degrees(rad) ((rad)*RAD_TO_DEG)
+#endif
+
+#ifndef sq
+#define sq(x)        ((x)*(x))
+#endif
+
+#ifndef lowByte
+#define lowByte(w)   ((uint8_t) ((w) & 0xff))
+#endif
+
+#ifndef highByte
+#define highByte(w)  ((uint8_t) ((w) >> 8))
+#endif
+
+#ifndef bitRead
+#define bitRead(value, bit)            (((value) >> (bit)) & 0x01)
+#endif
+
+#ifndef bitSet
+#define bitSet(value, bit)             ((value) |= (1UL << (bit)))
+#endif
+
+#ifndef bitClear
+#define bitClear(value, bit)           ((value) &= ~(1UL << (bit)))
+#endif
+
+#ifndef bitWrite
+#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+#endif
+
+#endif
 
 
 #endif	/* SPARK_WIRING_ARDUINO_H */
