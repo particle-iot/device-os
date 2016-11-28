8b3a057e93f40db56b96e68b2379474ebeedc9d4
Julien Vanier <julien@particle.io>
2016-11-28 17:05:19 -0500

    Do not set peripheral pins as input

--- hal/src/raspberrypi/core_hal.cpp
+++ hal/src/raspberrypi/core_hal.cpp
@@ -111,7 +111,8 @@ void core_log(const char* msg, ...)
 }
 
 void makePinsInput() {
-    for (pin_t pin = 0; pin < TOTAL_PINS; pin++) {
+    const pin_t ioPins[] = { D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14, D15, D16, };
+    for (pin_t pin : ioPins) {
         HAL_Pin_Mode(pin, INPUT);
     }
 }
