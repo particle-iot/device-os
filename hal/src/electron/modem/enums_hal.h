particle-iot/device-os

commit 428835a7a6aba8d028c9710c9c64e327ccf9829e

ElectronSerialPipe: correctly handle buffer overflow

hal/src/electron/modem/mdm_hal.cpp
@@ -2251,6 +2251,7 @@ int MDMParser::_getLine(Pipe<char>* pipe, char* buf, int len)
             { "\r\n>",                  NULL,               TYPE_PROMPT     }, // SMS
             { "\n>",                    NULL,               TYPE_PROMPT     }, // File
             { "\r\nABORTED\r\n",        NULL,               TYPE_ABORTED    }, // Current command aborted
+            { "\r\n",                   "\r\n",             TYPE_UNKNOWN    }, // If all else fails, break up generic strings
         };
         for (int i = 0; i < (int)(sizeof(lutF)/sizeof(*lutF)); i ++) {
             pipe->set(unkn);
@@ -2305,5 +2306,7 @@ int MDMElectronSerial::_send(const void* buf, int len)
 
 int MDMElectronSerial::getLine(char* buffer, int length)
 {
-    return _getLine(&_pipeRx, buffer, length);
+    int ret = _getLine(&_pipeRx, buffer, length);
+    rxResume();
+    return ret;
 }

hal/src/electron/modem/electronserialpipe_hal.cpp
@@ -218,13 +218,13 @@ void ElectronSerialPipe::rxIrqBuf(void)
         char c = USART_ReceiveData(USART3);
         _pipeRx.putc(c);
     } else {
-        /* TODO: Handle overflow?
-         * We should not read USART3->RDR register in order to
-         * signal to the modem that we are not ready to receive,
-         * then try to process and free the buffer.
-         */
-        (void)USART_ReceiveData(USART3);
+        USART_ITConfig(USART3, USART_IT_RXNE, DISABLE);
     }
+}
+
+void ElectronSerialPipe::rxResume(void)
+{
+    USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
 }
 
 /*******************************************************************************

hal/src/electron/modem/electronserialpipe_hal.h
@@ -96,6 +96,10 @@ public:
     */
     void txIrqBuf(void);
 
+    /** resumes paused receiver (hardware flow control)
+    */
+    void rxResume();
+
 protected:
     //! start transmission helper
     void txStart(void);

commit ede3370b65fe0114d66e7dc31e41f1b96c776e04

fixes double newline parser issue on G350 introduced in 428835a

hal/src/electron/modem/enums_hal.h
@@ -167,6 +167,7 @@ enum {
     TYPE_PLUS       = 0x400000,
     TYPE_TEXT       = 0x500000,
     TYPE_ABORTED    = 0x600000,
+    TYPE_DBLNEWLINE = 0x700000,
 
     // special timout constant
     TIMEOUT_BLOCKING = 0xffffffff

hal/src/electron/modem/mdm_hal.cpp
@@ -2253,6 +2253,7 @@ int MDMParser::_getLine(Pipe<char>* pipe, char* buf, int len)
             { "\r\n>",                  NULL,               TYPE_PROMPT     }, // SMS
             { "\n>",                    NULL,               TYPE_PROMPT     }, // File
             { "\r\nABORTED\r\n",        NULL,               TYPE_ABORTED    }, // Current command aborted
+            { "\r\n\r\n",               "\r\n",             TYPE_DBLNEWLINE }, // Double CRLF detected, discard one and reprocess line
             { "\r\n",                   "\r\n",             TYPE_UNKNOWN    }, // If all else fails, break up generic strings
         };
         for (int i = 0; i < (int)(sizeof(lutF)/sizeof(*lutF)); i ++) {
@@ -2270,6 +2271,14 @@ int MDMParser::_getLine(Pipe<char>* pipe, char* buf, int len)
             int ln = _parseMatch(pipe, len, lut[i].sta, lut[i].end);
             if (ln == WAIT && fr)
                 return WAIT;
+            // Double CRLF detected, discard one and reprocess line.
+            // This resolves a case on G350 where "\r\n" is generated after +USORF response, but missing
+            // on U260/U270, which would otherwise generate "\r\n\r\nOK\r\n" which is not parsable.
+            if ((ln > 0) && (lut[i].type == TYPE_DBLNEWLINE) && (unkn == 0)) {
+                char tmp[2];
+                pipe->get(tmp, 2);
+                break;
+            }
             if ((ln != NOT_FOUND) && (unkn > 0))
                 return TYPE_UNKNOWN | pipe->get(buf, unkn);
             if (ln > 0)