f61be993430032180af525dea4f35cff35eeba66
Sergey Polyakov <sergeuz@gmail.com>
2017-02-19 17:42:16 +0200

    Restore accidentally removed setting of an IRQ channel in the NVIC configuration structure

--- hal/src/core/interrupts_hal.c
+++ hal/src/core/interrupts_hal.c
@@ -153,6 +153,7 @@ void HAL_Interrupts_Attach(uint16_t pin, HAL_InterruptHandler handler, void* dat
   {
     //configure NVIC
     //select NVIC channel to configure
+    NVIC_InitStructure.NVIC_IRQChannel = GPIO_IRQn[GPIO_PinSource];
     if (config == NULL) {
       if(GPIO_PinSource > 4)
         NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 14;
