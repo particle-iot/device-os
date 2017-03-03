c82ad31cd48409c56c0747bf455159a974709c56
Andrey Tolstoy <andrey@enistek.com>
2017-03-03 17:16:46 +0700

    Bootloader size optimizations

--- bootloader/src/photon/startup.c
+++ bootloader/src/photon/startup.c
@@ -14,12 +14,35 @@ caddr_t _sbrk(int incr)
    return 0;
 }
 
-// Naive memcpy
+// Naive memXXX functions
+
 void* memcpy(void *dest, const void *src, size_t n) {
-    const char* p = src;
-    char* q = dest;
+    const uint8_t* p = src;
+    uint8_t* q = dest;
     while (n--) {
         *q++ = *p++;
     }
     return dest;
+}
+
+int memcmp(const void* s1, const void* s2, size_t n)
+{
+  const volatile uint8_t* p1 = (const volatile uint8_t*)s1;
+  const volatile uint8_t* p2 = (const volatile uint8_t*)s2;
+
+  while (n--)
+  {
+    if (*p1++ != *p2++)
+        return *p1 - *p2;
+  }
+  return 0;
+}
+
+void* memset(void* s, int c, size_t n) {
+    uint8_t* p = s;
+    uint8_t v = c & 0xff;
+    while (n--) {
+        *p++ = v;
+    }
+    return s;
 }
--- hal/src/photon/dct_hal.c
+++ hal/src/photon/dct_hal.c
@@ -2,10 +2,11 @@
 #include "wiced_result.h"
 #include "wiced_dct_common.h"
 #include "waf_platform.h"
+#include "module_info.h"
 
 extern wiced_dct_sdk_ver_t wiced_dct_validate_and_determine_version(uint32_t device_start_address, uint32_t device_end_address, int *initial_write, int *sequence);
 
-const void* dct_read_app_data(uint32_t offset) {
+static wiced_dct_sdk_ver_t wiced_check_current_version() {
     uint32_t current = (uint32_t)wiced_dct_get_current_address(DCT_INTERNAL_SECTION);
     uint32_t end = 0;
     if (current == PLATFORM_DCT_COPY1_START_ADDRESS) {
@@ -14,7 +15,11 @@ const void* dct_read_app_data(uint32_t offset) {
         end = PLATFORM_DCT_COPY2_END_ADDRESS;
     }
     wiced_dct_sdk_ver_t ver = wiced_dct_validate_and_determine_version(current, end, NULL, NULL);
+    return ver;
+}
 
+const void* dct_read_app_data(uint32_t offset) {
+    wiced_dct_sdk_ver_t ver = wiced_check_current_version();
     if (ver == DCT_BOOTLOADER_SDK_CURRENT) {
         return ((const void*)((uint32_t)wiced_dct_get_current_address(DCT_APP_SECTION) + offset));
     } else {
@@ -22,6 +27,13 @@ const void* dct_read_app_data(uint32_t offset) {
     }
 }
 
-int dct_write_app_data(const void* data, uint32_t offset, uint32_t size ) {
+int dct_write_app_data(const void* data, uint32_t offset, uint32_t size) {
+#if MODULE_FUNCTION == MOD_FUNC_BOOTLOADER
+    wiced_dct_sdk_ver_t ver = wiced_check_current_version();
+    if (ver != DCT_BOOTLOADER_SDK_CURRENT) {
+        // Do not write anything into DCT until firmware updates the DCT to the latest version
+        return 1;
+    }
+#endif
     return wiced_dct_write(data, DCT_APP_SECTION, offset, size);
 }
