9f15b6e9a89c900c3731ac86cf11d0c1ed0fa486
Andrey Tolstoy <andrey@enistek.com>
2017-03-07 21:10:27 +0700

    DCT compatibility fixes

--- bootloader/src/photon/dct_hal.c
+++ bootloader/src/photon/dct_hal.c
@@ -1,17 +1,11 @@
 #include <stdint.h>
+
 #ifndef CRC_INIT_VALUE
-#define CRC_INIT_VALUE                              0xffffffff
-
-extern uint32_t Compute_CRC32(const uint8_t *pBuffer, uint32_t bufferSize);
-
-uint32_t crc32(uint8_t* pdata, unsigned int nbytes, uint32_t crc) {
-   uint32_t crcres = Compute_CRC32(pdata, nbytes);
-   crcres ^= 0xffffffff;
-   crcres ^= crc;
-   return crcres;
-}
-
-#define CRC_FUNCTION(address, size, previous_value) (uint32_t)crc32(address, size, previous_value)
-typedef uint32_t    CRC_TYPE;
+#define CRC_INIT_VALUE 0xffffffff
 #endif
+
+#ifndef CRC_TYPE
+#define CRC_TYPE uint32_t
+#endif
+
 #include "../hal/src/photon/dct_hal.c"
--- hal/src/photon/dct_hal.c
+++ hal/src/photon/dct_hal.c
@@ -3,6 +3,20 @@
 #include "wiced_dct_common.h"
 #include "waf_platform.h"
 #include "module_info.h"
+#include "service_debug.h"
+#include "hal_irq_flag.h"
+
+extern uint32_t Compute_CRC32(const uint8_t *pBuffer, uint32_t bufferSize);
+
+// Compatibility crc32 function for WICED
+uint32_t crc32(uint8_t* pdata, unsigned int nbytes, uint32_t crc) {
+   uint32_t crcres = Compute_CRC32(pdata, nbytes);
+   // We need to XOR computed CRC32 value with 0xffffffff again as it was already xored in Compute_CRC32
+   // and apply passed crc value instead.
+   crcres ^= 0xffffffff;
+   crcres ^= crc;
+   return crcres;
+}
 
 extern wiced_dct_sdk_ver_t wiced_dct_validate_and_determine_version(uint32_t device_start_address, uint32_t device_end_address, int *initial_write, int *sequence);
 
@@ -14,7 +28,9 @@ static wiced_dct_sdk_ver_t wiced_check_current_version() {
     } else if (current == PLATFORM_DCT_COPY2_START_ADDRESS) {
         end = PLATFORM_DCT_COPY2_END_ADDRESS;
     }
-    wiced_dct_sdk_ver_t ver = wiced_dct_validate_and_determine_version(current, end, NULL, NULL);
+    int sequence = 0;
+    wiced_dct_sdk_ver_t ver = wiced_dct_validate_and_determine_version(current, end, NULL, &sequence);
+
     return ver;
 }
 
@@ -35,5 +51,7 @@ int dct_write_app_data(const void* data, uint32_t offset, uint32_t size) {
         return 1;
     }
 #endif
-    return wiced_dct_write(data, DCT_APP_SECTION, offset, size);
+    int res = wiced_dct_write(data, DCT_APP_SECTION, offset, size);
+
+    return res;
 }
