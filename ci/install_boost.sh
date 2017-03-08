e50c5a727b02e225ba40558c5fee1099da3cf882
Technobly <technobly@gmail.com>
2017-03-08 11:59:11 -0600

    move boost tarball to a fast host

--- ci/install_boost.sh
+++ ci/install_boost.sh
@@ -1,13 +1,15 @@
 BOOST_VERSION=1_59_0
 
-test -f boost_$BOOST_VERSION.tar.gz || wget --quiet http://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_$BOOST_VERSION.tar.gz 
+# this was previously hardcoded to 1.59.0 in the path, replacing with a fast source
+#test -f boost_$BOOST_VERSION.tar.gz || wget --quiet http://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_$BOOST_VERSION.tar.gz
+test -f boost_$BOOST_VERSION.tar.gz || wget --quiet https://www.dropbox.com/s/0whh8jivbjql7mf/boost_1_59_0.tar.gz
 export BOOST_HOME=$HOME/.ci/boost
 export BOOST_ROOT=$BOOST_HOME/boost_$BOOST_VERSION
 mkdir -p $BOOST_HOME
 test -d $BOOST_ROOT || (
    tar zxf boost_$BOOST_VERSION.tar.gz
    mv boost_$BOOST_VERSION  $BOOST_HOME
-) 
+)
 test -d $BOOST_ROOT || ( echo "boost root $BOOST_ROOT not created." && exit 1)
 export DYLD_LIBRARY_PATH="$BOOST_ROOT/stage/lib:$DYLD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="$BOOST_ROOT/stage/lib:$LD_LIBRARY_PATH"
