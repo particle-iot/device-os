152c1674ca9b857c5acda0f175a70c5ce6e082c7
Andrey Tolstoy <andrey@enistek.com>
2017-04-03 03:56:37 +0700

    Updated WICED build script

--- hal/src/photon/wiced.sh
+++ hal/src/photon/wiced.sh
@@ -7,7 +7,7 @@ if [ -z "$PHOTON_WICED_REPO_PATH" ]; then
     PHOTON_WICED_REPO_PATH=/spark/photon-wiced
 fi
 
-OPTS=
+OPTS="UPDATE_FROM_SDK=3_3_0_PARTICLE DEFINE_BOOTLOADER=1 DEFINE_PARTICLE_DCT_COMPATIBILITY=1"
 
 function update()
 {
@@ -38,10 +38,10 @@ update lib/FreeRTOS $PHOTON_WICED_REPO_PATH/build/demo_soft_ap-BCM9WCDUSI14-Free
 update lib/FreeRTOS $PHOTON_WICED_REPO_PATH/build/demo_soft_ap-BCM9WCDUSI09-FreeRTOS-LwIP-SDIO/libraries
 
 # Bootloader optimized build
-pushd $PHOTON_WICED_REPO_PATH
+# pushd $PHOTON_WICED_REPO_PATH
 # We have to clean anyway, otherwise PARTICLE_FLASH_SPACE_OPTIMIZE=y will not take effect
-./make clean
-./make demo.soft_ap-BCM9WCDUSI09-FreeRTOS-LwIP-SDIO $OPTS PARTICLE_FLASH_SPACE_OPTIMIZE=y
-popd
+# ./make clean
+# ./make demo.soft_ap-BCM9WCDUSI09-FreeRTOS-LwIP-SDIO $OPTS PARTICLE_FLASH_SPACE_OPTIMIZE=y
+# popd
 
 cp $PHOTON_WICED_REPO_PATH/build/demo_soft_ap-BCM9WCDUSI09-FreeRTOS-LwIP-SDIO/libraries/STM32F2xx.a lib/FreeRTOS/STM32F2xx_bootloader.a
