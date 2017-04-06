6059c141f41a8b2b888f1af1f27cfd7d7bd4213f
Andrey Tolstoy <andrey@enistek.com>
2017-04-06 11:51:37 +0700

    hal/src/photon/wiced.sh build script should also create Lib_crypto_open_part2.a

--- hal/src/photon/wiced.sh
+++ hal/src/photon/wiced.sh
@@ -45,3 +45,11 @@ update lib/FreeRTOS $PHOTON_WICED_REPO_PATH/build/demo.soft_ap-BCM9WCDUSI09-Free
 # popd
 
 cp $PHOTON_WICED_REPO_PATH/build/demo.soft_ap-BCM9WCDUSI09-FreeRTOS-LwIP-SDIO/libraries/STM32F2xx.a lib/FreeRTOS/STM32F2xx_bootloader.a
+
+# Split Lib_crypto_open.a into two separate libraries
+rm -f lib/Lib_crypto_open_part2.a
+rm -f des.o md4.o sha2.o x509parse.o md5.o
+arm-none-eabi-ar x lib/Lib_crypto_open.a des.o md4.o sha2.o x509parse.o md5.o
+arm-none-eabi-ar d lib/Lib_crypto_open.a des.o md4.o sha2.o x509parse.o md5.o
+arm-none-eabi-ar r lib/Lib_crypto_open_part2.a des.o md4.o sha2.o x509parse.o md5.o
+rm -f des.o md4.o sha2.o x509parse.o md5.o
