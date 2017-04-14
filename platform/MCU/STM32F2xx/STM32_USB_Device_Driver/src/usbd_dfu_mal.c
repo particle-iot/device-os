09d0351a327ca2f69a7927796769f1ff82d62499
Sergey Polyakov <sergeuz@gmail.com>
2017-04-14 21:28:39 +0200

    Disable reading/writing of DCT via DFU if DCT functions cannot be loaded dynamically

--- platform/MCU/STM32F2xx/STM32_USB_Device_Driver/src/usbd_dfu_mal.c
+++ platform/MCU/STM32F2xx/STM32_USB_Device_Driver/src/usbd_dfu_mal.c
@@ -154,7 +154,7 @@ uint16_t MAL_DeInit(void)
 uint16_t MAL_Erase(uint32_t Idx, uint32_t Add)
 {
   uint32_t memIdx = Idx;
-  
+
   if(MAL_OK != MAL_CheckAdd(Idx, Add))
   {
     return MAL_FAIL;
@@ -197,7 +197,7 @@ uint16_t MAL_Erase(uint32_t Idx, uint32_t Add)
 uint16_t MAL_Write (uint32_t Idx, uint32_t Add, uint32_t Len)
 {
   uint32_t memIdx = Idx;
-  
+
   if(MAL_OK != MAL_CheckAdd(Idx, Add))
   {
     return MAL_FAIL;
@@ -259,7 +259,7 @@ uint16_t MAL_Write (uint32_t Idx, uint32_t Add, uint32_t Len)
 const uint8_t *MAL_Read (uint32_t Idx, uint32_t Add, uint32_t Len)
 {
   uint32_t memIdx = Idx;
-  
+
   if(MAL_OK != MAL_CheckAdd(Idx, Add))
   {
     return MAL_Buffer;
@@ -294,7 +294,7 @@ const uint8_t *MAL_Read (uint32_t Idx, uint32_t Add, uint32_t Len)
 uint16_t MAL_GetStatus(uint32_t Idx, uint32_t Add , uint8_t Cmd, uint8_t *buffer)
 {
   uint32_t memIdx = Idx;
-  
+
   if(MAL_OK != MAL_CheckAdd(Idx, Add))
   {
     return MAL_FAIL;
@@ -325,15 +325,14 @@ uint16_t MAL_GetStatus(uint32_t Idx, uint32_t Add , uint8_t Cmd, uint8_t *buffer
   * @param  Add: Sector address/code (allow to determine which memory will be addressed)
   * @retval Index of the addressed memory.
   */
-static uint8_t  MAL_CheckAdd(uint32_t Idx, uint32_t Add)
+static uint8_t MAL_CheckAdd(uint32_t Idx, uint32_t Add)
 {
   uint32_t memIdx = Idx;
 
-    if (tMALTab[memIdx]->pMAL_CheckAdd(Add) == MAL_OK)
-    {
-     return MAL_OK;
+  if (tMALTab[memIdx]->pMAL_CheckAdd != NULL) {
+    return tMALTab[memIdx]->pMAL_CheckAdd(Add);
   }
-  
+
   return MAL_FAIL;
 }
 
