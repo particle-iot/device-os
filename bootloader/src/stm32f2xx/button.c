66f222e68eba1f379cfb99d6f2cbe41f77e202b3
Sergey Polyakov <sergeuz@gmail.com>
2017-04-16 13:29:52 +0200

    Load DCT functions on first call; error handling for DCT read operations

--- bootloader/src/stm32f2xx/button.c
+++ bootloader/src/stm32f2xx/button.c
@@ -64,6 +64,9 @@ int BUTTON_Debounce() {
 
 void BUTTON_Init_Ext() {
     const button_config_t* conf = (const button_config_t*)dct_read_app_data(DCT_MODE_BUTTON_MIRROR_OFFSET);
+    if (!conf) {
+        return;
+    }
 
     if (conf->active == 0xAA && conf->debounce_time == 0xBBCC) {
         //int32_t state = HAL_disable_irq();
