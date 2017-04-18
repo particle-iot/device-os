f865ec7b90641a19526e566b50425959b7b01869
Sergey Polyakov <sergeuz@gmail.com>
2017-04-19 00:53:35 +0200

    Enable dynamic loading of the DCT functions on Photon

--- bootloader/src/photon/bootloader_dct.c
+++ bootloader/src/photon/bootloader_dct.c
@@ -1,12 +1,9 @@
 #include "bootloader_dct.h"
 
+#include "flash_mal.h"
+
 #include <stdint.h>
 #include <stddef.h>
-
-#ifdef LOAD_DCT_FUNCTIONS
-
-#include "flash_mal.h"
-#include "usbd_dct_if.h"
 
 #define MODULAR_FIRMWARE 1
 #include "../../../hal/src/photon/ota_module_bounds.c"
@@ -85,17 +82,3 @@ int dct_write_app_data(const void* data, uint32_t offset, uint32_t size) {
     }
     return -1;
 }
-
-#else // !defined(LOAD_DCT_FUNCTIONS)
-
-#ifndef CRC_INIT_VALUE
-#define CRC_INIT_VALUE 0xffffffff
-#endif
-
-#ifndef CRC_TYPE
-#define CRC_TYPE uint32_t
-#endif
-
-#include "../hal/src/photon/dct_hal.c"
-
-#endif
--- bootloader/src/photon/bootloader_dct.h
+++ bootloader/src/photon/bootloader_dct.h
@@ -1,15 +1,6 @@
 #ifndef BOOTLOADER_DCT_H
 #define BOOTLOADER_DCT_H
 
-#include "platforms.h"
-
-#if PLATFORM_ID == PLATFORM_P1
-// Enable dynamic loading of the DCT functions
-#define LOAD_DCT_FUNCTIONS
-#endif
-
-#ifdef LOAD_DCT_FUNCTIONS
 void load_dct_functions();
-#endif
 
 #endif // BOOTLOADER_DCT_H
