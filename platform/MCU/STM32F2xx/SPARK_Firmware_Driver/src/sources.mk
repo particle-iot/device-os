044ef0c601e275ef7a22a10371a2a8c34346b923
Sergey Polyakov <sergeuz@gmail.com>
2017-04-27 00:50:32 +0200

    Unit test for platform system flags (stm32f2xx)

--- platform/MCU/STM32F2xx/SPARK_Firmware_Driver/src/sources.mk
+++ platform/MCU/STM32F2xx/SPARK_Firmware_Driver/src/sources.mk
@@ -13,6 +13,7 @@ CSRC += $(TARGET_SPARK_SRC_PATH)/flash_mal.c
 CSRC += $(TARGET_SPARK_SRC_PATH)/usb_bsp.c
 CSRC += $(TARGET_SPARK_SRC_PATH)/usbd_usr.c
 CSRC += $(TARGET_SPARK_SRC_PATH)/usbd_composite.c
+CSRC += $(TARGET_SPARK_SRC_PATH)/system_flags_impl.c
 # Moved to hal, since the same symbols are also defined in the bootloader
 # CSRC += $(TARGET_SPARK_SRC_PATH)/usbd_desc.c
 ifeq ("$(PLATFORM_ID)","5")
