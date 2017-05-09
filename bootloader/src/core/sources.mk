45c508958cf9269664a271f31ced1d3db5ca7cc7
Sergey Polyakov <sergeuz@gmail.com>
2017-05-10 01:15:33 +0200

    Ensure bootloader's RAM region doesn't overlap with the system modules

--- bootloader/src/core/sources.mk
+++ bootloader/src/core/sources.mk
@@ -1,3 +1,6 @@
 BOOTLOADER_SRC_COREV1_PATH = $(BOOTLOADER_MODULE_PATH)/src/core
 
 CSRC += $(call target_files,$(BOOTLOADER_SRC_COREV1_PATH)/,*.c)
+
+LDFLAGS += -T$(COMMON_BUILD)/arm/linker/linker_$(STM32_DEVICE_LC).ld
+LINKER_DEPS += $(COMMON_BUILD)/arm/linker/linker_$(STM32_DEVICE_LC).ld
