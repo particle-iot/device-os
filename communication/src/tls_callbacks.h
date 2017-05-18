574e7bbfe8acde136ef6c61d4ed4b3409c31c299
Andrey Tolstoy <andrey@enistek.com>
2017-05-18 21:02:27 +0700

    Remove separate TLS-related system-part1 implementations and provide them via TlsCallbacks

--- communication/src/tls_callbacks.h
+++ communication/src/tls_callbacks.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include <time.h>
+#include "wiced_result.h"
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
 typedef struct {
-	uint8_t version;
-	uint64_t (*tls_host_get_time_ms)();
+    uint8_t version;
+    uint64_t (*tls_host_get_time_ms)();
+    void* (*tls_host_malloc)(const char*, uint32_t);
+    void  (*tls_host_free)(void*);
+    wiced_result_t (*wiced_crypto_get_random)(void*, uint16_t);
 } TlsCallbacks;
 
 int tls_set_callbacks(TlsCallbacks callbacks);
