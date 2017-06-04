d1d099be9b7d8fdb15710923b6a133d0331b3f6a
Sergey Polyakov <sergeuz@gmail.com>
2017-06-04 16:47:35 +0200

    Do not try to initialize mutex until RTOS is initialized and malloc() is available

--- hal/src/stm32f2xx/static_recursive_mutex.h
+++ hal/src/stm32f2xx/static_recursive_mutex.h
@@ -1,21 +1,16 @@
 #pragma once
 
-#include "hal_irq_flag.h"
+#include "core_hal_stm32f2xx.h"
 #include "debug.h"
 
-#include "FreeRTOSConfig.h"
 #include "FreeRTOS.h"
 #include "semphr.h"
 
-#if PLATFORM_ID == 6 || PLATFORM_ID == 8
-#include "wwd_rtos_interface.h"
-#endif // PLATFORM_ID == 6 || PLATFORM_ID == 8
-
+// TODO: Use xSemaphoreCreateMutexStatic() after upgrading to FreeRTOS 9.x.x
 class StaticRecursiveMutex {
 public:
     StaticRecursiveMutex() :
             mutex_(nullptr) {
-        init();
     }
 
     ~StaticRecursiveMutex() {
@@ -25,27 +20,30 @@ public:
     }
 
     bool lock(unsigned timeout = 0) {
-        if (!mutex_) {
-            init();
+        if (!mutex_ && !initMutex()) {
+            return true; // FreeRTOS is not initialized
         }
         const TickType_t t = (timeout > 0) ? (timeout / portTICK_PERIOD_MS) : portMAX_DELAY;
         return (xSemaphoreTakeRecursive(mutex_, t) == pdTRUE);
     }
 
     bool unlock() {
+        if (!mutex_) {
+            return true;
+        }
         return (xSemaphoreGiveRecursive(mutex_) == pdTRUE);
     }
 
 private:
-    SemaphoreHandle_t mutex_;
-    StaticSemaphore_t buffer_;
+    volatile SemaphoreHandle_t mutex_;
 
-    void init() {
+    bool initMutex() {
         const int32_t state = HAL_disable_irq();
-        if (!mutex_) {
-            mutex_ = xSemaphoreCreateMutexStatic(&buffer_);
+        if (!mutex_ && rtos_started) {
+            mutex_ = xSemaphoreCreateRecursiveMutex();
             SPARK_ASSERT(mutex_);
         }
         HAL_enable_irq(state);
+        return (mutex_ != nullptr);
     }
 };
