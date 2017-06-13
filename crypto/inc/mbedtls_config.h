5514f279d92f795aec2c7e54fe5c51a995fc9769
Sergey Polyakov <sergeuz@gmail.com>
2017-06-13 15:05:32 +0200

    Decrease MBEDTLS_MPI_MAX_SIZE and MBEDTLS_MPI_WINDOW_SIZE parameters to reduce stack usage during cloud handshake

--- crypto/inc/mbedtls_config.h
+++ crypto/inc/mbedtls_config.h
@@ -2439,8 +2439,8 @@
  */
 
 /* MPI / BIGNUM options */
-//#define MBEDTLS_MPI_WINDOW_SIZE            6 /**< Maximum windows size used. */
-//#define MBEDTLS_MPI_MAX_SIZE            1024 /**< Maximum number of bytes for usable MPIs. */
+#define MBEDTLS_MPI_WINDOW_SIZE            5 /**< Maximum windows size used. */
+#define MBEDTLS_MPI_MAX_SIZE             512 /**< Maximum number of bytes for usable MPIs. */
 
 /* CTR_DRBG options */
 //#define MBEDTLS_CTR_DRBG_ENTROPY_LEN               48 /**< Amount of entropy used per seed by default (48 with SHA-512, 32 with SHA-256) */
