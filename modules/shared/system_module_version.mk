3e1edf3574f3af4587d5a5e9ef7d8d2cd783337c
Technobly <technobly@gmail.com>
2017-06-15 18:18:42 -0500

    capture module version update schema

--- modules/shared/system_module_version.mk
+++ modules/shared/system_module_version.mk
@@ -1,5 +1,13 @@
+# Skip to next 100 every v0.x.0 release (e.g. 108 for v0.6.2 to 200 for v0.7.0-rc.1)
+# Bump by 1 for every prerelease or release with the same v0.x.* base.
 SYSTEM_PART1_MODULE_VERSION ?= 200
 SYSTEM_PART2_MODULE_VERSION ?= 200
 SYSTEM_PART3_MODULE_VERSION ?= 200
+
+# Bump by 1 if Tinker has been updated
 USER_PART_MODULE_VERSION ?= 4
+
+# Skip to next 100 every v0.x.0 release (e.g. 11 for v0.6.2 to 100 for v0.7.0-rc.1),
+# but only if the bootloader has changed since the last v0.x.0 release.
+# Bump by 1 for every updated bootloader image for a release with the same v0.x.* base.
 BOOTLOADER_VERSION ?= 100
