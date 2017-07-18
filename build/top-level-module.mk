98fab357d48d7afbff901b04c793ff55e2168648
Matthew McGowan <mat.mcgowan+github@gmail.com>
2017-07-18 15:32:35 +0100

    allows `user` module make to be invoked from any current working directory.

--- build/top-level-module.mk
+++ build/top-level-module.mk
@@ -1,8 +1,10 @@
 # Makefile for building modules
 
 # assume project is at top level directory if PROJECT_ROOT not specified
-PROJECT_ROOT ?= ..
-MODULE_PATH=.
+# current_dir set in arm-tlk.mk
+PROJECT_ROOT ?= $(current_dir)/..
+# module_dir is optional, and will include final trailing slash
+MODULE_PATH?=$(module_dir).
 COMMON_BUILD=$(PROJECT_ROOT)/build
 BUILD_PATH_BASE?=$(COMMON_BUILD)/target
 
