3c9fff9de0634d42a2703f03d189879e60b6409d
Technobly <technobly@gmail.com>
2017-08-11 14:36:38 -0500

    bump module version for 0.7.0-rc.3 (v202), update CHANGELOG.md

--- modules/shared/system_module_version.mk
+++ modules/shared/system_module_version.mk
@@ -1,8 +1,8 @@
 # Skip to next 100 every v0.x.0 release (e.g. 108 for v0.6.2 to 200 for v0.7.0-rc.1)
 # Bump by 1 for every prerelease or release with the same v0.x.* base.
-SYSTEM_PART1_MODULE_VERSION ?= 201
-SYSTEM_PART2_MODULE_VERSION ?= 201
-SYSTEM_PART3_MODULE_VERSION ?= 201
+SYSTEM_PART1_MODULE_VERSION ?= 202
+SYSTEM_PART2_MODULE_VERSION ?= 202
+SYSTEM_PART3_MODULE_VERSION ?= 202
 
 # Bump by 1 if Tinker has been updated
 USER_PART_MODULE_VERSION ?= 5
