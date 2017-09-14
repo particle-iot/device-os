506eb1c129e652ebf66f8dcebb99edc9fb0ebf32
Andrey Tolstoy <avtolstoy@gmail.com>
2017-09-14 14:31:40 +0700

    wlan_hal: fixes scope

--- hal/src/photon/wlan_hal.cpp
+++ hal/src/photon/wlan_hal.cpp
@@ -1325,6 +1325,7 @@ void wlan_fetch_ipconfig(WLanConfig* config)
 
         if (dns_client_get_server_address(0, &addr)==WICED_SUCCESS) {
             setAddress(&addr, config->nw.aucDNSServer);
+        }
 
         // LwIP-specific
 #ifdef LWIP_DHCP
@@ -1334,7 +1335,6 @@ void wlan_fetch_ipconfig(WLanConfig* config)
             HAL_IPV4_SET(&config->nw.aucDHCPServer, ntohl(dhcp->server_ip_addr.addr));
         }
 #endif // LWIP_DHCP
-        }
     }
 
     wiced_mac_t my_mac_address;
