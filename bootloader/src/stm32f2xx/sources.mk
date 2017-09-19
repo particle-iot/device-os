ee742dde0ee9e738a2ed4ea871ce23d7b92b5ec5
Sergey Polyakov <sergeuz@gmail.com>
2017-09-19 12:43:49 +0200

    Add stubs for CRT functions dealing with memory allocations to the bootloader

--- bootloader/src/stm32f2xx/sources.mk
+++ bootloader/src/stm32f2xx/sources.mk
@@ -4,3 +4,5 @@ CSRC += $(call target_files,$(BOOTLOADER_SRC_STM32F2XX_PATH)/,*.c)
 
 LDFLAGS += -T$(COMMON_BUILD)/arm/linker/linker_$(STM32_DEVICE_LC)_bootloader.ld
 LINKER_DEPS += $(COMMON_BUILD)/arm/linker/linker_$(STM32_DEVICE_LC)_bootloader.ld
+
+include $(BOOTLOADER_MODULE_PATH)/src/stm32/sources.mk
