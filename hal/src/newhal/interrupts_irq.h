81473eaa7f4d61c5c57db6b2d2687464f1d77224
Andrey Tolstoy <avtolstoy@gmail.com>
2017-09-25 21:18:14 +0700

    HAL_Set_Direct_Interrupt_Handler() and supporting declarations

---
+++ hal/src/newhal/interrupts_irq.h
@@ -0,0 +1,18 @@
+#ifndef INTERRUPTS_IRQ_H
+#define INTERRUPTS_IRQ_H
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+typedef int32_t IRQn_Type;
+
+typedef enum hal_irq_t {
+    __Last_irq = 0
+} hal_irq_t;
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif  /* INTERRUPTS_IRQ_H */
