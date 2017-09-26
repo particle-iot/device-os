3eba9dcef34ec62323f2f16f76086daf71f22466
Andrey Tolstoy <avtolstoy@gmail.com>
2017-09-26 17:26:00 +0700

    Adds missing __gthread_time_t typedef to newhal concurrent_hal_impl.h to make it happy

--- hal/src/newhal/concurrent_hal_impl.h
+++ hal/src/newhal/concurrent_hal_impl.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <time.h>
+
 // redefine these for the underlying concurrency primitives available in the RTOS
 
 typedef int os_result_t;
@@ -11,3 +13,4 @@ typedef void*os_mutex_t;
 typedef void* condition_variable_t;
 typedef void* os_semaphore_t;
 typedef void* os_mutex_recursive_t;
+typedef struct timespec __gthread_time_t;
