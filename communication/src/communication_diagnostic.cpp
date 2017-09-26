1330b2fcd0036493a812d79982a5df4e68ce3aa1
Andrey Tolstoy <avtolstoy@gmail.com>
2017-09-26 17:16:34 +0700

    Specifies DIAG_ID_CLOUD_RATE_LIMITED_EVENTS and DIAG_ID_CLOUD_UNACKNOWLEDGED_MESSAGES string names in-place during construction

--- communication/src/communication_diagnostic.cpp
+++ communication/src/communication_diagnostic.cpp
@@ -1,4 +1,4 @@
 #include "communication_diagnostic.h"
 
-particle::SimpleIntegerDiagnosticData g_rateLimitedEventsCounter(DIAG_ID_CLOUD_RATE_LIMITED_EVENTS, DIAG_ID_CLOUD_RATE_LIMITED_EVENTS_NAME);
-particle::SimpleIntegerDiagnosticData g_unacknowledgedMessageCounter(DIAG_ID_CLOUD_UNACKNOWLEDGED_MESSAGES, DIAG_ID_CLOUD_UNACKNOWLEDGED_MESSAGES_NAME);
+particle::SimpleIntegerDiagnosticData g_rateLimitedEventsCounter(DIAG_ID_CLOUD_RATE_LIMITED_EVENTS, "cloud.rateLimited");
+particle::SimpleIntegerDiagnosticData g_unacknowledgedMessageCounter(DIAG_ID_CLOUD_UNACKNOWLEDGED_MESSAGES, "cloud.protoUnack");
