c1f1703756bef4fa99a5169e9c26806ef5611f4f
Andrey Tolstoy <andrey@enistek.com>
2017-10-06 14:08:03 +0700

    wiring/no_fixture: CLOUD_ tests fixes

--- user/tests/wiring/no_fixture/cloud.cpp
+++ user/tests/wiring/no_fixture/cloud.cpp
@@ -1,36 +1,13 @@
 #include "application.h"
 #include "unit-test/unit-test.h"
 
-test(CLOUD_01_Particle_Connect_Blocks_In_SemiAutomatic_Mode_With_Threading_Disabled) {
-    if (system_thread_get_state(nullptr) == spark::feature::ENABLED) {
-        skip();
-        return;
-    }
-
+test(CLOUD_01_Particle_Connect_Does_Not_Block_In_SemiAutomatic_Mode) {
     Particle.disconnect();
     waitFor(Particle.disconnected, 10000);
     assertTrue(Particle.disconnected());
-    
+
     // Switch to SEMI_AUTOMATIC mode
     set_system_mode(SEMI_AUTOMATIC);
-
-    Particle.connect();
-    assertTrue(Particle.connected());
-}
-
-test(CLOUD_02_Particle_Connect_Does_Not_Block_In_SemiAutomatic_Mode_With_Threading_Enabled) {
-    if (system_thread_get_state(nullptr) == spark::feature::DISABLED) {
-        skip();
-        return;
-    }
-    Particle.disconnect();
-    waitFor(Particle.disconnected, 10000);
-    assertTrue(Particle.disconnected());
-    
-    // Switch to SEMI_AUTOMATIC mode
-    set_system_mode(SEMI_AUTOMATIC);
-
-    assertTrue(system_thread_get_state(nullptr) == spark::feature::ENABLED);
 
     Particle.connect();
     assertFalse(Particle.connected());
