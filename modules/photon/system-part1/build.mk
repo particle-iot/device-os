d79f404b0db5424e065647674d93edefc67e5a95
Andrey Tolstoy <andrey@enistek.com>
2017-10-30 13:55:53 +0700

    Exports WICED's resource_read() from system-part1 to enable upgrade to a version supporting wifi firmware image compression

--- modules/photon/system-part1/build.mk
+++ modules/photon/system-part1/build.mk
@@ -1,5 +1,5 @@
 # This is specific to the photon. Include the WICED WIFI
-WIFI_MODULE_WICED_LIB_FILES = $(HAL_LIB_COREV2)/resources.a $(HAL_LIB_DIR)/src/photon/resources.o
+WIFI_MODULE_WICED_LIB_FILES = $(HAL_LIB_COREV2)/resources.a $(HAL_LIB_DIR)/src/photon/resources.o $(HAL_LIB_COREV2)/FreeRTOS/STM32F2xx.a
 # if !USE_MBEDTLS
 # WIFI_MODULE_WICED_LIB_FILES += $(HAL_LIB_COREV2)/BESL.ARM_CM3.release.a
 # WIFI_MODULE_WICED_LIB_FILES += $(HAL_LIB_COREV2)/Lib_crypto_open.a $(HAL_SRC_COREV2_PATH)/lib/Lib_micro_ecc.a
