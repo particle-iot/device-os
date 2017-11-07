21a687a96a4ebd63b821f57a1771868102e820e1
Andrey Tolstoy <avtolstoy@gmail.com>
2017-11-07 17:35:00 +0700

    Photon/P1: adds system-part2 v0.7.0-rc.4 as a dependency of system-part1 to avoid bricking devices when upgrading to 0.8.0-rc.1 from < 0.7.0-rc.4 version

--- modules/shared/system_module_version.mk
+++ modules/shared/system_module_version.mk
@@ -4,6 +4,18 @@ SYSTEM_PART1_MODULE_VERSION ?= 203
 SYSTEM_PART2_MODULE_VERSION ?= 203
 SYSTEM_PART3_MODULE_VERSION ?= 203
 
+RELEASE_080_MODULE_VERSION_BASE ?= 300
+RELEASE_070_RC4_MODULE_VERSION ?= 203
+
+ifneq (,$(filter $(PLATFORM_ID),6 8))
+ifeq ($(shell test $(SYSTEM_PART2_MODULE_VERSION) -ge $(RELEASE_080_MODULE_VERSION_BASE); echo $$?),0)
+# If this is >= 0.8.x release, Photon and P1 system-part1
+# needs to have a dependency on system-part2 of at least 0.7.0-rc.4
+# in order not to brick the device during OTA or Ymodem upgrade
+SYSTEM_PART1_MODULE_DEPENDENCY ?= ${MODULE_FUNCTION_SYSTEM_PART},2,${RELEASE_070_RC4_MODULE_VERSION}
+endif
+endif
+
 # Bump by 1 if Tinker has been updated
 USER_PART_MODULE_VERSION ?= 5
 
