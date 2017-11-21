4d95260456f1f6488a121e9feced631b2908cd4d
Matthew McGowan <mat.mcgowan+github@gmail.com>
2017-11-21 18:25:00 +0100

    wip parallel build

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# ensure we are in the root
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
-source ./ci/install_boost.sh
-./ci/build_boost.sh &&
-./ci/unit_tests.sh &&
+
+. ./ci/functions.sh
+
+cmd=""
+
+if contains "${BUILD_PLATFORM[*]}" unit-test; then
+	( source ./ci/install_boost.sh
+	./ci/build_boost.sh &&
+	./ci/unit_tests.sh ) || die
+fi
+
 ./ci/enumerate_build_matrix.sh
