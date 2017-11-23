fbae6db42e7aa1fa9572ba93523c4600d736b399
Andrey Tolstoy <andrey@enistek.com>
2017-11-23 09:13:31 +0700

    Adds initial version of protobuf-based control.proto

---
+++ system/src/control/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PROTO_FILE="${DIR}/control.proto"
+NANOPB_PATH="${DIR}/../../../services/nanopb"
+PROTOC_NANOPB_PLUGIN="${NANOPB_PATH}/generator/protoc-gen-nanopb"
+PROTOC_INCLUDE_PATH="-I${DIR} -I${NANOPB_PATH}/generator -I${NANOPB_PATH}/generator/proto"
+
+exec protoc ${PROTOC_INCLUDE_PATH} --plugin=protoc-gen-nanopb=${PROTOC_NANOPB_PLUGIN} --nanopb_out=${DIR} ${PROTO_FILE}
