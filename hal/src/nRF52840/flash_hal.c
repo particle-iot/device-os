b248b0cdf0334a2c41dc1379f77ad5cf4fee5612
yuting0501 <yuting0501@gmail.com>
2018-05-08 18:26:49 +0800

    clean code

--- hal/src/nRF52840/flash_hal.c
+++ hal/src/nRF52840/flash_hal.c
@@ -7,11 +7,11 @@
 #include "nrfx_config.h"
 #include "nrf_log.h"
 
-#ifdef SOFTDEVICE_PRESENT
-#include "nrf_fstorage_sd.h"
-#else
+// #ifdef SOFTDEVICE_PRESENT
+// #include "nrf_fstorage_sd.h"
+// #else
 #include "nrf_fstorage_nvmc.h"
-#endif
+// #endif
 
 
 #define CEIL_DIV(A, B)              (((A) + (B) - 1) / (B))
@@ -28,27 +28,14 @@ nrf_fstorage_t m_fs =
     .end_addr    = 0x100000
 };
 
-static uint32_t m_flash_operations_pending;
-
 
 static void fstorage_evt_handler(nrf_fstorage_evt_t * p_evt)
 {
-    if (NRF_LOG_ENABLED && (m_flash_operations_pending > 0))
+    if (p_evt->result != NRF_SUCCESS)
     {
-        m_flash_operations_pending--;
-    }
-
-    if (p_evt->result == NRF_SUCCESS)
-    {
-        NRF_LOG_INFO("*** Flash %s success: addr=%p, pending %d",
+        NRF_LOG_INFO("*** Flash %s FAILED! (0x%x): addr=%p, len=0x%x bytes",
                       (p_evt->id == NRF_FSTORAGE_EVT_WRITE_RESULT) ? "write" : "erase",
-                      p_evt->addr, m_flash_operations_pending);
-    }
-    else
-    {
-        NRF_LOG_INFO("*** Flash %s failed (0x%x): addr=%p, len=0x%x bytes, pending %d",
-                      (p_evt->id == NRF_FSTORAGE_EVT_WRITE_RESULT) ? "write" : "erase",
-                      p_evt->result, p_evt->addr, p_evt->len, m_flash_operations_pending);
+                      p_evt->result, p_evt->addr, p_evt->len);
     }
 
     if (p_evt->p_param)
@@ -62,8 +49,8 @@ static void fstorage_evt_handler(nrf_fstorage_evt_t * p_evt)
 int hal_flash_init(void)
 {
     uint32_t ret_code = nrf_fstorage_init(&m_fs, &nrf_fstorage_nvmc, NULL);
-    NRF_LOG_INFO("m_fs, start: 0x%x ~ 0x%x", m_fs.start_addr, m_fs.end_addr);
-    NRF_LOG_INFO("Initializing nrf_fstorage_nvmc backend.code: %d", ret_code);
+    NRF_LOG_INFO("m_fs, range: 0x%x ~ 0x%x, erase size: %d", m_fs.start_addr, m_fs.end_addr, m_fs.p_flash_info->erase_unit);
+    NRF_LOG_INFO("init nrf_fstorage_nvmc %s", ret_code == 0 ? "OK" : "FAILED!");
 
     return (ret_code == NRF_SUCCESS) ? 0 : -1;
 }
@@ -83,11 +70,11 @@ int hal_flash_write(uint32_t addr, const uint8_t * data_buf, uint32_t data_size)
     memcpy(&copy_data_buf[addr & 0x03], data_buf, (data_size > copy_size) ? copy_size : data_size);
     if (copy_size)
     {
-        LOG_DEBUG("write head, addr: 0x%x, head size: %d", ADDR_ALIGN_WORD(addr), copy_size);
+        NRF_LOG_DEBUG("write head, addr: 0x%x, head size: %d", ADDR_ALIGN_WORD(addr), copy_size);
         ret_code = nrf_fstorage_write(&m_fs, ADDR_ALIGN_WORD(addr), copy_data_buf, 4, NULL);
         if (ret_code)
         {
-            LOG_ERROR("ret_code: %d", ret_code);
+            NRF_LOG_ERROR("ret_code: %d", ret_code);
             return -1;
         }
     }
@@ -101,14 +88,14 @@ int hal_flash_write(uint32_t addr, const uint8_t * data_buf, uint32_t data_size)
     // write middle part
     if (data_size - index > 4)
     {
-        LOG_DEBUG("write middle, addr: 0x%x, size: %d", ADDR_ALIGN_WORD(addr) + 4, data_size - index);
+        NRF_LOG_DEBUG("write middle, addr: 0x%x, size: %d", ADDR_ALIGN_WORD(addr) + 4, data_size - index);
         uint8_t offset = 0;
         do {
             memcpy(copy_data_buf, &data_buf[index], 4);
             ret_code = nrf_fstorage_write(&m_fs, ADDR_ALIGN_WORD(addr) + 4 + offset, copy_data_buf, 4, NULL);
             if (ret_code)
             {
-                LOG_ERROR("ret_code: %d", ret_code);
+                NRF_LOG_ERROR("ret_code: %d", ret_code);
                 return -2;
             }
             index += 4;
@@ -123,11 +110,11 @@ int hal_flash_write(uint32_t addr, const uint8_t * data_buf, uint32_t data_size)
 
     if (copy_size)
     {
-        LOG_DEBUG("write tail, addr: 0x%x, tail size: %d", addr + data_size - copy_size, copy_size);
+        NRF_LOG_DEBUG("write tail, addr: 0x%x, tail size: %d", addr + data_size - copy_size, copy_size);
         ret_code = nrf_fstorage_write(&m_fs, addr + data_size - copy_size, copy_data_buf, 4, NULL);
         if (ret_code)
         {
-            LOG_ERROR("ret_code: %d", ret_code);
+            NRF_LOG_ERROR("ret_code: %d", ret_code);
             return -3;
         }
     }
@@ -137,20 +124,15 @@ int hal_flash_write(uint32_t addr, const uint8_t * data_buf, uint32_t data_size)
 
 int hal_flash_erase_sector(uint32_t addr, uint32_t num_sectors)
 {
-    uint32_t ret_code;
+    uint32_t ret_code = 0;
 
-    NRF_LOG_DEBUG("nrf_fstorage_erase(addr=0x%p, len=%d pages), queue usage: %d",
-                  addr, num_sectors, m_flash_operations_pending);
+    NRF_LOG_DEBUG("nrf_fstorage_erase(addr=0x%p, len=%d pages)", addr, num_sectors);
 
     //lint -save -e611 (Suspicious cast)
     ret_code = nrf_fstorage_erase(&m_fs, addr, num_sectors, NULL);
     //lint -restore
 
-    if ((NRF_LOG_ENABLED) && (ret_code == NRF_SUCCESS))
-    {
-        m_flash_operations_pending++;
-    }
-    else
+    if (ret_code)
     {
         NRF_LOG_WARNING("nrf_fstorage_erase() failed with error 0x%x.", ret_code);
     }
