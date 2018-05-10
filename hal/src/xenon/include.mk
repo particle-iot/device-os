96ca952b82724105704a1be64843d6ba478d39fd
Andrey Tolstoy <avtolstoy@gmail.com>
2018-05-10 23:10:02 +0700

    xenon: OpenThread platform implementation

--- hal/src/xenon/include.mk
+++ hal/src/xenon/include.mk
@@ -15,19 +15,20 @@ INCLUDE_DIRS += $(HAL_INCL_NRF52840_PATH)
 INCLUDE_DIRS += $(HAL_SRC_INCL_PATH)/lwip
 INCLUDE_DIRS += $(HAL_SRC_INCL_PATH)/freertos
 INCLUDE_DIRS += $(HAL_SRC_INCL_PATH)/openthread
+INCLUDE_DIRS += $(HAL_SRC_INCL_PATH)/mbedtls
 INCLUDE_DIRS += $(HAL_MODULE_PATH)/network/lwip
 endif
 
 HAL_LINK ?= $(findstring hal,$(MAKE_DEPENDENCIES))
 
-HAL_DEPS = third_party/lwip third_party/freertos
+HAL_DEPS = third_party/lwip third_party/freertos third_party/openthread
 HAL_DEPS_INCLUDE_SCRIPTS =$(foreach module,$(HAL_DEPS),$(PROJECT_ROOT)/$(module)/import.mk)
 include $(HAL_DEPS_INCLUDE_SCRIPTS)
 
 # if hal is used as a make dependency (linked) then add linker commands
 ifneq (,$(HAL_LINK))
 
-HAL_LIB_DEP += $(FREERTOS_LIB_DEP) $(LWIP_LIB_DEP)
+HAL_LIB_DEP += $(FREERTOS_LIB_DEP) $(LWIP_LIB_DEP) $(OPENTHREAD_LIB_DEP)
 LIBS += $(notdir $(HAL_DEPS))
 
 LINKER_FILE=$(HAL_SRC_INCL_PATH)/app_no_bootloader.ld
--- hal/src/xenon/tlm.mk
+++ hal/src/xenon/tlm.mk
@@ -1,5 +1,5 @@
 # this is included in the top-level-module makefile to provide
 # HAL-specific defines
 
-DEPENDENCIES += communication newlib_nano third_party/freertos third_party/lwip
-MAKE_DEPENDENCIES += third_party/freertos third_party/lwip
+DEPENDENCIES += communication newlib_nano third_party/freertos third_party/lwip third_party/openthread
+MAKE_DEPENDENCIES += third_party/freertos third_party/lwip third_party/openthread
