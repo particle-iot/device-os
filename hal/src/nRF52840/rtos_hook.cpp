231da953c35a86edba5ee0ef08c3d6327066e37d
Sergey Polyakov <sergeuz@gmail.com>
2018-05-11 23:36:57 +0200

    Add stack overflow detection callback

--- hal/src/nRF52840/rtos_hook.cpp
+++ hal/src/nRF52840/rtos_hook.cpp
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2018 Particle Industries, Inc.  All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <FreeRTOS.h>
 #include <task.h>
 #include "service_debug.h"
@@ -9,7 +26,7 @@ extern "C" {
 void vApplicationStackOverflowHook(TaskHandle_t, char*) {
     PANIC(StackOverflow, "Stack overflow detected");
 }
-#endif
+#endif /* DEBUG_BUILD */
 
 void vApplicationMallocFailedHook(size_t xWantedSize)
 {
