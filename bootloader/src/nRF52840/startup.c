a12b8a5bdaab9c7d64bdfa381cf2229e01f04896
Andrey Tolstoy <avtolstoy@gmail.com>
2018-05-25 16:44:29 +0700

    [nrf52840] bootloader: removed naive memxxx implementations

--- bootloader/src/nRF52840/startup.c
+++ bootloader/src/nRF52840/startup.c
@@ -31,40 +31,7 @@ void platform_startup()
 /* Define abort() */
 #include <stdlib.h>
 
-caddr_t _sbrk(int incr)
+__attribute__((used)) caddr_t _sbrk(int incr)
 {
    return 0;
-}
-
-// Naive memXXX functions
-
-void* memcpy(void *dest, const void *src, size_t n) {
-    const uint8_t* p = src;
-    uint8_t* q = dest;
-    while (n--) {
-        *q++ = *p++;
-    }
-    return dest;
-}
-
-int memcmp(const void* s1, const void* s2, size_t n)
-{
-  const volatile uint8_t* p1 = (const volatile uint8_t*)s1;
-  const volatile uint8_t* p2 = (const volatile uint8_t*)s2;
-
-  while (n--)
-  {
-    if (*p1++ != *p2++)
-        return *p1 - *p2;
-  }
-  return 0;
-}
-
-void* memset(void* s, int c, size_t n) {
-    uint8_t* p = s;
-    uint8_t v = c & 0xff;
-    while (n--) {
-        *p++ = v;
-    }
-    return s;
 }
