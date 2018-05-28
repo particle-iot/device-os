407f49dba03f6dbe77f63b5e2e57c9198eba8042
Andrey Tolstoy <avtolstoy@gmail.com>
2018-05-28 19:26:06 +0700

    [nrf52840] hal: disable/enable irq implementation

--- hal/src/nRF52840/interrupts_hal.cpp
+++ hal/src/nRF52840/interrupts_hal.cpp
@@ -66,16 +66,6 @@ void HAL_EXTI_Handler(uint8_t EXTI_Line)
 {
 }
 
-
-int HAL_disable_irq()
-{
-    return 0;
-}
-
-void HAL_enable_irq(int is)
-{
-}
-
 int HAL_Set_Direct_Interrupt_Handler(IRQn_Type irqn, HAL_Direct_Interrupt_Handler handler, uint32_t flags, void* reserved)
 {
     return 1;
