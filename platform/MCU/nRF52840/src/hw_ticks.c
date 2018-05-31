d1e1532f5a141b23769f05805b390f8ae9ca5862
Andrey Tolstoy <andrey@enistek.com>
2018-06-01 03:10:10 +0700

    [nrf52840] platform: hw_ticks functions should use __disable_irq()/__enable_irq() instead of softdevice functions

--- platform/MCU/nRF52840/src/hw_ticks.c
+++ platform/MCU/nRF52840/src/hw_ticks.c
@@ -1,22 +1,26 @@
-/*
- * Copyright (c) 2018 Particle Industries, Inc.  All rights reserved.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+/**
+  Copyright (c) 2013-2015 Particle Industries, Inc.  All rights reserved.
+
+  Copyright 2012 STMicroelectronics
+  http://www.st.com/software_license_agreement_liberty_v2
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, either
+  version 3 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************
  */
 
+
 #include "hw_ticks.h"
-#include <nrf_nvic.h>
 #include <limits.h>
 
 /**
@@ -35,13 +39,15 @@ static volatile system_tick_t system_millis_clock = 0;
  */
 void System1MsTick(void)
 {
-    uint8_t is = 0;
-    sd_nvic_critical_region_enter(&is);
+    int is = __get_PRIMASK();
+    __disable_irq();
 
     ++system_millis;
     system_millis_clock = DWT->CYCCNT;
 
-    sd_nvic_critical_region_exit(is);
+    if ((is & 1) == 0) {
+        __enable_irq();
+    }
 }
 
 /**
@@ -57,12 +63,14 @@ uint64_t GetSystem1MsTick64()
 {
     uint64_t millis = 0;
 
-    uint8_t is = 0;
-    sd_nvic_critical_region_enter(&is);
+    const int is = __get_PRIMASK();
+    __disable_irq();
 
     millis = system_millis + (DWT->CYCCNT - system_millis_clock) / SYSTEM_US_TICKS / 1000;
 
-    sd_nvic_critical_region_exit(is);
+    if ((is & 1) == 0) {
+        __enable_irq();
+    }
 
     return millis;
 }
@@ -76,15 +84,17 @@ system_tick_t GetSystem1UsTick()
     system_tick_t base_millis;
     system_tick_t base_clock;
 
-    uint8_t is = 0;
-    sd_nvic_critical_region_enter(&is);
+    int is = __get_PRIMASK();
+    __disable_irq();
 
     base_millis = system_millis;
     base_clock = system_millis_clock;
 
     system_tick_t elapsed_since_millis = ((DWT->CYCCNT-base_clock) / SYSTEM_US_TICKS);
 
-    sd_nvic_critical_region_exit(is);
+    if ((is & 1) == 0) {
+        __enable_irq();
+    }
     return (base_millis * 1000) + elapsed_since_millis;
 }
 
@@ -93,14 +103,16 @@ system_tick_t GetSystem1UsTick()
  */
 void __advance_system1MsTick(uint64_t millis, system_tick_t micros_from_rollover)
 {
-    uint8_t is = 0;
-    sd_nvic_critical_region_enter(&is);
+    const int is = __get_PRIMASK();
+    __disable_irq();
 
     DWT->CYCCNT = UINT_MAX - (micros_from_rollover * SYSTEM_US_TICKS);
     system_millis_clock = DWT->CYCCNT;
     system_millis = millis;
 
-    sd_nvic_critical_region_exit(is);
+    if ((is & 1) == 0) {
+        __enable_irq();
+    }
 }
 
 void SysTick_Disable() {
