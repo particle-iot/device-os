dbf5fed63d9274a9279c401457df445e1a4a115c
Andrey Tolstoy <avtolstoy@gmail.com>
2018-06-09 21:35:00 +0700

    [nrf52840] hal: optimized flash_hal and exflash_hal

--- bootloader/src/nRF52840/sources.mk
+++ bootloader/src/nRF52840/sources.mk
@@ -4,6 +4,8 @@ CSRC += $(call target_files,$(BOOTLOADER_SRC_PATH)/,*.c)
 CPPSRC += $(call target_files,$(BOOTLOADER_SRC_PATH)/,*.cpp)
 
 CSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/nRF52840/,flash_hal.c)
+CPPSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/nRF52840/,flash_common.cpp)
+CSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/nRF52840/,exflash_hal.c)
 CPPSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/nRF52840/,dct_hal.cpp)
 
 LDFLAGS += -T$(COMMON_BUILD)/arm/linker/linker_nrf52840_bootloader.ld
