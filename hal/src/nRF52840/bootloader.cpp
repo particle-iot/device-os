f95258139e99a43920c04f9cde7f8892eebe8518
Eugene <eugene@particle.io>
2018-06-11 12:20:46 +0800

    Correct Bootloader address and Bootloader version info offset

--- hal/src/nRF52840/bootloader.cpp
+++ hal/src/nRF52840/bootloader.cpp
@@ -6,12 +6,14 @@
 #include "module_info.h"
 #include "bootloader_hal.h"
 
+#define BOOTLOADER_ADDR         (NRF_UICR->NRFFW[0])
+
 #ifdef HAL_REPLACE_BOOTLOADER_OTA
 int bootloader_update(const void* bootloader_image, unsigned length)
 {
     HAL_Bootloader_Lock(false);
     int result = (FLASH_CopyMemory(FLASH_INTERNAL, (uint32_t)bootloader_image,
-        FLASH_INTERNAL, 0x8000000, length, MODULE_FUNCTION_BOOTLOADER,
+        FLASH_INTERNAL, BOOTLOADER_ADDR, length, MODULE_FUNCTION_BOOTLOADER,
         MODULE_VERIFY_DESTINATION_IS_START_ADDRESS|MODULE_VERIFY_CRC|MODULE_VERIFY_FUNCTION));
     HAL_Bootloader_Lock(true);
     return result;
@@ -34,9 +36,9 @@ bool bootloader_requires_update(const uint8_t* bootloader_image, uint32_t length
     if ((bootloader_image == nullptr) || length == 0)
         return false;
 
-    const uint32_t VERSION_OFFSET = 0x184+10;
+    const uint32_t VERSION_OFFSET = 0x200+10;
 
-    uint16_t current_version = *(uint16_t*)(0x8000000+VERSION_OFFSET);
+    uint16_t current_version = *(uint16_t*)(BOOTLOADER_ADDR+VERSION_OFFSET);
     uint16_t available_version = *(uint16_t*)(bootloader_image+VERSION_OFFSET);
 
     bool requires_update = current_version<available_version;
