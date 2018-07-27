0ec7c472dcd6ae99e2aeb972fd5ef87519686eb5
Eugene <yuting0501@gmail.com>
2018-07-27 20:56:24 +0800

    Bug fixes

--- hal/src/nRF52840/adc_hal.cpp
+++ hal/src/nRF52840/adc_hal.cpp
@@ -20,6 +20,8 @@
 #include "adc_hal.h"
 #include "pinmap_impl.h"
 
+static volatile bool m_adc_initiated = false;
+
 static const nrfx_saadc_config_t saadc_config = 
 {
     .resolution         = NRF_SAADC_RESOLUTION_12BIT,
@@ -44,9 +46,14 @@ void HAL_ADC_Set_Sample_Time(uint8_t ADC_SampleTime)
  */
 int32_t HAL_ADC_Read(uint16_t pin)
 {
+    if (!m_adc_initiated)
+    {
+        m_adc_initiated = true;
+        HAL_ADC_DMA_Init();
+    }
+
     int16_t    adc_value = 0;
     ret_code_t ret_code;
-    
     nrf_saadc_input_t nrf_adc_channel;
     NRF5x_Pin_Info *PIN_MAP = HAL_Pin_Map();
 
@@ -62,6 +69,11 @@ int32_t HAL_ADC_Read(uint16_t pin)
         case 7: nrf_adc_channel = NRF_SAADC_INPUT_AIN7; break;
         default:
             return 0;
+    }
+
+    if (PIN_MAP[pin].pin_func != PF_NONE && PIN_MAP[pin].pin_func != PF_DIO)
+    {
+        return 0;
     }
 
      //Single ended, negative input to ADC shorted to GND.
@@ -80,24 +92,29 @@ int32_t HAL_ADC_Read(uint16_t pin)
     ret_code = nrfx_saadc_channel_init(PIN_MAP[pin].adc_channel, &channel_config);
     if (ret_code)
     {
-        return 0;
+        goto err_ret;
     }
 
     ret_code = nrfx_saadc_sample_convert(PIN_MAP[pin].adc_channel, &adc_value);
     if (ret_code)
     {
-        return 0;
+        goto err_ret;
     }
 
     if (adc_value < 0)
     {
-        // Even in the single ended mode measured value can be {-0}. Saturation for avoid casting to a big integer.
-        return 0;
+        // Even in the single ended mode measured value can be negative value. Saturation for avoid casting to a big integer.
+        goto err_ret;
     }
     else
     {
+        nrfx_saadc_channel_uninit(PIN_MAP[pin].adc_channel);
         return (uint16_t) adc_value;
     }
+
+err_ret:
+    nrfx_saadc_channel_uninit(PIN_MAP[pin].adc_channel);
+    return 0;
 }
 
 /*
@@ -105,5 +122,6 @@ int32_t HAL_ADC_Read(uint16_t pin)
  */
 void HAL_ADC_DMA_Init()
 {
-    nrfx_saadc_init(&saadc_config, analog_in_event_handler);
+    uint32_t err_code = nrfx_saadc_init(&saadc_config, analog_in_event_handler);
+    SPARK_ASSERT(err_code == NRF_SUCCESS);
 }
