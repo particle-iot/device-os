1a48a9d1987103d7d81e7bcfe0d2f685f5778ec9
Andrey Tolstoy <avtolstoy@gmail.com>
2018-08-06 00:44:14 +0700

    [nrf52840] hal: thread-safe __cxa_guard_xxx

--- hal/src/nRF52840/newlib.cpp
+++ hal/src/nRF52840/newlib.cpp
@@ -58,22 +58,6 @@ void _exit(int status) {
 }
 
 
-
-/* Default implementation for call made to pure virtual function. */
-void __cxa_pure_virtual() {
-  PANIC(PureVirtualCall,"Call on pure virtual");
-  while (1);
-}
-
-/* Provide default implemenation for __cxa_guard_acquire() and
- * __cxa_guard_release(). Note: these must be revisited if a multitasking
- * OS is ported to this platform. */
-__extension__ typedef int __guard __attribute__((mode (__DI__)));
-int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);};
-void __cxa_guard_release (__guard *g) {*(char *)g = 1;};
-void __cxa_guard_abort (__guard *) {};
-
-
 int _write(int file, char *ptr, int len) { return 0; }
 int _read(int file, char *ptr, int len) { return 0; }
 int _close(int file) { return 0; }
