a42b34966d2d239224112580d43d1d573e6a7d4f
Matthew McGowan <mat.mcgowan+git@gmail.com>
2018-08-15 11:46:07 +0200

    adds build for argon bootloader

--- bootloader/src/nRF52840/sources.mk
+++ bootloader/src/nRF52840/sources.mk
@@ -10,8 +10,8 @@ CSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/nRF52840/,rgble
 CSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/nRF52840/,watchdog_hal.c)
 CPPSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/nRF52840/,dct_hal.cpp)
 # FIXME
-CPPSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/xenon/littlefs/,*.cpp)
-CSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/xenon/littlefs/,*.c)
+CPPSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/nRF52840/littlefs/,*.cpp)
+CSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/nRF52840/littlefs/,*.c)
 
 LDFLAGS += -T$(COMMON_BUILD)/arm/linker/linker_nrf52840_bootloader.ld
 LINKER_DEPS += $(COMMON_BUILD)/arm/linker/linker_nrf52840_bootloader.ld
