afc08dd83d7b6676f7858710ec92d953c622c8f2
Sergey Polyakov <sergeuz@gmail.com>
2018-08-15 17:52:21 +0200

    Define some lightweight abstractions for streams and files

---
+++ services/inc/file.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2018 Particle Industries, Inc.  All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <cstddef>
+
+namespace particle {
+
+// Base abstract class for a random access input file
+class InputFile {
+public:
+    virtual ~InputFile() = default;
+
+    virtual int read(char* data, size_t size, size_t offs) = 0;
+    virtual int size() const = 0;
+};
+
+} // particle
