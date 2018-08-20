d9c4d47da11dfd6555b051b528d847011982cb33
Sergey Polyakov <sergeuz@gmail.com>
2018-08-20 18:55:10 +0200

    Add test code for NCP firmware update

--- user/tests/app/xmodem_sender/xmodem_sender.cpp
+++ user/tests/app/xmodem_sender/xmodem_sender.cpp
@@ -1,78 +1,78 @@
 #include "application.h"
 
+#include "exflash_hal.h"
+
 #include "xmodem_sender.h"
+#include "check.h"
 #include "../../../../services/inc/stream.h"
-#include "file.h"
+
+#include <cstdarg>
 
 SYSTEM_MODE(SEMI_AUTOMATIC)
 
 namespace {
 
-const size_t FILE_SIZE = 3500;
-
-class DummyFile: public InputFile {
+// Input stream generating text data
+class TextStream: public InputStream {
 public:
-    DummyFile() :
-            size_(0) {
+    explicit TextStream(size_t size) :
+            size_(size),
+            c_('A') {
     }
 
-    ~DummyFile() {
-        destroy();
-    }
-
-    int init(size_t size) {
-        data_.reset(new(std::nothrow) char[size]);
-        if (!data_) {
-            return SYSTEM_ERROR_NO_MEMORY;
-        }
-        // Fill the buffer with some data
-        char c = 'A';
-        for (size_t i = 0; i < size; ++i) {
-            data_[i] = c++;
-            if (c > 'Z') {
-                c = 'A';
-            }
-        }
-        size_ = size;
-        return 0;
-    }
-
-    void destroy() {
-        data_.reset();
-        size_ = 0;
-    }
-
-    int read(char* data, size_t size, size_t offs) override {
-        if (offs + size > size_) {
+    int read(char* data, size_t size) override {
+        // XmodemSender shouldn't try to read more bytes than necessary
+        if (size > size_) {
             return SYSTEM_ERROR_OUT_OF_RANGE;
         }
-        memcpy(data, data_.get() + offs, size);
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = c_++;
+            if (c_ > 'Z') {
+                c_ = 'A';
+            }
+        }
+        size_ -= size;
         return size;
     }
 
-    int size() const override {
-        return size_;
+private:
+    size_t size_;
+    char c_;
+};
+
+// Input stream reading data from the external flash
+class ExtFlashStream: public InputStream {
+public:
+    explicit ExtFlashStream(size_t offs, size_t size) :
+            size_(size),
+            offs_(offs) {
+    }
+
+    int read(char* data, size_t size) override {
+        if (size > size_) {
+            return SYSTEM_ERROR_OUT_OF_RANGE;
+        }
+        const int ret = hal_exflash_read(offs_, (uint8_t*)data, size);
+        if (ret != 0) {
+            return SYSTEM_ERROR_UNKNOWN;
+        }
+        size_ -= size;
+        offs_ += size;
+        return size;
     }
 
 private:
-    std::unique_ptr<char[]> data_;
     size_t size_;
+    size_t offs_;
 };
 
-class Serial1Stream: public particle::Stream {
+class SerialStream: public particle::Stream {
 public:
-    ~Serial1Stream() {
-        destroy();
-    }
-
-    int init() {
+    SerialStream() {
         Serial1.begin(115200);
-        // FIXME: XMODEM transfer fails if the non-blocking mode is enabled
-        // Serial1.blockOnOverrun(false);
-        return 0;
     }
 
-    void destroy() {
+    ~SerialStream() {
         Serial1.end();
     }
 
@@ -85,33 +85,86 @@ public:
     }
 };
 
-std::unique_ptr<XmodemSender> g_sender;
-std::unique_ptr<Serial1Stream> g_stream;
-std::unique_ptr<DummyFile> g_file;
+class DummyModem {
+public:
+    explicit DummyModem(particle::Stream* strm) :
+            strm_(strm) {
+    }
+
+    int writeCommand(const char* fmt, ...) {
+        va_list args;
+        va_start(args, fmt);
+        char buf[256];
+        size_t n = CHECK(vsnprintf(buf, sizeof(buf) - 2, fmt, args));
+        buf[n++] = '\r';
+        buf[n++] = '\n';
+        CHECK(strm_->write(buf, n));
+        Log.write(buf, n);
+        return n;
+    }
+
+    int readResponse(unsigned timeout = 1000) {
+        char buf[256];
+        size_t size = 0;
+        system_tick_t t = millis();
+        do {
+            char c = 0;
+            const size_t n = CHECK(strm_->read(&c, 1));
+            if (n > 0) {
+                buf[size++] = c;
+            }
+        } while (millis() - t < timeout);
+        Log.write(buf, size);
+        return size;
+    }
+
+private:
+    particle::Stream* strm_;
+};
+
+std::unique_ptr<XmodemSender> sender;
+std::unique_ptr<SerialStream> serialStrm;
+std::unique_ptr<DummyModem> modem;
+std::unique_ptr<ExtFlashStream> extFlashStrm;
+std::unique_ptr<TextStream> textStrm;
 
 const RttLogHandler logHandler(LOG_LEVEL_ALL);
+
+void initTextStream() {
+    const size_t SIZE = 3500;
+    textStrm.reset(new TextStream(SIZE));
+    sender->init(serialStrm.get(), textStrm.get(), SIZE);
+}
+
+void initExtFlashStream() {
+    const size_t SIZE = 825168;
+    const size_t OFFSET = 0x00200000;
+    extFlashStrm.reset(new ExtFlashStream(OFFSET, SIZE));
+    sender->init(serialStrm.get(), extFlashStrm.get(), SIZE);
+    modem->writeCommand("AT+FWUPD=%u", SIZE);
+    modem->readResponse();
+}
 
 } // unnamed
 
 void setup() {
-    g_file.reset(new(std::nothrow) DummyFile);
-    g_file->init(FILE_SIZE);
-    g_stream.reset(new(std::nothrow) Serial1Stream);
-    g_stream->init();
-    g_sender.reset(new(std::nothrow) XmodemSender);
-    g_sender->init(g_stream.get(), g_file.get());
+    serialStrm.reset(new SerialStream);
+    sender.reset(new XmodemSender);
+    modem.reset(new DummyModem(serialStrm.get()));
+    initTextStream();
+    // initExtFlashStream();
 }
 
 void loop() {
-    if (g_sender) {
-        const int ret = g_sender->run();
+    if (sender) {
+        const int ret = sender->run();
         if (ret != XmodemSender::RUNNING) {
             if (ret == XmodemSender::DONE) {
                 Log.info("XMODEM transfer finished");
-            } else if (ret < 0) {
+            } else {
                 Log.error("XMODEM transfer failed: %d", ret);
             }
-            g_sender.reset();
+            sender.reset();
         }
     }
 }
