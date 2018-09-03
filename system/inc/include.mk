d27a9000057350565f00303751fbfef578dcb557
Sergey Polyakov <sergeuz@gmail.com>
2018-09-03 13:45:36 +0200

    Update dependencies of the system module

--- system/inc/include.mk
+++ system/inc/include.mk
@@ -1,3 +1,13 @@
 
 # Add include to all objects built for this target
 INCLUDE_DIRS += $(SYSTEM_MODULE_PATH)/inc
+
+SYSTEM_DEPS = third_party/miniz
+SYSTEM_DEPS_INCLUDE_SCRIPTS =$(foreach module,$(SYSTEM_DEPS),$(PROJECT_ROOT)/$(module)/import.mk)
+include $(SYSTEM_DEPS_INCLUDE_SCRIPTS)
+
+ifneq ($(filter services,$(LIBS)),)
+SYSTEM_LIB_DEP += $(MINIZ_LIB_DEP)
+LIBS += $(notdir $(SYSTEM_DEPS))
+LIB_DIRS += $(MINIZ_LIB_DIR)
+endif
