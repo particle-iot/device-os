bf25978b4474cc8afa3c2591dce78b3c9138291c
Sergey Polyakov <sergeuz@gmail.com>
2018-09-03 16:20:19 +0200

    Fix a copy-paste error

--- system/inc/include.mk
+++ system/inc/include.mk
@@ -6,7 +6,7 @@ SYSTEM_DEPS = third_party/miniz
 SYSTEM_DEPS_INCLUDE_SCRIPTS =$(foreach module,$(SYSTEM_DEPS),$(PROJECT_ROOT)/$(module)/import.mk)
 include $(SYSTEM_DEPS_INCLUDE_SCRIPTS)
 
-ifneq ($(filter services,$(LIBS)),)
+ifneq ($(filter system,$(LIBS)),)
 SYSTEM_LIB_DEP += $(MINIZ_LIB_DEP)
 LIBS += $(notdir $(SYSTEM_DEPS))
 LIB_DIRS += $(MINIZ_LIB_DIR)
