a61b49560b63eebc826f9ccd51a2bf887a96a4ff
XuGuohui <xghui999@126.com>
2018-09-05 15:28:08 +0800

    Mesh: temporarily not import the HAL USB functions.

--- hal-dynalib/src/hal_usb.c
+++ hal-dynalib/src/hal_usb.c
@@ -1,3 +1,8 @@
+// FIXME: Remove it when USB CDC is implemented.
+#if PLATFORM_ID == 12 || PLATFORM_ID == 13 || PLATFORM_ID == 14
+#define HAL_USB_EXCLUDE
+#endif
+
 #ifndef HAL_USB_EXCLUDE
 #include "hal_dynalib_usb.h"
 #endif
