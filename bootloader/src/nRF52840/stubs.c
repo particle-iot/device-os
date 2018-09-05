9586fa7ac005025473b08b62241c506066d255ed
Eugene <yuting0501@gmail.com>
2018-09-05 23:27:44 +0800

    Copy the implementation of backup register to bootloader

--- bootloader/src/nRF52840/stubs.c
+++ bootloader/src/nRF52840/stubs.c
@@ -1,24 +1,37 @@
 #include "core_hal.h"
+#include "nrf_delay.h"
+
+#define BACKUP_REGISTER_NUM        10
+static int backup_register[BACKUP_REGISTER_NUM] __attribute__((section(".backup_system")));
 
 int32_t HAL_Core_Backup_Register(uint32_t BKP_DR)
 {
-    /* FIXME */
-    return -1;
+    if ((BKP_DR == 0) || (BKP_DR > BACKUP_REGISTER_NUM)) {
+        return -1;
+    }
+
+    return BKP_DR - 1;
 }
 
 void HAL_Core_Write_Backup_Register(uint32_t BKP_DR, uint32_t Data)
 {
-    /* FIXME */
+    int32_t BKP_DR_Index = HAL_Core_Backup_Register(BKP_DR);
+    if (BKP_DR_Index != -1) {
+        backup_register[BKP_DR_Index] = Data;
+    }
 }
 
 uint32_t HAL_Core_Read_Backup_Register(uint32_t BKP_DR)
 {
-    /* FIXME */
+    int32_t BKP_DR_Index = HAL_Core_Backup_Register(BKP_DR);
+    if (BKP_DR_Index != -1) {
+        return backup_register[BKP_DR_Index];
+    }
     return 0xFFFFFFFF;
 }
 
 void HAL_Delay_Microseconds(uint32_t uSec) {
-    /* FIXME */
+    nrf_delay_us(uSec);
 }
 
 void HAL_Core_System_Reset_Ex(int reason, uint32_t data, void *reserved) {
