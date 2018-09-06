7a728f0316451ba365964ad223d7f66730ac9064
Andrey Tolstoy <avtolstoy@gmail.com>
2018-09-06 17:43:35 +0700

    [nrf52840] sdk_config_system: disabling NRX_UARTE

--- platform/MCU/nRF52840/inc/sdk_config_system.h
+++ platform/MCU/nRF52840/inc/sdk_config_system.h
@@ -88,7 +88,7 @@
 
 #define NRFX_PRS_ENABLED        1
 #define NRFX_UART_ENABLED       0
-#define NRFX_UARTE_ENABLED      1
+#define NRFX_UARTE_ENABLED      0
 #define NRFX_LPCOMP_ENABLED     0
 
 #define NRFX_TWI_ENABLED        0
