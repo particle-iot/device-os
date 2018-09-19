46a2e758da5b37676e472d0970b725c6ab7a4362
XuGuohui <xghui999@126.com>
2018-09-19 10:36:20 +0800

    Mesh: move the platform specific bootloader_platform_xx.c to under the corresponding platform folder.

---
+++ hal/src/argon/bootloader_platform_12.c
@@ -0,0 +1,4 @@
+const unsigned char bootloader_platform_12_bin[] = {
+
+};
+const unsigned int bootloader_platform_12_bin_len = 0;
---
+++ hal/src/boron/bootloader_platform_13.c
@@ -0,0 +1,4 @@
+const unsigned char bootloader_platform_13_bin[] = {
+
+};
+const unsigned int bootloader_platform_13_bin_len = 0;
