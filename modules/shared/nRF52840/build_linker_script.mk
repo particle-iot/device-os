1f9d9e77f44e22d6a708940897faaba94afed488
XuGuohui <xghui999@126.com>
2018-09-19 20:31:35 +0800

    Mesh: use echo command to generate the module_user_memory.ld

--- modules/shared/nRF52840/build_linker_script.mk
+++ modules/shared/nRF52840/build_linker_script.mk
@@ -1,12 +1,12 @@
 
-WRITE_FILE_CREATE =$(file >$(1),$(2))
-WRITE_FILE_APPEND =$(file >>$(1),$(2))
+WRITE_FILE_CREATE = $(shell echo "$(2)" >$(1))
+WRITE_FILE_APPEND = $(shell echo "$(2)" >>$(1))
 
 COMMA := ,
 
 ifneq (,$(PREBUILD))
 # Should declare enough RAM for inermediate linker script: 96K
-USER_SRAM_LENGTH = 0x18000
+USER_SRAM_LENGTH = 96K
 else
 DATA_SECTION_LEN  = $(shell arm-none-eabi-objdump -h --section=.data $(INTERMEDIATE_ELF) | grep .data)
 DATA_SECTION_LEN := 0x$(word 3,$(DATA_SECTION_LEN))
@@ -21,7 +21,7 @@ endif
 # 8K is the backup ram length plusing the stack length
 all:
 	@echo Creating module_user_memory.ld ...
-	$(call WRITE_FILE_APPEND, module_user_memory.ld,user_module_app_flash_origin = 0xD4000;)
+	$(call WRITE_FILE_CREATE, module_user_memory.ld,user_module_app_flash_origin = 0xD4000;)
 	$(call WRITE_FILE_APPEND, module_user_memory.ld,user_module_app_flash_length = 128K;)
 	$(call WRITE_FILE_APPEND, module_user_memory.ld,)
 	$(call WRITE_FILE_APPEND, module_user_memory.ld,user_module_sram_origin = 0x20040000 - 6K - $(USER_SRAM_LENGTH);)
