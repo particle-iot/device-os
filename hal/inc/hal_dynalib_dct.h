82662ba9d5efd051889f805cf7cee021140ac6d1
Andrey Tolstoy <avtolstoy@gmail.com>
2018-09-24 21:14:30 +0700

    [nrf52840] hal: removes deprecated dct functions from dynalib

--- hal/inc/hal_dynalib_dct.h
+++ hal/inc/hal_dynalib_dct.h
@@ -9,11 +9,16 @@
 
 DYNALIB_BEGIN(hal_dct)
 
+#if HAL_PLATFORM_DCT_NO_DEPRECATED
+DYNALIB_FN(0, hal_dct, dct_read_app_data_copy, int(uint32_t, void*, size_t))
+DYNALIB_FN(1, hal_dct, dct_write_app_data, int(const void*, uint32_t, uint32_t))
+DYNALIB_FN(2, hal_dct, fetch_or_generate_setup_ssid, bool(device_code_t*))
+#else
 DYNALIB_FN(0, hal_dct, dct_read_app_data, const void*(uint32_t))
 DYNALIB_FN(1, hal_dct, dct_read_app_data_copy, int(uint32_t, void*, size_t))
 DYNALIB_FN(2, hal_dct, dct_read_app_data_lock, const void*(uint32_t))
 DYNALIB_FN(3, hal_dct, dct_read_app_data_unlock, int(uint32_t))
 DYNALIB_FN(4, hal_dct, dct_write_app_data, int(const void*, uint32_t, uint32_t))
 DYNALIB_FN(5, hal_dct, fetch_or_generate_setup_ssid, bool(device_code_t*))
-
+#endif // HAL_PLATFORM_DCT_NO_DEPRECATED
 DYNALIB_END(hal_dct)
