fe5cd839d810f6d4e121ef62b63b4249038ff05c
XuGuohui <xghui999@126.com>
2018-09-27 22:28:39 +0800

    Mesh: update the bootloader images.

--- modules/shared/system_module_version.mk
+++ modules/shared/system_module_version.mk
@@ -31,7 +31,7 @@ endif
 # Skip to next 100 every v0.x.0 release (e.g. 11 for v0.6.2 to 100 for v0.7.0-rc.1),
 # but only if the bootloader has changed since the last v0.x.0 release.
 # Bump by 1 for every updated bootloader image for a release with the same v0.x.* base.
-BOOTLOADER_VERSION ?= 209
+BOOTLOADER_VERSION ?= 210
 
 # the version of the bootloader that the system firmware requires
 BOOTLOADER_DEPENDENCY = 101
