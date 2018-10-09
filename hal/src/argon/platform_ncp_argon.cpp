0b2aeb9aabdc033bb1e99d8345df9c2a3cc00b3f
Sergey Polyakov <sergeuz@gmail.com>
2018-10-09 14:51:14 +0200

    Whitespace fixes

--- hal/src/argon/platform_ncp_argon.cpp
+++ hal/src/argon/platform_ncp_argon.cpp
@@ -12,12 +12,12 @@
 #include <algorithm>
 
 MeshNCPIdentifier platform_current_ncp_identifier() {
-	return MESH_NCP_ESP32;
+    return MESH_NCP_ESP32;
 }
 
 class OtaUpdateSourceStream : public particle::InputStream {
 public:
-	OtaUpdateSourceStream(const uint8_t* buffer, size_t length) : buffer(buffer), remaining(length) {}
+    OtaUpdateSourceStream(const uint8_t* buffer, size_t length) : buffer(buffer), remaining(length) {}
 
     int read(char* data, size_t size) override {
         CHECK(peek(data, size));
@@ -62,35 +62,35 @@ public:
     }
 
 private:
-	const uint8_t* buffer;
-	size_t remaining;
+    const uint8_t* buffer;
+    size_t remaining;
 };
 
 // FIXME: This function accesses the module info via XIP and may fail to parse it correctly under
 // some not entirely clear circumstances. Disabling compiler optimizations helps to work around
 // the problem
 __attribute__((optimize("O0"))) hal_update_complete_t platform_ncp_update_module(const hal_module_t* module) {
-	const auto ncpClient = particle::wifiNetworkManager()->ncpClient();
+    const auto ncpClient = particle::wifiNetworkManager()->ncpClient();
     SPARK_ASSERT(ncpClient);
     CHECK_RETURN(ncpClient->on(), HAL_UPDATE_ERROR);
-	// we pass only the actual binary after the module info and up to the suffix
-	const uint8_t* start = (const uint8_t*)module->info;
-	static_assert(sizeof(module_info_t)==24, "expected module info size to be 24");
-	start += sizeof(module_info_t);		// skip the module info
-	const uint8_t* end = start + (uint32_t(module->info->module_end_address) - uint32_t(module->info->module_start_address));
-	const unsigned length = end-start;
-	OtaUpdateSourceStream moduleStream(start, length);
-	uint16_t version = 0;
-	int r = ncpClient->getFirmwareModuleVersion(&version);
+    // we pass only the actual binary after the module info and up to the suffix
+    const uint8_t* start = (const uint8_t*)module->info;
+    static_assert(sizeof(module_info_t)==24, "expected module info size to be 24");
+    start += sizeof(module_info_t); // skip the module info
+    const uint8_t* end = start + (uint32_t(module->info->module_end_address) - uint32_t(module->info->module_start_address));
+    const unsigned length = end-start;
+    OtaUpdateSourceStream moduleStream(start, length);
+    uint16_t version = 0;
+    int r = ncpClient->getFirmwareModuleVersion(&version);
     if (r == 0) {
         LOG(INFO, "Updating ESP32 firmware from version %d to version %d", version, module->info->module_version);
     }
-	r = ncpClient->updateFirmware(&moduleStream, length);
+    r = ncpClient->updateFirmware(&moduleStream, length);
     LED_On(LED_RGB);
     CHECK_RETURN(r, HAL_UPDATE_ERROR);
-	r = ncpClient->getFirmwareModuleVersion(&version);
+    r = ncpClient->getFirmwareModuleVersion(&version);
     if (r == 0) {
-	   LOG(INFO, "ESP32 firmware version updated to version %d", version);
+        LOG(INFO, "ESP32 firmware version updated to version %d", version);
     }
-	return HAL_UPDATE_APPLIED;
+    return HAL_UPDATE_APPLIED;
 }
