9355bb375e5d0ffdf15d157a9bc79094150f6eae
Matthew McGowan <mat.mcgowan+git@gmail.com>
2018-11-13 15:00:44 +0100

    comments on panic codes

--- services/inc/panic_codes.h
+++ services/inc/panic_codes.h
@@ -9,21 +9,23 @@
 // followed by 300 blinks of the value of code
 /// ...---...    code   ...---...
 
-def_panic_codes(Faults,RGB_COLOR_RED,HardFault)
-def_panic_codes(Faults,RGB_COLOR_RED,NMIFault)
-def_panic_codes(Faults,RGB_COLOR_RED,MemManage)
-def_panic_codes(Faults,RGB_COLOR_RED,BusFault)
-def_panic_codes(Faults,RGB_COLOR_RED,UsageFault)
+def_panic_codes(Faults,RGB_COLOR_RED,HardFault)		// 1
+def_panic_codes(Faults,RGB_COLOR_RED,NMIFault)		// 2
+def_panic_codes(Faults,RGB_COLOR_RED,MemManage)		// 3
+def_panic_codes(Faults,RGB_COLOR_RED,BusFault)		// 4
+def_panic_codes(Faults,RGB_COLOR_RED,UsageFault)	// 5
 
-def_panic_codes(Cloud,RGB_COLOR_RED,InvalidLenth)
+def_panic_codes(Cloud,RGB_COLOR_RED,InvalidLenth)	// 6
 
-def_panic_codes(System,RGB_COLOR_RED,Exit)
-def_panic_codes(System,RGB_COLOR_RED,OutOfHeap)
+def_panic_codes(System,RGB_COLOR_RED,Exit)			// 7
+def_panic_codes(System,RGB_COLOR_RED,OutOfHeap)		// 8
 
-def_panic_codes(System,RGB_COLOR_RED,SPIOverRun)
+def_panic_codes(System,RGB_COLOR_RED,SPIOverRun)	// 9
 
-def_panic_codes(Software,RGB_COLOR_RED,AssertionFailure)
-def_panic_codes(Software,RGB_COLOR_RED,InvalidCase)
-def_panic_codes(Software,RGB_COLOR_RED,PureVirtualCall)
+def_panic_codes(Software,RGB_COLOR_RED,AssertionFailure)	// 10
+def_panic_codes(Software,RGB_COLOR_RED,InvalidCase)			// 11
+def_panic_codes(Software,RGB_COLOR_RED,PureVirtualCall)		// 12
 
-def_panic_codes(System,RGB_COLOR_RED,StackOverflow)
+def_panic_codes(System,RGB_COLOR_RED,StackOverflow)			// 13
+
+def_panic_codes(System,RGB_COLOR_RED,SemaphoreLockTimeout)	// 14
