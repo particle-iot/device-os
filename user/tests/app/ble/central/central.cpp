fbbb06753dadb3015967b9e2e529f6c96d7d0bbe
XuGuohui <xghui999@126.com>
2019-01-24 17:18:57 +0800

    BLE: implement BLE database discovery APIs.

--- user/tests/app/ble/central/central.cpp
+++ user/tests/app/ble/central/central.cpp
@@ -25,6 +25,8 @@ SYSTEM_MODE(MANUAL);
 
 Serial1LogHandler log(115200, LOG_LEVEL_ALL);
 
+uint8_t svcUUID[] = {0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x08,0x09,0x0a,0x0b,0x00,0x00,0x0e,0x10};
+
 static void handleBleEvent(hal_ble_event_t *event)
 {
     if (event->evt_type == BLE_EVT_TYPE_SCAN_RESULT) {
@@ -42,14 +44,73 @@ static void handleBleEvent(hal_ble_event_t *event)
             }
         }
     }
-    if (event->evt_type == BLE_EVT_TYPE_CONNECTION) {
+    else if (event->evt_type == BLE_EVT_TYPE_CONNECTION) {
         if (event->conn_event.evt_id == BLE_CONN_EVT_ID_CONNECTED) {
             LOG(TRACE, "BLE connected, handle: %d", event->conn_event.conn_handle);
+
+            LOG(TRACE, "Start discovering service...");
+            ble_discover_all_services(event->conn_event.conn_handle);
+            //ble_discover_service_by_uuid128(event->conn_event.conn_handle, svcUUID);
+            //ble_discover_service_by_uuid16(event->conn_event.conn_handle, 0x1234);
         }
         else {
             LOG(TRACE, "BLE disconnected, handle: %d", event->conn_event.conn_handle);
             LOG(TRACE, "Start scanning...");
             ble_start_scanning();
+        }
+    }
+    else if (event->evt_type == BLE_EVT_TYPE_DISCOVERY) {
+        if (event->disc_event.evt_id == BLE_DISC_EVT_ID_SVC_DISCOVERED) {
+            LOG(TRACE, "BLE primary service discovered %d", event->disc_event.count);
+            for (uint8_t i = 0; i < event->disc_event.count; i++) {
+                LOG(TRACE, "Handle range: %d ~ %d", event->disc_event.services[i].start_handle, event->disc_event.services[i].end_handle);
+                if (event->disc_event.services[i].uuid.type == BLE_UUID_TYPE_16BIT) {
+                    LOG(TRACE, "Service UUID: 0x%04X", event->disc_event.services[i].uuid.uuid16);
+                }
+                else if (event->disc_event.services[i].uuid.type == BLE_UUID_TYPE_128BIT) {
+                    Serial1.print("Service UUID: 0x");
+                    for (uint8_t j = 0; j < BLE_SIG_UUID_128BIT_LEN; j++) {
+                        Serial1.printf("%02X", event->disc_event.services[i].uuid.uuid128[j]);
+                    }
+                    Serial1.print("\r\n");
+                }
+            }
+
+            ble_discover_characteristics(event->disc_event.conn_handle, 14, 16);
+        }
+        else if (event->disc_event.evt_id == BLE_DISC_EVT_ID_CHAR_DISCOVERED) {
+            LOG(TRACE, "BLE characteristics discovered: %d", event->disc_event.count);
+            for (uint8_t i = 0; i < event->disc_event.count; i++) {
+                if (event->disc_event.characteristics[i].uuid.type == BLE_UUID_TYPE_16BIT) {
+                    LOG(TRACE, "Characteristic UUID: 0x%04X", event->disc_event.characteristics[i].uuid.uuid16);
+                }
+                else if (event->disc_event.characteristics[i].uuid.type == BLE_UUID_TYPE_128BIT) {
+                    Serial1.print("Characteristic UUID: 0x");
+                    for (uint8_t j = 0; j < BLE_SIG_UUID_128BIT_LEN; j++) {
+                        Serial1.printf("%02X", event->disc_event.characteristics[i].uuid.uuid128[j]);
+                    }
+                    Serial1.print("\r\n");
+                }
+            }
+
+            ble_discover_descriptors(event->disc_event.conn_handle, 17, 65535);
+        }
+        else if (event->disc_event.evt_id == BLE_DISC_EVT_ID_DESC_DISCOVERED) {
+            LOG(TRACE, "BLE descriptors discovered: %d", event->disc_event.count);
+            for (uint8_t i = 0; i < event->disc_event.count; i++) {
+                if (event->disc_event.descriptors[i].uuid.type == BLE_UUID_TYPE_16BIT) {
+                    LOG(TRACE, "Descriptor handle: %d, UUID: 0x%04X",
+                            event->disc_event.descriptors[i].handle,
+                            event->disc_event.descriptors[i].uuid.uuid16);
+                }
+                else if (event->disc_event.descriptors[i].uuid.type == BLE_UUID_TYPE_128BIT) {
+                    Serial1.printf("Descriptors handle: %d, UUID: 0x", event->disc_event.descriptors[i].handle);
+                    for (uint8_t j = 0; j < BLE_SIG_UUID_128BIT_LEN; j++) {
+                        Serial1.printf("%02X", event->disc_event.descriptors[i].uuid.uuid128[j]);
+                    }
+                    Serial1.print("\r\n");
+                }
+            }
         }
     }
 }
