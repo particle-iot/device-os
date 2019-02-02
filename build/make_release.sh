280f15395ae636f4963edfa4c12a7e107fe0fb43
Zachary J. Fields <zachary_fields@yahoo.com>
2019-02-01 20:31:53 -0600

    Update release.sh parameter handling
    Now uses `getopt` to parse named parameters
    Allows for specification of the output files
    Restructures the output folder hierarchy
    Tweek filenames to adhere to semantic versioning

--- build/make_release.sh
+++ build/make_release.sh
@@ -15,22 +15,18 @@ fi
 echo attempting build for PLATFORM: $MAKE_RELEASE_PLATFORM
 
 if [ $MAKE_RELEASE_PLATFORM == "photon" ] || [ $MAKE_RELEASE_PLATFORM == "6" ]; then
-	./release.sh 6 photon
-else if [ $MAKE_RELEASE_PLATFORM == "p1" ] || [ $MAKE_RELEASE_PLATFORM == "8" ]; then
-	./release.sh 8 p1
-else if [ $MAKE_RELEASE_PLATFORM == "electron" ] || [ $MAKE_RELEASE_PLATFORM == "10" ]; then
-	./release.sh 10 electron
-else if [ $MAKE_RELEASE_PLATFORM == "core" ] || [ $MAKE_RELEASE_PLATFORM == "0" ]; then
-	./release.sh 0 core
-else if [ $MAKE_RELEASE_PLATFORM == "all" ]; then
-	./release.sh 6 photon
-	./release.sh 8 p1
-	./release.sh 10 electron
-	./release.sh 0 core
+	./release.sh --platform photon
+elif [ $MAKE_RELEASE_PLATFORM == "p1" ] || [ $MAKE_RELEASE_PLATFORM == "8" ]; then
+	./release.sh --platform p1
+elif [ $MAKE_RELEASE_PLATFORM == "electron" ] || [ $MAKE_RELEASE_PLATFORM == "10" ]; then
+	./release.sh --platform electron
+elif [ $MAKE_RELEASE_PLATFORM == "core" ] || [ $MAKE_RELEASE_PLATFORM == "0" ]; then
+	./release.sh --platform core
+elif [ $MAKE_RELEASE_PLATFORM == "all" ]; then
+	./release.sh --platform photon
+	./release.sh --platform p1
+	./release.sh --platform electron
+	./release.sh --platform core
 else
 	echo ERROR, $MAKE_RELEASE_PLATFORM not valid!!
-fi
-fi
-fi
-fi
 fi
