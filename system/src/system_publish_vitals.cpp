45b631831a2399538bac02915ab35cb710e49c71
Zachary J. Fields <zachary_fields@yahoo.com>
2019-03-31 10:27:57 -0500

    Unit-test VitalsPublisher

--- system/src/system_publish_vitals.cpp
+++ system/src/system_publish_vitals.cpp
@@ -3,15 +3,15 @@
 
 #include <limits>
 
-#include "logging.h"
-
 using namespace particle::system;
 
 template <class Timer>
 VitalsPublisher<Timer>::VitalsPublisher (
     publish_fn_t publish_fn_,
-    const Timer & timer_
+    Timer * timer_,
+    log_fn_t log_fn_
 ) :
+    _log(log_fn_),
     _period_s(std::numeric_limits<system_tick_t>::max()),
     _publishVitals(publish_fn_),
     _timer(timer_)
@@ -23,7 +23,7 @@ template <class Timer>
 VitalsPublisher<Timer>::~VitalsPublisher (
     void
 ) {
-    _timer.dispose();
+    _timer->dispose();
 }
 
 template <class Timer>
@@ -31,7 +31,7 @@ void
 VitalsPublisher<Timer>::disablePeriodicPublish (
     void
 ) {
-    _timer.stop();
+    _timer->stop();
 }
 
 template <class Timer>
@@ -39,7 +39,7 @@ void
 VitalsPublisher<Timer>::enablePeriodicPublish (
     void
 ) {
-    _timer.start();
+    _timer->start();
 }
 
 template <class Timer>
@@ -56,18 +56,18 @@ VitalsPublisher<Timer>::period (
     system_tick_t period_s_
 ) {
     const system_tick_t period_ms = period_s_ * 1000;
-    const bool was_active = _timer.isActive();
+    const bool was_active = _timer->isActive();
 
-    if ( _timer.changePeriod(period_ms) ) {
-        LOG(ERROR, "Unable to update vitals timer period!");
+    if ( !_timer->changePeriod(period_ms) ) {
+        _log("Unable to update vitals timer period!");
     } else {
         _period_s = period_s_;
 
         // Maintain pre-existing state
         if ( was_active ) {
-            _timer.reset();
+            _timer->reset();
         } else {
-            _timer.stop();
+            _timer->stop();
         }
     }
 }
@@ -86,3 +86,6 @@ VitalsPublisher<Timer>::publish (
   #include "spark_wiring_timer.h"
   template class VitalsPublisher<Timer>;
 #endif // PLATFORM_SPARK_CORE
+
+#include "../test/unit_tests/mock_types.h"
+template class VitalsPublisher<particle::mock_type::Timer>;
