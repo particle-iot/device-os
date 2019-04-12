d8be4196ed30714ce95934ba47d2d0bb71dcf6ce
Matthew McGowan <mat.mcgowan+git@gmail.com>
2019-04-11 19:56:12 -0700

    1.2.0-alpha.1

--- system/inc/system_version.h
+++ system/inc/system_version.h
@@ -103,7 +103,13 @@ extern "C" {
 #define SYSTEM_VERSION_v101     0x01000100
 #define SYSTEM_VERSION_v110RC1  0x01010001
 #define SYSTEM_VERSION_v110RC2  0x01010002
-#define SYSTEM_VERSION SYSTEM_VERSION_v110RC2
+#define SYSTEM_VERSION_v120ALPHA1  0x01020001
+#define SYSTEM_VERSION SYSTEM_VERSION_v120ALPHA1
+
+/* previously we would set the least significant byte to 0 for the final release, but to make
+ * version comparisons simpler, we will continue incrementing the LSB for the final release in a given
+ * release line.
+ */
 
 /**
  * For Library/App creators. Can be used to ensure features/api's are present.
@@ -184,6 +190,7 @@ extern "C" {
 #define SYSTEM_VERSION_101
 #define SYSTEM_VERSION_110RC1
 #define SYSTEM_VERSION_110RC2
+#define SYSTEM_VERSION_120ALPHA1
 
 typedef struct __attribute__((packed)) SystemVersionInfo
 {
