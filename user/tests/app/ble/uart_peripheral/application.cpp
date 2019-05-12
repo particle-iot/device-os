78d9c5b3d41e231b0fa6d700dede4c8342aca364
XuGuohui <xghui999@126.com>
2019-05-13 00:30:37 +0800

    BLE: rc.1 reworking stash.

--- user/tests/app/ble/uart_peripheral/application.cpp
+++ user/tests/app/ble/uart_peripheral/application.cpp
@@ -30,7 +30,7 @@ const char* rxUuid = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
 const char* txUuid = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
 
 BleCharacteristic txCharacteristic("tx", BleCharacteristicProperty::NOTIFY, txUuid, serviceUuid);
-BleCharacteristic rxCharacteristic("rx", BleCharacteristicProperty::WRITE_WO_RSP, rxUuid, serviceUuid, onDataReceived);
+BleCharacteristic rxCharacteristic("rx", BleCharacteristicProperty::WRITE_WO_RSP, rxUuid, serviceUuid, onDataReceived, nullptr);
 
 uint8_t txBuf[UART_TX_BUF_SIZE];
 size_t txLen = 0;
@@ -38,7 +38,7 @@ size_t txLen = 0;
 
 void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context) {
     LOG(TRACE, "Received data from: %02X:%02X:%02X:%02X:%02X:%02X:",
-            peer.address[0], peer.address[1], peer.address[2], peer.address[3], peer.address[4], peer.address[5]);
+            peer.address()[0], peer.address()[1], peer.address()[2], peer.address()[3], peer.address()[4], peer.address()[5]);
 
     BleCharacteristic* characteristic = static_cast<BleCharacteristic*>(context);
     BleUuid uuid = characteristic->UUID();
@@ -56,7 +56,7 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
 void setup() {
     Serial.begin(115200);
 
-    //LOG(TRACE, "Application started.");
+    LOG(TRACE, "Application started.");
 
     BLE.addCharacteristic(txCharacteristic);
     BLE.addCharacteristic(rxCharacteristic);
