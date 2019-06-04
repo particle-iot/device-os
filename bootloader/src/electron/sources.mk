fa0f15eb49b68a465ba48fd11521ee27a24aac13
Sergey Polyakov <sergeuz@gmail.com>
2019-06-04 11:32:15 +0200

    Rename ram_map.ld to platform_ram.ld

--- bootloader/src/electron/sources.mk
+++ bootloader/src/electron/sources.mk
@@ -6,6 +6,3 @@ CSRC += $(call target_files,$(BOOTLOADER_SRC_ELECTRON_PATH)/,*.c)
 CSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/electron/,watchdog_hal.c)
 CPPSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/electron/,dct_hal.cpp)
 CPPSRC += $(call target_files,$(BOOTLOADER_MODULE_PATH)/../hal/src/stm32/,newlib.cpp)
-
-LDFLAGS += -L$(PROJECT_ROOT)/modules/electron
-LINKER_DEPS += $(PROJECT_ROOT)/modules/electron/ram.ld
--- bootloader/src/photon/sources.mk
+++ bootloader/src/photon/sources.mk
@@ -10,6 +10,3 @@ WICED_LIB_FILES = $(addprefix $(HAL_LIB_COREV2)/,$(addsuffix .a,$(WICED_LIBS)))
 WICED_LIB_FILES = $(HAL_LIB_COREV2)/FreeRTOS/STM32F2xx_bootloader.a
 
 LIBS_EXT += -Wl,--whole-archive $(WICED_LIB_FILES) -Wl,--no-whole-archive
-
-LDFLAGS += -L$(PROJECT_ROOT)/modules/photon
-LINKER_DEPS += $(PROJECT_ROOT)/modules/photon/ram.ld
