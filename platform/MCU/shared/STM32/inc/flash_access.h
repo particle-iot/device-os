a83b31f9f101c3f2ee4156969b31ceb8e0f3e16d
Eugene <eugene@particle.io>
2019-09-25 15:51:23 +0800

    [hal] Implement OTP store feature on Gen2 device

--- platform/MCU/shared/STM32/inc/flash_access.h
+++ platform/MCU/shared/STM32/inc/flash_access.h
@@ -71,7 +71,8 @@ void FLASH_Restore(uint32_t FLASH_Address);
 void FLASH_Begin(uint32_t FLASH_Address, uint32_t imageSize);
 int FLASH_Update(const uint8_t *pBuffer, uint32_t address, uint32_t bufferSize);
 void FLASH_End(void);
-
+int FLASH_ReadOTP(uint32_t offset, uint8_t* pBuffer, uint32_t bufferSize);
+int FLASH_WriteOTP(uint32_t offset, const uint8_t* pBuffer, uint32_t bufferSize);
 
 #ifdef __cplusplus
 }
