9875a20023ca79dd7d3cb0ae773a71c5f68b0a47
John Wass <wassj@ctc.com>
2019-11-05 16:30:53 -0500

    reference objdump through environment fixing cross compile

--- modules/shared/nRF52840/build_linker_script.mk
+++ modules/shared/nRF52840/build_linker_script.mk
@@ -3,13 +3,15 @@ WRITE_FILE_APPEND = $(shell echo "$(2)" >> $(1))
 
 COMMA := ,
 
+include ../../../build/common-tools.mk
+
 ifneq (,$(PREBUILD))
 # Should declare enough RAM for inermediate linker script: 89K
 USER_SRAM_LENGTH = 89K
 else
-DATA_SECTION_LEN  = $(shell arm-none-eabi-objdump -h --section=.data $(INTERMEDIATE_ELF) | grep .data)
+DATA_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.data $(INTERMEDIATE_ELF) | grep .data)
 DATA_SECTION_LEN := 0x$(word 3,$(DATA_SECTION_LEN))
-BSS_SECTION_LEN   = $(shell arm-none-eabi-objdump -h --section=.bss $(INTERMEDIATE_ELF) | grep .bss)
+BSS_SECTION_LEN   = $(shell $(OBJDUMP) -h --section=.bss $(INTERMEDIATE_ELF) | grep .bss)
 BSS_SECTION_LEN  := 0x$(word 3,$(BSS_SECTION_LEN))
 
 # Note: reserving 16 bytes for alignment just in case
