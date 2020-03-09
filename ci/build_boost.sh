8208845db1cbf4f592ff771a55d66a47013d25f3
Sergey Polyakov <sergeuz@gmail.com>
2020-03-09 15:36:49 +0200

    Fix building of Boost on macOS

--- ci/build_boost.sh
+++ ci/build_boost.sh
@@ -1,4 +1,9 @@
+toolset="gcc"
+if [[ $OSTYPE == darwin* ]]; then
+  toolset="darwin"
+fi
+
 pushd $BOOST_ROOT
 ./bootstrap.sh
-./b2  --with-thread  --with-system --with-program_options --with-random --with-regex --threading=multi
+./b2  --with-thread  --with-system --with-program_options --with-random --with-regex --threading=multi toolset=$toolset
 popd
