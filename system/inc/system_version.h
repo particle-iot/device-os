9b88b504f0500c06606d24876592ea35a312ef1c
Andrey Tolstoy <avtolstoy@gmail.com>
2020-05-06 23:51:03 +0700

    bump module version for 1.5.1 (1511)

--- system/inc/system_version.h
+++ system/inc/system_version.h
@@ -147,7 +147,8 @@ extern "C" {
 #define SYSTEM_VERSION_v150RC2        SYSTEM_VERSION_RC(1, 5, 0, 2)
 #define SYSTEM_VERSION_v150         SYSTEM_VERSION_DEFAULT(1, 5, 0)
 #define SYSTEM_VERSION_v151RC1        SYSTEM_VERSION_RC(1, 5, 1, 1)
-#define SYSTEM_VERSION SYSTEM_VERSION_v151RC1
+#define SYSTEM_VERSION_v151         SYSTEM_VERSION_DEFAULT(1, 5, 1)
+#define SYSTEM_VERSION SYSTEM_VERSION_v151
 
 /**
  * Previously we would set the least significant byte to 0 for the final release, but to make
@@ -259,6 +260,7 @@ extern "C" {
 #define SYSTEM_VERSION_150RC2
 #define SYSTEM_VERSION_150
 #define SYSTEM_VERSION_151RC1
+#define SYSTEM_VERSION_151
 
 typedef struct __attribute__((packed)) SystemVersionInfo
 {
