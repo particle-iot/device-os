ae1ffd6319a52e4682f7e73e88034b1ed84b6ff8
Andrey Tolstoy <avtolstoy@gmail.com>
2020-07-03 04:35:39 +0700

    [test] check for PARTICLE_TEST_RUNNER in tests to be built by test runner

--- user/tests/wiring/no_fixture_ble/application.cpp
+++ user/tests/wiring/no_fixture_ble/application.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2019 Particle Industries, Inc.  All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PARTICLE_TEST_RUNNER
+
 #include "application.h"
 #include "unit-test/unit-test.h"
 
@@ -17,3 +36,5 @@ SYSTEM_THREAD(ENABLED);
 #endif
 
 SYSTEM_MODE(MANUAL);
+
+#endif // PARTICLE_TEST_RUNNER
