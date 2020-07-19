6379518f8364c082fa453a76d9f626b9aadb24c6
keeramis <keerthy.amisagadda@gmail.com>
2020-07-19 14:25:23 -0500

    Fix output_mode_drain

--- hal/src/nRF52840/gpio_hal.cpp
+++ hal/src/nRF52840/gpio_hal.cpp
@@ -117,8 +117,9 @@ int HAL_Pin_Configure(pin_t pin, const hal_gpio_config_t* conf) {
             }
             case OUTPUT_OPEN_DRAIN: {
                 auto drive_str = NRF_GPIO_PIN_H0D1; // High drive up to 5mA
-                if (conf->drive_strength == HAL_GPIO_DRIVE_HIGH) {
-                    drive_str = NRF_GPIO_PIN_H0H1;
+                if (conf->drive_strength == HAL_GPIO_DRIVE_STANDARD) {
+                    drive_str = NRF_GPIO_PIN_S0D1;
+                }
                 nrf_gpio_cfg(nrfPin,
                     NRF_GPIO_PIN_DIR_OUTPUT,
                     NRF_GPIO_PIN_INPUT_DISCONNECT,
