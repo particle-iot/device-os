c0ff79d73c19a9140219b0e2f3b0b270e3e14722
Sergey Polyakov <sergeuz@gmail.com>
2020-08-25 17:08:12 +0200

    Minor fix

--- hal/src/stm32f2xx/ota_flash_hal_stm32f2xx.cpp
+++ hal/src/stm32f2xx/ota_flash_hal_stm32f2xx.cpp
@@ -629,7 +629,6 @@ int HAL_Set_System_Config(hal_system_config_t config_item, const void* data, uns
     case SYSTEM_CONFIG_SERVER_KEY:
         if (udp) {
             offset = DCT_ALT_SERVER_PUBLIC_KEY_OFFSET;
-            length = DCT_ALT_SERVER_PUBLIC_KEY_SIZE;
             // Server address for UDP devices is in a separate, adjacent DCT field unlike TCP devices.
             // To update the server address along with the key, we need to write more data.
             length = DCT_ALT_SERVER_PUBLIC_KEY_SIZE + DCT_ALT_SERVER_ADDRESS_SIZE;
