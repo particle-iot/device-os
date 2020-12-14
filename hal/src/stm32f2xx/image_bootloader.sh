8c96735729d7aa04b5c568a952e89922710b9009
Andrey Tolstoy <avtolstoy@gmail.com>
2020-12-14 23:57:18 +0700

    bump module version for 2.0.1-alpha.2 (2010)

--- hal/src/stm32f2xx/image_bootloader.sh
+++ hal/src/stm32f2xx/image_bootloader.sh
@@ -14,7 +14,7 @@ function update_bl()
    pushd ../../../bootloader
    make -s PLATFORM_ID=$1 clean all || die   
    popd
-   cp $bl_dir/platform-$1-lto/bootloader.bin bootloader_platform_$1.bin || die
+   cp $bl_dir/platform-$1-m-lto/bootloader.bin bootloader_platform_$1.bin || die
    xxd -i bootloader_platform_$1.bin > bootloader_platform_$1.c || die
    rm bootloader_platform_$1.bin || die
    if [[ "$platform" == "Darwin" ]]; then
@@ -23,6 +23,5 @@ function update_bl()
      sed -i 's/unsigned/const unsigned/' bootloader_platform_$1.c || die
    fi
 }
-update_bl 6
-update_bl 8
 update_bl 10
+
