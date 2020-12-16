17fcd7e20bba121fd700ca2fbf841d4ec6576073
Andrey Tolstoy <avtolstoy@gmail.com>
2020-12-17 01:32:57 +0700

    bump module version for 2.0.1 (2011)

--- system/inc/system_version.h
+++ system/inc/system_version.h
@@ -166,7 +166,8 @@ extern "C" {
 #define SYSTEM_VERSION_v200         SYSTEM_VERSION_DEFAULT(2, 0, 0)
 #define SYSTEM_VERSION_v201ALPHA1   SYSTEM_VERSION_ALPHA(2, 0, 1, 1)
 #define SYSTEM_VERSION_v201ALPHA2   SYSTEM_VERSION_ALPHA(2, 0, 1, 2)
-#define SYSTEM_VERSION SYSTEM_VERSION_v201ALPHA2
+#define SYSTEM_VERSION_v201         SYSTEM_VERSION_DEFAULT(2, 0, 1)
+#define SYSTEM_VERSION SYSTEM_VERSION_v201
 
 /**
  * Previously we would set the least significant byte to 0 for the final release, but to make
@@ -297,6 +298,7 @@ extern "C" {
 #define SYSTEM_VERSION_200
 #define SYSTEM_VERSION_201ALPHA1
 #define SYSTEM_VERSION_201ALPHA2
+#define SYSTEM_VERSION_201
 
 typedef struct __attribute__((packed)) SystemVersionInfo
 {
