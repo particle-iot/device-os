b326bf5ba425b701733c9628b17cb56a97187f0d
Joe Goggins <joe.goggins@gmail.com>
2021-01-27 18:24:19 -0600

    [test] Ensure startup free ram assertions are correct for all platforms
    If you have a photon and a boron plugged in, you can execute the tests
    with:  `device-os-test run boron photon communication/startup-slos`

--- user/tests/integration/communication/startup-slos/slos.cpp
+++ user/tests/integration/communication/startup-slos/slos.cpp
@@ -1,22 +1,11 @@
 #include "application.h"
 #include "test.h"
 
-test(particle_publish_publishes_an_event) {
+test(slo_startup_stats)
+{
     Particle.connect();
-	static bool published_once = false;
-
-	if (!published_once) {
-		if (Particle.connected()) {
-			unsigned long delta_millis = millis() - initial_time;
-			uint32_t seconds = (uint32_t)(delta_millis / 1000);
-			uint32_t free_mem = System.freeMemory();
-			String stats = String::format("{\"seconds\": %u, \"ram\": %u }", seconds, free_mem);
-			Particle.publish("startup_stats", stats);
-			published_once = true;
-		}
-		else {
-			// Joe comment: I don't think this is needed
-			// Particle.process();
-		}
-	}
+    waitUntil(Particle.connected);
+    uint32_t free_mem = System.freeMemory();
+    String stats = String::format("{\"free_mem\": %u }", free_mem);
+    Particle.publish("startup_stats", stats);
 }
