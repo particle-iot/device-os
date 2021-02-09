68907f4bb4bd0f3dbc262231ae9823215bb101bf
Joe Goggins <joe.goggins@gmail.com>
2021-02-09 14:00:33 -0600

    Fix integration test Move extern up

--- user/tests/integration/startup-slos/startup-slos.cpp
+++ user/tests/integration/startup-slos/startup-slos.cpp
@@ -1,16 +1,17 @@
+///
+// This code meant to be a direct port of the canonical startup stats firmware: https://github.com/particle-iot/pqa/blob/main/firmware/publish-startup-stats-once/src/main.cpp
+// Since this test framework does not have a setup() + loop() function, it is somewhat different.
+///
 #include "application.h"
 #include "test.h"
 
-// This code meant to be a direct port of the canonical startup stats firmware: https://github.com/particle-iot/pqa/blob/main/firmware/publish-startup-stats-once/src/main.cpp
-// Since this test framework does not have a setup() + loop() function, it is somewhat different.
+//required for linking FLASH_ModuleLength on Gen3 to work: 
+extern "C" int hal_exflash_read(uintptr_t addr, uint8_t* data_buf, size_t data_size) {
+    return SYSTEM_ERROR_NOT_SUPPORTED;
+}
+
 test(slo_startup_stats)
-{
-        //required for linking FLASH_ModuleLength on Gen3 to work: 
-    extern "C" int hal_exflash_read(uintptr_t addr, uint8_t* data_buf, size_t data_size) {
-        return SYSTEM_ERROR_NOT_SUPPORTED;
-    }
-    
-    
+{    
     // capture the time at which app initialization takes place
     static unsigned long base_time = millis();
     
