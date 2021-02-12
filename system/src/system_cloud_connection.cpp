a5543bef13527d90f00320c806460f9fc707a9c1
Sergey Polyakov <sergeuz@gmail.com>
2021-02-12 19:27:12 +0200

    Allow disabling automatic initialization of the test runner at compile time; minor fixes

--- system/src/system_cloud_connection.cpp
+++ system/src/system_cloud_connection.cpp
@@ -86,7 +86,7 @@ int SessionConnection::load(const ServerAddress& addr)
             return -1;
         }
     } else {
-        LOG(ERROR, "Failed to load session data from persistent storage");
+        LOG(WARN, "Failed to load session data from persistent storage");
         discard();
         return -1;
     }
