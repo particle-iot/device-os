97ec42ca72fa030ac1b7a4db4515d3da7a603eb3
Andrey Tolstoy <avtolstoy@gmail.com>
2021-03-11 21:29:12 +0700

    [test] network: minor

--- user/tests/wiring/no_fixture_long_running/network.cpp
+++ user/tests/wiring/no_fixture_long_running/network.cpp
@@ -96,7 +96,13 @@ test(NETWORK_01_LargePacketsDontCauseIssues_ResolveMtu) {
     const size_t IPV4_HEADER_LENGTH = 20;
     const size_t UDP_HEADER_LENGTH = 8;
     const size_t IPV4_PLUS_UDP_HEADER_LENGTH = IPV4_HEADER_LENGTH + UDP_HEADER_LENGTH;
+#if PLATFORM_ID != PLATFORM_ELECTRON
     const size_t MAX_MTU = 1500;
+#else
+    // Socket interface on Electrons is limited by 1024 byte payload maximum
+    // Adjust maximum MTU accordingly.
+    const size_t MAX_MTU = 1024 + IPV4_PLUS_UDP_HEADER_LENGTH;
+#endif // PLATFORM_ID != PLATFORM_ELECTRON
     const size_t MIN_MTU = IPV4_PLUS_UDP_HEADER_LENGTH;
     const system_tick_t UDP_ECHO_REPLY_WAIT_TIME = 10000;
     const unsigned UDP_ECHO_RETRIES = 5;
@@ -262,4 +268,3 @@ test(NETWORK_03_network_connection_recovers_after_ncp_uart_sleep) {
 }
 
 #endif // HAL_PLATFORM_NCP_AT
-SerialLogHandler dbg(LOG_LEVEL_ALL);
