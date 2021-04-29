f1e3c02b5bdd959f5f5a3ef5f683e9cc39f9badb
XuGuohui <xghui999@126.com>
2021-04-30 00:20:49 +0800

    Fixes the issue that NCP and radio stack dependency check may fail.

--- modules/shared/nRF52840/inc/system_part1_loader.c
+++ modules/shared/nRF52840/inc/system_part1_loader.c
@@ -20,18 +20,8 @@ bool is_user_module_valid()
  */
 void system_part1_pre_init() {
     // HAL_Core_Config() has been invoked in startup_nrf52840.S
-
-    const bool bootloader_validated = HAL_Core_Validate_Modules(1, NULL);
-
-    // Validate user module
-    if (bootloader_validated) {
-        module_user_part_validated = HAL_Core_Validate_User_Module();
-    }
-
-    bool safe_mode = HAL_Core_Enter_Safe_Mode_Requested();
-
-    if (!bootloader_validated || !is_user_module_valid() || safe_mode) {
-        // indicate to the system that it shouldn't run user code
+    
+    if (HAL_Core_Enter_Safe_Mode_Requested()) {
         set_system_mode(SAFE_MODE);
     }
 }
@@ -45,8 +35,18 @@ void system_part1_init() {
 void system_part2_post_init() __attribute__((alias("system_part1_post_init")));
 
 void system_part1_post_init() {
-    if (is_user_module_valid()) {
-        module_user_init();
+    const bool bootloader_validated = HAL_Core_Validate_Modules(1, NULL);
+
+    // Validate user module
+    if (bootloader_validated) {
+        module_user_part_validated = HAL_Core_Validate_User_Module();
+        if (is_user_module_valid()) {
+            module_user_init();
+        }
+    }
+    if (!bootloader_validated || !is_user_module_valid()) {
+        // indicate to the system that it shouldn't run user code
+        set_system_mode(SAFE_MODE);
     }
 }
 
