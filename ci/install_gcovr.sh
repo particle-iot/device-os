9c220df805ad2a6fff0fce051c6fe6b4a88a71cd
Andrey Tolstoy <avtolstoy@gmail.com>
2021-05-12 20:27:38 +0700

    [ci] fix MarkupSafe weirdness

--- ci/install_gcovr.sh
+++ ci/install_gcovr.sh
@@ -20,7 +20,9 @@ else
 fi
 
 
-pip3 install git+https://github.com/gcovr/gcovr.git
 pip3 install requests
+pip3 install git+https://github.com/gcovr/gcovr.git
+# FIXME: MarkupSafe is broken on python3.5
+ln -s /usr/local/lib/python3.5/dist-packages/MarkupSafe-0.0.0.dist-info /usr/local/lib/python3.5/dist-packages/MarkupSafe-2.0.0.dist-info
 
 gcovr --version
