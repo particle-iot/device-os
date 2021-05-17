9757cf5cf8b36cfb7a499c404856658d702daa27
Sergey Polyakov <sergeuz@gmail.com>
2021-05-17 10:48:39 +0200

    Collect network and cloud connection times separately for debugging purposes

--- user/tests/integration/slo/connect_time/connect_time.cpp
+++ user/tests/integration/slo/connect_time/connect_time.cpp
@@ -6,7 +6,7 @@ namespace {
 // Number of connection time measurements to make. When changing this parameter, make sure to change
 // the number of cloud_connect_time_from_cold_boot_XX and cloud_connect_time_from_warm_boot_XX tests
 // and update the spec file accordingly
-const unsigned SAMPLE_COUNT = 10;
+const unsigned CONNECT_COUNT = 10;
 
 // Delay before resetting the device after a test
 const system_tick_t DELAY_AFTER_TEST = 2000;
@@ -15,10 +15,14 @@ const system_tick_t DELAY_AFTER_TEST = 2000;
 const system_tick_t WAIT_FOR_TIMEOUT = 10 * 60 * 1000;
 
 struct Stats {
-    system_tick_t cloudConnectTimeFromColdBoot[SAMPLE_COUNT];
-    system_tick_t cloudConnectTimeFromWarmBoot[SAMPLE_COUNT];
-    unsigned cloudConnectTimeFromColdBootCount;
-    unsigned cloudConnectTimeFromWarmBootCount;
+    system_tick_t cloudConnectTimeFromColdBoot[CONNECT_COUNT];
+    system_tick_t cloudConnectTimeFromWarmBoot[CONNECT_COUNT];
+    system_tick_t networkColdConnectDuration[CONNECT_COUNT];
+    system_tick_t networkWarmConnectDuration[CONNECT_COUNT];
+    system_tick_t cloudFullHandshakeDuration[CONNECT_COUNT];
+    system_tick_t cloudSessionResumeDuration[CONNECT_COUNT];
+    unsigned coldBootCount;
+    unsigned warmBootCount;
 };
 
 retained Stats stats = {};
@@ -42,6 +46,9 @@ inline NetworkClass& network() {
 }
 
 bool testCloudConnectTimeFromColdBoot() {
+    if (stats.coldBootCount >= CONNECT_COUNT) {
+        return false;
+    }
     network().off(); // Make sure the network interface is tuned off
     waitFor(network().isOff, WAIT_FOR_TIMEOUT);
     if (!network().isOff()) {
@@ -49,48 +56,78 @@ bool testCloudConnectTimeFromColdBoot() {
     }
     Particle.disconnect(CloudDisconnectOptions().clearSession(true)); // Clear the session data
     const auto t1 = millis();
+    network().connect();
+    waitFor(network().ready, WAIT_FOR_TIMEOUT);
+    if (!network().ready()) {
+        return false;
+    }
+    const auto t2 = millis();
     Particle.connect();
     waitFor(Particle.connected, WAIT_FOR_TIMEOUT);
     if (!Particle.connected()) {
         return false;
     }
-    const auto n = stats.cloudConnectTimeFromColdBootCount++;
-    stats.cloudConnectTimeFromColdBoot[n] = millis() - t1 + setupTimeFromStartup; // As if Particle.connect() was called in setup()
+    const auto t3 = millis();
+    const auto n = stats.coldBootCount++;
+    stats.cloudConnectTimeFromColdBoot[n] = t3 - t1 + setupTimeFromStartup; // As if Particle.connect() was called in setup()
+    stats.networkColdConnectDuration[n] = t2 - t1;
+    stats.cloudFullHandshakeDuration[n] = t3 - t2;
     delay(DELAY_AFTER_TEST);
     return true;
 }
 
 bool testCloudConnectTimeFromWarmBoot() {
+    if (stats.warmBootCount >= CONNECT_COUNT) {
+        return false;
+    }
     if (network().ready()) {
         return false;
     }
     const auto t1 = millis();
+    network().connect();
+    waitFor(network().ready, WAIT_FOR_TIMEOUT);
+    if (!network().ready()) {
+        return false;
+    }
+    const auto t2 = millis();
     Particle.connect();
     waitFor(Particle.connected, WAIT_FOR_TIMEOUT);
     if (!Particle.connected()) {
         return false;
     }
-    const auto n = stats.cloudConnectTimeFromWarmBootCount++;
-    stats.cloudConnectTimeFromWarmBoot[n] = millis() - t1 + setupTimeFromStartup;
+    const auto t3 = millis();
+    const auto n = stats.warmBootCount++;
+    stats.cloudConnectTimeFromWarmBoot[n] = t3 - t1 + setupTimeFromStartup;
+    stats.networkWarmConnectDuration[n] = t2 - t1;
+    stats.cloudSessionResumeDuration[n] = t3 - t2;
     delay(DELAY_AFTER_TEST);
     return true;
+}
+
+template<typename T>
+void serializeArrayAsJson(JSONBufferWriter* writer, const T* values, size_t size) {
+    writer->beginArray();
+    for (size_t i = 0; i < size; ++i) {
+        writer->value(values[i]);
+    }
+    writer->endArray();
 }
 
 size_t serializeStatsAsJson(char* buf, size_t size) {
     JSONBufferWriter w(buf, size);
     w.beginObject();
     w.name("cloud_connect_time_from_cold_boot");
-    w.beginArray();
-    for (unsigned i = 0; i < SAMPLE_COUNT; ++i) {
-        w.value((unsigned)stats.cloudConnectTimeFromColdBoot[i]);
-    }
-    w.endArray();
+    serializeArrayAsJson(&w, stats.cloudConnectTimeFromColdBoot, CONNECT_COUNT);
     w.name("cloud_connect_time_from_warm_boot");
-    w.beginArray();
-    for (unsigned i = 0; i < SAMPLE_COUNT; ++i) {
-        w.value((unsigned)stats.cloudConnectTimeFromWarmBoot[i]);
-    }
-    w.endArray();
+    serializeArrayAsJson(&w, stats.cloudConnectTimeFromWarmBoot, CONNECT_COUNT);
+    w.name("network_cold_connect_duration");
+    serializeArrayAsJson(&w, stats.networkColdConnectDuration, CONNECT_COUNT);
+    w.name("network_warm_connect_duration");
+    serializeArrayAsJson(&w, stats.networkWarmConnectDuration, CONNECT_COUNT);
+    w.name("cloud_full_handshake_duration");
+    serializeArrayAsJson(&w, stats.cloudFullHandshakeDuration, CONNECT_COUNT);
+    w.name("cloud_session_resume_duration");
+    serializeArrayAsJson(&w, stats.cloudSessionResumeDuration, CONNECT_COUNT);
     w.endObject();
     return w.dataSize();
 }
