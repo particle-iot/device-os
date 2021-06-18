dc020e4a4ab7d87cad12e610e6b96ec329d78b98
XuGuohui <xghui999@126.com>
2021-06-18 19:54:34 +0800

    [RTL872x]: rgb led hal driver

--- hal/inc/rgbled_hal.h
+++ hal/inc/rgbled_hal.h
@@ -33,20 +33,19 @@ extern "C" {
 #include "rgbled_hal_impl.h"
 
 // #if MODULE_FUNCTION != MOD_FUNC_BOOTLOADER
-// extern const led_config_t HAL_Leds_Default[];
+// extern const hal_led_config_t HAL_Leds_Default[];
 // #else
-// extern led_config_t HAL_Leds_Default[];
+// extern hal_led_config_t HAL_Leds_Default[];
 // #endif // MODULE_FUNCTION != MOD_FUNC_BOOTLOADER
 
-void HAL_Led_Init(uint8_t led, led_config_t* conf, void* reserved);
-void HAL_Led_Rgb_Set_Values(uint16_t r, uint16_t g, uint16_t b, void* reserved);
-void HAL_Led_Rgb_Get_Values(uint16_t* rgb, void* reserved);
-uint32_t HAL_Led_Rgb_Get_Max_Value(void* reserved);
-void HAL_Led_User_Set(uint8_t state, void* reserved);
-void HAL_Led_User_Toggle(void* reserved);
-
-led_config_t* HAL_Led_Set_Configuration(uint8_t led, led_config_t* conf, void* reserved);
-led_config_t* HAL_Led_Get_Configuration(uint8_t led, void* reserved);
+void hal_led_init(uint8_t led, hal_led_config_t* conf, void* reserved);
+void hal_led_set_rgb_values(uint16_t r, uint16_t g, uint16_t b, void* reserved);
+void hal_led_get_rgb_values(uint16_t* rgb, void* reserved);
+uint32_t hal_led_get_max_rgb_values(void* reserved);
+void hal_led_set_user(uint8_t state, void* reserved);
+void hal_led_toggle_user(void* reserved);
+hal_led_config_t* hal_led_set_configuration(uint8_t led, hal_led_config_t* conf, void* reserved);
+hal_led_config_t* hal_led_get_configuration(uint8_t led, void* reserved);
 
 // This is the low-level api to the LED
 // Deprecated, not exported in HAL
@@ -55,7 +54,6 @@ void Get_RGB_LED_Values(uint16_t* values);
 uint16_t Get_RGB_LED_Max_Value(void);
 void Set_User_LED(uint8_t state);
 void Toggle_User_LED(void);
-uint16_t Get_RGB_LED_Max_Value(void);
 void RGB_LED_Uninit();
 
 #ifdef	__cplusplus
