b96f50809b31e1f0ef17c3a55e8e53bc0d5754e7
Andrey Tolstoy <avtolstoy@gmail.com>
2021-07-09 06:25:01 +0700

    [test] wiring/no_fixture: add CLOUD_04_socket_errors_do_not_cause_a_full_handshake

--- user/tests/wiring/no_fixture/cloud.cpp
+++ user/tests/wiring/no_fixture/cloud.cpp
@@ -1,5 +1,19 @@
 #include "application.h"
 #include "unit-test/unit-test.h"
+#include "scope_guard.h"
+
+namespace {
+
+struct HandshakeState {
+    volatile int handshakeType = -1;
+    bool operator()() {
+        return handshakeType != -1;
+    }
+};
+
+HandshakeState handshakeState;
+
+}
 
 test(CLOUD_01_Particle_Connect_Does_Not_Block_In_SemiAutomatic_Mode) {
     Particle.disconnect();
@@ -18,3 +32,41 @@ test(CLOUD_01_Particle_Connect_Does_Not_Block_In_SemiAutomatic_Mode) {
 test(CLOUD_03_Restore_System_Mode) {
     set_system_mode(AUTOMATIC);
 }
+
+#if HAL_PLATFORM_CLOUD_UDP
+test(CLOUD_04_socket_errors_do_not_cause_a_full_handshake) {
+    const int GET_CLOUD_SOCKET_HANDLE_INTERNAL_ID = 3;
+
+    Particle.connect();
+    assertTrue(waitFor(Particle.connected, 60000));
+
+    sock_handle_t cloudSock = (sock_handle_t)system_internal(GET_CLOUD_SOCKET_HANDLE_INTERNAL_ID, nullptr);
+    assertTrue(socket_handle_valid(cloudSock));
+
+    auto evHandler = [](system_event_t event, int param, void* ctx) {
+        if (event == cloud_status) {
+            if (param == cloud_status_handshake || param == cloud_status_session_resume) {
+                if (handshakeState.handshakeType == -1) {
+                    handshakeState.handshakeType = param;
+                }
+            }
+        }
+    };
+
+    System.on(cloud_status, evHandler);
+    SCOPE_GUARD({
+        System.off(cloud_status, evHandler);
+    });
+    // Pull the rug, this should cause a socket error on recv/send
+#if HAL_USE_SOCKET_HAL_POSIX
+    assertEqual(0, sock_close(cloudSock));
+#else
+    assertEqual(0, socket_close(cloudSock));
+#endif // HAL_USE_SOCKET_HAL_POSIX
+    // Force a publish just in case
+    (void)Particle.publish("test", "test");
+    assertTrue(waitFor(handshakeState, 60000));
+    assertEqual((int)handshakeState.handshakeType, (int)cloud_status_session_resume);
+    assertTrue(waitFor(Particle.connected, 60000));
+}
+#endif // HAL_PLATFORM_CLOUD_UDP
