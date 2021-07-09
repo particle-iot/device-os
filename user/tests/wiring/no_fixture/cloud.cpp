9a9f5b473143326c0c0706536eec31131a519142
Andrey Tolstoy <avtolstoy@gmail.com>
2021-07-09 07:26:39 +0700

    [test] wiring/no_fixture: add CLOUD_05_loss_of_cellular_network_connectivity_does_not_cause_full_handshake, minor fixes

--- user/tests/wiring/no_fixture/cloud.cpp
+++ user/tests/wiring/no_fixture/cloud.cpp
@@ -9,6 +9,9 @@ struct HandshakeState {
     bool operator()() {
         return handshakeType != -1;
     }
+    void reset() {
+        handshakeType = -1;
+    }
 };
 
 HandshakeState handshakeState;
@@ -38,7 +41,7 @@ test(CLOUD_04_socket_errors_do_not_cause_a_full_handshake) {
     const int GET_CLOUD_SOCKET_HANDLE_INTERNAL_ID = 3;
 
     Particle.connect();
-    assertTrue(waitFor(Particle.connected, 60000));
+    assertTrue(waitFor(Particle.connected, 120000));
 
     sock_handle_t cloudSock = (sock_handle_t)system_internal(GET_CLOUD_SOCKET_HANDLE_INTERNAL_ID, nullptr);
     assertTrue(socket_handle_valid(cloudSock));
@@ -53,6 +56,7 @@ test(CLOUD_04_socket_errors_do_not_cause_a_full_handshake) {
         }
     };
 
+    handshakeState.reset();
     System.on(cloud_status, evHandler);
     SCOPE_GUARD({
         System.off(cloud_status, evHandler);
@@ -65,8 +69,49 @@ test(CLOUD_04_socket_errors_do_not_cause_a_full_handshake) {
 #endif // HAL_USE_SOCKET_HAL_POSIX
     // Force a publish just in case
     (void)Particle.publish("test", "test");
-    assertTrue(waitFor(handshakeState, 60000));
+    assertTrue(waitFor(handshakeState, 120000));
     assertEqual((int)handshakeState.handshakeType, (int)cloud_status_session_resume);
     assertTrue(waitFor(Particle.connected, 60000));
 }
+
+#if HAL_PLATFORM_CELLULAR
+test(CLOUD_05_loss_of_cellular_network_connectivity_does_not_cause_full_handshake) {
+    Particle.connect();
+    assertTrue(waitFor(Particle.connected, 120000));
+
+    auto evHandler = [](system_event_t event, int param, void* ctx) {
+        if (event == cloud_status) {
+            if (param == cloud_status_handshake || param == cloud_status_session_resume) {
+                if (handshakeState.handshakeType == -1) {
+                    handshakeState.handshakeType = param;
+                }
+            }
+        }
+    };
+
+    handshakeState.reset();
+    System.on(cloud_status, evHandler);
+    SCOPE_GUARD({
+        System.off(cloud_status, evHandler);
+    });
+    // Pull the rug, this should cause a socket error on recv/send
+#if HAL_PLATFORM_NCP_AT
+    assertEqual((int)RESP_OK, Cellular.command("AT+CFUN=0,0\r\n"));
+    // Force a publish just in case
+    (void)Particle.publish("test", "test");
+    assertEqual((int)RESP_OK, Cellular.command("AT+CFUN=1,0\r\n"));
+#else
+    // Electrons don't take too well to being switched to minimum functionality mode
+    // and perform a reset. Deactivating internal context seems to work better.
+    assertEqual((int)RESP_OK, Cellular.command("AT+UPSDA=0,4\r\n"));
+    // Force a publish just in case
+    (void)Particle.publish("test", "test");
+    assertEqual((int)RESP_OK, Cellular.command("AT+UPSDA=0,3\r\n"));
+#endif // HAL_PLATFORM_NCP_AT
+    assertTrue(waitFor(handshakeState, 120000));
+    assertEqual((int)handshakeState.handshakeType, (int)cloud_status_session_resume);
+    assertTrue(waitFor(Particle.connected, 60000));
+}
+
+#endif // HAL_PLATFORM_CELLULAR
 #endif // HAL_PLATFORM_CLOUD_UDP
