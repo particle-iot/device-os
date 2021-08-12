f86a84c756a2f0e07e31af97556bc818b5b4621b
Eugene <yuting0501@gmail.com>
2021-08-12 17:13:55 +0800

    [RTL872x]: enable configUSE_NEWLIB_REENTRANT

--- hal/src/rtl872x/freertos/FreeRTOSConfig.h
+++ hal/src/rtl872x/freertos/FreeRTOSConfig.h
@@ -109,7 +109,7 @@ extern void vApplicationTaskDeleteHook(void *pvTaskToDelete, volatile long* pxPe
 /* Constants that build features in or out. */
 #define configUSE_MUTEXES								1
 #define configUSE_APPLICATION_TASK_TAG					0
-#define configUSE_NEWLIB_REENTRANT						0
+#define configUSE_NEWLIB_REENTRANT						1
 // #define configUSE_CO_ROUTINES							1 ///
 #define configUSE_CO_ROUTINES							0 ///
 #define configMAX_CO_ROUTINE_PRIORITIES 				( 2 )
