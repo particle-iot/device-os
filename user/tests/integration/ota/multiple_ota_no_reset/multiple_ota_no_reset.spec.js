80f80150c9d4c98c30bbf52fbd2fbe21568c6479
Sergey Polyakov <sergeuz@gmail.com>
2021-08-20 17:56:36 +0200

    Refactor the integration test

--- user/tests/integration/ota/multiple_ota_no_reset/multiple_ota_no_reset.spec.js
+++ user/tests/integration/ota/multiple_ota_no_reset/multiple_ota_no_reset.spec.js
@@ -14,18 +14,23 @@ let auth = null;
 let device = null;
 let deviceId = null;
 
-async function waitFlashSuccessEvent(ctx) {
+async function flash(ctx, binFile, { expectedStatus = 'success' } = {}) {
+	const appName = path.basename(binFile, '.bin');
+	await api.flashDevice({ deviceId, files: { [appName]: binFile }, auth });
+	await waitFlashStatusEvent(ctx, expectedStatus);
+	await delay(1000);
+}
+
+async function waitFlashStatusEvent(ctx, status) {
 	let data = null;
 	do {
 		data = await ctx.particle.receiveEvent('spark/flash/status');
 		ctx.particle.log.verbose('spark/flash/status:', data);
-	} while (!data.startsWith('success'));
+	} while (!data.startsWith(status));
 }
 
-async function flash(ctx, binFile) {
-	const appName = path.basename(binFile, '.bin');
-	await api.flashDevice({ deviceId, files: { [appName]: binFile }, auth });
-	await waitFlashSuccessEvent(ctx);
+async function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 before(function() {
@@ -35,35 +40,41 @@ before(function() {
 	deviceId = device.id;
 });
 
-test('01_disable_resets_and_connect', () => {
+test('01_disable_resets_and_connect', async function () {
 	// See the test app
 });
 
-test('02_flash_binaries', async function () {
+test('02_flash_binaries_and_reset', async function () {
 	// Get the module binary of the test application that is currently running on the device
 	const origAppData = await readFile(device.testAppBinFile);
 	const parser = new HalModuleParser();
 	const { prefixInfo: origPrefix, suffixInfo: origSuffix } = await parser.parseBuffer({ fileBuffer: origAppData });
-	// The first binary is exactly the same as the one already flashed on the device but with a
-	// different SHA checksum
+	// The first binary is exactly the same as the one already flashed on the device but it has a
+	// different SHA checksum so that we can identify it later
 	let appData = Buffer.from(origAppData);
 	let suffix = { ...origSuffix };
 	suffix.fwUniqueId = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
 	expect(suffix.fwUniqueId).to.not.equal(origSuffix.fwUniqueId);
 	updateModuleSuffix(appData, suffix);
-	const appFile1 = await tempy.write(appData, { name: 'app1.bin' });
-	// The second binary has a missing dependency
+	let appFile = await tempy.write(appData, { name: 'app1.bin' });
+	// The device should accept this update
+	await flash(this, appFile);
+	// The second binary has an incompatible platform ID
 	appData = Buffer.from(origAppData);
 	const prefix = { ...origPrefix };
-	expect(prefix.depModuleFunction).to.equal(ModuleInfo.FunctionType.SYSTEM_PART);
-	++prefix.depModuleVersion;
+	++prefix.platformID;
 	updateModulePrefix(appData, prefix);
 	suffix = { ...origSuffix };
 	suffix.fwUniqueId = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb';
 	expect(suffix.fwUniqueId).to.not.equal(origSuffix.fwUniqueId);
 	updateModuleSuffix(appData, suffix);
-	const appFile2 = await tempy.write(appData, { name: 'app2.bin' });
-	// Flash the binaries to the device
-	await flash(this, appFile1);
-	await flash(this, appFile2);
+	appFile = await tempy.write(appData, { name: 'app2.bin' });
+	// The device should reject this update as well as clear the previously received one
+	await flash(this, appFile, { expectedStatus: 'failed' });
+	// Reset the device so that it can apply pending updates (it shouldn't have any)
+	await device.reset();
+});
+
+test('03_validate_module_info', async function () {
+	// See the test app
 });
