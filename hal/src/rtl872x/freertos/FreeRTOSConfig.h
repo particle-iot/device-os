773b71d9143fe5888f6d7ebba5db5f953c81e67c
Eugene <yuting0501@gmail.com>
2021-08-23 23:51:14 +0800

    [RTL872x] Adjust the thread priority

--- hal/src/rtl872x/freertos/FreeRTOSConfig.h
+++ hal/src/rtl872x/freertos/FreeRTOSConfig.h
@@ -131,10 +131,10 @@ extern void vApplicationTaskDeleteHook(void *pvTaskToDelete, volatile long* pxPe
 
 /* Software timer definitions. */
 #define configUSE_TIMERS								1
-// #define configTIMER_TASK_PRIORITY                       1
+#define configTIMER_TASK_PRIORITY                       2
 // #define configTIMER_QUEUE_LENGTH						( 10 + 32 )
 // #define configTIMER_TASK_STACK_DEPTH					( 512  )
-#define configTIMER_TASK_PRIORITY						( configMAX_PRIORITIES - 1 )
+// #define configTIMER_TASK_PRIORITY						( configMAX_PRIORITIES - 1 )
 #define configTIMER_QUEUE_LENGTH						( 64 )
 #define configTIMER_TASK_STACK_DEPTH					( ( unsigned short ) (4096 / sizeof( portSTACK_TYPE )) )
 
