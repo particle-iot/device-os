77dbaf7413b2ad1a98d6e00f4b888a4cd119d549
Sergey Polyakov <sergeuz@gmail.com>
2021-08-24 18:28:53 +0200

    Add unit tests; minor fixes

--- services/inc/str_util.h
+++ services/inc/str_util.h
@@ -118,17 +118,15 @@ inline size_t toHex(const void* src, size_t srcSize, char* dest, size_t destSize
     static const char alpha[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
     size_t n = 0;
     auto srcBytes = (const uint8_t*)src;
-    for (size_t i = 0; i < srcSize && n < destSize; ++i) {
+    for (size_t i = 0; i < srcSize && n + 1 < destSize; ++i) {
         const auto b = srcBytes[i];
         dest[n++] = alpha[b >> 4];
-        if (n < destSize) {
+        if (n + 1 < destSize) {
             dest[n++] = alpha[b & 0x0f];
         }
     }
     if (n < destSize) {
         dest[n] = '\0';
-    } else if (destSize > 0) {
-        dest[destSize - 1] = '\0';
     }
     return n;
 }
