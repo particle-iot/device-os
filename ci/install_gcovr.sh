93613d2526b27c1768d0f31ea82ef4d914da5152
Todd Stellanova <tstellanova@users.noreply.github.com>
2021-10-02 15:38:50 -0700

    more gcovr dependency juggling

--- ci/install_gcovr.sh
+++ ci/install_gcovr.sh
@@ -19,15 +19,20 @@ else
   apt-get -qq install git python3-pip libxml2-dev libxslt-dev)
 fi
 
-
+# get the last version of pip compatible with python 3.5
 pip3 install --upgrade pip==20.3.4 
 
 pip3 install requests
-# install latest stable gcovr. Warning: may not support python3.5
-pip3 install gcovr==5.0
+
+# install a specific gcovr version. May not work if that version isn't compatible with the pip3 version
+# pip3 install gcovr==5.0
+
+# install latest stable gcovr version
+pip3 install gcovr
 
 # install gcovr latest develop branch
-#pip3 install git+https://github.com/gcovr/gcovr.git
+# pip3 install git+https://github.com/gcovr/gcovr.git
+
 # FIXME: MarkupSafe is broken on python3.5
 ln -s /usr/local/lib/python3.5/dist-packages/MarkupSafe-0.0.0.dist-info /usr/local/lib/python3.5/dist-packages/MarkupSafe-2.0.0.dist-info
 
