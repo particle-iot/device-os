3e141631e6bcebcf0040e789d42472a082e34169
keeramis <keerthy.amisagadda@gmail.com>
2021-12-15 11:57:45 -0600

    tests

--- user/tests/app/ble/prov_mode/application.cpp
+++ user/tests/app/ble/prov_mode/application.cpp
@@ -27,6 +27,7 @@ Serial1LogHandler log(115200, LOG_LEVEL_ALL);
 
 STARTUP(System.enableFeature(FEATURE_DISABLE_LISTENING_MODE));
 
+const char* advServiceUuid = "6E460120-B5A3-F393-E0A9-E50E24DCCA9E";
 const char* serviceUuid = "6E400021-B5A3-F393-E0A9-E50E24DCCA9E";
 const char* rxUuid = "6E400022-B5A3-F393-E0A9-E50E24DCCA9E";
 const char* txUuid = "6E400023-B5A3-F393-E0A9-E50E24DCCA9E";
@@ -39,6 +40,7 @@ void setup() {
     // Must run before initialising BLE for the first time
     // Even better to call in STARTUP()
     BLE.setProvisioningUuids(serviceUuid, txUuid, rxUuid);
+    BLE.setProvisioningAdvServiceUuid(advServiceUuid);
     BLE.provisioningMode(true);
     LOG(TRACE, "BLE prov mode status: %d", BLE.provisioningStatus());
 
