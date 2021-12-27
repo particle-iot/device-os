cede340519443492211ed7bf2ab48a0ff3ea9ee3
XuGuohui <xghui999@126.com>
2021-12-28 02:30:40 +0800

    [rtl872x]: stop mode verification

---
+++ bootloader/prebootloader/src/tron/part1/cxx_constructors.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 Particle Industries, Inc.  All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Define caddr_t as char* */
+#include <sys/types.h>
+#include <errno.h>
+#include <malloc.h>
+/* Define abort() */
+#include <stdlib.h>
+
+extern "C" {
+
+void *__dso_handle = nullptr;
+
+}
