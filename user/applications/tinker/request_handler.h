69fd41ac3aeba7ff836c1a790205539181ecffcf
Scott Brust <scott.brust@particle.io>
2022-03-18 16:04:40 -0700

    Move fqc handler/commands to own code

--- user/applications/tinker/request_handler.h
+++ user/applications/tinker/request_handler.h
@@ -1,9 +1,6 @@
 #pragma once
 
 #include "system_control.h"
-#include "spark_wiring_tcpclient.h"
-#include "spark_wiring_vector.h"
-#include "spark_wiring_json.h"
 
 namespace particle {
 
@@ -24,24 +21,9 @@ private:
     class Request;
 
     bool inited_;
-    uint8_t tcpServer[4];
-    int tcpPort;
-    TCPClient tcpClient;
 
     int request(ctrl_request* ctrlReq);
     int request(Request* req);
-    const auto passResponse(bool success);
-    const auto tcpErrorResponse(int tcpError);
-    const auto wifiScanNetworksResponse(Vector<WiFiAccessPoint> * accessPoints);
-    void parseIpAndPort(spark::JSONValue parameters);
-    int sendTCPMessage(const char * tx_data, char * rx_data_buffer, int rx_data_buffer_length, int response_poll_ms = 5000);
-    
-    int writeSerial1(Request* req);
-    int useAntenna(Request* req);
-    int bleScan(Request* req);
-    int ioTest(Request* req);
-    int wifiNetcat(Request* req);
-    int wifiScanNetworks(Request* req);
 };
 
 } // namespace particle
