dc61e5cd6d3ca26f380bfa27273530a828dff530
Eugene <yuting0501@gmail.com>
2022-03-21 22:47:59 +0800

    [rtl872x] SPI Test: shouldn't send dummy after SPI enabled on P2

--- user/tests/wiring/spi_master_slave/spi_master/spi_master.cpp
+++ user/tests/wiring/spi_master_slave/spi_master/spi_master.cpp
@@ -228,7 +228,9 @@ static void SPI_Master_Configure()
         MY_SPI.begin(MY_CS);
 
         // Clock dummy byte just in case
-        (void)MY_SPI.transfer(0xff);
+        if (PLATFORM_ID != PLATFORM_P2) { // Since SPI is enabled, the dummy byte will generate gabage data in the fifo on P2
+            (void)MY_SPI.transfer(0xff);
+        }
     }
 }
 
