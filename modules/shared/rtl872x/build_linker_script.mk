d67f4e5faa12aced8d7b0a761bf56cef197bacd8
Eugene <yuting0501@gmail.com>
2022-03-29 11:42:34 +0800

    [rtl872x] fixes compatibility issues of GNU tools on windows

--- modules/shared/rtl872x/build_linker_script.mk
+++ modules/shared/rtl872x/build_linker_script.mk
@@ -1,11 +1,11 @@
 WRITE_FILE_CREATE = $(shell echo "$(2)" > $(1))
 WRITE_FILE_APPEND = $(shell echo "$(2)" >> $(1))
 
-sector_aligned = $(shell if [ `expr $1 % 4096` -eq 0 ]; then \
-                             echo $1; \
-						 else \
-						     echo `expr '(' $1 / 4096 + 1 ')' '*' 4096`; \
-						 fi)
+ifeq ($(OS),Windows_NT)
+AWK = gawk
+else
+AWK = awk
+endif
 
 COMMA := ,
 
@@ -18,19 +18,19 @@ USER_SRAM_LENGTH = 128K
 USER_PSRAM_LENGTH = 1M
 USER_FLASH_LENGTH = 2M
 else
-TEXT_SECTION_LEN  = $(shell $(SIZE) --format=berkeley $(INTERMEDIATE_ELF) | awk 'NR==2 {print $$1}')
-DATA_SECTION_LEN  = $(shell $(SIZE) --format=berkeley $(INTERMEDIATE_ELF) | awk 'NR==2 {print $$2}')
-BSS_SECTION_LEN   = $(shell $(SIZE) --format=berkeley $(INTERMEDIATE_ELF) | awk 'NR==2 {print $$3}')
+TEXT_SECTION_LEN  = $(shell $(SIZE) --format=berkeley $(INTERMEDIATE_ELF) | $(AWK) 'NR==2 {print $$1}')
+DATA_SECTION_LEN  = $(shell $(SIZE) --format=berkeley $(INTERMEDIATE_ELF) | $(AWK) 'NR==2 {print $$2}')
+BSS_SECTION_LEN   = $(shell $(SIZE) --format=berkeley $(INTERMEDIATE_ELF) | $(AWK) 'NR==2 {print $$3}')
 
-PSRAM_TEXT_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.psram_text $(INTERMEDIATE_ELF) | grep -E '^\s*[0-9]+\s+\.psram_text\s+')
+PSRAM_TEXT_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.psram_text $(INTERMEDIATE_ELF) | grep -E '.psram_text')
 PSRAM_TEXT_SECTION_LEN := 0x$(word 3,$(PSRAM_TEXT_SECTION_LEN))
-PSRAM_DATA_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.data_alt $(INTERMEDIATE_ELF) | grep -E '^\s*[0-9]+\s+\.data_alt\s+')
+PSRAM_DATA_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.data_alt $(INTERMEDIATE_ELF) | grep -E '.data_alt')
 PSRAM_DATA_SECTION_LEN := 0x$(word 3,$(PSRAM_DATA_SECTION_LEN))
-PSRAM_BSS_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.bss_alt $(INTERMEDIATE_ELF) | grep -E '^\s*[0-9]+\s+\.bss_alt\s+')
+PSRAM_BSS_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.bss_alt $(INTERMEDIATE_ELF) | grep -E '.bss_alt')
 PSRAM_BSS_SECTION_LEN := 0x$(word 3,$(PSRAM_BSS_SECTION_LEN))
-USER_MODULE_END = 0x$(shell $(OBJDUMP) -t $(INTERMEDIATE_ELF) | grep link_module_info_crc_end | awk '{ print $$1 }')
-USER_MODULE_START = 0x$(shell $(OBJDUMP) -t $(INTERMEDIATE_ELF) | grep link_module_start | awk '{ print $$1 }')
-USER_MODULE_SUFFIX_START = 0x$(shell $(OBJDUMP) -t $(INTERMEDIATE_ELF) | grep link_module_info_static_start | awk '{ print $$1 }')
+USER_MODULE_END = 0x$(shell $(OBJDUMP) -t $(INTERMEDIATE_ELF) | grep link_module_info_crc_end | $(AWK) '{ print $$1 }')
+USER_MODULE_START = 0x$(shell $(OBJDUMP) -t $(INTERMEDIATE_ELF) | grep link_module_start | $(AWK) '{ print $$1 }')
+USER_MODULE_SUFFIX_START = 0x$(shell $(OBJDUMP) -t $(INTERMEDIATE_ELF) | grep link_module_info_static_start | $(AWK) '{ print $$1 }')
 MODULE_INFO_SUFFIX_LEN := ( $(USER_MODULE_END) - $(USER_MODULE_SUFFIX_START) )
 
 # Note: reserving 16 bytes for alignment just in case
@@ -38,7 +38,7 @@ USER_SRAM_LENGTH = ( $(DATA_SECTION_LEN) + $(BSS_SECTION_LEN) + 16 )
 USER_PSRAM_LENGTH = ( $(PSRAM_TEXT_SECTION_LEN) + $(PSRAM_DATA_SECTION_LEN) + $(PSRAM_BSS_SECTION_LEN) + 16)
 
 USER_FLASH_LENGTH = $(shell let var=($(USER_MODULE_END) - $(USER_MODULE_START) + 16); echo $$var)
-USER_FLASH_LENGTH := $(call sector_aligned,$(USER_FLASH_LENGTH))
+USER_FLASH_LENGTH := $(shell echo $$((($(USER_FLASH_LENGTH) + 4095) / 4096 * 4096)))
 
 $(call WRITE_FILE_APPEND, "$(MODULE_USER_MEMORY_FILE_GEN)",platform_user_part_trimmed = 1;)
 $(call WRITE_FILE_APPEND, "$(MODULE_USER_MEMORY_FILE_GEN)",platform_module_info_suffix_size = $(MODULE_INFO_SUFFIX_LEN);)
@@ -51,4 +51,3 @@ all:
 	$(call WRITE_FILE_APPEND, "$(MODULE_USER_MEMORY_FILE_GEN)",platform_user_part_static_ram_size = $(USER_SRAM_LENGTH);)
 	$(call WRITE_FILE_APPEND, "$(MODULE_USER_MEMORY_FILE_GEN)",platform_user_part_psram_size = $(USER_PSRAM_LENGTH);)
 	$(call WRITE_FILE_APPEND, "$(MODULE_USER_MEMORY_FILE_GEN)",platform_user_part_flash_size = $(USER_FLASH_LENGTH);)
-
