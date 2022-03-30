6837c9138134c217d514d8239cbdab39618cce36
Andrey Tolstoy <avtolstoy@gmail.com>
2022-03-31 06:42:51 +0700

    [test] ota/multiple_ota_no_reset for P2

--- user/tests/integration/ota/multiple_ota_no_reset/multiple_ota_no_reset.cpp
+++ user/tests/integration/ota/multiple_ota_no_reset/multiple_ota_no_reset.cpp
@@ -9,12 +9,15 @@
 
 namespace {
 
-#if PLATFORM_GEN == 3
+#if HAL_PLATFORM_NRF52840
 const auto OTA_SECTION_STORAGE = HAL_STORAGE_ID_EXTERNAL_FLASH;
 const auto OTA_SECTION_ADDRESS = EXTERNAL_FLASH_OTA_ADDRESS;
 #elif PLATFORM_ID == PLATFORM_ELECTRON || PLATFORM_ID == PLATFORM_PHOTON || PLATFORM_ID == PLATFORM_P1
 const auto OTA_SECTION_STORAGE = HAL_STORAGE_ID_INTERNAL_FLASH;
 const auto OTA_SECTION_ADDRESS = 0x080c0000; // See the platform's ota_module_bounds.c
+#elif HAL_PLATFORM_RTL872X
+const auto OTA_SECTION_STORAGE = HAL_STORAGE_ID_INTERNAL_FLASH;
+uintptr_t OTA_SECTION_ADDRESS = 0; // Runtime
 #else
 #error "Unsupported platform"
 #endif
@@ -41,6 +44,30 @@ bool getAppHash(char* buf, size_t size) {
     return false;
 }
 
+#if HAL_PLATFORM_RTL872X
+uintptr_t getOtaAddressRtl() {
+    hal_system_info_t info = {};
+    info.size = sizeof(info);
+    const int r = system_info_get_unstable(&info, 0 /* flags */, nullptr /* reserved */);
+    if (r != 0) {
+        return 0;
+    }
+    SCOPE_GUARD({
+        system_info_free_unstable(&info, nullptr /* reserved */);
+    });
+    for (size_t i = 0; i < info.module_count; ++i) {
+        const auto& module = info.modules[i];
+        if (module.info.module_function == MODULE_FUNCTION_SYSTEM_PART) {
+            uintptr_t end = (uintptr_t)module.info.module_end_address;
+            end += 4; // CRC32
+            end = ((end) & 0xFFFFF000) + 0x1000; // 4K aligned
+            return end;
+        }
+    }
+    return 0;
+}
+#endif // HAL_PLATFORM_RTL872X
+
 } // namespace
 
 test(01_disable_resets_and_connect) {
@@ -65,6 +92,10 @@ test(03_fix_ota_binary_and_reset) {
     // ID in the binary that is currently stored in the OTA section so that the bootloader can apply
     // it if there still happens to be a valid module slot in the DCT
     const uint16_t id = PLATFORM_ID;
+#if HAL_PLATFORM_RTL872X
+    OTA_SECTION_ADDRESS = getOtaAddressRtl();
+    assertNotEqual(OTA_SECTION_ADDRESS, (uintptr_t)0);
+#endif // HAL_PLATFORM_RTL872X
     assertEqual(hal_storage_write(OTA_SECTION_STORAGE, OTA_SECTION_ADDRESS + offsetof(module_info_t, platform_id),
             (const uint8_t*)&id, sizeof(id)), sizeof(id));
 }
