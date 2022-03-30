27777dae98ba0f20cace13e9e571fa6612f5cc55
Andrey Tolstoy <avtolstoy@gmail.com>
2022-03-30 21:46:08 +0700

    minor

--- system/src/system_task.cpp
+++ system/src/system_task.cpp
@@ -718,12 +718,8 @@ void cancel_connection()
 namespace {
 
 // Memory pool for small and short-lived allocations
-#if HAL_PLATFORM_RTL872X
 uint8_t __attribute__((aligned(4))) s_buffer[HAL_PLATFORM_SYSTEM_POOL_SIZE];
 SimpleStaticPool g_memPool(s_buffer, sizeof(s_buffer));
-#else
-SimpleAllocedPool g_memPool(HAL_PLATFORM_SYSTEM_POOL_SIZE);
-#endif
 
 } // namespace
 
