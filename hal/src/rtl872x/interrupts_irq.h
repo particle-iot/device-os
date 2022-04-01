89d0343204329fd33d0dad3ac34bcccf00378790
Andrey Tolstoy <avtolstoy@gmail.com>
2022-04-01 07:00:05 +0700

    minor

--- hal/src/rtl872x/interrupts_irq.h
+++ hal/src/rtl872x/interrupts_irq.h
@@ -28,19 +28,41 @@ extern "C" {
 // FIXME
 typedef int32_t IRQn_Type;
 #ifdef AMEBAD_TODO
+#ifndef __ARMV8MML_REV
 #define __ARMV8MML_REV                 0x0000U  /*!< ARMV8MML Core Revision                                                    */
+#endif // __ARMV8MML_REV
+#ifndef __Vendor_SysTickConfig
 #define __Vendor_SysTickConfig         0        /*!< Set to 1 if different SysTick Config is used                              */
+#endif // __Vendor_SysTickConfig
+#ifndef __VTOR_PRESENT
 #define __VTOR_PRESENT                 1        /*!< Set to 1 if CPU supports Vector Table Offset Register                     */
+#endif // __VTOR_PRESENT
+#ifndef __FPU_DP
 #define __FPU_DP                       0        /*!< Double Precision FPU                                                      */
+#endif // __FPU_DP
 #endif
+#ifndef __CM3_REV
 #define __CM3_REV                      0x0200    /**< Core revision r0p0 */
+#endif // __CM3_REV
+#ifndef __MPU_PRESENT
 #define __MPU_PRESENT                  1         /**< Defines if an MPU is present or not */
+#endif // __MPU_PRESENT
+#ifndef __NVIC_PRIO_BITS
 #define __NVIC_PRIO_BITS               3         /**< Number of priority bits implemented in the NVIC */
+#endif // __NVIC_PRIO_BITS
+#ifndef __Vendor_SysTickConfig
 #define __Vendor_SysTickConfig         1         /**< Vendor specific implementation of SysTickConfig is defined *///see vPortSetupTimerInterrupt
+#endif // __Vendor_SysTickConfig
+#ifndef __SAUREGION_PRESENT
 #define __SAUREGION_PRESENT            1        /*!< SAU present or not                                                        */
+#endif // __SAUREGION_PRESENT
 
+#ifndef __FPU_PRESENT
 #define __FPU_PRESENT             1       /*!< FPU present                                   */
+#endif // __FPU_PRESENT
+#ifndef __VFP_FP__
 #define __VFP_FP__	1
+#endif // __VFP_FP__
 #ifndef __ARM_FEATURE_CMSE
 #define __ARM_FEATURE_CMSE	3
 #endif
