e6da279ea96b9f22f0743a967412a615a65eec41
Technobly <technobly@gmail.com>
2022-04-11 18:30:46 -0500

    [gen2][supply secure] bootloader

--- bootloader/src/stm32f2xx/bkpreg_hal.c
+++
@@ -1,4 +0,0 @@
-
-// pull in the sources from the HAL. It's a bit of a hack, but is simpler than trying to link the
-// full hal library.
-#include "../src/stm32f2xx/bkpreg_hal.c"
--- bootloader/src/stm32f2xx/core_hal.c
+++
@@ -1,5 +0,0 @@
-#include "core_hal.h"
-
-void HAL_Core_System_Reset_Ex(int reason, uint32_t data, void *reserved) {
-    NVIC_SystemReset();
-}
