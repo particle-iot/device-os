fd7110eb55bd2d1b85d3bfdedd5e2ffb4ea57137
keeramis <keerthy.amisagadda@gmail.com>
2022-04-12 11:49:45 -0500

    [gen2][supply secure] crypto

--- crypto/inc/crypto_dynalib.h
+++ crypto/inc/crypto_dynalib.h
@@ -38,13 +38,6 @@
 #include "mbedtls/sha256.h"
 #include "mbedtls/bignum.h"
 #include "mbedtls_util.h"
-#if PLATFORM_ID == 6 || PLATFORM_ID == 8
-#include "mbedtls/md4.h"
-#include "mbedtls/md5.h"
-#include "mbedtls/des.h"
-#include "mbedtls/asn1.h"
-#include "mbedtls/sha512.h"
-#endif
 #endif
 #endif // USE_MBEDTLS
 
@@ -132,82 +125,7 @@ DYNALIB_FN(70, crypto, mbedtls_sha256_process, void(mbedtls_sha256_context*, con
 DYNALIB_FN(71, crypto, mbedtls_rsa_check_pubkey, int(const mbedtls_rsa_context*))
 DYNALIB_FN(72, crypto, mbedtls_rsa_check_privkey, int(const mbedtls_rsa_context*))
 DYNALIB_FN(73, crypto, mbedtls_rsa_check_pub_priv, int(const mbedtls_rsa_context*, const mbedtls_rsa_context*))
-#if PLATFORM_ID == 6 || PLATFORM_ID == 8
-// MD4
-DYNALIB_FN(74, crypto, mbedtls_md4_init, void(mbedtls_md4_context*))
-DYNALIB_FN(75, crypto, mbedtls_md4_free, void(mbedtls_md4_context*))
-DYNALIB_FN(76, crypto, mbedtls_md4_clone, void(mbedtls_md4_context*, const mbedtls_md4_context*))
-DYNALIB_FN(77, crypto, mbedtls_md4_starts, void(mbedtls_md4_context*))
-DYNALIB_FN(78, crypto, mbedtls_md4_update, void(mbedtls_md4_context*, const unsigned char*, size_t))
-DYNALIB_FN(79, crypto, mbedtls_md4_finish, void(mbedtls_md4_context*, unsigned char[16]))
-DYNALIB_FN(80, crypto, mbedtls_md4, void(const unsigned char*, size_t, unsigned char[16]))
-DYNALIB_FN(81, crypto, mbedtls_md4_process, void(mbedtls_md4_context*, const unsigned char[64]))
-
-// MD5
-DYNALIB_FN(82, crypto, mbedtls_md5_init, void(mbedtls_md5_context*))
-DYNALIB_FN(83, crypto, mbedtls_md5_free, void(mbedtls_md5_context*))
-DYNALIB_FN(84, crypto, mbedtls_md5_clone, void(mbedtls_md5_context*, const mbedtls_md5_context*))
-DYNALIB_FN(85, crypto, mbedtls_md5_starts, void(mbedtls_md5_context*))
-DYNALIB_FN(86, crypto, mbedtls_md5_update, void(mbedtls_md5_context*, const unsigned char*, size_t))
-DYNALIB_FN(87, crypto, mbedtls_md5_finish, void(mbedtls_md5_context*, unsigned char[16]))
-DYNALIB_FN(88, crypto, mbedtls_md5_process, void(mbedtls_md5_context*, const unsigned char[64]))
-DYNALIB_FN(89, crypto, mbedtls_md5, void(const unsigned char*, size_t, unsigned char[16]))
-
-// DES
-DYNALIB_FN(90, crypto, mbedtls_des_init, void(mbedtls_des_context*))
-DYNALIB_FN(91, crypto, mbedtls_des_free, void(mbedtls_des_context*))
-DYNALIB_FN(92, crypto, mbedtls_des3_init, void(mbedtls_des3_context*))
-DYNALIB_FN(93, crypto, mbedtls_des3_free, void(mbedtls_des3_context*))
-DYNALIB_FN(94, crypto, mbedtls_des_setkey_enc, int(mbedtls_des_context*, const unsigned char[MBEDTLS_DES_KEY_SIZE]))
-DYNALIB_FN(95, crypto, mbedtls_des_setkey_dec, int(mbedtls_des_context*, const unsigned char[MBEDTLS_DES_KEY_SIZE]))
-DYNALIB_FN(96, crypto, mbedtls_des3_set3key_dec, int(mbedtls_des3_context*, const unsigned char[MBEDTLS_DES_KEY_SIZE * 3]))
-DYNALIB_FN(97, crypto, mbedtls_des_crypt_ecb, int(mbedtls_des_context*, const unsigned char[8], unsigned char[8]))
-DYNALIB_FN(98, crypto, mbedtls_des_crypt_cbc, int(mbedtls_des_context*, int, size_t, unsigned char[8], const unsigned char*, unsigned char*))
-DYNALIB_FN(99, crypto, mbedtls_des3_crypt_ecb, int(mbedtls_des3_context*, const unsigned char[8], unsigned char[8]))
-DYNALIB_FN(100, crypto, mbedtls_des3_crypt_cbc, int(mbedtls_des3_context*, int, size_t, unsigned char[8], const unsigned char*, unsigned char*))
-DYNALIB_FN(101, crypto, mbedtls_des_setkey, void(uint32_t[32], const unsigned char[MBEDTLS_DES_KEY_SIZE]))
-// Unused
-//DYNALIB_FN(XXX, crypto, mbedtls_des3_set3key_enc, int(mbedtls_des3_context*, const unsigned char[MBEDTLS_DES_KEY_SIZE * 3]))
-//DYNALIB_FN(XXX, crypto, mbedtls_des_key_set_parity, void(unsigned char[MBEDTLS_DES_KEY_SIZE]))
-
-// MD
-DYNALIB_FN(102, crypto, mbedtls_md_list, const int*(void))
-DYNALIB_FN(103, crypto, mbedtls_md_info_from_string, const mbedtls_md_info_t*(const char *))
-DYNALIB_FN(104, crypto, mbedtls_md_info_from_type, const mbedtls_md_info_t*(mbedtls_md_type_t))
-DYNALIB_FN(105, crypto, mbedtls_md_init, void(mbedtls_md_context_t*))
-DYNALIB_FN(106, crypto, mbedtls_md_free, void(mbedtls_md_context_t*))
-DYNALIB_FN(107, crypto, mbedtls_md_setup, int(mbedtls_md_context_t*, const mbedtls_md_info_t*, int))
-DYNALIB_FN(108, crypto, mbedtls_md_starts, int(mbedtls_md_context_t*))
-DYNALIB_FN(109, crypto, mbedtls_md_hmac_starts, int(mbedtls_md_context_t*, const unsigned char*, size_t))
-DYNALIB_FN(110, crypto, mbedtls_md_hmac_update, int(mbedtls_md_context_t*, const unsigned char*, size_t))
-DYNALIB_FN(111, crypto, mbedtls_md_hmac_finish, int(mbedtls_md_context_t*, unsigned char*))
-DYNALIB_FN(112, crypto, mbedtls_md_hmac, int(const mbedtls_md_info_t*, const unsigned char*, size_t, const unsigned char*, size_t, unsigned char*))
-DYNALIB_FN(113, crypto, mbedtls_md, int(const mbedtls_md_info_t*, const unsigned char*, size_t, unsigned char*))
-// Unused
-//DYNALIB_FN(XXX, crypto, mbedtls_md_clone, int(mbedtls_md_context_t*, const mbedtls_md_context_t*))
-//DYNALIB_FN(XXX, crypto, mbedtls_md_finish, int(mbedtls_md_context_t*, unsigned char*))
-//DYNALIB_FN(XXX, crypto, mbedtls_md_hmac_reset, int(mbedtls_md_context_t*))
-
-DYNALIB_FN(114, crypto, mbedtls_sha512_init, void(mbedtls_sha512_context*))
-DYNALIB_FN(115, crypto, mbedtls_sha512_free, void(mbedtls_sha512_context*))
-DYNALIB_FN(116, crypto, mbedtls_sha512_clone, void(mbedtls_sha512_context*, const mbedtls_sha512_context*))
-DYNALIB_FN(117, crypto, mbedtls_sha512_starts, void(mbedtls_sha512_context*, int))
-DYNALIB_FN(118, crypto, mbedtls_sha512_update, void(mbedtls_sha512_context*, const unsigned char*, size_t))
-DYNALIB_FN(119, crypto, mbedtls_sha512_finish, void(mbedtls_sha512_context*, unsigned char output[64]))
-DYNALIB_FN(120, crypto, mbedtls_sha512_process, void(mbedtls_sha512_context*, const unsigned char data[128]))
-
-// MD5 with return value
-DYNALIB_FN(121, crypto, mbedtls_md5_starts_ret, int(mbedtls_md5_context*))
-DYNALIB_FN(122, crypto, mbedtls_md5_update_ret, int(mbedtls_md5_context*, const unsigned char*, size_t))
-DYNALIB_FN(123, crypto, mbedtls_md5_finish_ret, int(mbedtls_md5_context*, unsigned char[16]))
-DYNALIB_FN(124, crypto, mbedtls_internal_md5_process, int(mbedtls_md5_context*, const unsigned char[64]))
-
-#define BASE_IDX 125 // Base index for all subsequent functions
-#else // !(PLATFORM_ID == 6 || PLATFORM_ID == 8)
-#define BASE_IDX 74
-#endif
-
-DYNALIB_FN(BASE_IDX + 0, crypto, mbedtls_to_system_error, int(int))
+DYNALIB_FN(74, crypto, mbedtls_to_system_error, int(int))
 
 DYNALIB_END(crypto)
 
