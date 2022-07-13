a2e33a4f31bfcc542ce935eaecbb1fade9e75b37
Scott Brust <scott.brust@particle.io>
2022-07-12 22:47:23 -0700

    Fix broken tone and servo tests for p2 platform

--- user/tests/wiring/no_fixture_long_running/tone.cpp
+++ user/tests/wiring/no_fixture_long_running/tone.cpp
@@ -27,18 +27,20 @@
 #include "tone_hal.h"
 #include "unit-test/unit-test.h"
 
-#if PLATFORM_ID == PLATFORM_ESOMX
-static const pin_t pin = D1;//pin under test
+#if PLATFORM_ID == PLATFORM_P2
+static const hal_pin_t pin = D1;//pin under test
+#elif PLATFORM_ID == PLATFORM_ESOMX
+static const hal_pin_t pin = A3;//pin under test
 #else
-static const pin_t pin = A1;//pin under test
+static const hal_pin_t pin = A1;//pin under test
 #endif
 
 test(TONE_01_NoGenerateWhenPinSelectedIsNotTimerChannel) {
 #if HAL_PLATFORM_NRF52840
 # if PLATFORM_ID == PLATFORM_TRACKER
-    pin_t pin = BTN;
+    hal_pin_t pin = BTN;
 # elif PLATFORM_ID == PLATFORM_ESOMX
-    pin_t pin = A0;
+    hal_pin_t pin = A0;
 # else
     hal_pin_t pin = D0;
 # endif
