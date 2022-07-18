fc6828b44ee3570dd1cd5c49819b7aac95818b2f
Andrey Tolstoy <avtolstoy@gmail.com>
2022-07-18 20:12:18 +0700

    [rtl872x] usart: flush DMA FIFO data when required

--- third_party/freertos/include.mk
+++ third_party/freertos/include.mk
@@ -3,16 +3,15 @@ INCLUDE_DIRS += $(FREERTOS_MODULE_PATH)/freertos/FreeRTOS/Source/include
 
 # FreeRTOS port
 ifneq ("$(PLATFORM_FREERTOS)","")
-TARGET_FREERTOS_PORT_INCLUDE_PATH = $(FREERTOS_MODULE_PATH)/freertos/FreeRTOS/Source/portable/GCC/$(PLATFORM_FREERTOS)
-endif
-
+INCLUDE_DIRS += $(FREERTOS_MODULE_PATH)/freertos/FreeRTOS/Source/portable/GCC/$(PLATFORM_FREERTOS)
+else
 # FIXME
 ifeq ($(SOFTDEVICE_PRESENT),y)
 INCLUDE_DIRS += $(TARGET_NRF5_SDK_PATH)/nrf5_sdk/external/freertos/portable/GCC/nrf52
 INCLUDE_DIRS += $(TARGET_NRF5_SDK_PATH)/nrf5_sdk/external/freertos/portable/CMSIS/nrf52
 else
-INCLUDE_DIRS += $(TARGET_FREERTOS_PORT_INCLUDE_PATH)
-endif
-
-
+ifeq ($(MCU_DEVICE),rtl872x)
 INCLUDE_DIRS += $(TARGET_AMBD_SDK_PATH)/ambd_sdk/component/os/freertos/freertos_v10.2.0/Source/portable/GCC/RTL8721D_HP/non_secure
+endif
+endif
+endif
