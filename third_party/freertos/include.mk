4c84a4e1cc5301cb84356e683e4c84e2e1efbb4e
Andrey Tolstoy <avtolstoy@gmail.com>
2022-07-19 02:42:05 +0700

    [third_party] freertos: use PLATFORM_MCU vs MCU_DEVICE as P2 has sub-cores that have core-suffixed MCU_DEVICE

--- third_party/freertos/include.mk
+++ third_party/freertos/include.mk
@@ -10,7 +10,7 @@ ifeq ($(SOFTDEVICE_PRESENT),y)
 INCLUDE_DIRS += $(TARGET_NRF5_SDK_PATH)/nrf5_sdk/external/freertos/portable/GCC/nrf52
 INCLUDE_DIRS += $(TARGET_NRF5_SDK_PATH)/nrf5_sdk/external/freertos/portable/CMSIS/nrf52
 else
-ifeq ($(MCU_DEVICE),rtl872x)
+ifeq ($(PLATFORM_MCU),rtl872x)
 INCLUDE_DIRS += $(TARGET_AMBD_SDK_PATH)/ambd_sdk/component/os/freertos/freertos_v10.2.0/Source/portable/GCC/RTL8721D_HP/non_secure
 endif
 endif
