8270eb0db1d9632352cadfa35784780dcf507fe3
James Wilson <james.wilson@particle.io>
2022-08-22 11:52:10 -0700

    Update demux driver and pinmap

--- hal/shared/demux.h
+++ hal/shared/demux.h
@@ -33,11 +33,11 @@
 #define DEMUX_MAX_PIN_COUNT     8
 #if HAL_PLATFORM_NRF52840
 #define DEMUX_NRF_PORT          (NRF_P1)
-#endif
 #define DEMUX_PIN_0_MASK        0x00000400
 #define DEMUX_PIN_1_MASK        0x00000800
 #define DEMUX_PIN_2_MASK        0x00001000
 #define DEMUX_PINS_SHIFT        (10)
+#endif
 
 
 namespace particle {
