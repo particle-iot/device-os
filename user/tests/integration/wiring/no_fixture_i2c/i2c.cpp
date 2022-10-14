ee1f74cf41bd887955affa78b62df020f3a98853
Scott Brust <scott.brust@particle.io>
2022-10-14 09:54:45 -0700

    Feedback

--- user/tests/integration/wiring/no_fixture_i2c/i2c.cpp
+++ user/tests/integration/wiring/no_fixture_i2c/i2c.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022 Particle Industries, Inc.  All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "application.h"
 #include "unit-test/unit-test.h"
@@ -6,7 +22,7 @@
 
 static const int WIRE_ACQUIRE_BUFFER_SIZE = HAL_PLATFORM_I2C_BUFFER_SIZE(HAL_I2C_INTERFACE1) + 1;
 static const int WIRE1_ACQUIRE_BUFFER_SIZE = HAL_PLATFORM_I2C_BUFFER_SIZE(HAL_I2C_INTERFACE2) + 1;
-static const int WIRE1_TRACKER_MINIMUM_BUFFER_SIZE = 512;
+static const int TRACKER_MINIMUM_BUFFER_SIZE = 512;
 
 static hal_i2c_config_t allocateWireConfig(uint32_t bufferSize) {
     hal_i2c_config_t config = {
@@ -46,9 +62,12 @@ test(I2C_00_hal_init_default_buffer)
 #if (PLATFORM_ID == PLATFORM_TRACKER) || (PLATFORM_ID == PLATFORM_TRACKERM)
     // Tracker platforms should have buffers at least 512 bytes large, allocated by the system on startup
     // IE trying to re-initialize the HAL with same size should fail
-    hal_i2c_config_t wire1_small_buffers = allocateWireConfig(WIRE1_TRACKER_MINIMUM_BUFFER_SIZE);
-    assertEqual(hal_i2c_init(HAL_I2C_INTERFACE2, &wire1_small_buffers), (int)SYSTEM_ERROR_NOT_ENOUGH_DATA);
-#endif 
+    hal_i2c_config_t smallBuffers = allocateWireConfig(TRACKER_MINIMUM_BUFFER_SIZE);
+    hal_i2c_interface_t largeBufferInterface = (PLATFORM_ID == PLATFORM_TRACKER) ? HAL_I2C_INTERFACE2 : HAL_I2C_INTERFACE1;
+    assertEqual(hal_i2c_init(largeBufferInterface, &smallBuffers), (int)SYSTEM_ERROR_NOT_ENOUGH_DATA);
+    freeWireBuffers(&smallBuffers);
+#endif
+
 }
 
 test(I2C_01_test_acquire_wire_buffer) {
@@ -59,30 +78,30 @@ test(I2C_01_test_acquire_wire_buffer) {
     // System Defaults < User values from acquireWireBuffer < Values used here to re-initialize the HAL
 
     // Equal size buffers should fail
-    hal_i2c_config_t wire_smaller_buffers = allocateWireConfig(WIRE_ACQUIRE_BUFFER_SIZE);
-    assertEqual(hal_i2c_init(HAL_I2C_INTERFACE1, &wire_smaller_buffers), (int)SYSTEM_ERROR_NOT_ENOUGH_DATA);
-    freeWireBuffers(&wire_smaller_buffers);
+    hal_i2c_config_t wireSmallerBuffers = allocateWireConfig(WIRE_ACQUIRE_BUFFER_SIZE);
+    assertEqual(hal_i2c_init(HAL_I2C_INTERFACE1, &wireSmallerBuffers), (int)SYSTEM_ERROR_NOT_ENOUGH_DATA);
+    freeWireBuffers(&wireSmallerBuffers);
 
     // Larger sized buffers should succeed
-    hal_i2c_config_t wire_larger_buffers = allocateWireConfig(WIRE_ACQUIRE_BUFFER_SIZE + 1);
-    assertEqual(hal_i2c_init(HAL_I2C_INTERFACE1, &wire_larger_buffers), (int)SYSTEM_ERROR_NONE);
-    freeWireBuffers(&wire_larger_buffers);
+    hal_i2c_config_t wireLargerBuffers = allocateWireConfig(WIRE_ACQUIRE_BUFFER_SIZE + 1);
+    assertEqual(hal_i2c_init(HAL_I2C_INTERFACE1, &wireLargerBuffers), (int)SYSTEM_ERROR_NONE);
+    freeWireBuffers(&wireLargerBuffers);
 
     // Reinitializing with a NULL config is not allowed
     assertEqual(hal_i2c_init(HAL_I2C_INTERFACE1, nullptr), (int)SYSTEM_ERROR_INVALID_ARGUMENT);
 
-    #if Wiring_Wire1
+#if Wiring_Wire1
     Wire1.begin();
-    hal_i2c_config_t wire1_smaller_buffers = allocateWireConfig(WIRE1_ACQUIRE_BUFFER_SIZE);
-    assertEqual(hal_i2c_init(HAL_I2C_INTERFACE2, &wire1_smaller_buffers), (int)SYSTEM_ERROR_NOT_ENOUGH_DATA);
-    freeWireBuffers(&wire1_smaller_buffers);
+    hal_i2c_config_t wire1SmallerBuffers = allocateWireConfig(WIRE1_ACQUIRE_BUFFER_SIZE);
+    assertEqual(hal_i2c_init(HAL_I2C_INTERFACE2, &wire1SmallerBuffers), (int)SYSTEM_ERROR_NOT_ENOUGH_DATA);
+    freeWireBuffers(&wire1SmallerBuffers);
 
-    hal_i2c_config_t wire1_larger_buffers = allocateWireConfig(WIRE1_ACQUIRE_BUFFER_SIZE + 1);
-    assertEqual(hal_i2c_init(HAL_I2C_INTERFACE2, &wire1_larger_buffers), (int)SYSTEM_ERROR_NONE);
-    freeWireBuffers(&wire1_larger_buffers);
+    hal_i2c_config_t wire1LargerBuffers = allocateWireConfig(WIRE1_ACQUIRE_BUFFER_SIZE + 1);
+    assertEqual(hal_i2c_init(HAL_I2C_INTERFACE2, &wire1LargerBuffers), (int)SYSTEM_ERROR_NONE);
+    freeWireBuffers(&wire1LargerBuffers);
 
     assertEqual(hal_i2c_init(HAL_I2C_INTERFACE2, nullptr), (int)SYSTEM_ERROR_INVALID_ARGUMENT);
-    #endif
+#endif
 }
 
 #if HAL_PLATFORM_EXTERNAL_RTC
@@ -94,8 +113,8 @@ test(I2C_02_long_read)
 
     // Read full register range
     const uint8_t REGISTER_RANGE = 64;
-    uint8_t long_read_buffer[REGISTER_RANGE] = {};
-    uint8_t read_length = REGISTER_RANGE; 
+    uint8_t longReadBuffer[REGISTER_RANGE] = {};
+    uint8_t readLength = REGISTER_RANGE; 
 
     // Check read values against ID registers
     const uint8_t ID0_REGISTER = 0x28;
@@ -103,27 +122,29 @@ test(I2C_02_long_read)
     const uint8_t ID1_REGISTER = 0x29;
     const uint8_t ID1_REGISTER_VALUE = 0x05;
 
-    hal_i2c_interface_t wire_interface = HAL_PLATFORM_EXTERNAL_RTC_I2C;
-    uint8_t wire_rtc_address = HAL_PLATFORM_EXTERNAL_RTC_I2C_ADDR;
-    uint8_t start_rtc_register = 0x00;
+    hal_i2c_interface_t wireInterface = HAL_PLATFORM_EXTERNAL_RTC_I2C;
+    uint8_t rtcAddress = HAL_PLATFORM_EXTERNAL_RTC_I2C_ADDR;
+    uint8_t rtcRegister = 0x00;
 
-    Wire1.beginTransmission(wire_rtc_address);
-    Wire1.write(&start_rtc_register, sizeof(start_rtc_register));
-    Wire1.endTransmission();
-    Wire1.requestFrom(wire_rtc_address, read_length);
-    int32_t size = Wire1.available();
-    assertMore(Wire1.available(), 0);
+    hal_i2c_begin_transmission(wireInterface, rtcAddress, NULL);
+    hal_i2c_write(wireInterface, rtcRegister, NULL);
+    hal_i2c_end_transmission(wireInterface, true, NULL);
+    auto config = WireTransmission(rtcAddress).quantity(readLength).stop(true).halConfig();
+    hal_i2c_request_ex(wireInterface, &config, nullptr);
+
+    int32_t size = hal_i2c_available(wireInterface, NULL);
+    assertMore(size, 0);
 
     for (int32_t i = 0; i < size; i++) {
-        long_read_buffer[i] = (uint8_t)Wire1.read();
+        longReadBuffer[i] = (uint8_t)hal_i2c_read(wireInterface, NULL);
     }
 
     // Log.info("I2C_02_long_read result %d\n", size);
-    // Log.dump(long_read_buffer, read_length);
+    // Log.dump(long_read_buffer, readLength);
     // Log.info(" \n");
 
-    assertEqual(long_read_buffer[ID0_REGISTER], ID0_REGISTER_VALUE);
-    assertEqual(long_read_buffer[ID1_REGISTER], ID1_REGISTER_VALUE);
+    assertEqual(longReadBuffer[ID0_REGISTER], ID0_REGISTER_VALUE);
+    assertEqual(longReadBuffer[ID1_REGISTER], ID1_REGISTER_VALUE);
 
     assertTrue(true);
 }
