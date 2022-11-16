26b873c609eea2d48459a2ece78f9a529915df6e
Technobly <technobly@gmail.com>
2022-11-16 17:35:04 -0600

    [gen3] fixes TEST=wiring/no_fixture fastpin tests

--- user/tests/wiring/no_fixture/fastpin.cpp
+++ user/tests/wiring/no_fixture/fastpin.cpp
@@ -1,69 +1,86 @@
-
 #include "application.h"
 #include "unit-test/unit-test.h"
 
-
-
 test(FASTPIN_01_MaxDuration_PinSet) {
-    // Attempt pinSetFast and pinResetFast numerous times and check speed.
+    // Attempt pinSetFast numerous times and check speed.
 
 #if HAL_PLATFORM_GEN == 3
-    // expected max ticks of pinSetFast / pinResetFast on Gen3
-    const uint32_t MAX_DURATION_PINSET_TICKS = 70;
+#if PLATFORM_ID == PLATFORM_ESOMX
+    const pin_t pin = D5;
+#else
+    const pin_t pin = D7;
+#endif // PLATFORM_ID == PLATFORM_ESOMX
+
+#if HAL_PLATFORM_RTL872X
+    const uint32_t MAX_DURATION_PINSET_NS = 3050; // 1% higher than measured (includes for() loop overhead)
+#else
+    const uint32_t MAX_DURATION_PINSET_NS = 1030; // 1% higher than measured (includes for() loop overhead)
+#endif // HAL_PLATFORM_RTL872X
 #else
 #error "No gpio fastpin timing benchmark yet measured for this platform"
-#endif
+#endif // HAL_PLATFORM_GEN == 3
+
+    SCOPE_GUARD ({
+        pinMode(pin, INPUT);
+    });
 
     const uint32_t NUM_ITERATIONS = 100;
     uint32_t start, finish;
+    pinMode(pin, OUTPUT);
 
     ATOMIC_BLOCK()  {
-        start = System.ticks();
+        start = HAL_Timer_Get_Micro_Seconds() * 1000;
         for (uint32_t i = 0; i < NUM_ITERATIONS; i++) {
-        #if PLATFORM_ID == PLATFORM_ESOMX
-            pinSetFast(D5);
-        #else
-            pinSetFast(D7);
-        #endif
-            //pinResetFast(D7);
+            pinSetFast(pin);
+            // pinResetFast(pin);
         }
-        finish = System.ticks();
+        finish = HAL_Timer_Get_Micro_Seconds() * 1000;
     }
     uint32_t duration = finish - start;
-//    Serial.print("Set duration:");
-//    Serial.println(duration);
-    assertLessOrEqual(duration, NUM_ITERATIONS*MAX_DURATION_PINSET_TICKS);
+    uint32_t expected = NUM_ITERATIONS * MAX_DURATION_PINSET_NS;
+    // Serial.printlnf("pinSetFast total duration: %lu vs. expected duration: %lu", duration, expected);
+    assertNotEqual(duration, 0);
+    assertLessOrEqual(duration, expected);
 }
 
 test(FASTPIN_02_MaxDuration_PinReset) {
     // Attempt pinResetFast numerous times and check speed.
 
 #if HAL_PLATFORM_GEN == 3
-    // expected max ticks of pinResetFast on Gen3
-    const uint32_t MAX_DURATION_PINRESET_TICKS = 70;
+#if PLATFORM_ID == PLATFORM_ESOMX
+    const pin_t pin = D5;
+#else
+    const pin_t pin = D7;
+#endif // PLATFORM_ID == PLATFORM_ESOMX
+
+#if HAL_PLATFORM_RTL872X
+    const uint32_t MAX_DURATION_PINRESET_NS = 3000; // 1% higher than measured (includes for() loop overhead)
+#else
+    const uint32_t MAX_DURATION_PINRESET_NS = 1030; // 1% higher than measured (includes for() loop overhead)
+#endif // HAL_PLATFORM_RTL872X
 #else
 #error "No gpio fastpin timing benchmark yet measured for this platform"
-#endif
+#endif // HAL_PLATFORM_GEN == 3
+
+    SCOPE_GUARD ({
+        pinMode(pin, INPUT);
+    });
 
     const uint32_t NUM_ITERATIONS = 100;
     uint32_t start, finish;
+    pinMode(pin, OUTPUT);
 
     ATOMIC_BLOCK()  {
-        start = System.ticks();
+        start = HAL_Timer_Get_Micro_Seconds() * 1000;
         for (uint32_t i = 0; i < NUM_ITERATIONS; i++) {
-        #if PLATFORM_ID == PLATFORM_ESOMX
-            pinResetFast(D5);
-        #else
-            pinResetFast(D7);
-        #endif
+            // pinSetFast(pin);
+            pinResetFast(pin);
         }
-        finish = System.ticks();
+        finish = HAL_Timer_Get_Micro_Seconds() * 1000;
     }
     uint32_t duration = finish - start;
-//    Serial.print("Reset duration:");
-//    Serial.println(duration);
-    assertLessOrEqual(duration, NUM_ITERATIONS*MAX_DURATION_PINRESET_TICKS);
+    uint32_t expected = NUM_ITERATIONS * MAX_DURATION_PINRESET_NS;
+    // Serial.printlnf("pinResetFast total duration: %lu vs. expected duration: %lu", duration, expected);
+    assertNotEqual(duration, 0);
+    assertLessOrEqual(duration, expected);
 }
-
-
-
