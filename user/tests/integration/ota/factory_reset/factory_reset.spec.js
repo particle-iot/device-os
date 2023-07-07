130b5b96fc27762e9872f80de7d24700d66c3275
Scott Brust <scott.brust@particle.io>
2023-07-07 10:11:43 -0700

    use usb factoryReset directly, refactor flash function

--- user/tests/integration/ota/factory_reset/factory_reset.spec.js
+++ user/tests/integration/ota/factory_reset/factory_reset.spec.js
@@ -54,11 +54,7 @@ before(function() {
 	deviceId = device.id;
 });
 
-test('01_disable_resets_and_connect', async function () {
-	// See the test app
-});
-
-test('02_flash_binary', async function () {
+async function flashBinary(ctx, fwUniqueId) {
 	// Get the module binary of the test app that is currently running on the device
 	const origAppData = await readFile(device.testAppBinFile);
 	const parser = new HalModuleParser();
@@ -67,14 +63,22 @@ test('02_flash_binary', async function () {
 	// different SHA checksum so that we can identify it later
 	let appData = Buffer.from(origAppData);
 	let suffix = { ...origSuffix };
-	suffix.fwUniqueId = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+	suffix.fwUniqueId = fwUniqueId;
 	expect(suffix.fwUniqueId).to.not.equal(origSuffix.fwUniqueId);
 	updateModuleSuffix(appData, suffix);
 	updateModuleCrc32(appData);
 	let appFile = await tempy.write(appData, { name: 'app1.bin' });
 	// The device should accept this update
-	await flash(this, appFile);
+	await flash(ctx, appFile);
 	await delay(2000);
+}
+
+test('01_disable_resets_and_connect', async function () {
+	// See the test app
+});
+
+test('02_flash_binary', async function () {
+	await flashBinary(this, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
 });
 
 test('03_move_ota_binary_to_factory_slot', async function () {
@@ -95,22 +99,7 @@ test('06_disable_resets_and_connect', async function () {
 });
 
 test('07_flash_binary', async function () {
-	// Get the module binary of the test app that is currently running on the device
-	const origAppData = await readFile(device.testAppBinFile);
-	const parser = new HalModuleParser();
-	const { prefixInfo: origPrefix, suffixInfo: origSuffix } = await parser.parseBuffer({ fileBuffer: origAppData });
-	// The first binary is exactly the same as the one already flashed on the device but it has a
-	// different SHA checksum so that we can identify it later
-	let appData = Buffer.from(origAppData);
-	let suffix = { ...origSuffix };
-	suffix.fwUniqueId = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb';
-	expect(suffix.fwUniqueId).to.not.equal(origSuffix.fwUniqueId);
-	updateModuleSuffix(appData, suffix);
-	updateModuleCrc32(appData);
-	let appFile = await tempy.write(appData, { name: 'app1.bin' });
-	// The device should accept this update
-	await flash(this, appFile);
-	await delay(2000);
+	await flashBinary(this, 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb');
 });
 
 test('08_move_ota_binary_to_factory_slot', async function () {
@@ -118,7 +107,9 @@ test('08_move_ota_binary_to_factory_slot', async function () {
 });
 
 test('09_usb_command_factory_reset', async function () {
-	await device.factoryReset();
+	let usbDevice = await device.getUsbDevice();
+	device.setWillDetach(true);
+	await usbDevice.factoryReset({ timeout: 10000 });
 });
 
 test('10_validate_factory_reset_worked', async function () {
