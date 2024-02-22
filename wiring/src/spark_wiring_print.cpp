b5657393dc8f191301dc912e11935c21ac588f07
Sergey Polyakov <sergeuz@gmail.com>
2024-02-22 18:33:55 +0200

    Inline methods that depend on PARTICLE_WIRING_PRINT_NO_FLOAT to avoid linker errors

--- wiring/src/spark_wiring_print.cpp
+++ wiring/src/spark_wiring_print.cpp
@@ -124,18 +124,6 @@ size_t Print::print(char c)
   return write(c);
 }
 
-#ifndef PARTICLE_WIRING_PRINT_NO_FLOAT
-size_t Print::print(float n, int digits)
-{
-  return printFloat((double)n, digits);
-}
-
-size_t Print::print(double n, int digits)
-{
-  return printFloat(n, digits);
-}
-#endif // PARTICLE_WIRING_PRINT_NO_FLOAT
-
  size_t Print::print(const Printable& x)
  {
    return x.printTo(*this);
@@ -166,20 +154,6 @@ size_t Print::println(char c)
   n += println();
   return n;
 }
-
-#ifndef PARTICLE_WIRING_PRINT_NO_FLOAT
-size_t Print::println(float num, int digits)
-{
-    return println((double)num, digits);
-}
-
-size_t Print::println(double num, int digits)
-{
-  size_t n = print(num, digits);
-  n += println();
-  return n;
-}
-#endif // PARTICLE_WIRING_PRINT_NO_FLOAT
 
  size_t Print::println(const Printable& x)
  {
@@ -233,53 +207,6 @@ size_t Print::printNumber(unsigned long n, uint8_t base) {
 
   return write(str);
 }
-
-#ifndef PARTICLE_WIRING_PRINT_NO_FLOAT
-size_t Print::printFloat(double number, uint8_t digits)
-{
-  size_t n = 0;
-
-  if (isnan(number)) return print("nan");
-  if (isinf(number)) return print("inf");
-  if (number > 4294967040.0) return print ("ovf");  // constant determined empirically
-  if (number <-4294967040.0) return print ("ovf");  // constant determined empirically
-
-  // Handle negative numbers
-  if (number < 0.0)
-  {
-     n += print('-');
-     number = -number;
-  }
-
-  // Round correctly so that print(1.999, 2) prints as "2.00"
-  double rounding = 0.5;
-  for (uint8_t i=0; i<digits; ++i)
-    rounding /= 10.0;
-
-  number += rounding;
-
-  // Extract the integer part of the number and print it
-  unsigned long int_part = (unsigned long)number;
-  double remainder = number - (double)int_part;
-  n += print(int_part);
-
-  // Print the decimal point, but only if there are digits beyond
-  if (digits > 0) {
-    n += print(".");
-  }
-
-  // Extract digits from the remainder one at a time
-  while (digits-- > 0)
-  {
-    remainder *= 10.0;
-    int toPrint = int(remainder);
-    n += print(toPrint);
-    remainder -= toPrint;
-  }
-
-  return n;
-}
-#endif // PARTICLE_WIRING_PRINT_NO_FLOAT
 
 size_t Print::printVariant(const Variant& var) {
     JSONStreamWriter writer(*this);
