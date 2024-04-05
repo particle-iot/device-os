a587311a203847e70b413e6a0b5390410a7062c7
Scott Brust <scott.brust@particle.io>
2024-04-05 16:03:39 -0700

    wip basic test working

--- user/tests/integration/ota/min_max_app_size/min_max_app_size.cpp
+++ user/tests/integration/ota/min_max_app_size/min_max_app_size.cpp
@@ -1,9 +1,15 @@
 #include "application.h"
 #include "test.h"
 
+// TODO: remove after done debugging
+Serial1LogHandler g_logHandler(115200, LOG_LEVEL_WARN, {
+    { "app", LOG_LEVEL_ALL }
+});
+
 namespace {
 
 volatile int updateResult = firmware_update_failed;
+bool slowAppLoop = false;
 
 void prepareOta(bool connect = true) {
     System.disableReset();
@@ -42,7 +48,63 @@ void waitOta() {
     assertEqual(0, TestRunner::instance()->pushMailbox(Test::MailboxEntry().type(Test::MailboxEntry::Type::RESET_PENDING), 5000));
 }
 
+void highPriorityFunc() {
+    while (1) {
+        //Log.info("High priority thread");
+        delay(1);
+    }
+}
+
+void bleScanResultCallBack(const BleScanResult *result, void *context) {
+    Log.info("BLE adv: %s", result->address().toString().c_str());
+}
+
+void lowPriorityFunc() {
+    while (1) {
+#if HAL_PLATFORM_BLE
+        static bool bleOn = false;
+
+        if (!bleOn) {
+            BLE.on();
+            bleOn = true;
+        }
+
+        BleScanParams params = {};
+        params.version = BLE_API_VERSION;
+        params.size = sizeof(BleScanParams);
+        params.timeout = 50; // *10ms = 500ms overall duration
+        params.interval = 800; // *0.625ms = 500ms
+        params.window = 800; // *0.625 = 500s
+        params.active = false;
+        
+        BLE.setScanParameters(params);
+        BLE.scan(bleScanResultCallBack, nullptr);
+#endif
+
+        // TODO: Wifi scanning?
+
+        //Log.info("Low priority thread");
+        delay(1000);
+    }
+}
+
 } // anonymous
+
+STARTUP({
+    testAppInit();
+});
+
+void setup() {
+    testAppSetup();
+}
+
+void loop() {
+    if (slowAppLoop) {
+        auto delayMillisMax = 50;
+        delay(random(delayMillisMax));
+    }
+    testAppLoop();
+}
 
 
 test(01_check_current_application) {
@@ -115,3 +177,37 @@ test(18_usb_flash_original_application_compressed_wait) {
 test(19_check_original_application) {
 }
 
+test(20_ota_max_busy_application_start) {
+    Thread *highPriorityThread  = nullptr;
+    Thread *lowPriorityThread = nullptr;
+
+    Log.info("20_ota_max_busy_application_start");
+
+    // Start some high priority threads
+    highPriorityThread = new Thread("high_prio", highPriorityFunc, OS_THREAD_PRIORITY_CRITICAL);
+    lowPriorityThread = new Thread("low_prio", lowPriorityFunc, OS_THREAD_PRIORITY_DEFAULT);
+
+    slowAppLoop = true;
+
+    prepareOta();
+}
+
+test(21_ota_max_busy_application_wait) {
+    waitOta();
+}
+
+test(22_check_max_application) {
+
+}
+
+test(23_ota_original_application_start) {
+    prepareOta();
+}
+
+test(24_ota_original_application_wait) {
+    waitOta();
+}
+
+test(25_check_original_application) {
+
+}
