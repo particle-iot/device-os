cbdf67a0ab262ece5a31a33ed7acd113f22159ad
Andrey Tolstoy <avtolstoy@gmail.com>
2024-06-12 02:40:31 +0700

    [network] return ERR_RTE when interface is down to trigger EHOSTUNREACH

--- hal/network/lwip/realtek/rtlncpnetif.cpp
+++ hal/network/lwip/realtek/rtlncpnetif.cpp
@@ -419,7 +419,7 @@ int RealtekNcpNetif::downImpl() {
 err_t RealtekNcpNetif::linkOutput(pbuf* p) {
     if (!(netif_is_up(interface()) && netif_is_link_up(interface()))) {
         // LOG(ERROR, "linkOutput up=%d link_up=%d", netif_is_up(interface()), netif_is_link_up(interface()));
-        return ERR_IF;
+        return ERR_RTE;
     }
 
     // LOG_DEBUG(TRACE, "link output %x %u", p->payload, p->tot_len);
