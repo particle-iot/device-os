9cc9763b90a50869e00ab7f057e2adc83bb82fd2
Technobly <technobly@gmail.com>
2024-08-13 01:01:30 -0500

    [tests] add wiring/i2c_master_slave config files

---
+++ user/tests/wiring/i2c_master_slave/i2c_master_slave_1_bw.config.js
@@ -0,0 +1,16 @@
+{
+    "fixtures": [
+        {
+            "name": "i2c_slave",
+            "devices": [
+                "0a10aced202194944a0408f8" // Photon 2
+            ]
+        },
+        {
+            "name": "i2c_master",
+            "devices": [
+                "e00fce68db4bf859656a8cb1" // Argon
+            ]
+        }
+    ]
+}
---
+++ user/tests/wiring/i2c_master_slave/i2c_master_slave_4_bw.config.js
@@ -0,0 +1,16 @@
+{
+    "fixtures": [
+        {
+            "name": "i2c_master",
+            "devices": [
+                "e00fce68e8835942f242ab86" // Argon Left
+            ]
+        },
+        {
+            "name": "i2c_slave",
+            "devices": [
+                "e00fce682e1435f9a333a27b" // Argon Right
+            ]
+        }
+    ]
+}
