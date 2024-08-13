550d68fb8ca9df8d4a95c473b4e35b6f56bbf469
Technobly <technobly@gmail.com>
2024-08-13 01:06:21 -0500

    [tests] add wiring/i2c_mcp23017 config files

---
+++ user/tests/wiring/i2c_mcp23017/i2c_mcp23017_1_bw.config.js
@@ -0,0 +1,10 @@
+{
+    "fixtures": [
+        {
+            "name": "i2c_mcp23017",
+            "devices": [
+                "e00fce68db4bf859656a8cb1" // Argon
+            ]
+        }
+    ]
+}
---
+++ user/tests/wiring/i2c_mcp23017/i2c_mcp23017_2_bw.config.js
@@ -0,0 +1,10 @@
+{
+    "fixtures": [
+        {
+            "name": "i2c_mcp23017",
+            "devices": [
+                "0a10aced202194944a0408f8" // Photon 2
+            ]
+        }
+    ]
+}
