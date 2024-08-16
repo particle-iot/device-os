7216c4d273dcb8f620ced4b4fca5ceeac0322611
Technobly <technobly@gmail.com>
2024-08-16 11:08:29 -0500

    [tests] add wiring/sleep20 config file

---
+++ user/tests/wiring/sleep20/sleep20_bw.config.js
@@ -0,0 +1,16 @@
+{
+    "fixtures": [
+        {
+            "name": "sleep20-tester",
+            "devices": [
+                "e00fce68066985ba6a7fd484" // Argon Tester
+            ]
+        },
+        {
+            "name": "sleep20-device",
+            "devices": [
+                "e00fce685b051f7dd1c44a82" // Argon Device Under Test
+            ]
+        }
+    ]
+}
