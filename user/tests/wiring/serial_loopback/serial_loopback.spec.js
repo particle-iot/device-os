d642b6c97d2b038a65203a3fa15e8256a68cb2ac
XuGuohui <xghui999@126.com>
2024-08-29 20:29:56 +0800

    [test] add gen4 platform tag for serial_loopback tests

--- user/tests/wiring/serial_loopback/serial_loopback.spec.js
+++ user/tests/wiring/serial_loopback/serial_loopback.spec.js
@@ -1,5 +1,5 @@
 suite('Serial loopback');
-platform('gen3');
+platform('gen3', 'gen4');
 fixture('serial_loopback');
 systemThread('enabled');
 // This tag should be filtered out by default
--- user/tests/wiring/serial_loopback2/serial_loopback2.spec.js
+++ user/tests/wiring/serial_loopback2/serial_loopback2.spec.js
@@ -1,5 +1,5 @@
 suite('Serial loopback2');
-platform('gen3');
+platform('gen3', 'gen4');
 fixture('serial_loopback2');
 systemThread('enabled');
 // This tag should be filtered out by default
