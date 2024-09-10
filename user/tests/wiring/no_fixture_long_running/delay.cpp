39a7d901632fb98e4cbd0ae8212ba4c04a904c9d
Sergey Polyakov <sergeuz@gmail.com>
2024-09-10 10:19:22 +0200

    Add tests

--- user/tests/wiring/no_fixture_long_running/delay.cpp
+++ user/tests/wiring/no_fixture_long_running/delay.cpp
@@ -9,15 +9,13 @@ test(DELAY_01_init) {
 }
 
 test(DELAY_02_accuracy_is_within_tolerance) {
-    const unsigned minDelay = 2; // ms
-    const unsigned maxDelay = 10; // ms
     const unsigned callCount = 1000;
 
     Vector<unsigned> variations;
     assertTrue(variations.resize(callCount));
 
     for (unsigned i = 0; i < callCount; ++i) {
-        auto duration = rand() % (maxDelay - 1) + minDelay;
+        auto duration = rand() % 9 + 2; // 2-10ms
         auto t1 = micros();
         delay(duration);
         auto dt = micros() - t1;
@@ -33,11 +31,11 @@ test(DELAY_02_accuracy_is_within_tolerance) {
     }
 
     std::sort(variations.begin(), variations.end());
-    auto dtPct = variations[std::floor(callCount * 0.995)]; // Nth percentile
-    auto dtMax = variations.last();
+    auto dtPct = variations[std::floor(callCount * 0.995)]; // Nth percentile variation, us
+    auto dtMax = variations.last(); // Maximum variation, us
 
-    assertLess(dtPct, 100); // us
-    assertLess(dtMax, 1000); // us
+    assertLess(dtPct, 100);
+    assertLess(dtMax, 1000);
 
     pushMailboxMsg(String::format("%u,%u", dtPct, dtMax), 5000 /* wait */);
 }
