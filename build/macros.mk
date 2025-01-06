particle-iot/device-os

commit 38ced0de0a2f04bd2fae597338d0eb8583bbc1f1

[build] include alignment into section size calculations

build/macros.mk
@@ -52,3 +52,5 @@ target_files_dirs = $(foreach d,$(call remove_slash,$1),$(patsubst $d/%,$(notdir
 check_modular = $(if $(PLATFORM_DYNALIB_MODULES),,$(error "Platform '$(PLATFORM)' does not support dynamic modules"))
 
 get_module_start_address = 0x$(word 1,$(shell $(OBJDUMP) --syms $(TARGET_BASE).elf | grep 'link_module_start'))
+
+get_section_size_with_alignment = $(shell echo $$(($(shell $(OBJDUMP) -h --section=$1 $2 | grep -E '$1' | awk '{ print "0x"$$3" + "$$7" -1"}'))))

modules/shared/nRF52840/build_linker_script.mk
@@ -5,18 +5,16 @@ COMMA := ,
 
 COMMON_BUILD=../../../build
 include $(COMMON_BUILD)/arm-tools.mk
+include $(COMMON_BUILD)/macros.mk
 
 ifneq (,$(PREBUILD))
 # Should declare enough RAM for inermediate linker script: 89K
 USER_SRAM_LENGTH = 89K
 else
-DATA_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.data $(INTERMEDIATE_ELF) | grep -E '^\s*[0-9]+\s+\.data\s+')
-DATA_SECTION_LEN := 0x$(word 3,$(DATA_SECTION_LEN))
-BSS_SECTION_LEN   = $(shell $(OBJDUMP) -h --section=.bss $(INTERMEDIATE_ELF) | grep -E '^\s*[0-9]+\s+\.bss\s+')
-BSS_SECTION_LEN  := 0x$(word 3,$(BSS_SECTION_LEN))
+DATA_SECTION_LEN  = $(call get_section_size_with_alignment,.data,$(INTERMEDIATE_ELF))
+BSS_SECTION_LEN   = $(call get_section_size_with_alignment,.bss,$(INTERMEDIATE_ELF))
 
-# Note: reserving 16 bytes for alignment just in case
-USER_SRAM_LENGTH = ( $(DATA_SECTION_LEN) + $(BSS_SECTION_LEN) + 16 )
+USER_SRAM_LENGTH = ( $(DATA_SECTION_LEN) + $(BSS_SECTION_LEN) )
 
 all: $(INTERMEDIATE_ELF)
 endif

modules/shared/rtl872x/build_linker_script.mk
@@ -12,6 +12,7 @@ COMMA := ,
 
 COMMON_BUILD=../../../build
 include $(COMMON_BUILD)/arm-tools.mk
+include $(COMMON_BUILD)/macros.mk
 
 ifneq (,$(PREBUILD))
 # Should declare enough RAM for inermediate linker script: 128K
@@ -20,26 +21,21 @@ USER_SRAM_LENGTH = 128K
 USER_PSRAM_LENGTH = 1536K
 USER_FLASH_LENGTH = 1536K
 else
-TEXT_SECTION_LEN  = $(shell $(SIZE) --format=berkeley $(INTERMEDIATE_ELF) | $(AWK) 'NR==2 {print $$1}')
-DATA_SECTION_LEN  = $(shell $(SIZE) --format=berkeley $(INTERMEDIATE_ELF) | $(AWK) 'NR==2 {print $$2}')
-BSS_SECTION_LEN   = $(shell $(SIZE) --format=berkeley $(INTERMEDIATE_ELF) | $(AWK) 'NR==2 {print $$3}')
+TEXT_SECTION_LEN  = $(call get_section_size_with_alignment,.text,$(INTERMEDIATE_ELF))
+DATA_SECTION_LEN  = $(call get_section_size_with_alignment,.data,$(INTERMEDIATE_ELF))
+BSS_SECTION_LEN   = $(call get_section_size_with_alignment,.bss,$(INTERMEDIATE_ELF))
 
-PSRAM_TEXT_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.psram_text $(INTERMEDIATE_ELF) | grep -E '.psram_text')
-PSRAM_TEXT_SECTION_LEN := 0x$(word 3,$(PSRAM_TEXT_SECTION_LEN))
-PSRAM_DATA_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.data_alt $(INTERMEDIATE_ELF) | grep -E '.data_alt')
-PSRAM_DATA_SECTION_LEN := 0x$(word 3,$(PSRAM_DATA_SECTION_LEN))
-PSRAM_BSS_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.bss_alt $(INTERMEDIATE_ELF) | grep -E '.bss_alt')
-PSRAM_BSS_SECTION_LEN := 0x$(word 3,$(PSRAM_BSS_SECTION_LEN))
-PSRAM_DYNALIB_SECTION_LEN  = $(shell $(OBJDUMP) -h --section=.dynalib $(INTERMEDIATE_ELF) | grep -E '.dynalib')
-PSRAM_DYNALIB_SECTION_LEN := 0x$(word 3,$(PSRAM_DYNALIB_SECTION_LEN))
+PSRAM_TEXT_SECTION_LEN  = $(call get_section_size_with_alignment,.psram_text,$(INTERMEDIATE_ELF))
+PSRAM_DATA_SECTION_LEN  = $(call get_section_size_with_alignment,.data_alt,$(INTERMEDIATE_ELF))
+PSRAM_BSS_SECTION_LEN  = $(call get_section_size_with_alignment,.bss_alt,$(INTERMEDIATE_ELF))
+PSRAM_DYNALIB_SECTION_LEN  = $(call get_section_size_with_alignment,.dynalib,$(INTERMEDIATE_ELF))
 USER_MODULE_END = 0x$(shell $(OBJDUMP) -t $(INTERMEDIATE_ELF) | grep link_module_info_crc_end | $(AWK) '{ print $$1 }')
 USER_MODULE_START = 0x$(shell $(OBJDUMP) -t $(INTERMEDIATE_ELF) | grep link_module_start | $(AWK) '{ print $$1 }')
 USER_MODULE_SUFFIX_START = 0x$(shell $(OBJDUMP) -t $(INTERMEDIATE_ELF) | grep link_module_info_static_start | $(AWK) '{ print $$1 }')
 MODULE_INFO_SUFFIX_LEN := ( $(USER_MODULE_END) - $(USER_MODULE_SUFFIX_START) )
 
-# Note: reserving 16 bytes for alignment just in case
-USER_SRAM_LENGTH = ( $(DATA_SECTION_LEN) + $(BSS_SECTION_LEN) + 16 )
-USER_PSRAM_LENGTH = ( $(PSRAM_TEXT_SECTION_LEN) + $(PSRAM_DATA_SECTION_LEN) + $(PSRAM_BSS_SECTION_LEN) + $(PSRAM_DYNALIB_SECTION_LEN) + 16)
+USER_SRAM_LENGTH = ( $(DATA_SECTION_LEN) + $(BSS_SECTION_LEN) )
+USER_PSRAM_LENGTH = ( $(PSRAM_TEXT_SECTION_LEN) + $(PSRAM_DATA_SECTION_LEN) + $(PSRAM_BSS_SECTION_LEN) + $(PSRAM_DYNALIB_SECTION_LEN) )
 
 USER_FLASH_LENGTH = $(shell let var=($(USER_MODULE_END) - $(USER_MODULE_START) + 16); echo $$var)
 USER_FLASH_LENGTH := $(shell echo $$((($(USER_FLASH_LENGTH) + 4095) / 4096 * 4096)))

commit 3372e13bd6a8132631c2f8de827daadfa6221f9a

Fix section size calc for some filenames
Commit 38ced0de0a2f04bd2fae597338d0eb8583bbc1f1 introduced alignment
into section size calculations, but failed on file paths that contain
strings "data", "bss", etc. Apply a small fix to the Makefile macro.

Bug report:
https://community.particle.io/t/section-bss-will-not-fit-in-region-sram/66641

build/macros.mk
@@ -54,6 +54,6 @@ check_modular = $(if $(PLATFORM_DYNALIB_MODULES),,$(error "Platform '$(PLATFORM)
 get_module_start_address = 0x$(word 1,$(shell $(OBJDUMP) --syms $(TARGET_BASE).elf | grep 'link_module_start'))
 
 # FIXME: Cygwin gawk is weird, current awk script seems to work, adding -1 within it breaks everything for some reason
-get_section_size_with_alignment = $(shell echo $$(($(shell $(OBJDUMP) -h --section=$1 $2 | grep -E '$1' | $(AWK) '{ print "0x"$$3" + "$$7}') - 1)))
+get_section_size_with_alignment = $(shell echo $$(($(shell $(OBJDUMP) -h --section=$1 $2 | grep -F '$1' | tail -n 1 | $(AWK) '{ print "0x"$$3" + "$$7}') - 1)))
 
 get_symbol_address = 0x$(shell $(READELF) -W -s $1 | grep -E '$2$$' | $(AWK) '{ print $$2 }')