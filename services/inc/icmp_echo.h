b57aec33eb6084773320ff33df26137c62da1d26
Andrey Tolstoy <avtolstoy@gmail.com>
2025-01-23 23:48:38 +0700

    add missing pragma once

--- services/inc/icmp_echo.h
+++ services/inc/icmp_echo.h
@@ -15,6 +15,8 @@
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
+#pragma once
+
 #include "socket_hal_posix.h"
 #include "netdb_hal.h"
 #include "inet_hal_posix.h"
