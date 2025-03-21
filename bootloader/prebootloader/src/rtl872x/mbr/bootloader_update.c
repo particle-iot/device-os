46ed9477afe8a6a0ed2c799923972d2d493fd911
Scott Brust <scott.brust@particle.io>
2025-03-21 09:48:04 -0700

    fixup

--- bootloader/prebootloader/src/rtl872x/mbr/bootloader_update.c
+++ bootloader/prebootloader/src/rtl872x/mbr/bootloader_update.c
@@ -41,7 +41,7 @@ static void flash_init(void) {
 
     FLASH_StructInit(&flash_init_para);
     FLASH_Init(SpicOneBitMode);
-    
+
     uint8_t flashId[3];
     FLASH_RxCmd(flash_init_para.FLASH_cmd_rd_id, 3, flashId);
     if (flashId[0] == 0x20) {
