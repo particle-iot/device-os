dbad95a55148528f81bd202c97cba9f42357a9a8
Andrey Tolstoy <avtolstoy@gmail.com>
2026-03-12 13:47:19 +0700

    minor

--- user/tests/wiring/no_fixture/time.cpp
+++ user/tests/wiring/no_fixture/time.cpp
@@ -56,10 +56,12 @@ void assertTimeSourcesAreSane() {
     assertLessOrEqual(absTimeDiff(systemTime, internalTime), TIME_SOURCE_MAX_DRIFT_SECONDS);
     assertEqual(InternalTime.isValid(), Time.isValid());
     if (shouldCheckExternalTime()) {
+#if HAL_PLATFORM_EXTERNAL_RTC
         const auto externalTime = ExternalTime.now();
         assertLessOrEqual(absTimeDiff(systemTime, externalTime), TIME_SOURCE_MAX_DRIFT_SECONDS);
         assertLessOrEqual(absTimeDiff(internalTime, externalTime), TIME_SOURCE_MAX_DRIFT_SECONDS);
         assertEqual(ExternalTime.isValid(), Time.isValid());
+#endif // HAL_PLATFORM_EXTERNAL_RTC
     }
 }
 
