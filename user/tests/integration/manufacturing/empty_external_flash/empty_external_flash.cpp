6a5d228e8d73d21a31567c04a3abb62209b83d7b
Andrey Tolstoy <avtolstoy@gmail.com>
2026-04-25 02:07:20 +0700

    fix bootloader filesystem initialization, adds manufacturing/empty_external_flash test

---
+++ user/tests/integration/manufacturing/empty_external_flash/empty_external_flash.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2026 Particle Industries, Inc.  All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "application.h"
+#include "test.h"
+
+test(01_backup_external_flash_and_erase) {
+}
+
+test(99_verify) {
+    Particle.connect();
+    assertTrue(waitFor(Particle.connected, HAL_PLATFORM_MAX_CLOUD_CONNECT_TIME));
+}
