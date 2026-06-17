08715fedf808450da091b62b8e1a010844744538
Scott Brust <scott.brust@particle.io>
2026-06-17 09:13:46 -0700

    [test] Add OTA flash to ensure device service OTA attempts are reset

--- user/tests/integration/00_before/before.spec.js
+++ user/tests/integration/00_before/before.spec.js
@@ -6,7 +6,8 @@ systemThread('enabled');
 const { unsetDeviceVariables } = require('../test/env');
 const Particle = require('particle-api-js');
 
-const { waitFlashStatusEvent } = require('../test/ota');
+const { waitFlashStatusEvent, flash } = require('../test/ota');
+const { readFile } = require('fs').promises;
 
 let device;
 let deviceId;
@@ -29,46 +30,60 @@ after(function() {
     device.removeAllListeners('mailbox');
 });
 
-test('01_erase_factory_module', async function () {
+test('01_ota_self_flash_start', async function () {
+    this.timeout(10 * 60 * 1000);
+    const appData = await readFile(device.testAppBinFile);
+    // Reset device-service OTA flash attempt count by OTA flashing the current app test binary.
+    // This should ensure that subsequent OTA downloads (like repeated empty env var asset OTAs) 
+    // should succeed for the duration of the test runner suites as long as we do not exceed the
+    // device service maxSameBinaryAttempts threshold (defaults to 30).
+    await flash(this, appData, { filename: '00_before.bin' });
+});
+
+test('02_ota_self_flash_finalize', async function () {
 
 });
 
-test('02_remove_static_ip', async function () {
+test('03_erase_factory_module', async function () {
 
 });
 
-test('03_enable_listening_mode', async function () {
+test('04_remove_static_ip', async function () {
 
 });
 
-test('04_clear_env', async function () {
+test('05_enable_listening_mode', async function () {
+
+});
+
+test('06_clear_env', async function () {
     await unsetDeviceVariables(api, deviceId);
 });
 
-test('05_restore_cloud_after_env_clear', async function () {
+test('07_restore_cloud_after_env_clear', async function () {
     await waitFlashStatusEvent(this, { status: 'success' });
 });
 
-test('06_finalize_env_clear', async function () {
+test('08_finalize_env_clear', async function () {
 });
 
-test('07_disable_external_rtc', async function() {
+test('09_disable_external_rtc', async function() {
 });
 
-test('08_verify_external_rtc_default_state', async function() {
+test('10_verify_external_rtc_default_state', async function() {
 
 });
 
-test('09_report_muon_presence', async function() {
+test('11_report_muon_presence', async function() {
     const msg = device.mailBox.pop();
     console.log(`Muon detected: ${msg.d === 'muon=true' ? 'yes' : 'no'}`);
 });
 
-test('10_configure_muon_board_and_exrtc', async function() {
+test('12_configure_muon_board_and_exrtc', async function() {
 
 });
 
-test('11_verify_muon_exrtc_configuration', async function() {
+test('13_verify_muon_exrtc_configuration', async function() {
 
 });
 
