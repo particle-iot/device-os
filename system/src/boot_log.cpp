369bcbeb8c3794de9e32af437d44aff765a884a3
Sergey Polyakov <sergeuz@gmail.com>
2026-08-05 16:23:25 +0200

    Refactoring

--- system/src/boot_log.cpp
+++ system/src/boot_log.cpp
@@ -17,53 +17,53 @@ namespace particle::system {
 
 namespace {
 
-class RotatingLogReader: public InputStream {
+const auto BOOT_LOG_FILE1 = "/sys/bootlog.1";
+const auto BOOT_LOG_FILE2 = "/sys/bootlog.2";
+
+const size_t DEFAULT_BOOT_LOG_SIZE = 100000;
+
+class BootLogReader: public InputStream {
 public:
-    RotatingLogReader(size_t maxSize, const char* fileName1, const char* fileName2, bool hasSecond, bool writeSecond) :
-            fileName1_(fileName1),
-            fileName2_(fileName2),
-            maxSize_(maxSize),
+    BootLogReader() :
             bytesAvail_(0),
-            hasSecond_(hasSecond),
-            readSecond_(false),
-            writeSecond_(writeSecond) {
+            hasSecond_(false),
+            readSecond_(false) {
     }
 
-    ~RotatingLogReader() {
+    ~BootLogReader() {
         fs::FsLock lock;
 
         file_.close();
     }
 
-    int init() {
+    int init(size_t maxSize, bool hasSecond, bool writeSecond) {
         fs::FsLock lock;
 
         fs::File file;
         size_t bytesAvail = 0;
-        bool readSecond = writeSecond_;
+        bool readSecond = writeSecond;
 
         // Open the latest file
-        auto fileName = writeSecond_ ? fileName2_ : fileName1_;
+        auto fileName = readSecond ? BOOT_LOG_FILE2 : BOOT_LOG_FILE1;
         int r = file.open(fileName, LFS_O_RDONLY);
         if (r < 0 && r != SYSTEM_ERROR_FILESYSTEM_NOENT) {
             return r;
         }
         if (r != SYSTEM_ERROR_FILESYSTEM_NOENT) {
             size_t lastFileSize = CHECK(file.size());
-            if (lastFileSize < maxSize_ && hasSecond_) {
+            if (lastFileSize < maxSize && hasSecond) {
                 // Open the previous file
-                auto fileName = writeSecond_ ? fileName1_ : fileName2_;
+                readSecond = !readSecond;
+                auto fileName = readSecond ? BOOT_LOG_FILE2 : BOOT_LOG_FILE1;
                 CHECK(file.open(fileName, LFS_O_RDONLY)); // Must exist
+
                 size_t prevFileSize = CHECK(file.size());
-
-                size_t prevFileAvail = std::min(maxSize_ - lastFileSize, prevFileSize);
+                size_t prevFileAvail = std::min(maxSize - lastFileSize, prevFileSize);
                 CHECK(file.seek(prevFileAvail, LFS_SEEK_END));
-
                 bytesAvail = prevFileAvail + lastFileSize;
-                readSecond = !writeSecond_;
-            } else if (lastFileSize > maxSize_) {
-                CHECK(file.seek(lastFileSize - maxSize_));
-                bytesAvail = maxSize_;
+            } else if (lastFileSize > maxSize) {
+                CHECK(file.seek(maxSize, LFS_SEEK_END));
+                bytesAvail = maxSize;
             } else {
                 bytesAvail = lastFileSize;
             }
@@ -71,6 +71,7 @@ public:
 
         file_ = std::move(file);
         bytesAvail_ = bytesAvail;
+        hasSecond_ = hasSecond;
         readSecond_ = readSecond;
         return 0;
     }
@@ -88,7 +89,8 @@ public:
                 return SYSTEM_ERROR_BAD_DATA;
             }
             // Open the latest file
-            auto fileName = readSecond_ ? fileName1_ : fileName2_;
+            readSecond_ = !readSecond_;
+            auto fileName = readSecond_ ? BOOT_LOG_FILE2 : BOOT_LOG_FILE1;
             CHECK(file_.open(fileName, LFS_O_RDONLY));
 
             bytesToRead -= totalBytesRead;
@@ -99,19 +101,12 @@ public:
             totalBytesRead += bytesRead;
             hasSecond_ = false;
         }
-        
+
         bytesAvail_ -= totalBytesRead;
         if (!bytesAvail_) {
             file_.close();
         }
         return totalBytesRead;
-    }
-
-    int skip(size_t size) override {
-        fs::FsLock lock;
-
-        size_t n = CHECK(read(nullptr, size));
-        return n;
     }
 
     int availForRead() override {
@@ -121,6 +116,10 @@ public:
     }
 
     int peek(char* data, size_t size) override {
+        return SYSTEM_ERROR_NOT_SUPPORTED;
+    }
+
+    int skip(size_t size) override {
         return SYSTEM_ERROR_NOT_SUPPORTED;
     }
 
@@ -134,26 +133,20 @@ public:
 
 private:
     fs::File file_;
-    const char* fileName1_;
-    const char* fileName2_;
-    size_t maxSize_;
     size_t bytesAvail_;
     bool hasSecond_;
     bool readSecond_;
-    bool writeSecond_;
 };
 
-class RotatingLog {
+class BootLog {
 public:
-    RotatingLog(size_t maxSize, const char* fileName1, const char* fileName2) :
-            fileName1_(fileName1),
-            fileName2_(fileName2),
+    explicit BootLog(size_t maxSize) :
             maxSize_(maxSize),
             hasSecond_(false),
             writeSecond_(false) {
     }
 
-    ~RotatingLog() {
+    ~BootLog() {
         fs::FsLock lock;
 
         file_.close();
@@ -166,29 +159,31 @@ public:
         bool writeSecond = false;
 
         lfs_info info = {};
-        int r = fs::stat(fileName1_, &info);
+        int r = fs::stat(BOOT_LOG_FILE1, &info);
         if (r < 0 && r != SYSTEM_ERROR_FILESYSTEM_NOENT) {
             return r;
         }
         if (r == SYSTEM_ERROR_FILESYSTEM_NOENT || info.type != LFS_TYPE_REG) {
             if (r != SYSTEM_ERROR_FILESYSTEM_NOENT) {
-                rmrf(fileName1_); // Log file is not a regular file
+                rmrf(BOOT_LOG_FILE1); // Log file is not a regular file
             }
             // For consistency, make sure the second file doesn't exist too
-            rmrf(fileName2_);
+            rmrf(BOOT_LOG_FILE2);
         }
 
-        r = fs::stat(fileName2_, &info);
+        r = fs::stat(BOOT_LOG_FILE2, &info);
         if (r < 0 && r != SYSTEM_ERROR_FILESYSTEM_NOENT) {
             return r;
         }
-        if (r != SYSTEM_ERROR_FILESYSTEM_NOENT && info.type == LFS_TYPE_REG) {
-            hasSecond = true;
-            if (info.size < maxSize_) {
-                writeSecond = true;
+        if (r != SYSTEM_ERROR_FILESYSTEM_NOENT) {
+            if (info.type == LFS_TYPE_REG) {
+                hasSecond = true;
+                if (info.size < maxSize_) {
+                    writeSecond = true;
+                }
+            } else {
+                rmrf(BOOT_LOG_FILE2); // Log file is not a regular file
             }
-        } else if (r != SYSTEM_ERROR_FILESYSTEM_NOENT) {
-            rmrf(fileName2_); // Log file is not a regular file
         }
 
         hasSecond_ = hasSecond;
@@ -200,7 +195,7 @@ public:
         fs::FsLock lock;
 
         if (!file_.isOpen()) {
-            auto fileName = writeSecond_ ? fileName2_ : fileName1_;
+            auto fileName = writeSecond_ ? BOOT_LOG_FILE2 : BOOT_LOG_FILE1;
             CHECK(file_.open(fileName, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_APPEND));
         }
 
@@ -210,23 +205,19 @@ public:
         if (bytesWritten != bytesToWrite) {
             return SYSTEM_ERROR_FILESYSTEM_IO;
         }
-        if (bytesWritten == size) {
-            return size;
-        }
 
-        // Rotate the log
-        CHECK(file_.close());
-        writeSecond_ = !writeSecond_;
-        auto fileName = writeSecond_ ? fileName2_ : fileName1_;
-        CHECK(file_.open(fileName, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC | LFS_O_APPEND)); // Note LFS_O_TRUNC
-        if (writeSecond_) {
+        if (bytesWritten < size) {
+            // Rotate the log
             hasSecond_ = true;
-        }
+            writeSecond_ = !writeSecond_;
+            auto fileName = writeSecond_ ? BOOT_LOG_FILE2 : BOOT_LOG_FILE1;
+            CHECK(file_.open(fileName, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC | LFS_O_APPEND)); // Truncate
 
-        bytesToWrite = size - bytesWritten;
-        bytesWritten = CHECK(file_.write(data + bytesWritten, bytesToWrite));
-        if (bytesWritten != bytesToWrite) {
-            return SYSTEM_ERROR_FILESYSTEM_IO;
+            bytesToWrite = size - bytesWritten;
+            bytesWritten = CHECK(file_.write(data + bytesWritten, bytesToWrite));
+            if (bytesWritten != bytesToWrite) {
+                return SYSTEM_ERROR_FILESYSTEM_IO;
+            }
         }
         return size;
     }
@@ -236,19 +227,20 @@ public:
         va_list args;
         va_start(args, fmt);
         int n = vsnprintf(buf, sizeof(buf), fmt, args);
+        va_end(args);
         if (n < 0) {
             return SYSTEM_ERROR_IO;
         }
-        va_end(args);
-        if ((size_t)n >= sizeof(buf)) {
+        if ((size_t)n < sizeof(buf)) {
+            write(buf, n);
+        } else {
             char buf[n + 1]; // Use a larger buffer
             va_start(args, fmt);
             n = vsnprintf(buf, sizeof(buf), fmt, args);
             va_end(args);
-            if (n > 0) {
-                write(buf, n);
+            if (n < 0) {
+                return SYSTEM_ERROR_IO;
             }
-        } else if (n > 0) {
             write(buf, n);
         }
         return n;
@@ -258,8 +250,8 @@ public:
         fs::FsLock lock;
 
         file_.close();
-        rmrf(fileName1_);
-        rmrf(fileName2_);
+        rmrf(BOOT_LOG_FILE1);
+        rmrf(BOOT_LOG_FILE2);
 
         hasSecond_ = false;
         writeSecond_ = false;
@@ -269,12 +261,11 @@ public:
     int openForRead(std::unique_ptr<InputStream>& stream) {
         fs::FsLock lock;
 
-        std::unique_ptr<RotatingLogReader> reader(new(std::nothrow) RotatingLogReader(maxSize_, fileName1_,
-                fileName2_, hasSecond_, writeSecond_));
+        std::unique_ptr<BootLogReader> reader(new(std::nothrow) BootLogReader());
         if (!reader) {
             return SYSTEM_ERROR_NO_MEMORY;
         }
-        CHECK(reader->init());
+        CHECK(reader->init(maxSize_, hasSecond_, writeSecond_));
 
         stream = std::move(reader);
         return 0;
@@ -282,8 +273,6 @@ public:
 
 private:
     fs::File file_;
-    const char* fileName1_;
-    const char* fileName2_;
     size_t maxSize_;
     bool hasSecond_;
     bool writeSecond_;
@@ -296,40 +285,65 @@ struct BootLogConfig {
     bool enabled;
 };
 
-const auto BOOT_LOG_FILE1 = "/sys/bootlog.1";
-const auto BOOT_LOG_FILE2 = "/sys/bootlog.2";
-
-const size_t DEFAULT_BOOT_LOG_SIZE = 100000;
-
-retained_system BootLogConfig bootLogConfig = {};
-std::unique_ptr<RotatingLog> bootLog;
+retained_system BootLogConfig g_bootLogConfig = {};
+std::unique_ptr<BootLog> g_bootLog;
 
 void bootLogMessageCallback(const char* msg, int level, const char* category, const LogAttributes* attr, void* reserved) {
     fs::FsLock lock;
 
-    if (!bootLog) {
+    if (!g_bootLog) {
         return;
     }
-    bootLog->write(msg, std::strlen(msg));
-    bootLog->write("\r\n", 2);
+    g_bootLog->write(msg, std::strlen(msg));
+    g_bootLog->write("\r\n", 2);
 }
 
 void bootLogWriteCallback(const char* data, size_t size, int level, const char* category, void* reserved) {
     fs::FsLock lock;
 
-    if (!bootLog) {
+    if (!g_bootLog) {
         return;
     }
-    bootLog->write(data, size);
+    g_bootLog->write(data, size);
 }
 
 int bootLogEnabledCallback(int level, const char* category, void* reserved) {
     fs::FsLock lock;
 
-    if (!bootLog) {
+    if (!g_bootLog) {
         return 0;
     }
     return 1;
+}
+
+int flushBootLog(int level, const char* category) {
+    fs::FsLock lock;
+
+    if (!g_bootLog) {
+        return 0;
+    }
+
+    stopWritingBootLog();
+
+    if (level < LOG_LEVEL_NONE) {
+        std::unique_ptr<InputStream> in;
+        CHECK(g_bootLog->openForRead(in));
+    
+        char buf[256];
+        for (;;) {
+            size_t bytesAvail = CHECK(in->availForRead());
+            if (!bytesAvail) {
+                break;
+            }
+            size_t n = std::min(bytesAvail, sizeof(buf));
+            CHECK(in->read(buf, n));
+            log_write(level, category, buf, n, nullptr /* reserved */);
+        }
+        in.reset();
+    }
+
+    g_bootLog->clear();
+    return 0;
 }
 
 } // namespace
@@ -337,18 +351,18 @@ int bootLogEnabledCallback(int level, const char* category, void* reserved) {
 int initBootLog() {
     fs::FsLock lock;
 
-    if (!bootLogConfig.enabled) {
+    if (!g_bootLogConfig.enabled) {
         return 0;
     }
 
     CHECK(fs::mount());
 
-    std::unique_ptr<RotatingLog> log(new(std::nothrow) RotatingLog(bootLogConfig.maxSize, BOOT_LOG_FILE1, BOOT_LOG_FILE2));
+    std::unique_ptr<BootLog> log(new(std::nothrow) BootLog(g_bootLogConfig.maxSize));
     if (!log) {
         return SYSTEM_ERROR_NO_MEMORY;
     }
     CHECK(log->init());
-    bootLog = std::move(log);
+    g_bootLog = std::move(log);
 
     log_set_callbacks(bootLogMessageCallback, bootLogWriteCallback, bootLogEnabledCallback, nullptr /* reserved */);
     return 0;
@@ -366,50 +380,25 @@ void system_enable_boot_log(bool enabled, const system_boot_log_config* config, 
     fs::FsLock lock;
 
     if (!enabled) {
-        bootLog.reset();
+        g_bootLog.reset();
     }
 
-    bootLogConfig = BootLogConfig();
-    bootLogConfig.enabled = enabled;
+    g_bootLogConfig = BootLogConfig();
+    g_bootLogConfig.enabled = enabled;
     if (enabled) {
         if (config) {
             if (config->category) {
-                strlcpy(bootLogConfig.category, config->category, sizeof(bootLogConfig.category));
+                strlcpy(g_bootLogConfig.category, config->category, sizeof(g_bootLogConfig.category));
             }
-            bootLogConfig.maxSize = config->max_size ? config->max_size : DEFAULT_BOOT_LOG_SIZE;
-            bootLogConfig.level = config->level;
+            g_bootLogConfig.maxSize = config->max_size ? config->max_size : DEFAULT_BOOT_LOG_SIZE;
+            g_bootLogConfig.level = config->level;
         } else {
-            bootLogConfig.maxSize = DEFAULT_BOOT_LOG_SIZE;
-            bootLogConfig.level = LOG_LEVEL_ALL;
+            g_bootLogConfig.maxSize = DEFAULT_BOOT_LOG_SIZE;
+            g_bootLogConfig.level = LOG_LEVEL_ALL;
         }
     }
 }
 
 void system_flush_boot_log(int level, const char* category, void* reserved) {
-    fs::FsLock lock;
-
-    if (!bootLog) {
-        return;
-    }
-
-    std::unique_ptr<InputStream> in;
-    int r = bootLog->openForRead(in);
-    if (r < 0) {
-        return;
-    }
-
-    if (level < LOG_LEVEL_NONE) {
-        char buf[256];
-        int bytesAvail = 0;
-        while ((bytesAvail = in->availForRead()) > 0) {
-            size_t n = std::min<size_t>(bytesAvail, sizeof(buf));
-            int r = in->read(buf, n);
-            if (r < 0) {
-                return;
-            }
-            log_write(level, category, buf, n, nullptr /* reserved */);
-        }
-    }
-
-    bootLog->clear();
+    flushBootLog(level, category);
 }
