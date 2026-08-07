3b48f58fadc2f7e168cd75aa863d32791d57c9fa
Sergey Polyakov <sergeuz@gmail.com>
2026-08-07 12:47:49 +0200

    Add docs

--- system/inc/system_config.h
+++ system/inc/system_config.h
@@ -3,10 +3,25 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+/**
+ * Boot log configuration.
+ */
 typedef struct {
-    size_t size;
+    size_t size; ///< Size of this structure.
+    /**
+     * If not `NULL`, only the messages logged for a category starting with this string will be stored
+     * in the boot log.
+     */
     const char* category;
+    /**
+     * Maximum size of the boot log in bytes.
+     *
+     * Up to twice this amount of storage may be used internally. If 0, a default size is used.
+     */
     size_t max_size;
+    /**
+     * Minimum logging level as defined by the `LogLevel` enum.
+     */
     int level;
 } system_boot_log_config;
 
@@ -14,7 +29,27 @@ typedef struct {
 extern "C" {
 #endif
 
+/**
+ * Enable or disable the boot log.
+ *
+ * The boot log stores the messages logged while the device is booting so that they can be retrieved
+ * by the application once it starts.
+ *
+ * The configuration is stored in the backup RAM and applied when the device boots next time.
+ *
+ * @param enabled Whether the boot log is enabled.
+ * @param config Boot log configuration. If `NULL`, the default configuration will be used.
+ * @param reserved Reserved argument. Must be set to `NULL`.
+ */
 void system_enable_boot_log(bool enabled, const system_boot_log_config* config, void* reserved);
+
+/**
+ * Print the contents of the boot log and delete it.
+ *
+ * @param level Logging level at which the contents of the boot log are written.
+ * @param category Logging category with which the contents of the boot log are written.
+ * @param reserved Reserved argument. Must be set to `NULL`.
+ */
 void system_flush_boot_log(int level, const char* category, void* reserved);
 
 #ifdef __cplusplus
