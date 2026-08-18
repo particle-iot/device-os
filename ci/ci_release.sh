9a24b0a61e8e91517828a2d9d5a43eb14e19b694
Andrey Tolstoy <avtolstoy@gmail.com>
2026-08-18 23:55:25 +0700

    [ci] allow device-os-test build step to use nproc cores

--- ci/ci_release.sh
+++ ci/ci_release.sh
@@ -33,7 +33,7 @@ for PLATFORM in $CI_RELEASE_PLATFORMS; do
     popd
     ls -laR $TEST_DIR
     rm -rf /firmware/build/target
-    device-os-test --device-os-dir=/firmware --target-dir=$TEST_DIR --binary-dir=$TEST_DIR --config-file=/tmp/test.config.json build $PLATFORM
+    MAKEFLAGS=-j$(nproc) device-os-test --device-os-dir=/firmware --target-dir=$TEST_DIR --binary-dir=$TEST_DIR --config-file=/tmp/test.config.json build $PLATFORM
     testcase "0 PLATFORM=\"$PLATFORM\" device-os-test"
     pushd /firmware/user/tests/integration
     find -L ./ -name '*.spec.js' -exec cp --parents {} $TEST_DIR/$PLATFORM \;
